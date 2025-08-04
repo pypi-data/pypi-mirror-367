8b7833c9adfc339ed00dc0e732214b00b4ce5477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:06:54 +0000

    Add package grikod/1.1.4/grikod-1.1.4-py3-none-any.whl

---
+++ packages/grikod/grikod-1.1.4-py3-none-any.whl/grikod/_version.py
@@ -0,0 +1,9 @@
+# _version.py
+
+__version__ = "1.1.4"
+__license__ = "MIT"
+__description__ = "Binary to Gray code conversion package for efficient data encoding."
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/KuantumBS/grikod"
+__docs__ = "https://kuantumbs.github.io/grikod"  # Opsiyonel: Dokümantasyon linki
+__dependencies__ = ["python>=3.8"]  # Diğer bağımlılıkları da ekleyebilirsiniz
