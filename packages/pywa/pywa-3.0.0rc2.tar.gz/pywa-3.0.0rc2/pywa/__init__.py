269a0be4762878d692eaff6e9f6117a96b9e4349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:07:19 +0000

    Add package pywa/3.0.0rc2/pywa-3.0.0rc2.tar.gz

---
+++ packages/pywa/pywa-3.0.0rc2.tar.gz/pywa-3.0.0rc2/pywa/__init__.py
@@ -0,0 +1,14 @@
+"""
+Build WhatsApp Bots in Python • Fast, Effortless, Powerful 🚀
+
+    - GitHub: https://github.com/david-lev/pywa
+    - Docs: https://pywa.readthedocs.io/
+    - PyPI: https://pypi.org/project/pywa/
+"""
+
+from pywa.client import WhatsApp
+from pywa.utils import Version
+
+__version__ = "3.0.0-rc.2"
+__author__ = "David Lev"
+__license__ = "MIT"
