6354f604070c9a50d5b9aefa9fbef98c408b8a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:06:44 +0000

    Add package datasure/0.3.24/datasure-0.3.24.tar.gz

---
+++ packages/datasure/datasure-0.3.24.tar.gz/datasure-0.3.24/src/datasure/__init__.py
@@ -0,0 +1,16 @@
+"""
+DataSure - IPA Data Management System Dashboard.
+
+A web-based interface for data quality monitoring and high-frequency checks
+in survey data collection projects.
+"""
+
+__version__ = "0.2.1"
+__author__ = "Innovations for Poverty Action"
+__email__ = "researchsupport@poverty-action.org"
+
+# Package metadata
+__title__ = "DataSure"
+__description__ = "IPA Data Management System Dashboard"
+__url__ = "https://github.com/PovertyAction/datasure"
+__license__ = "MIT"
