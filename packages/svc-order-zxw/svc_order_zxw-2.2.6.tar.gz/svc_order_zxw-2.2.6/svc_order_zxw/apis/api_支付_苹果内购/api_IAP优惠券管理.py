5a968ea194483f80ef0d8b641b1466dfc8f84b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:06:58 +0000

    Add package svc-order-zxw/2.2.6/svc_order_zxw-2.2.6.tar.gz

---
+++ packages/svc-order-zxw/svc_order_zxw-2.2.6.tar.gz/svc_order_zxw-2.2.6/svc_order_zxw/apis/api_支付_苹果内购/api_IAP优惠券管理.py
@@ -0,0 +1,10 @@
+"""
+# File       : api_IAP优惠券管理.py
+# Time       ：2025/7/29 15:24
+# Author     ：xuewei zhang
+# Email      ：shuiheyangguang@gmail.com
+# version    ：python 3.12
+# Description：
+"""
+from svc_order_zxw.interface.interface_苹果内购_优惠券 import get_IAP促销id, get_IAP优惠卷, Model促销优惠签名结果, \
+    Model_IAP促销活动
