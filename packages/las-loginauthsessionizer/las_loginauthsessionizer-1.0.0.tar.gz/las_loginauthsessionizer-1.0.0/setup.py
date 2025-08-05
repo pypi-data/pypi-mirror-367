4dc264106725ece9bdebfe1a21e8fd58dbbba016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:33:39 +0000

    Add package las-loginauthsessionizer/1.0.0/las_loginauthsessionizer-1.0.0.tar.gz

---
+++ packages/las-loginauthsessionizer/las_loginauthsessionizer-1.0.0.tar.gz/las_loginauthsessionizer-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="las-loginauthsessionizer",
+    version="1.0.0",
+    description="Login, Authenticate and save the session for future use.",
+    author="[Ph4nt01]",
+    author_email="ph4nt0.84@gmail.com",
+    url="https://github.com/Ph4nt01/LP-LoginParser",
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4",
+        "requests",
+        "colorama"
+    ],
+    entry_points={
+        "console_scripts": [
+            "las=las.cli:cli_main"
+        ],
+    },
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Security",
+        "Intended Audience :: Developers",
+    ]
+)
