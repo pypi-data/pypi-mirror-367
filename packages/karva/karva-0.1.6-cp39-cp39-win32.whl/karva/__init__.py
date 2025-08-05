0180457b04f62fab897b31705b08336d266e4bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 05:10:52 +0000

    Add package karva/0.1.6/karva-0.1.6-cp39-cp39-win32.whl

---
+++ packages/karva/karva-0.1.6-cp39-cp39-win32.whl/karva/__init__.py
@@ -0,0 +1,7 @@
+"""Karva is a Python test runner, written in Rust."""
+
+from karva._karva import fixture, karva_run, tag, tags
+
+__version__ = "0.1.6"
+
+__all__ = ["fixture", "karva_run", "tag", "tags"]
