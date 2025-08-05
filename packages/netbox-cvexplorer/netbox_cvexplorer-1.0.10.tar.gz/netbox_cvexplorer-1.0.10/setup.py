b71388f118fca1b76256e7cac39b3ee04fcafcde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:33:30 +0000

    Add package netbox-cvexplorer/1.0.10/netbox_cvexplorer-1.0.10.tar.gz

---
+++ packages/netbox-cvexplorer/netbox_cvexplorer-1.0.10.tar.gz/netbox_cvexplorer-1.0.10/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import json
+from pathlib import Path
+here = Path(__file__).parent.resolve()
+
+pluginvars={}
+
+with open(f"{here}/netbox_cvexplorer/info.json", "r") as pluginvarsfile:
+    pluginvars = json.load(pluginvarsfile)
+    for pluginvar in pluginvars:
+        locals()[f"{pluginvar}"]=pluginvars[pluginvar]
+
+long_description = ''
+with open(f"{here}/README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = __name__,
+    version = __version__,
+    description = __description__,
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = __url__,
+    author = __author__,
+    license='MIT License',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
