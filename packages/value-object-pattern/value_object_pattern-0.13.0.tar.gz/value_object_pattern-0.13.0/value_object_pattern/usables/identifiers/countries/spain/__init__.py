96f84416e326c554f2e9c0d0bfd68d040c04936d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:14 +0000

    Add package value-object-pattern/0.13.0/value_object_pattern-0.13.0.tar.gz

---
+++ packages/value-object-pattern/value_object_pattern-0.13.0.tar.gz/value_object_pattern-0.13.0/value_object_pattern/usables/identifiers/countries/spain/__init__.py
@@ -0,0 +1,9 @@
+from .dni_value_object import DniValueObject
+from .nie_value_object import NieValueObject
+from .passport_value_object import PassportValueObject
+
+__all__ = (
+    'DniValueObject',
+    'NieValueObject',
+    'PassportValueObject',
+)
