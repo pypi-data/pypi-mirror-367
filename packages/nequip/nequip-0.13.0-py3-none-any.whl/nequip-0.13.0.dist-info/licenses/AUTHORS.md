27e078f8123ba3e38a4286aac5c0330788eb1970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 05:10:55 +0000

    Add package nequip/0.13.0/nequip-0.13.0-py3-none-any.whl

---
+++ packages/nequip/nequip-0.13.0-py3-none-any.whl/nequip-0.13.0.dist-info/licenses/AUTHORS.md
@@ -0,0 +1,27 @@
+# NequIP Developers
+
+Present and past contributors include, in alphabetical order by last name:
+
+ - [Simon Batzner](https://simonbatzner.github.io/)
+ - [Vivek Bharadwaj](https://vivek-bharadwaj.com)
+ - [Marc Descoteaux](https://www.marcdescoteaux.com/)
+ - [Mario Geiger](https://mariogeiger.ch/)
+ - Austin Glover
+ - Y. Richard Hu
+ - Anders Johansson
+ - [Seán Kavanagh](https://seankavanagh.com/)
+ - [Mit Kotak](https://mitkotak.github.io/)
+ - [Prof. Boris Kozinsky](https://mir.g.harvard.edu/)
+ - Albert Musaelian
+ - Gabriel de Miranda Nascimento
+ - Aadit Saluja
+ - Viraj Uday Singh
+ - [Prof. Tess Smidt](https://blondegeek.github.io/)
+ - Lixin Sun
+ - [Myles Stapelberg](https://github.com/mstapelberg)
+ - [Chuin Wei Tan](https://cw-tan.github.io/)
+ - Ulrik Unneberg
+ - Menghang (David) Wang
+ - William C. Witt
+ - Albert Zhu
+ - Laura Zichi
---
+++ packages/nequip/nequip-0.13.0-py3-none-any.whl/nequip/train/__init__.py
@@ -0,0 +1,43 @@
+# This file is a part of the `nequip` package. Please see LICENSE and README at the root for information on using it.
+from .metrics import (
+    MeanAbsoluteError,
+    MeanSquaredError,
+    RootMeanSquaredError,
+    MaximumAbsoluteError,
+    HuberLoss,
+    StratifiedHuberForceLoss,
+)
+from .metrics_manager import (
+    MetricsManager,
+    EnergyForceLoss,
+    EnergyForceMetrics,
+    EnergyForceStressLoss,
+    EnergyForceStressMetrics,
+    EnergyOnlyLoss,
+    EnergyOnlyMetrics,
+)
+from .lightning import NequIPLightningModule
+from .ema import EMALightningModule
+from .config import ConFIGLightningModule, EMAConFIGLightningModule
+from .simple_ddp import SimpleDDPStrategy
+
+__all__ = [
+    "NequIPLightningModule",
+    "EMALightningModule",
+    "ConFIGLightningModule",
+    "EMAConFIGLightningModule",
+    "MetricsManager",
+    "EnergyForceLoss",
+    "EnergyForceMetrics",
+    "EnergyForceStressLoss",
+    "EnergyForceStressMetrics",
+    "EnergyOnlyLoss",
+    "EnergyOnlyMetrics",
+    "MeanAbsoluteError",
+    "MeanSquaredError",
+    "RootMeanSquaredError",
+    "MaximumAbsoluteError",
+    "HuberLoss",
+    "StratifiedHuberForceLoss",
+    "SimpleDDPStrategy",
+]
