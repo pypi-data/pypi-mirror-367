b01058e18ca0451c0e9d32c545c1a3e6d51bcc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 14:08:10 +0000

    Add package passgent/0.1.0/passgent-0.1.0.tar.gz

---
+++ packages/passgent/passgent-0.1.0.tar.gz/passgent-0.1.0/passgent/__init__.py
@@ -0,0 +1,1 @@
+from genpass import *
---
+++ packages/passgent/passgent-0.1.0.tar.gz/passgent-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='passgent',
+    version='0.1.0',
+    description='Password generator',
+    author='MrSanZz',
+    author_email='drownedmrsanzz@proton.me',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrSanZz/passgent",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
