23fe0ab7c91edb4b7f1fd0b12143e2643a98bc4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 07:07:53 +0000

    Add package fluidattacks-core/2.10.5/fluidattacks_core-2.10.5-py3-none-any.whl

---
+++ packages/fluidattacks-core/fluidattacks_core-2.10.5-py3-none-any.whl/fluidattacks_core/logging/__init__.py
@@ -0,0 +1,10 @@
+from fluidattacks_core.logging.presets import BATCH_LOGGING, DATE_FORMAT
+from fluidattacks_core.logging.types import JobMetadata
+from fluidattacks_core.logging.utils import get_job_metadata
+
+__all__ = [
+    "BATCH_LOGGING",
+    "DATE_FORMAT",
+    "JobMetadata",
+    "get_job_metadata",
+]
