08fcf9049b1f9689cdd63a123e5bb59fa9c6b038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:43 +0000

    Add package charminder/0.1.0/charminder-0.1.0-py3-none-any.whl

---
+++ packages/charminder/charminder-0.1.0-py3-none-any.whl/charminder/__init__.py
@@ -0,0 +1,7 @@
+"""CharMinder is a character encoding detective for text files and URLs."""
+
+from .cli import main
+
+__version__ = "0.1.0"
+
+__all__ = ["main"]
---
+++ packages/charminder/charminder-0.1.0-py3-none-any.whl/charminder/bin/check_encoding.py
@@ -0,0 +1,6 @@
+"""Check text encoding of MDF file for unexpected characters."""
+
+from charminder import main
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
