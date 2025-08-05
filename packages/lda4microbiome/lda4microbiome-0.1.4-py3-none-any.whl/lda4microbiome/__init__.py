491e114815384f71b0dbed2f453856e2738d4f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 04:23:17 +0000

    Add package lda4microbiome/0.1.4/lda4microbiome-0.1.4-py3-none-any.whl

---
+++ packages/lda4microbiome/lda4microbiome-0.1.4-py3-none-any.whl/lda4microbiome/__init__.py
@@ -0,0 +1,29 @@
+"""
+LDA4Microbiome: A comprehensive workflow for LDA analysis of microbiome data using MALLET
+
+This package provides tools for:
+- Taxonomic data preprocessing
+- LDA model training with MALLET
+- Model selection and evaluation
+- Results visualization and analysis
+"""
+
+__version__ = "0.1.4"
+__author__ = "Peiyang Huo"
+__email__ = "peiyang.huo@kuleuven.be"
+
+# Import main classes for easy access
+from .preprocessing import TaxonomyProcessor
+from .training import LDATrainer  
+from .selection import SankeyDataProcessor
+from .visualization import LDAModelVisualizer, TopicFeatureProcessor, MCComparison
+
+__all__ = [
+    'TaxonomyProcessor',
+    'LDATrainer', 
+    'SankeyDataProcessor',
+    'LDAModelVisualizer',
+    'TopicFeatureProcessor',
+    'MCComparison'
+]
+
