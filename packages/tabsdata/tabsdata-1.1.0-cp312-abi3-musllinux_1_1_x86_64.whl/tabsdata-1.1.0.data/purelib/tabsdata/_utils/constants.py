cd63795c641c3b161dc5350c8ddafb464baa2d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 14:08:21 +0000

    Add package tabsdata/1.1.0/tabsdata-1.1.0-cp312-abi3-musllinux_1_1_x86_64.whl

---
+++ packages/tabsdata/tabsdata-1.1.0-cp312-abi3-musllinux_1_1_x86_64.whl/tabsdata-1.1.0.data/purelib/tabsdata/_utils/constants.py
@@ -0,0 +1,13 @@
+#
+# Copyright 2025 Tabs Data Inc.
+#
+
+TRUE_VALUES = {"1", "true", "yes", "y", "on"}
+
+NO_VERSION = "♾️"
+
+TABSDATA_MODULE_NAME = "tabsdata"
+TABSDATA_CONNECTOR_DATABRICKS_MODULE_NAME = "tabsdata_databricks"
+TABSDATA_CONNECTOR_MONGODB_MODULE_NAME = "tabsdata_mongodb"
+TABSDATA_CONNECTOR_SALESFORCE_MODULE_NAME = "tabsdata_salesforce"
+TABSDATA_CONNECTOR_SNOWFLAKE_MODULE_NAME = "tabsdata_snowflake"
