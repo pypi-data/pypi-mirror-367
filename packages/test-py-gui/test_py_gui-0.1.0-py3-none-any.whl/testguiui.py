fd6c41acd87c2663f765e978cc490fea050079cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 16:05:34 +0000

    Add package test-py-gui/0.1.0/test_py_gui-0.1.0-py3-none-any.whl

---
+++ packages/test-py-gui/test_py_gui-0.1.0-py3-none-any.whl/testguiui.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import pathlib
+import tkinter as tk
+import pygubu
+
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "test-py.ui"
+RESOURCE_PATHS = [PROJECT_PATH]
+
+
+class TestGUIUI:
+    def __init__(
+        self,
+        master=None,
+        translator=None,
+        on_first_object_cb=None,
+        data_pool=None
+    ):
+        self.builder = pygubu.Builder(
+            translator=translator,
+            on_first_object=on_first_object_cb,
+            data_pool=data_pool
+        )
+        self.builder.add_resource_paths(RESOURCE_PATHS)
+        self.builder.add_from_file(PROJECT_UI)
+        # Main widget
+        self.mainwindow: tk.Toplevel = self.builder.get_object(
+            "Toplevel1", master)
+        self.builder.connect_callbacks(self)
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+    def clickMe(self):
+        pass
