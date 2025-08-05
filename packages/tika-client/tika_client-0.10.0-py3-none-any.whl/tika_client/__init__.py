58d14543dde404b66626c46c0d3474f0c4566d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 05:10:59 +0000

    Add package tika-client/0.10.0/tika_client-0.10.0-py3-none-any.whl

---
+++ packages/tika-client/tika_client-0.10.0-py3-none-any.whl/tika_client/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2023-present Trenton H <rda0128ou@mozmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from tika_client.client import AsyncTikaClient
+from tika_client.client import TikaClient
+from tika_client.data_models import DublinCoreKey
+from tika_client.data_models import TikaKey
+from tika_client.data_models import XmpKey
+
+__all__ = ["AsyncTikaClient", "DublinCoreKey", "TikaClient", "TikaKey", "XmpKey"]
