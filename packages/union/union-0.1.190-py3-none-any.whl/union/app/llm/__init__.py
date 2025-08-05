f6341c3fbf469e226d799e31069da2672343e6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 05:10:58 +0000

    Add package union/0.1.190/union-0.1.190-py3-none-any.whl

---
+++ packages/union/union-0.1.190-py3-none-any.whl/union/app/llm/__init__.py
@@ -0,0 +1,4 @@
+from ._sglang import OPTIMIZED_SGLANG_IMAGE, SGLangApp
+from ._vllm import OPTIMIZED_VLLM_IMAGE, VLLMApp
+
+__all__ = ["OPTIMIZED_SGLANG_IMAGE", "OPTIMIZED_VLLM_IMAGE", "SGLangApp", "VLLMApp"]
