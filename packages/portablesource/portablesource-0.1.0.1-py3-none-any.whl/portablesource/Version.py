31516ae66a329a8328e56c16136321e8863b4b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:34:11 +0000

    Add package portablesource/0.1.0.1/portablesource-0.1.0.1-py3-none-any.whl

---
+++ packages/portablesource/portablesource-0.1.0.1-py3-none-any.whl/portablesource/Version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+__version__ = "0.1.0.1"
+
+if __name__ == "__main__":
+    print(__version__)
---
+++ packages/portablesource/portablesource-0.1.0.1-py3-none-any.whl/portablesource/__init__.py
@@ -0,0 +1,5 @@
+from .Version import __version__
+from .config import *
+from .envs_manager import *
+from .get_gpu import *
+from .repository_installer import *
