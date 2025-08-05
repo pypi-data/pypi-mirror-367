775eb36582a2d4953a54f2104495cc840c58b934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:24 +0000

    Add package eth-wake/5.0.0rc0/eth_wake-5.0.0rc0-cp38-cp38-win32.whl

---
+++ packages/eth-wake/eth_wake-5.0.0rc0-cp38-cp38-win32.whl/wake/utils/connect_context.py
@@ -0,0 +1,31 @@
+class ConnectContext:
+    def __init__(self, chain, accounts, chain_id, fork, hardfork):
+        self.chain = chain
+        self.accounts = accounts
+        self.chain_id = chain_id
+        self.fork = fork
+        self.hardfork = hardfork
+
+    def __enter__(self):
+        self.chain._connect(self.accounts, self.chain_id, self.fork, self.hardfork)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        from bdb import BdbQuit
+
+        from wake.development.globals import get_exception_handler
+
+        try:
+            if exc_type is not None and not isinstance(exc_value, BdbQuit):
+                exception_handler = get_exception_handler()
+                if exception_handler is not None:
+                    exception_handler(exc_type, exc_value, traceback)
+        finally:
+            self.chain._disconnect()
+
+    def __call__(self, fn):
+        def wrapper(*args, **kwargs):
+            with self:
+                return fn(*args, **kwargs)
+
+        return wrapper
