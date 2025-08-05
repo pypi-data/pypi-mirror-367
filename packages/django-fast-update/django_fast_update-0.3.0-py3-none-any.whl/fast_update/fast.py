a2b8dc8e43af0ef16f9284e57ca307ded2e9bc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 05:11:00 +0000

    Add package django-fast-update/0.3.0/django_fast_update-0.3.0-py3-none-any.whl

---
+++ packages/django-fast-update/django_fast_update-0.3.0-py3-none-any.whl/fast_update/fast.py
@@ -0,0 +1,412 @@
+from weakref import WeakKeyDictionary
+from django.db import connections
+from django.db.transaction import atomic
+from django.db.models import QuerySet, Model
+from django.db.models.functions import Cast
+from django.db.models.expressions import Col
+from django.db.models.sql import UpdateQuery
+from operator import attrgetter
+from .update import flat_update, group_fields
+
+# typing imports
+from django.db.models import Field
+from typing import Callable, Dict, List, Sequence, Any, Tuple, Union, cast, Optional
+from django.db.models.sql.compiler import SQLCompiler
+from django.db.backends.utils import CursorWrapper
+from django.db.backends.base.base import BaseDatabaseWrapper
+
+
+"""
+DB vendor low level interfaces
+
+To register fast update implementations, call:
+
+    register_implementation('vendor', check_function)
+
+where `vendor` is the vendor name as returned by `connection.vendor`.
+The check function gets called once (lazy) with `connection` and is meant
+to find a suitable implementation (you can provide multiple for different
+server versions), either actively by probing against the db server,
+or directly if it can be determined upfront.
+The check function should return a tuple of (create_sql, prepare_data | None)
+for supported backends, or an empty tuple, if the backend is unsupported.
+
+create_sql function:
+
+    def as_sql_xy(
+        tname: str,
+        pkname: str,
+        fields: Sequence[Field],
+        placeholders: List[str],
+        compiler: SQLCompiler,
+        connection: BaseDatabaseWrapper
+    ) -> str: ...
+
+    `tname` - name of the target table as pulled from the ORM
+    `pkname` - name of the pk field (always the first in a row)
+    `fields` - ordered fields as occuring in the data after pk
+    `placeholders` - [col][row] placeholders (transposed)
+    `compiler` - current update compiler
+    `connection` - current write connection
+
+    The placeholders are already enriched from .get_placeholder.
+    They come transposed (column based), thus most likely need to be
+    transposed back into row based for sql templating.
+    The SQL function should return an SQL template with proper placeholders
+    applied to do the update from the prepared data list.
+
+prepare_data function:
+
+    def prepare_data_xy(
+        data: List[Any],
+        width: int,
+        height: int
+    ) -> List[Any]: ...
+
+    `data` - flat 1-d data list (all values prepared for save)
+    `width` - row width
+    `height` - column height
+
+    This function is meant to customize the data list, in case more than
+    the flat data table preparation is needed. The data is row based,
+    and contains field values in [pk] + fields order.
+    Return the altered data listing according to the SQL needs.
+"""
+
+
+# fast update implementation types
+SQLFunction = Callable[[str, str, Sequence[Field], List[List[str]], SQLCompiler, BaseDatabaseWrapper], str]
+PrepareFunction = Callable[[List[Any], int, int], List[Any]]
+CheckResult = Tuple[SQLFunction, Optional[PrepareFunction]]
+CheckFunction = Callable[[BaseDatabaseWrapper], CheckResult]
+Registry = Dict[BaseDatabaseWrapper, Union[CheckResult, Tuple]]
+
+
+# Fast update implementations for postgres, sqlite and mysql.
+
+
+def pq_cast(
+    tname: str,
+    field: Field,
+    compiler: SQLCompiler,
+    connection: Any
+) -> str:
+    """
+    Column type cast for postgres.
+    """
+    return Cast(Col(tname, field), output_field=field).as_sql(compiler, connection)[0]
+
+
+def as_postgresql(
+    tname: str,
+    pkname: str,
+    fields: Sequence[Field],
+    placeholders: List[List[str]],
+    compiler: SQLCompiler,
+    connection: BaseDatabaseWrapper
+) -> str:
+    """
+    Uses UPDATE FROM VALUES with column aliasing.
+
+    Other than other supported backends postgres is very picky about data types
+    in the SET clause, therefore we always place an explicit type cast.
+    """
+    dname = 'd' if tname != 'd' else 'c'
+    cols = ','.join(f'"{f.column}"={pq_cast(dname, f, compiler, connection)}' for f in fields)
+    values = ','.join([f'({",".join(row)})' for row in zip(*placeholders)])
+    dcols = f'"{pkname}",' + ','.join(f'"{f.column}"' for f in fields)
+    where = f'"{tname}"."{pkname}"="{dname}"."{pkname}"'
+    return (
+        f'UPDATE "{tname}" '
+        f'SET {cols} FROM (VALUES {values}) AS "{dname}" ({dcols}) WHERE {where}'
+    )
+
+
+def as_sqlite(
+    tname: str,
+    pkname: str,
+    fields: Sequence[Field],
+    placeholders: List[List[str]],
+    compiler: SQLCompiler,
+    connection: BaseDatabaseWrapper
+) -> str:
+    """
+    sqlite >= 3.32 implements basic UPDATE FROM VALUES support following postgres' syntax.
+    Other than postgres, sqlite does not allow column aliasing, it always uses a fixed
+    naming scheme (column1, column2, ...).
+    """
+    dname = 'd' if tname != 'd' else 'c'
+    cols = ','.join(f'"{f.column}"="{dname}"."column{i + 2}"'
+        for i, f in enumerate(fields))
+    values = ','.join([f'({",".join(row)})' for row in zip(*placeholders)])
+    where = f'"{tname}"."{pkname}"="{dname}"."column1"'
+    return f'UPDATE "{tname}" SET {cols} FROM (VALUES {values}) AS "{dname}" WHERE {where}'
+
+
+def as_sqlite_cte(
+    tname: str,
+    pkname: str,
+    fields: Sequence[Field],
+    placeholders: List[List[str]],
+    compiler: SQLCompiler,
+    connection: BaseDatabaseWrapper
+) -> str:
+    """
+    sqlite >=3.15 <3.33 does not support yet the FROM VALUES pattern, but has
+    CTE support (>=3.8) with row assignment in UPDATE (>=3.15).
+    So we can use CTE to construct the values table upfront with UNION ALL,
+    and do row level update in the SET clause.
+
+    To limit the updated rows we normally would do a correlated existance test
+    in the update WHERE clause:
+
+        WHERE EXISTS (SELECT 1 FROM target WHERE target.pk = cte.pk)
+
+    but this shows very bad performance due to repeated rescanning of the
+    values table. We can achieve the same filter condition by providing
+    the pks with an IN test:
+
+        WHERE target.pk in (pk1, pk2, ...)
+
+    This sacrifices some bandwidth, but is only ~40% slower than the
+    FROM VALUES table join.
+    """
+    dname = 'd' if tname != 'd' else 'c'
+    cols = ', '.join([f'"{f.column}"' for f in fields])
+    rows = [','.join(row) for row in zip(*placeholders)]
+    values = ' UNION ALL '.join([' SELECT ' + row for row in rows])
+    where = f'"{tname}"."{pkname}"={dname}."{pkname}"'
+    pks = ','.join(placeholders[0])
+    return (
+        f'WITH {dname}("{pkname}", {cols}) AS ({values}) '
+        f'UPDATE "{tname}" '
+        f'SET ({cols}) = (SELECT {cols} FROM {dname} WHERE {where}) '
+        f'WHERE "{tname}"."{pkname}" in ({pks})'
+    )
+
+
+def prepare_data_sqlite_cte(
+    data: List[Any],
+    width: int,
+    height: int
+) -> List[Any]:
+    return data + [data[i] for i in range(0, len(data), width)]
+
+
+def as_mysql(
+    tname: str,
+    pkname: str,
+    fields: Sequence[Field],
+    placeholders: List[List[str]],
+    compiler: SQLCompiler,
+    connection: BaseDatabaseWrapper
+) -> str:
+    """
+    For MySQL we use old style SELECT + UNION ALL to create the values table.
+    This also keeps it compatible to older MySQL and MariaDB versions.
+
+    Newer db versions have a more direct way to load literal values with
+    TVC (MariaDB 10.3.3+) and extended VALUES statement (MySQL 8.0.19+).
+    We dont use those anymore, as they dont show a performance improvement,
+    while creating much code/branching noise for no good reason.
+    """
+    dname = 'd' if tname != 'd' else 'c'
+    cols = ','.join(f'`{tname}`.`{f.column}`=`{dname}`.`{f.column}`' for f in fields)
+    colnames = [pkname] + [f.column for f in fields]
+    row_phs = [ph for ph in zip(*placeholders)]
+    first_row = 'SELECT '
+    first_row += ','.join([
+        f'{ph} AS `{colname}`'
+        for ph, colname in zip(row_phs[0], colnames)
+    ])
+    later_rows = ' UNION ALL '.join(['SELECT ' + ','.join(ph) for ph in row_phs[1:]])
+    values = f'{first_row} UNION ALL {later_rows}' if later_rows else first_row
+    where = f'`{tname}`.`{pkname}` = `{dname}`.`{pkname}`'
+    return (
+        f'UPDATE `{tname}`, ({values}) {dname} '
+        f'SET {cols} WHERE {where}'
+    )
+
+
+# Implementation registry.
+
+
+# memorize fast_update vendor on connection object
+SEEN_CONNECTIONS = cast(Registry, WeakKeyDictionary())
+CHECKER: Dict[str, CheckFunction] = {}
+
+
+def register_implementation(
+    vendor: str,
+    func: CheckFunction
+) -> None:
+    """
+    Register fast update implementation for db vendor.
+
+    `vendor` is the vendor name as returned by `connection.vendor`.
+    `func` is a lazy called function to check support for a certain
+    implementation at runtime for `connection`. The function should return
+    a tuple of (create_sql, prepare_data | None) for supported backends,
+    otherwise an empty tuple (needed to avoid re-eval).
+    """
+    CHECKER[vendor] = func
+
+
+def get_impl(conn: BaseDatabaseWrapper):
+    """
+    Try to get a fast update implementation for `conn`.
+    Calls once the check function of `register_implementation` and
+    memorizes its result for `conn`.
+    Returns a tuple (create_sql, prepare_data | None) for supported backends,
+    otherwise an empty tuple.
+    """
+    impl = SEEN_CONNECTIONS.get(conn)
+    if impl is not None:
+        return impl
+    check = CHECKER.get(conn.vendor)
+    if not check:   # pragma: no cover
+        SEEN_CONNECTIONS[conn] = tuple()
+        return tuple()
+    impl = check(conn) or tuple()   # NOTE: in case check returns something nullish
+    SEEN_CONNECTIONS[conn] = impl
+    return impl
+
+
+# Register default db implementations from above.
+register_implementation(
+    'postgresql',
+    lambda _: (as_postgresql, None)
+)
+register_implementation(
+    'sqlite',
+    # NOTE: check function does not handle versions <3.15 anymore
+    lambda conn: (as_sqlite, None) if conn.Database.sqlite_version_info >= (3, 33)
+        else (as_sqlite_cte, prepare_data_sqlite_cte)
+)
+register_implementation(
+    'mysql',
+    lambda _: (as_mysql, None)
+)
+
+
+# Update implementation.
+
+
+def update_from_values(
+    c: CursorWrapper,
+    tname: str,
+    pk_field: Field,
+    fields: List[Field],
+    counter: int,
+    data: List[Any],
+    compiler: SQLCompiler,
+    connection: BaseDatabaseWrapper
+) -> int:
+    """
+    Generate vendor specific SQL statement and execute it for given data.
+    """
+    # The following placeholder calc is quite cumbersome:
+    # For fast processing we approach the data col-based (transposed)
+    # to save runtime for funcion pointer juggling for every single row
+    # which is ~90% faster than a more direct row-based evaluation.
+    # This is still alot slower than direct flat layouting, but not significant
+    # anymore for the total runtime (<<1%, a row-based approach takes 3-7%).
+    row_fields = [pk_field] + fields
+    row_length = len(row_fields)
+    default_placeholder = ['%s'] * counter
+    placeholders = [
+        ([
+            f.get_placeholder(data[i], compiler, connection)
+            for i in range(pos, len(data), row_length)
+        ] if hasattr(f, 'get_placeholder') else default_placeholder)
+        for pos, f in enumerate(row_fields)
+    ]
+    create_sql, prepare_data = get_impl(connection)
+    sql = create_sql(tname, pk_field.column, fields, placeholders, compiler, connection)
+    if prepare_data:
+        data = prepare_data(data, row_length, counter)
+    c.execute(sql, data)
+    rows_updated = c.rowcount
+    # NOTE: SQLite cannot report correct rowcount from a CTE context
+    # as per Python DB API 2.0 (PEP 249) we treat -1 in rowcount
+    # as fault and return counter instead
+    if rows_updated == -1:
+        return counter
+    return rows_updated
+
+
+def _fast_update(
+    qs: QuerySet,
+    objs: Sequence[Model],
+    fieldnames: List[str],
+    batch_size: Union[int, None],
+    connection: BaseDatabaseWrapper
+) -> int:
+    qs._for_write = True
+    model = qs.model
+
+    # prepare all needed arguments for update
+    max_batch_size = connection.ops.bulk_batch_size(['pk'] + fieldnames, objs)
+    batch_size_adjusted = min(batch_size or 2 ** 31, max_batch_size)
+    fields = [model._meta.get_field(f) for f in fieldnames]
+    pk_field = model._meta.pk
+    get = attrgetter(pk_field.attname, *(f.attname for f in fields))
+    prep_save = [pk_field.get_db_prep_save] + [f.get_db_prep_save for f in fields]
+    compiler = UpdateQuery(model).get_compiler(connection.alias)
+    table = model._meta.db_table
+    
+    rows_updated = 0
+    with connection.cursor() as cursor:
+        # update data either batched or in one go
+        data = []
+        counter = 0
+        for o in objs:
+            counter += 1
+            data += [prep(value, connection) for prep, value in zip(prep_save, get(o))]
+            if counter >= batch_size_adjusted:
+                rows_updated += update_from_values(
+                    cursor, table, pk_field, fields,
+                    counter, data, compiler, connection
+                )
+                data = []
+                counter = 0
+        if data:
+            rows_updated += update_from_values(
+                cursor, table, pk_field, fields,
+                counter, data, compiler, connection
+            )
+    return rows_updated
+
+
+def fast_update(
+    qs: QuerySet,
+    objs: Sequence[Model],
+    fieldnames: Sequence[str],
+    batch_size: Union[int, None],
+    unfiltered: bool = False
+) -> int:
+    qs._for_write = True
+    connection: BaseDatabaseWrapper = connections[qs.db]
+
+    # fall back to flat_update if we dont have a working fast_update impl
+    if not get_impl(connection):  # pragma: no cover
+        return flat_update(qs, objs, fieldnames, unfiltered=unfiltered)
+
+    if not unfiltered and qs.query.where:
+        # prefiltering needs an extra db roundtrip
+        pks = set(qs.values_list('pk', flat=True))
+        objs = [o for o in objs if o.pk in pks]
+
+    rows_updated = 0
+    with atomic(using=qs.db, savepoint=False):
+        for model, local_fields in group_fields(qs.model, fieldnames).items():
+            if local_fields:
+                ru = _fast_update(
+                    model._base_manager.using(qs.db),
+                    objs,
+                    local_fields,
+                    batch_size,
+                    connection
+                )
+                rows_updated = max(rows_updated, ru)
+        return rows_updated
---
+++ packages/django-fast-update/django_fast_update-0.3.0-py3-none-any.whl/fast_update/query.py
@@ -0,0 +1,210 @@
+from django.db import connections
+from django.db.models import QuerySet, Model, Manager
+from django.db.utils import NotSupportedError
+from typing import Any, Dict, Iterable, Optional, Type, Tuple, Sequence
+
+from .fast import fast_update
+from .update import flat_update, merged_update
+
+
+def sanity_check(
+    model: Type[Model],
+    objs: Tuple[Model, ...],
+    fields: Sequence[str],
+    op: str,
+    batch_size: Optional[int] = None
+) -> None:
+    # basic sanity checks (most taken from bulk_update)
+    if batch_size is not None and batch_size < 0:
+        raise ValueError('Batch size must be a positive integer.')
+    if not fields:
+        raise ValueError(f'Field names must be given to {op}().')
+    pks = set(obj.pk for obj in objs)
+    if len(pks) < len(objs):
+        raise ValueError(f'{op}() cannot update duplicates.')
+    if None in pks:
+        raise ValueError(f'All {op}() objects must have a primary key set.')
+    if len(set(fields)) < len(fields):
+        raise ValueError(f'{op}() has duplicated fieldnames.')
+    fields_ = [model._meta.get_field(name) for name in fields]
+    if any(not f.concrete or f.many_to_many for f in fields_):
+        raise ValueError(f'{op}() can only be used with concrete fields.')
+    if any(f.primary_key for f in fields_):
+        raise ValueError(f'{op}() cannot be used with primary key fields.')
+    for obj in objs:
+        # TODO: This is really heavy in the runtime books, any elegant way to speedup?
+        # TODO: django main has an additional argument 'fields' (saves some runtime?)
+        # FIXME: although this is fixed upstream we still cannot use the fields argument
+        obj._prepare_related_fields_for_save(operation_name=op)
+        # additionally raise on f-expression
+        for field in fields_:
+            # TODO: use faster attrgetter
+            if hasattr(getattr(obj, field.attname), 'resolve_expression'):
+                raise ValueError(f'{op}() cannot be used with f-expressions.')
+
+
+class FastUpdateQuerySet(QuerySet):
+    def fast_update(
+        self,
+        objs: Iterable[Model],
+        fields: Iterable[str],
+        batch_size: Optional[int] = None,
+        unfiltered: bool = False
+    ) -> int:
+        """
+        Faster alternative for ``bulk_update`` with a compatible method
+        signature.
+
+        Due to the way the update works internally with constant VALUES tables,
+        f-expressions cannot be used anymore. Beside that it has similar
+        restrictions as ``bulk_update`` (e.g. primary keys cannot be updated).
+
+        The internal implementation relies on recent versions of database
+        backends and will fall back to ``bulk_update`` if the backend is not
+        supported. It will also invoke ``bulk_update`` for non-local fields
+        (e.g. for multi-table inheritance).
+
+        ``batch_size`` can be set to much higher values than typically
+        for ``bulk_update`` (if needed at all).
+
+        ``unfiltered`` denotes whether not to limit updates to prefilter
+        conditions. Note that prefiltering causes an additional database query
+        to retrieve matching pks. For better performance set it to false, which
+        will avoid the pk lookup and instead apply updates for all instances
+        in ``objs`` (default is false to be in line with ``bulk_update``).
+
+        Returns the number of affected rows.
+        """
+        if not objs:
+            return 0
+        objs = tuple(objs)
+        fields = tuple(fields or [])
+        sanity_check(self.model, objs, fields, 'fast_update', batch_size)
+        return fast_update(self, objs, fields, batch_size, unfiltered)
+
+    fast_update.alters_data = True
+
+
+    def merged_update(
+        self,
+        objs: Iterable[Model],
+        fields: Iterable[str],
+        batch_size: Optional[int] = None,
+        unfiltered: bool = False
+    ) -> int:
+        """
+        Alternative for ``bulk_update`` with a compatible method
+        signature.
+
+        The method uses ``update`` internally and attempts to merge values
+        and pks into less UPDATE statements for better performance.
+        This works well with sparse or heavily intersecting data.
+        The method will fall back to ``flat_update`` if the merging does
+        not promise any speed advantage.
+
+        ``batch_size`` is a dummy argument to keep the interface compatible.
+        ``unfiltered`` is false by default. Set it to true to ignore
+        any prefiltering on the queryset.
+
+        This is update method currently flagged as ALPHA, as the merge and
+        prediction still need algorithmic rework and a proper test base.
+
+        Returns the number objects. Due to the heavy update shuffling
+        a more exact number of rows cannot be provided anymore.
+        """
+        if not objs:
+            return 0
+        objs = tuple(objs)
+        fields = tuple(fields or [])
+        sanity_check(self.model, objs, fields, 'merged_update', batch_size)
+        return merged_update(self, objs, fields, unfiltered=unfiltered)
+
+    merged_update.alters_data = True
+
+
+    def flat_update(
+        self,
+        objs: Iterable[Model],
+        fields: Iterable[str],
+        batch_size: Optional[int] = None,
+        unfiltered: bool = False
+    ) -> int:
+        """
+        Alternative for ``bulk_update`` with a compatible method
+        signature.
+
+        This method is the most straight-forward usage of ``update``
+        calling it once per object with all model-local fields applied.
+        Use this for dense updates, where field values have little
+        to no intersections or update order is important.
+
+        ``batch_size`` is a dummy argument to keep the interface compatible.
+        ``unfiltered`` is false by default. Set it to true to ignore
+        any prefiltering on the queryset.
+
+        Returns the number of affected rows.
+        """
+        if not objs:
+            return 0
+        objs = tuple(objs)
+        fields = tuple(fields or [])
+        sanity_check(self.model, objs, fields, 'flat_update', batch_size)
+        return flat_update(self, objs, fields, unfiltered=unfiltered)
+
+    flat_update.alters_data = True
+
+
+    def copy_update(
+        self,
+        objs: Iterable[Model],
+        fields: Iterable[str],
+        field_encoders: Optional[Dict[str, Any]] = None,
+        encoding: Optional[str] = None
+    ) -> int:
+        """
+        PostgreSQL only method (raises an exception on any other backend)
+        to update a large amount of model instances via COPY FROM.
+        The method follows the same interface idea of ``bulk_update`` or ``fast_update``,
+        but will perform much better for bigger updates, even than ``fast_update``.
+
+        Other than for ``fast_update``, there is no ``batch_size`` argument anymore,
+        as the update is always done in one single big batch by copying the data into
+        a temporary table and run the update from there.
+
+        For the data transport postgres' TEXT format is used. For this the field values
+        get encoded by special encoders. The encoders are globally registered for
+        django's standard field types (works similar to `get_db_prep_value`).
+        With ``field_encoders`` custom encoders can be attached to update fields
+        for a single call. This might come handy for additional conversion work or
+        further speedup by omitting the base type checks of the default encoders
+        (do this only if the data was checked by other means, otherwise malformed
+        updates may happen).
+
+        ``encoding`` overwrites the text encoding used in the COPY FROM transmission
+        (default is psycopg's connection encoding).
+
+        Note that any prefiltering on the queryset is ignored, thus all objects given
+        will be updated.
+
+        Returns the number of affected rows.
+
+        NOTE: The underlying implementation is only a PoC and probably will be replaced
+        soon by the much safer and superior COPY support of psycopg3.
+        """
+        self._for_write = True
+        connection = connections[self.db]
+        if connection.vendor != 'postgresql':
+            raise NotSupportedError(f'copy_update() is not supported on "{connection.vendor}" backend')
+        from .copy import copy_update
+        if not objs:
+            return 0
+        objs = tuple(objs)
+        fields = tuple(fields or [])
+        sanity_check(self.model, objs, fields, 'copy_update')
+        return copy_update(self, objs, fields, field_encoders, encoding)
+    
+    copy_update.alters_data = True
+
+
+class FastUpdateManager(Manager.from_queryset(FastUpdateQuerySet)):
+    pass
