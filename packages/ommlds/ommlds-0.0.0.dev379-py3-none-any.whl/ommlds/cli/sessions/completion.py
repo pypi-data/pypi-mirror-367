eb02be7dff494425e464b47d9c9e1e8887f21e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:58 +0000

    Add package ommlds/0.0.0.dev379/ommlds-0.0.0.dev379-py3-none-any.whl

---
+++ packages/ommlds/ommlds-0.0.0.dev379-py3-none-any.whl/ommlds/cli/sessions/completion.py
@@ -0,0 +1,54 @@
+import dataclasses as dc
+import typing as ta
+
+from omlish import check
+from omlish import lang
+
+from ... import minichain as mc
+from .base import Session
+
+
+if ta.TYPE_CHECKING:
+    from ...minichain.backends.llamacpp import completion as mc_lcc_completion
+    from ...minichain.backends.openai import completion as mc_openai_completion
+    from ...minichain.backends.transformers import transformers as mc_tfm
+else:
+    mc_lcc_completion = lang.proxy_import('...minichain.backends.llamacpp.completion', __package__)
+    mc_openai_completion = lang.proxy_import('...minichain.backends.openai.completion', __package__)
+    mc_tfm = lang.proxy_import('...minichain.backends.transformers.transformers', __package__)
+
+
+##
+
+
+DEFAULT_COMPLETION_MODEL_BACKEND = 'openai'
+
+COMPLETION_MODEL_BACKENDS: ta.Mapping[str, ta.Callable[[], type[mc.CompletionService]]] = {
+    'llamacpp': lambda: mc_lcc_completion.LlamacppCompletionService,
+    'openai': lambda: mc_openai_completion.OpenaiCompletionService,
+    'transformers': lambda: mc_tfm.TransformersCompletionService,
+}
+
+
+##
+
+
+class CompletionSession(Session['CompletionSession.Config']):
+    @dc.dataclass(frozen=True)
+    class Config(Session.Config):
+        content: mc.Content
+
+        _: dc.KW_ONLY
+
+        backend: str | None = None
+
+    def __init__(self, config: Config) -> None:
+        super().__init__(config)
+
+    def run(self) -> None:
+        prompt = check.isinstance(self._config.content, str)
+        with lang.maybe_managing(
+                COMPLETION_MODEL_BACKENDS[self._config.backend or DEFAULT_COMPLETION_MODEL_BACKEND]()(),
+        ) as mdl:
+            response = mdl.invoke(mc.CompletionRequest(prompt))
+            print(response.v.strip())
---
+++ packages/ommlds/ommlds-0.0.0.dev379-py3-none-any.whl/ommlds/minichain/chat/tools/execution/messages.py
@@ -0,0 +1,25 @@
+from ...messages import ToolExecRequest
+from ...messages import ToolExecResultMessage
+from .context import ToolContext
+from .executors import ToolExecutor
+
+
+##
+
+
+def execute_tool_request(
+        ctx: ToolContext,
+        tex: ToolExecutor,
+        ter: ToolExecRequest,
+) -> ToolExecResultMessage:
+    result_str = tex.execute_tool(
+        ctx,
+        ter.name,
+        ter.args,
+    )
+
+    return ToolExecResultMessage(
+        id=ter.id,
+        name=ter.name,
+        c=result_str,
+    )
