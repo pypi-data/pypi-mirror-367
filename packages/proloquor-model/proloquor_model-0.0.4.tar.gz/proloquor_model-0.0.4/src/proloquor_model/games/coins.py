cc2fcc64f9cb3996fb407da753cf357e46cc93e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 15:06:19 +0000

    Add package proloquor-model/0.0.4/proloquor_model-0.0.4.tar.gz

---
+++ packages/proloquor-model/proloquor_model-0.0.4.tar.gz/proloquor_model-0.0.4/src/proloquor_model/games/coins.py
@@ -0,0 +1,18 @@
+from enum import Enum
+import numpy as np
+
+class CoinSide(Enum):
+    HEADS = 1
+    TAILS = 2
+
+class Coin:
+    def __init__(self):
+        self.side = CoinSide.HEADS
+
+    def __str__(self):    
+        return self.side.name
+    
+    def flip(self):
+        self.side = np.random.choice([c for c in CoinSide])
+        return self
+    
