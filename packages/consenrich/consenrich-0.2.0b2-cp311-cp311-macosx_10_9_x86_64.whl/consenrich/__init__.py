06938e5bf51ae762d71afd59ddab909081cbf50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 15:06:43 +0000

    Add package consenrich/0.2.0b2/consenrich-0.2.0b2-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/consenrich/consenrich-0.2.0b2-cp311-cp311-macosx_10_9_x86_64.whl/consenrich/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from importlib import import_module
+cconsenrich = import_module(__name__ + ".cconsenrich")
+from .cconsenrich import *
+from . import core, misc_util, constants, detrorm
+from .core import *
+from .misc_util import *
+from .constants import *
+from .detrorm import *
+
