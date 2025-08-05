4e634e308dfc636d022e990093335ef1e0806580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 16:06:17 +0000

    Add package harmonify-patcher/1.4.0/harmonify_patcher-1.4.0.tar.gz

---
+++ packages/harmonify-patcher/harmonify_patcher-1.4.0.tar.gz/harmonify_patcher-1.4.0/src/harmonify/__init__.py
@@ -0,0 +1,9 @@
+from .core import *
+from .flow_control import *
+from .func_types import *
+from .patch import *
+from .injector import *
+from .injector_security import *
+from .hook import *
+
+version = "1.4.0"
