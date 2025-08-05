0533cb27307898d6087150891c64eed9185ca250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 15:06:39 +0000

    Add package mat3ra-made/2025.8.5.post0/mat3ra_made-2025.8.5.post0-py3-none-any.whl

---
+++ packages/mat3ra-made/mat3ra_made-2025.8.5.post0-py3-none-any.whl/mat3ra/made/tools/analyze/interface/utils/holders.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from mat3ra.code.entity import InMemoryEntityPydantic
+
+from ....build.slab.strained_supercell_slab.configuration import SlabStrainedSupercellConfiguration
+
+
+class MatchedSubstrateFilmConfigurationHolder(InMemoryEntityPydantic):
+    match_id: int
+    substrate_configuration: SlabStrainedSupercellConfiguration
+    film_configuration: SlabStrainedSupercellConfiguration
+    total_strain_percentage: Optional[float] = None
