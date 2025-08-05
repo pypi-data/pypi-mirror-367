aa289ec6d83ddfbb69210eb09898ef51d3a15a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:17 +0000

    Add package eth-wake/5.0.0rc0/eth_wake-5.0.0rc0-cp313-cp313t-win_amd64.whl

---
+++ packages/eth-wake/eth_wake-5.0.0rc0-cp313-cp313t-win_amd64.whl/wake/development/constants.py
@@ -0,0 +1,39 @@
+# tab space width for indentation
+TAB_WIDTH = 4
+
+DEFAULT_IMPORTS: str = """
+from __future__ import annotations
+
+import dataclasses
+from typing import List, Dict, Optional, overload, Union, Callable, Tuple
+from typing_extensions import Literal
+
+from wake.development.core import Contract, Library, Address, Account, Chain, RequestType, SignedAuthorization, Struct
+from wake.development.primitive_types import *
+from wake.development.transactions import TransactionAbc, RevertError
+"""
+
+
+INIT_CONTENT: str = """
+import wake.development.core
+from wake.utils import get_package_version
+
+if get_package_version("eth-wake") != "{version}":
+    raise RuntimeError("Pytypes generated for a different version of wake. Please regenerate.")
+
+def recursive_update(d, u):
+    for k, v in u.items():
+        if isinstance(v, dict):
+            d[k] = recursive_update(d.get(k, {{}}), v)
+        else:
+            d[k] = v
+    return d
+
+recursive_update(wake.development.core.errors, {errors})
+recursive_update(wake.development.core.events, {events})
+wake.development.core.contracts_by_fqn.update({contracts_by_fqn})
+wake.development.core.contracts_by_metadata.update({contracts_by_metadata})
+wake.development.core.contracts_inheritance.update({contracts_inheritance})
+wake.development.core.creation_code_index.extend({creation_code_index})
+wake.development.core.user_defined_value_types_index.update({user_defined_value_types_index})
+"""
