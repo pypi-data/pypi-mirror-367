30f6c9b9dad116ea037e3ecefafc48439bfbee3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 16:06:10 +0000

    Add package maleo-soma/0.1.11/maleo_soma-0.1.11-py3-none-any.whl

---
+++ packages/maleo-soma/maleo_soma-0.1.11-py3-none-any.whl/maleo_soma/managers/key/rsa.py
@@ -0,0 +1,47 @@
+from maleo_soma.dtos.settings import Settings
+from maleo_soma.enums.secret import SecretFormat
+from maleo_soma.managers.client.google.secret import GoogleSecretManager
+from maleo_soma.schemas.key.rsa import Complete
+from maleo_soma.types.base import OptionalUUID
+
+
+class RSAKeyManager:
+    def __init__(
+        self,
+        settings: Settings,
+        secret_manager: GoogleSecretManager,
+        operation_id: OptionalUUID = None,
+    ) -> None:
+        if settings.KEY_PASSWORD is not None:
+            password = settings.KEY_PASSWORD
+        else:
+            read_key_password = secret_manager.read(
+                SecretFormat.STRING,
+                name="maleo-key-password",
+                operation_id=operation_id,
+            )
+            if read_key_password.data.old is None:
+                raise ValueError("Key password not found")
+            password = read_key_password.data.old.value
+
+        if settings.PRIVATE_KEY is not None:
+            private = settings.PRIVATE_KEY
+        else:
+            read_private_key = secret_manager.read(
+                SecretFormat.STRING, name="maleo-private-key", operation_id=operation_id
+            )
+            if read_private_key.data.old is None:
+                raise ValueError("Private key not found")
+            private = read_private_key.data.old.value
+
+        if settings.PUBLIC_KEY is not None:
+            public = settings.PUBLIC_KEY
+        else:
+            read_public_key = secret_manager.read(
+                SecretFormat.STRING, name="maleo-public-key", operation_id=operation_id
+            )
+            if read_public_key.data.old is None:
+                raise ValueError("Public key not found")
+            public = read_public_key.data.old.value
+
+        self.keys = Complete(password=password, private=private, public=public)
