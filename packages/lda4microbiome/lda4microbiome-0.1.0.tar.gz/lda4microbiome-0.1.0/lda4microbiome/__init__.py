daa21658b1ed2a96ea34ddb23abba0ba03fa0880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:33:39 +0000

    Add package lda4microbiome/0.1.0/lda4microbiome-0.1.0.tar.gz

---
+++ packages/lda4microbiome/lda4microbiome-0.1.0.tar.gz/lda4microbiome-0.1.0/lda4microbiome/__init__.py
@@ -0,0 +1,28 @@
+"""
+LDA4Microbiome: A comprehensive workflow for LDA analysis of microbiome data using MALLET
+
+This package provides tools for:
+- Taxonomic data preprocessing
+- LDA model training with MALLET
+- Model selection and evaluation
+- Results visualization and analysis
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+# Import main classes for easy access
+from .preprocessing import TaxonomyProcessor
+from .training import LDATrainer  
+from .selection import SankeyDataProcessor
+from .visualization import LDAModelVisualizer, TopicFeatureProcessor
+
+__all__ = [
+    'TaxonomyProcessor',
+    'LDATrainer', 
+    'SankeyDataProcessor',
+    'LDAModelVisualizer',
+    'TopicFeatureProcessor'
+]
+
---
+++ packages/lda4microbiome/lda4microbiome-0.1.0.tar.gz/lda4microbiome-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lda4microbiome',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A workflow for LDA analysis of microbiome data using MALLET',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/lda4microbiome',  # Update this to your URL
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'gensim',
+        'little-mallet-wrapper',
+    ],
+)
+
