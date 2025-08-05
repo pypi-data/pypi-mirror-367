454620a30bc05d21556b5a2eeb6cf64fa1339caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:33:49 +0000

    Add package bigquery-views-manager/1.0.12/bigquery_views_manager-1.0.12-py3-none-any.whl

---
+++ packages/bigquery-views-manager/bigquery_views_manager-1.0.12-py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,22 @@
+import logging
+from pathlib import Path
+from unittest.mock import MagicMock
+
+import pytest
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_logging():
+    logging.basicConfig(level='INFO')
+    for name in ['tests', 'bigquery_views_manager']:
+        logging.getLogger(name).setLevel('DEBUG')
+
+
+@pytest.fixture(name="bq_client")
+def _bq_client():
+    return MagicMock()
+
+
+@pytest.fixture()
+def temp_dir(tmp_path: Path) -> Path:
+    return tmp_path
