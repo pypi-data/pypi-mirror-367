b37d0cd2e4c6a6a65cf2ff835ba8602ce98fc2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 14:08:28 +0000

    Add package amps-python-client/5.3.5.0/amps_python_client-5.3.5.0.tar.gz

---
+++ packages/amps-python-client/amps_python_client-5.3.5.0.tar.gz/amps-python-client-5.3.5.0/src/memorypublishstore_docs.h
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2010-2025 60East Technologies Inc., All Rights Reserved.
+//
+// This computer software is owned by 60East Technologies Inc. and is
+// protected by U.S. copyright laws and other laws and by international
+// treaties.  This computer software is furnished by 60East Technologies
+// Inc. pursuant to a written license agreement and may be used, copied,
+// transmitted, and stored only in accordance with the terms of such
+// license agreement and with the inclusion of the above copyright notice.
+// This computer software or any other copies thereof may not be provided
+// or otherwise made available to any other person.
+//
+// U.S. Government Restricted Rights.  This computer software: (a) was
+// developed at private expense and is in all respects the proprietary
+// information of 60East Technologies Inc.; (b) was not developed with
+// government funds; (c) is a trade secret of 60East Technologies Inc.
+// for all purposes of the Freedom of Information Act; and (d) is a
+// commercial item and thus, pursuant to Section 12.212 of the Federal
+// Acquisition Regulations (FAR) and DFAR Supplement Section 227.7202,
+// Government's use, duplication or disclosure of the computer software
+// is subject to the restrictions set forth by 60East Technologies Inc..
+//
+////////////////////////////////////////////////////////////////////////////
+
+static const char* memorypublishstore_class_doc = R"docstring(MemoryPublishStore()
+A publish store that keeps messages in memory. This class is the
+default publish store for a Python HAClient. The HAClient manages
+storing messages in the publish store, replaying messages to the
+server after failover, and removing messages from the store. With
+this publish store, an application typically checks to be sure
+that the publish store is empty (that is, all messages have been
+persisted in the AMPS server) before exiting.
+)docstring";
