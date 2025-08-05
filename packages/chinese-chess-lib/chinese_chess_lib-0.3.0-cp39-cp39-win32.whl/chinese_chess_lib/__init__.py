cb30aafe4f375c4a0185312c58ff2c53c71d6816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 16:05:32 +0000

    Add package chinese-chess-lib/0.3.0/chinese_chess_lib-0.3.0-cp39-cp39-win32.whl

---
+++ packages/chinese-chess-lib/chinese_chess_lib-0.3.0-cp39-cp39-win32.whl/chinese_chess_lib/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from ._core import __doc__, __version__, get_legal_moves, warn, dead, Chess
+
+__all__ = ["__doc__", "__version__", "get_legal_moves", "warn", "dead", "Chess"]
