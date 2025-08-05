7e8e00d22b3929c7f9771eccf57e6b9ae22ceb95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 15:06:33 +0000

    Add package signal-petrophysics/0.0.1b24/signal_petrophysics-0.0.1b24.tar.gz

---
+++ packages/signal-petrophysics/signal_petrophysics-0.0.1b24.tar.gz/signal_petrophysics-0.0.1b24/src/signal_petrophysics/postprocessing/__init__.py
@@ -0,0 +1,25 @@
+# src/signal_petrophysics/postprocessing/__init__.py
+
+"""Postprocessing module for signal petrophysics package."""
+
+# Import functions from the postprocessing module
+from .postprocessing import (
+    process_depth_ranges,
+    merge_and_label_dataframes,
+    export_labeled_logs_to_csv,
+    concatenate_all_depth_ranges,
+    extend_labels
+)
+
+# Import required functions from other modules that are used in postprocessing
+from ..pattern_find import signal_sampling_by_depth, calc
+from ..signal_adapt import adjust_signal_length
+
+# Define what gets exported when using "from postprocessing import *"
+__all__ = [
+    'process_depth_ranges',
+    'merge_and_label_dataframes', 
+    'export_labeled_logs_to_csv',
+    'concatenate_all_depth_ranges',
+    'extend_labels'
+]
