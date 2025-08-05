f4a4d3979074492572c960b10c66f5aaf79aa2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:33:35 +0000

    Add package publicmodel/2.2.8/publicmodel-2.2.8.tar.gz

---
+++ packages/publicmodel/publicmodel-2.2.8.tar.gz/publicmodel-2.2.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="publicmodel",
+    version="2.2.8",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="YanXinle",
+    author_email="1020121123@qq.com",
+    description="作者: YanXinle",
+    url="https://github.com/Yanxinle1123/LeleComm",
+)
