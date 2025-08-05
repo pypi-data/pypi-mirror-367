4ca4de85a3bfeb4d1c05e8b724380a9537f77b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 14:08:07 +0000

    Add package midasflow/0.1.4.post2/midasflow-0.1.4.post2.tar.gz

---
+++ packages/midasflow/midasflow-0.1.4.post2.tar.gz/midasflow-0.1.4.post2/package.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+set -e
+rm -rf midasflow* build dist README.md LICENSE setup.py deploy.sh
+test "$1" = "clean" && exit
+
+cp -a ../midasflow ../README.md .
+cp -a ../COPYING LICENSE
+
+cat<<EOT > setup.py
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read().rstrip()
+
+with open("midasflow/VERSION") as f:
+    version = f.read().rstrip()
+
+setuptools.setup(
+    name="midasflow",
+    version=version,
+    license="GPLv3+",
+    author="Huidae Cho",
+    author_email="grass4u@gmail.com",
+    description="MIDASFlow is the Python package for the Memory-Efficient I/O-Improved Drainage Analysis System (MIDAS).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HuidaeCho/midasflow",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3",
+    package_data={"midasflow": ["VERSION", "midasflow.db"]},
+    entry_points={"console_scripts": ["midasflow=midasflow:main"]},
+)
+EOT
+
+cat<<EOT > deploy.sh
+#!/bin/sh
+set -e
+pip3 install --user --upgrade twine
+twine upload dist/*
+EOT
+chmod a+x deploy.sh
+
+pip3 install --user --upgrade setuptools wheel
+python3 setup.py sdist bdist_wheel
