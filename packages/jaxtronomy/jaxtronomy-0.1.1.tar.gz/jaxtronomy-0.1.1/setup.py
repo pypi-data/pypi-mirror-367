c27b2049fec29dc9443fba7a067e35af6b3deb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 07:08:04 +0000

    Add package jaxtronomy/0.1.1/jaxtronomy-0.1.1.tar.gz

---
+++ packages/jaxtronomy/jaxtronomy-0.1.1.tar.gz/jaxtronomy-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+requires = [
+    "lenstronomy>=1.13.2",
+    "numpy>=2.0.0",
+    "jax>=0.7.0",
+    "jaxlib>=0.7.0",
+]
+tests_require = ["pytest"]
+
+setup(
+    name="jaxtronomy",
+    version="0.1.1",
+    url="https://github.com/lenstronomy/JAXtronomy",
+    author="jaxtronomy developers",
+    description="lenstronomy, but in JAX",
+    packages=find_packages(),
+    license="BSD-3",
+    install_requires=requires,
+    tests_require=tests_require,
+    keywords="jaxtronomy",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
