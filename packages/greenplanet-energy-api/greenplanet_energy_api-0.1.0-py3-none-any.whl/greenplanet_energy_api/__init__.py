9987744dbc8b0e61ff83768feb985aa91b010533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 07:07:54 +0000

    Add package greenplanet-energy-api/0.1.0/greenplanet_energy_api-0.1.0-py3-none-any.whl

---
+++ packages/greenplanet-energy-api/greenplanet_energy_api-0.1.0-py3-none-any.whl/greenplanet_energy_api/__init__.py
@@ -0,0 +1,16 @@
+"""Green Planet Energy API client."""
+
+from .api import GreenPlanetEnergyAPI
+from .exceptions import (
+    GreenPlanetEnergyAPIError,
+    GreenPlanetEnergyConnectionError,
+    GreenPlanetEnergyError,
+)
+
+__version__ = "0.1.0"
+__all__ = [
+    "GreenPlanetEnergyAPI",
+    "GreenPlanetEnergyError",
+    "GreenPlanetEnergyConnectionError", 
+    "GreenPlanetEnergyAPIError",
+]
