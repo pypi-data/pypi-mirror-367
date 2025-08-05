7c60b66ba46ce7df3cd229868d773d2b50ddac95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 04:23:04 +0000

    Add package fr24sdk/0.1.0/fr24sdk-0.1.0-py3-none-any.whl

---
+++ packages/fr24sdk/fr24sdk-0.1.0-py3-none-any.whl/fr24sdk/__init__.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: Copyright Flightradar24
+#
+# SPDX-License-Identifier: MIT
+"""Flightradar24 API SDK for Python."""
+
+__version__ = "0.1.0"
+
+from .client import Client
+
+# from .async_client import AsyncClient # To be added later
+from .exceptions import (
+    Fr24SdkError,
+    TransportError,
+    ApiError,
+    AuthenticationError,
+    RateLimitError,
+    PaymentRequiredError,
+    BadRequestError,
+    NotFoundError,
+)
+
+__all__ = [
+    "Client",
+    # "AsyncClient",
+    "Fr24SdkError",
+    "TransportError",
+    "ApiError",
+    "AuthenticationError",
+    "RateLimitError",
+    "PaymentRequiredError",
+    "BadRequestError",
+    "NotFoundError",
+    "__version__",
+]
