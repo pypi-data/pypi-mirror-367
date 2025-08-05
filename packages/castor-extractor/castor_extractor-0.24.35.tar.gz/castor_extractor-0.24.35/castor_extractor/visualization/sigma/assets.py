7497fa21c8b08ac98504bf03366a5d043a3f3009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 04:21:41 +0000

    Add package castor-extractor/0.24.35/castor_extractor-0.24.35.tar.gz

---
+++ packages/castor-extractor/castor_extractor-0.24.35.tar.gz/castor_extractor-0.24.35/castor_extractor/visualization/sigma/assets.py
@@ -0,0 +1,13 @@
+from ...types import ExternalAsset
+
+
+class SigmaAsset(ExternalAsset):
+    """Sigma assets"""
+
+    DATASETS = "datasets"
+    ELEMENTS = "elements"
+    FILES = "files"
+    LINEAGES = "lineages"
+    MEMBERS = "members"
+    QUERIES = "queries"
+    WORKBOOKS = "workbooks"
