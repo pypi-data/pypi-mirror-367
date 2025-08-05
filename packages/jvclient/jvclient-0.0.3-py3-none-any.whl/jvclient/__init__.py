da905e422bd6854c3fdce152fcd929ca278dd6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 07:08:29 +0000

    Add package jvclient/0.0.3/jvclient-0.0.3-py3-none-any.whl

---
+++ packages/jvclient/jvclient-0.0.3-py3-none-any.whl/jvclient/__init__.py
@@ -0,0 +1,8 @@
+"""
+jvclient package initialization.
+
+This package provides a library of utils and components for action ui development.
+"""
+
+__version__ = "0.0.3"
+__supported__jivas__versions__ = ["2.0.0"]
