f8de2f0212fa9ce35f11b423702f76c3b710da54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 08:04:18 +0000

    Add package windmill-api/1.518.1/windmill_api-1.518.1-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.518.1-py3-none-any.whl/windmill_api/models/list_assets_by_usage_response_200_item_item_kind.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class ListAssetsByUsageResponse200ItemItemKind(str, Enum):
+    RESOURCE = "resource"
+    S3OBJECT = "s3object"
+
+    def __str__(self) -> str:
+        return str(self.value)
---
+++ packages/windmill-api/windmill_api-1.518.1-py3-none-any.whl/windmill_api/models/raw_script_assets_item_kind.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class RawScriptAssetsItemKind(str, Enum):
+    RESOURCE = "resource"
+    S3OBJECT = "s3object"
+
+    def __str__(self) -> str:
+        return str(self.value)
