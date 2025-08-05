cc021e9827a47a76523423c337a5abf1ef762c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:30 +0000

    Add package pybmx/0.2.0/pybmx-0.2.0.tar.gz

---
+++ packages/pybmx/pybmx-0.2.0.tar.gz/pybmx-0.2.0/src/pybmx/types.py
@@ -0,0 +1,9 @@
+import ctypes
+import typing as t
+
+U8: t.TypeAlias = ctypes.c_uint8
+S8: t.TypeAlias = ctypes.c_int8
+S16: t.TypeAlias = ctypes.c_int16
+U16: t.TypeAlias = ctypes.c_uint16
+S32: t.TypeAlias = ctypes.c_int32
+U32: t.TypeAlias = ctypes.c_uint32
