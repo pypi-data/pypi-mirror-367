f93aa3378e7061e51924efdc223ae2371dc43bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:59 +0000

    Add package qbraid/0.9.9.dev20250804200302/qbraid-0.9.9.dev20250804200302-py3-none-any.whl

---
+++ packages/qbraid/qbraid-0.9.9.dev20250804200302-py3-none-any.whl/qbraid/runtime/aws/job.py
@@ -0,0 +1,135 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module defining BraketQuantumTask Class
+
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from braket.aws import AwsQuantumTask
+from braket.tasks.analog_hamiltonian_simulation_quantum_task_result import (
+    AnalogHamiltonianSimulationQuantumTaskResult,
+)
+from braket.tasks.gate_model_quantum_task_result import GateModelQuantumTaskResult
+
+from qbraid._logging import logger
+from qbraid.runtime.enums import JobStatus
+from qbraid.runtime.exceptions import JobStateError
+from qbraid.runtime.job import QuantumJob
+from qbraid.runtime.result import Result
+from qbraid.runtime.result_data import AhsResultData, GateModelResultData
+
+from .result_builder import BraketAhsResultBuilder, BraketGateModelResultBuilder
+from .tracker import get_quantum_task_cost
+
+if TYPE_CHECKING:
+    from decimal import Decimal
+
+    from braket.aws import AwsSession
+
+AWS_TASK_STATUS_MAP = {
+    "CREATED": JobStatus.INITIALIZING,
+    "QUEUED": JobStatus.QUEUED,
+    "RUNNING": JobStatus.RUNNING,
+    "CANCELLING": JobStatus.CANCELLING,
+    "CANCELLED": JobStatus.CANCELLED,
+    "COMPLETED": JobStatus.COMPLETED,
+    "FAILED": JobStatus.FAILED,
+}
+
+
+class AmazonBraketVersionError(Exception):
+    """Exception raised for Amazon Braket SDK errors due to versioning."""
+
+
+class BraketQuantumTask(QuantumJob):
+    """Wrapper class for Amazon Braket ``QuantumTask`` objects."""
+
+    def __init__(self, task_id: str, task: Optional[AwsQuantumTask] = None, **kwargs):
+        """Create a BraketQuantumTask."""
+
+        super().__init__(task_id, **kwargs)
+        self._task = task or AwsQuantumTask(task_id)
+
+    def status(self):
+        """Returns status from Braket QuantumTask object metadata."""
+        state = self._task.state()
+        status = AWS_TASK_STATUS_MAP.get(state, JobStatus.UNKNOWN)
+        self._cache_metadata["status"] = status
+        return status
+
+    def queue_position(self) -> Optional[int]:
+        """Returns queue position from Braket QuantumTask.
+        '>2000' returns as 2000 for typing consistency."""
+        try:
+            position = self._task.queue_position().queue_position
+            if isinstance(position, str):
+                if position.startswith(">"):
+                    position = position[1:]
+                return int(position)
+            return position
+        except AttributeError as err:
+            raise AmazonBraketVersionError(
+                "Queue visibility is only available for amazon-braket-sdk>=1.56.0"
+            ) from err
+
+    def result(self) -> Result:
+        """Return the results of the job."""
+        if not self.is_terminal_state():
+            logger.info("Result will be available when the job has reached a final state.")
+
+        bk_result = self._task.result()
+        metadata = self._task.metadata()
+        success = metadata["status"] == "COMPLETED"
+        device_id = metadata["deviceArn"]
+        job_id = metadata["quantumTaskArn"]
+
+        result_mapping = {
+            GateModelQuantumTaskResult: (BraketGateModelResultBuilder, GateModelResultData),
+            AnalogHamiltonianSimulationQuantumTaskResult: (BraketAhsResultBuilder, AhsResultData),
+        }
+
+        builder_class, data_class = result_mapping.get(type(bk_result), (None, None))
+
+        if not builder_class or not data_class:
+            raise ValueError(f"Unsupported result type: {type(bk_result).__name__}")
+
+        result_data = {
+            "measurement_counts": builder_class(bk_result).get_counts() if success else None,
+            "measurements": builder_class(bk_result).measurements() if success else None,
+        }
+
+        data = data_class(**result_data)
+        return Result(device_id=device_id, job_id=job_id, success=success, data=data, **metadata)
+
+    def cancel(self) -> None:
+        """Cancel the quantum task."""
+        task = self._task
+
+        if self.is_terminal_state():
+            raise JobStateError("Cannot cancel quantum job in terminal state.")
+
+        try:
+            task.cancel()
+        except RuntimeError:
+            task._aws_session.cancel_quantum_task(self.id)
+
+    @staticmethod
+    def _get_cost(task_arn: str, aws_session: Optional[AwsSession] = None) -> Decimal:
+        """Return the cost of the quantum task."""
+        return get_quantum_task_cost(task_arn, aws_session=aws_session)
+
+    def get_cost(self) -> float:
+        """Return the cost of the job."""
+        decimal_cost = self._get_cost(self.id)
+        return float(decimal_cost)
---
+++ packages/qbraid/qbraid-0.9.9.dev20250804200302-py3-none-any.whl/qbraid/runtime/aws/result_builder.py
@@ -0,0 +1,116 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module defining BraketGateModelResultBuilder Class
+
+"""
+from __future__ import annotations
+
+from collections import Counter
+from typing import TYPE_CHECKING, Optional
+
+import numpy as np
+
+from qbraid.runtime.exceptions import QbraidRuntimeError
+from qbraid.runtime.result_data import AhsShotResult
+
+if TYPE_CHECKING:
+    from braket.tasks.analog_hamiltonian_simulation_quantum_task_result import (
+        AnalogHamiltonianSimulationQuantumTaskResult,
+    )
+    from braket.tasks.gate_model_quantum_task_result import GateModelQuantumTaskResult
+
+
+class ResultDecodingError(QbraidRuntimeError):
+    """Exception raised for errors that occur during the decoding of result data."""
+
+
+class BraketGateModelResultBuilder:
+    """Wrapper class for Amazon Braket result objects."""
+
+    def __init__(self, result: GateModelQuantumTaskResult):
+        self._result = result
+
+    def measurements(self) -> np.ndarray:
+        """
+        2d array - row is shot and column is qubit. Default is None.
+        Only available when shots > 0. The qubits in `measurements` are
+        the ones in `GateModelQuantumTaskResult.measured_qubits`.
+
+        """
+        result: GateModelQuantumTaskResult = self._result
+        return np.flip(result.measurements, 1)
+
+    def get_counts(self) -> dict[str, int]:
+        """Returns the histogram data of the run"""
+        result: GateModelQuantumTaskResult = self._result
+        braket_counts = dict(result.measurement_counts)
+        qbraid_counts = {}
+        for key in braket_counts:
+            str_key = "".join(reversed([str(i) for i in key]))
+            qbraid_counts[str_key] = braket_counts[key]
+        return qbraid_counts
+
+
+class BraketAhsResultBuilder:
+    """Result from an Analog Hamiltonian Simulation (AHS)."""
+
+    def __init__(self, result: AnalogHamiltonianSimulationQuantumTaskResult):
+        self._result = result
+
+    def measurements(self) -> list[AhsShotResult]:
+        """Get the list of shot results from the AHS job."""
+        result: AnalogHamiltonianSimulationQuantumTaskResult = self._result
+        return [
+            AhsShotResult(
+                success=m.status.name == "SUCCESS",
+                pre_sequence=m.pre_sequence,
+                post_sequence=m.post_sequence,
+            )
+            for m in result.measurements
+        ]
+
+    def get_counts(self) -> Optional[dict[str, int]]:
+        """
+        Aggregate state counts from AHS shot results.
+
+        This function decodes the state of atoms at different sites based on
+        pre- and post-measurement sequences. Each atom can be in one of three states:
+        empty site ('e'), Rydberg state ('r'), or ground state ('g').
+
+        Returns:
+            Optional[dict[str, int]]: A dictionary mapping each unique state configuration to
+                                      the count of its occurrences across all successful shots,
+                                      or None if there are no successful measurements.
+
+        Raises:
+            ResultDecodingError: If there is an error accessing required attributes from
+                the result object.
+
+        """
+        state_counts = Counter()
+        states = ["e", "r", "g"]
+        try:
+            for shot in self.measurements():
+                if shot.success:
+                    pre = shot.pre_sequence
+                    post = shot.post_sequence
+                    # converting presequence and postsequence measurements to state_idx
+                    state_idx = [
+                        0 if pre_i == 0 else 1 if post_i == 0 else 2
+                        for pre_i, post_i in zip(pre, post)
+                    ]
+                    state = "".join(states[s_idx] for s_idx in state_idx)
+                    state_counts.update([state])
+        except AttributeError as err:
+            raise ResultDecodingError from err
+
+        return None if not state_counts else dict(state_counts)
