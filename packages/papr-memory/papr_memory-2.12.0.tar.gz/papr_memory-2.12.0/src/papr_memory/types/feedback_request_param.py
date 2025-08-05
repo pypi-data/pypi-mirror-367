f5ca49d76bf0444bce952377426d01406e3cf101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 05:10:28 +0000

    Add package papr-memory/2.12.0/papr_memory-2.12.0.tar.gz

---
+++ packages/papr-memory/papr_memory-2.12.0.tar.gz/papr_memory-2.12.0/src/papr_memory/types/feedback_request_param.py
@@ -0,0 +1,89 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Literal, Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["FeedbackRequestParam", "FeedbackData", "FeedbackDataAssistantMessage", "FeedbackDataUserMessage"]
+
+
+class FeedbackDataAssistantMessage(TypedDict, total=False):
+    class_name: Required[Annotated[str, PropertyInfo(alias="className")]]
+
+    object_id: Required[Annotated[str, PropertyInfo(alias="objectId")]]
+
+    _type: Annotated[str, PropertyInfo(alias="__type")]
+
+
+class FeedbackDataUserMessage(TypedDict, total=False):
+    class_name: Required[Annotated[str, PropertyInfo(alias="className")]]
+
+    object_id: Required[Annotated[str, PropertyInfo(alias="objectId")]]
+
+    _type: Annotated[str, PropertyInfo(alias="__type")]
+
+
+class FeedbackData(TypedDict, total=False):
+    feedback_source: Required[
+        Annotated[
+            Literal["inline", "post_query", "session_end", "memory_citation", "answer_panel"],
+            PropertyInfo(alias="feedbackSource"),
+        ]
+    ]
+    """Where the feedback was provided from"""
+
+    feedback_type: Required[
+        Annotated[
+            Literal[
+                "thumbs_up",
+                "thumbs_down",
+                "rating",
+                "correction",
+                "report",
+                "copy_action",
+                "save_action",
+                "create_document",
+                "memory_relevance",
+                "answer_quality",
+            ],
+            PropertyInfo(alias="feedbackType"),
+        ]
+    ]
+    """Types of feedback that can be provided"""
+
+    assistant_message: Annotated[Optional[FeedbackDataAssistantMessage], PropertyInfo(alias="assistantMessage")]
+    """A pointer to a Parse object"""
+
+    cited_memory_ids: Annotated[Optional[List[str]], PropertyInfo(alias="citedMemoryIds")]
+
+    cited_node_ids: Annotated[Optional[List[str]], PropertyInfo(alias="citedNodeIds")]
+
+    feedback_impact: Annotated[Optional[str], PropertyInfo(alias="feedbackImpact")]
+
+    feedback_processed: Annotated[Optional[bool], PropertyInfo(alias="feedbackProcessed")]
+
+    feedback_score: Annotated[Optional[float], PropertyInfo(alias="feedbackScore")]
+
+    feedback_text: Annotated[Optional[str], PropertyInfo(alias="feedbackText")]
+
+    feedback_value: Annotated[Optional[str], PropertyInfo(alias="feedbackValue")]
+
+    user_message: Annotated[Optional[FeedbackDataUserMessage], PropertyInfo(alias="userMessage")]
+    """A pointer to a Parse object"""
+
+
+class FeedbackRequestParam(TypedDict, total=False):
+    feedback_data: Required[Annotated[FeedbackData, PropertyInfo(alias="feedbackData")]]
+    """The feedback data containing all feedback information"""
+
+    search_id: Required[str]
+    """The search_id from SearchResponse that this feedback relates to"""
+
+    external_user_id: Optional[str]
+    """External user ID for developer API keys acting on behalf of end users"""
+
+    user_id: Optional[str]
+    """Internal user ID (if not provided, will be resolved from authentication)"""
