c0d9e33fbb44df53968bbad6e04bfd9d0d6ec9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:34:25 +0000

    Add package siberia/0.0.1/siberia-0.0.1-py3-none-any.whl

---
+++ packages/siberia/siberia-0.0.1-py3-none-any.whl/siberia/__init__.py
@@ -0,0 +1,4 @@
+from .TSeriesClass import *
+
+__version__ = '0.0.1.'
+__author__ = 'Marzio Di Vece (marzio.divece@sns.it), Emanuele Agrimi (emanuele.agrimi@imtlucca.it), Samuele Tatullo (samuele.tatullo@imtlucca.it)'
