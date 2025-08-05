ee3bdcd79c1899565ae9cc9023858ede42dc2c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:35:00 +0000

    Add package MaixPy/4.11.9/maixpy-4.11.9-cp312-cp312-manylinux2014_x86_64.whl

---
+++ packages/MaixPy/maixpy-4.11.9-cp312-cp312-manylinux2014_x86_64.whl/maix/__signal_handle__.pyi
@@ -0,0 +1,9 @@
+from __future__ import annotations
+from maix import _maix
+import signal as signal
+import sys as sys
+__all__ = ['register_signal_handle', 'signal', 'signal_handle', 'sys']
+def register_signal_handle():
+    ...
+def signal_handle(signum, frame):
+    ...
