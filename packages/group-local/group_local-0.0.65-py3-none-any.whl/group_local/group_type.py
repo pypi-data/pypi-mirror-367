b0fbad84860342942d319ab1e5a2d419b423d942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:38 +0000

    Add package group-local/0.0.65/group_local-0.0.65-py3-none-any.whl

---
+++ packages/group-local/group_local-0.0.65-py3-none-any.whl/group_local/group_type.py
@@ -0,0 +1,23 @@
+# TODO: Shall we generate this file with sql2code?
+group_type = {
+    'Unknown': 0,
+    'Location': 1000,
+    'City': 1200,
+    'Country': 1300,
+    'State': 1400,
+    'Neighborhood': 1500,
+    'Organization': 2000,
+    'Job Title': 3001,
+    'Interest': 4000,
+    'Sport- Interest': 4100,
+    'Looking for employees': 5100,
+    'Looking for job': 6100,
+    'Gender': 7010,
+    'Marital status': 7020,
+    'Relationship Status': 7030,
+    'Event': 8001,
+    'First Name': 9001,
+    'Last Name': 9002,
+    'County': 9004,
+    'Region': 9005
+}
