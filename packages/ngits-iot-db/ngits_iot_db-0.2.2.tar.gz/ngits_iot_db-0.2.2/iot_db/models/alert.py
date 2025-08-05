4c76c167fe76d1e5521203823dc166b4949a2bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 08:04:20 +0000

    Add package ngits-iot-db/0.2.2/ngits_iot_db-0.2.2.tar.gz

---
+++ packages/ngits-iot-db/ngits_iot_db-0.2.2.tar.gz/ngits_iot_db-0.2.2/iot_db/models/alert.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, UniqueConstraint
+from sqlalchemy.dialects.postgresql import JSON
+
+from .base import AlertBase
+
+
+class AlertDefinition(AlertBase):
+    __tablename__ = "alert_definition"
+
+    properties = Column(JSON)
