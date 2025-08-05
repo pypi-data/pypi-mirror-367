ffe36d5436d0e7e47445e9557dfb9a9c2176106f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:24 +0000

    Add package swarmflow/0.4.0/swarmflow-0.4.0.tar.gz

---
+++ packages/swarmflow/swarmflow-0.4.0.tar.gz/swarmflow-0.4.0/swarmflow/__init__.py
@@ -0,0 +1,26 @@
+from .core.task import swarm_task, run
+from .core.flow import SwarmFlow
+from .hooks import (
+    write_output_to_memory,
+    read_memory_into_arg,
+    skip_if_flag_set,
+    log_input_output,
+    enforce_max_cost,
+    set_flag_on_failure,
+    append_output_to_memory_list,
+    inject_retry_count_into_arg,
+)
+
+__all__ = [
+    "swarm_task", 
+    "run", 
+    "SwarmFlow",
+    "write_output_to_memory",
+    "read_memory_into_arg", 
+    "skip_if_flag_set",
+    "log_input_output",
+    "enforce_max_cost",
+    "set_flag_on_failure",
+    "append_output_to_memory_list",
+    "inject_retry_count_into_arg",
+]
