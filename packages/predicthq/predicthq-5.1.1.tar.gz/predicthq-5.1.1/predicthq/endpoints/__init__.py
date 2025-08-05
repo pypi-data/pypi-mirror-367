7f4e2e64e02f24abb3c446fd26113943d953dfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 15:06:30 +0000

    Add package predicthq/5.1.1/predicthq-5.1.1.tar.gz

---
+++ packages/predicthq/predicthq-5.1.1.tar.gz/predicthq-5.1.1/predicthq/endpoints/__init__.py
@@ -0,0 +1,20 @@
+from .oauth2 import OAuth2Endpoint
+from .v1.accounts import AccountsEndpoint
+from .v1.broadcasts import BroadcastsEndpoint
+from .v1.events import EventsEndpoint
+from .v1.features import FeaturesEndpoint
+from .v1.places import PlacesEndpoint
+from .v1.radius import SuggestedRadiusEndpoint
+from .v1.beam import BeamEndpoint
+
+
+__all__ = [
+    "OAuth2Endpoint",
+    "AccountsEndpoint",
+    "BroadcastsEndpoint",
+    "EventsEndpoint",
+    "FeaturesEndpoint",
+    "PlacesEndpoint",
+    "SuggestedRadiusEndpoint",
+    "BeamEndpoint",
+]
