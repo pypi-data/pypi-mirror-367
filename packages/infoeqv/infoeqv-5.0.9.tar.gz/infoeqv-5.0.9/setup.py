cea5fc250e2c546e6d5d35d0505a441f506e58e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 05:10:21 +0000

    Add package infoeqv/5.0.9/infoeqv-5.0.9.tar.gz

---
+++ packages/infoeqv/infoeqv-5.0.9.tar.gz/infoeqv-5.0.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# ✅ Read long description using UTF-8
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="infoeqv",
+    version="5.0.9",
+    author="Rohit Kumar Behera",
+    author_email="rohitmbl24@gmail.com",
+    description="Information Equivalent Designs (Two-Point Reduction)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Important for PyPI formatting
+    url="https://github.com/muinrohit/infoeqv",   # Optional: your GitHub repo
+    packages=find_packages(),
+    install_requires=["numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    license="MIT",
+)
