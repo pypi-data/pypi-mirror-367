76c03bb83d045609fc678d9c4ad478e9b6825619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 07:07:57 +0000

    Add package structifyai/1.135.2/structifyai-1.135.2-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.135.2-py3-none-any.whl/structify/types/list_chat_sessions_response.py
@@ -0,0 +1,26 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+from datetime import datetime
+
+from .._models import BaseModel
+
+__all__ = ["ListChatSessionsResponse", "Session"]
+
+
+class Session(BaseModel):
+    id: str
+
+    created_at: datetime
+
+    project_id: str
+
+    title: str
+
+    updated_at: datetime
+
+    user_id: str
+
+
+class ListChatSessionsResponse(BaseModel):
+    sessions: List[Session]
