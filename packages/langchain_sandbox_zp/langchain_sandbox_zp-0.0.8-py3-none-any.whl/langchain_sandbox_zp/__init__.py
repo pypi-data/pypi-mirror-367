7b6c6135765127bb20fa227ef79168cbc8a446bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 16:05:42 +0000

    Add package langchain_sandbox_zp/0.0.8/langchain_sandbox_zp-0.0.8-py3-none-any.whl

---
+++ packages/langchain_sandbox_zp/langchain_sandbox_zp-0.0.8-py3-none-any.whl/langchain_sandbox_zp/__init__.py
@@ -0,0 +1,13 @@
+"""LangChain code sandbox."""
+
+from langchain_sandbox_zp.pyodide import (
+    PyodideSandbox,
+    PyodideSandboxTool,
+    SyncPyodideSandbox,
+)
+
+__all__ = [
+    "PyodideSandbox",
+    "PyodideSandboxTool",
+    "SyncPyodideSandbox",
+]
