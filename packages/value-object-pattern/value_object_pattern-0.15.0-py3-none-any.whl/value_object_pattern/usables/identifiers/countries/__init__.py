a3b20ffbd238f4a3304533a58284970c9a4818af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 06:05:23 +0000

    Add package value-object-pattern/0.15.0/value_object_pattern-0.15.0-py3-none-any.whl

---
+++ packages/value-object-pattern/value_object_pattern-0.15.0-py3-none-any.whl/value_object_pattern/usables/identifiers/countries/__init__.py
@@ -0,0 +1,7 @@
+from .iso3166_alpha2_code_value_object import Iso3166Alpha2CodeValueObject
+from .vin_value_object import VinValueObject
+
+__all__ = (
+    'Iso3166Alpha2CodeValueObject',
+    'VinValueObject',
+)
---
+++ packages/value-object-pattern/value_object_pattern-0.15.0-py3-none-any.whl/value_object_pattern/usables/identifiers/countries/spain/__init__.py
@@ -0,0 +1,11 @@
+from .dni_value_object import DniValueObject
+from .nie_value_object import NieValueObject
+from .nif_value_object import NifValueObject
+from .passport_value_object import PassportValueObject
+
+__all__ = (
+    'DniValueObject',
+    'NieValueObject',
+    'NifValueObject',
+    'PassportValueObject',
+)
