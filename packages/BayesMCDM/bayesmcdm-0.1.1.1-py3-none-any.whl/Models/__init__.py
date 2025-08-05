ff10819803eb57e4c00a69645866e17f7a024a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:34:31 +0000

    Add package BayesMCDM/0.1.1.1/bayesmcdm-0.1.1.1-py3-none-any.whl

---
+++ packages/BayesMCDM/bayesmcdm-0.1.1.1-py3-none-any.whl/Models/__init__.py
@@ -0,0 +1,10 @@
+from .BWM.StandardBWM import StandardBWM
+
+# /Users/majidmohammadi/surfdrive/Research/MCDM/BayesianMCDM/Models/__init__.py
+
+# Redirect BayesMCDM.BWM.StandardBWM to Models.BWM.StandardBWM.StandardBWM
+
+
+# Optionally, provide a namespace for BWM if needed
+class BWM:
+    StandardBWM = StandardBWM
