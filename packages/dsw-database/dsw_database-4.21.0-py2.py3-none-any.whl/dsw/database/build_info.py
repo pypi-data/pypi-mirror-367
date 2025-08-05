d283f73d4fe7de8cc97d3a4f3f19aaaac9fa3401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 17:05:25 +0000

    Add package dsw-database/4.21.0/dsw_database-4.21.0-py2.py3-none-any.whl

---
+++ packages/dsw-database/dsw_database-4.21.0-py2.py3-none-any.whl/dsw/database/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v4.21.0~12713f1',
+    built_at='2025-08-05 09:21:07Z',
+    sha='12713f1988c2a33d5c40dd2cb899cdac7be35668',
+    branch='HEAD',
+    tag='v4.21.0',
+)
