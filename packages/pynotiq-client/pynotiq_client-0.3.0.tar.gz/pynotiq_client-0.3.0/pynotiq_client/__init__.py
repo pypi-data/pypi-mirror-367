cb438db23e7014715b3e041efe8d714a2bf28276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 08:04:11 +0000

    Add package pynotiq-client/0.3.0/pynotiq_client-0.3.0.tar.gz

---
+++ packages/pynotiq-client/pynotiq_client-0.3.0.tar.gz/pynotiq_client-0.3.0/pynotiq_client/__init__.py
@@ -0,0 +1,4 @@
+from .queue_manager import PyNotiQ
+from .config import QUEUE_FILE_PATH
+
+__all__ = ["PyNotiQ", "QUEUE_FILE_PATH"]
---
+++ packages/pynotiq-client/pynotiq_client-0.3.0.tar.gz/pynotiq_client-0.3.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pynotiq_client",
+    version="0.3.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+        "urllib3>=1.26.0"
+    ],
+    description="Python client library for adding messages to PyNotiQ queue with local and remote support",
+    author="Your Name",
+    url="https://github.com/downlevel/pynotiq_client",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
