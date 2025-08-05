9864bd9ca23a782db3c2e01a6f8571a97160ec97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 03:34:48 +0000

    Add package backend-project-fast-api/0.1.1/backend_project_fast_api-0.1.1.tar.gz

---
+++ packages/backend-project-fast-api/backend_project_fast_api-0.1.1.tar.gz/backend_project_fast_api-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="backend_project_fast_api",
+    version="0.1.1",
+    author="Jan Proczek",
+    author_email="jan@example.com",
+    description="Prosta aplikacja FastAPI",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "fastapi",
+        "uvicorn"
+    ],
+    entry_points={
+        "console_scripts": [
+            "runapp=backend_project_fast_api.main:app"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: FastAPI",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
