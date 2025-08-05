da6fa8e8070914b024f2c44c2328cf82fbde08a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 16:06:10 +0000

    Add package netbox-cvexplorer/1.0.14/netbox_cvexplorer-1.0.14.tar.gz

---
+++ packages/netbox-cvexplorer/netbox_cvexplorer-1.0.14.tar.gz/netbox_cvexplorer-1.0.14/netbox_cvexplorer/plugin.py
@@ -0,0 +1,28 @@
+from netbox.plugins import PluginConfig
+from importlib.metadata import metadata
+from pathlib import Path
+
+plugin = metadata('netbox-cvexplorer')
+
+class CVExplorerConfig(PluginConfig):
+    name = plugin.get('Name').replace('-', '_')
+    verbose_name = plugin.get('Name').replace('-', ' ').title()
+    version = plugin.get('Version')
+    description = plugin.get('Summary')
+    author = plugin.get('Author')
+    author_email = plugin.get('Author-email')
+    base_url = "netbox_cvexplorer"
+    min_version = '4.0'
+    required_settings = []
+    default_settings = {
+        'top_level_menu': True,
+        'static_image_directory': "netbox_cvexplorer/img"
+    }
+    caching_config = {
+        '*': None
+    }
+    # Richtige Referenz auf menu_items (als Modulobjekt!)
+    from netbox_cvexplorer.navigation import menu_items
+    menu_items = menu_items
+
+config = CVExplorerConfig
