3b16ffa10a7c8b58c11c35376185d8665ac44ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-05 07:07:48 +0000

    Add package AiologySimilarity/0.0.2/aiologysimilarity-0.0.2-py3-none-any.whl

---
+++ packages/AiologySimilarity/aiologysimilarity-0.0.2-py3-none-any.whl/AiologySimilarity/__init__.py
@@ -0,0 +1,24 @@
+"""
+## 🔎 Aiology-Similarity
+
+Aiology-Similarity is a package which pressents you Similarity module to determine how similar two things are ,such as text ,and images
+
+## 💡 Tip
+
+This module doesn't need any AI token to work ,and it is an offline module which works based on its algorithms ,and it includes two algorithms :
+
+### Image detection algorithm
+
+I called this algorithm IMA-SAME-ALGORITHM 😁 which I wrote based on my math knowledge ,This algorithm designed to be as fast as possible ,and detect all similarity also in images pixels !!
+
+### Text detection algorithm
+
+I called this algorithm WORD-SAME-ALGORITHM 😁 which I wrote based on my math knowledge ,This algorithm designed to be as fast as possible ,and detect all similarity also in a word letters !!
+
+## Author
+
+✍️ Seyed Moied Seyedi
+
+😁 I will be glad to see your tricks 
+"""
+from .main import Similarity
