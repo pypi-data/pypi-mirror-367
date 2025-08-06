96db47a431ce1ee4d93a526c8e85001849932ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:22 +0000

    Add package gh-templates-darwin-x64/0.1.39/gh_templates_darwin_x64-0.1.39.tar.gz

---
+++ packages/gh-templates-darwin-x64/gh_templates_darwin_x64-0.1.39.tar.gz/gh_templates_darwin_x64-0.1.39/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gh-templates-darwin-x64',
+    version='0.1.39',
+    description='GitHub Templates CLI tool',
+    packages=find_packages(),
+    package_data={'gh_templates_bin': ['*']},
+    entry_points={'console_scripts': ['gh-templates=gh_templates_bin:main']},
+    license='Apache-2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    python_requires='>=3.7',
+)
