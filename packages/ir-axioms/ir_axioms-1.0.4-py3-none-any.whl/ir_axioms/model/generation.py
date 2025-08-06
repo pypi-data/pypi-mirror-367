f3a8e6e133ac16b912a1f36c1b119bdbbcf67104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:09:04 +0000

    Add package ir-axioms/1.0.4/ir_axioms-1.0.4-py3-none-any.whl

---
+++ packages/ir-axioms/ir_axioms-1.0.4-py3-none-any.whl/ir_axioms/model/generation.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Optional, Sequence
+
+
+@dataclass(frozen=True, kw_only=True)
+class GenerationInput:
+    id: Optional[str] = None
+    text: str
+    context: Optional[Sequence[str]] = None
+    reference_output: Optional["GenerationOutput"] = None
+
+
+@dataclass(frozen=True, kw_only=True)
+class GenerationOutput:
+    id: Optional[str] = None
+    text: str
+    context: Optional[Sequence[str]] = None
+    """Override context from input if necessary."""
