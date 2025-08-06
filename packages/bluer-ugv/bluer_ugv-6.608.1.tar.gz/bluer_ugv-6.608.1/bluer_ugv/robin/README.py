febb64467710fa32a25724123b347153561a0f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:20:46 +0000

    Add package bluer-ugv/6.608.1/bluer_ugv-6.608.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.608.1.tar.gz/bluer_ugv-6.608.1/bluer_ugv/robin/README.py
@@ -0,0 +1,16 @@
+from bluer_objects.README.items import ImageItems
+
+assets2 = "https://github.com/kamangir/assets2/blob/main/bluer-robin"
+
+items = ImageItems(
+    {
+        f"{assets2}/20250712_114819.jpg?raw=true": "",
+        f"{assets2}/20250713_172325.jpg?raw=true": "",
+        f"{assets2}/20250713_172413.jpg?raw=true": "",
+        f"{assets2}/20250713_172442_1.gif?raw=true": "",
+        f"{assets2}/20250723_095022.jpg?raw=true": "",
+        f"{assets2}/20250723_095155~2_1.gif?raw=true": "",
+        f"{assets2}/20250728_112123.jpg?raw=true": "",
+        f"{assets2}/20250726_134024.jpg?raw=true": "",
+    }
+)
