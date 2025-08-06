4c99d1d8617feabf5ee5e08b409da84a096be65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 03:34:08 +0000

    Add package telegram-gifts-fetcher/1.0.0/telegram_gifts_fetcher-1.0.0.tar.gz

---
+++ packages/telegram-gifts-fetcher/telegram_gifts_fetcher-1.0.0.tar.gz/telegram_gifts_fetcher-1.0.0/telegram_gifts_fetcher/__init__.py
@@ -0,0 +1,10 @@
+"""Telegram Gifts Fetcher - The first library to fetch Telegram Star Gifts"""
+
+from .fetcher import get_user_gifts_extended
+
+__version__ = "1.0.0"
+__author__ = "Th3ryks"
+__email__ = ""
+__description__ = "The first and only library to fetch Telegram Star Gifts from user profiles"
+
+__all__ = ["get_user_gifts_extended"]
