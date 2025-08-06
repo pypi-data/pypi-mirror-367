b36b1615867a4aff79749953dfaf2aa0368407e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 15:06:15 +0000

    Add package maleo-metadata/0.1.79/maleo_metadata-0.1.79.tar.gz

---
+++ packages/maleo-metadata/maleo_metadata-0.1.79.tar.gz/maleo_metadata-0.1.79/maleo_metadata/client/controllers/http/user_type.py
@@ -0,0 +1,118 @@
+from httpx import Response
+from typing import Dict, Optional
+from maleo_soma.managers.client.base import BearerAuth
+from maleo_soma.managers.client.maleo import MaleoClientHTTPController
+from maleo_soma.schemas.authentication import Authentication
+from maleo_soma.schemas.authorization import Authorization
+from maleo_soma.schemas.parameter.general import ReadSingleQueryParameterSchema
+from maleo_soma.utils.merger import merge_dicts
+from maleo_soma.utils.token import reencode
+from maleo_metadata.schemas.parameter.general.user_type import (
+    ReadSingleParameter,
+)
+from maleo_metadata.schemas.parameter.client.user_type import (
+    ReadMultipleParameter,
+    ReadMultipleQueryParameter,
+)
+
+
+class MaleoMetadataUserTypeHTTPController(MaleoClientHTTPController):
+    async def get_user_types(
+        self,
+        authentication: Authentication,
+        parameters: ReadMultipleParameter,
+        authorization: Optional[Authorization] = None,
+        headers: Optional[Dict[str, str]] = None,
+    ) -> Response:
+        """Fetch user types from MaleoMetadata"""
+        async with self._manager.get_client() as client:
+            # Define URL
+            url = f"{self._manager.url}/v1/user-types/"
+
+            # Parse parameters to query params
+            params = ReadMultipleQueryParameter.model_validate(
+                parameters.model_dump()
+            ).model_dump(exclude={"sort_columns", "date_filters"}, exclude_none=True)
+
+            # Create headers
+            base_headers = {"Content-Type": "application/json"}
+            if headers is not None:
+                headers = merge_dicts(base_headers, headers)
+            else:
+                headers = base_headers
+
+            # Create auth
+            token = None
+            if authentication.credentials.token is not None:
+                try:
+                    token = reencode(
+                        payload=authentication.credentials.token.payload,
+                        key=self._private_key,
+                    )
+                except Exception:
+                    pass
+
+            if (
+                token is None
+                and authorization is not None
+                and authorization.scheme == "Bearer"
+            ):
+                token = authorization.credentials
+
+            if token is None:
+                token = self._credential_manager.token
+
+            auth = BearerAuth(token) if token else None
+
+            # Send request and wait for response
+            return await client.get(url=url, params=params, headers=headers, auth=auth)
+
+    async def get_user_type(
+        self,
+        authentication: Authentication,
+        parameters: ReadSingleParameter,
+        authorization: Optional[Authorization] = None,
+        headers: Optional[Dict[str, str]] = None,
+    ) -> Response:
+        """Fetch user type from MaleoMetadata"""
+        async with self._manager.get_client() as client:
+            # Define URL
+            url = f"{self._manager.url}/v1/user-types/{parameters.identifier}/{parameters.value}"
+
+            # Parse parameters to query params
+            params = ReadSingleQueryParameterSchema.model_validate(
+                parameters.model_dump()
+            ).model_dump(exclude_none=True)
+
+            # Create headers
+            base_headers = {"Content-Type": "application/json"}
+            if headers is not None:
+                headers = merge_dicts(base_headers, headers)
+            else:
+                headers = base_headers
+
+            # Create auth
+            token = None
+            if authentication.credentials.token is not None:
+                try:
+                    token = reencode(
+                        payload=authentication.credentials.token.payload,
+                        key=self._private_key,
+                    )
+                except Exception:
+                    pass
+
+            if (
+                token is None
+                and authorization is not None
+                and authorization.scheme == "Bearer"
+            ):
+                token = authorization.credentials
+
+            if token is None:
+                token = self._credential_manager.token
+
+            auth = BearerAuth(token) if token else None
+
+            # Send request and wait for response
+            return await client.get(url=url, params=params, headers=headers, auth=auth)
