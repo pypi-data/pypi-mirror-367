bd812801aa86ef917248bad6a9fc8507c31779d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:06:32 +0000

    Add package scrapegraph-py/1.18.1/scrapegraph_py-1.18.1-py3-none-any.whl

---
+++ packages/scrapegraph-py/scrapegraph_py-1.18.1-py3-none-any.whl/scrapegraph_py/models/__init__.py
@@ -0,0 +1,17 @@
+from .crawl import CrawlRequest, GetCrawlRequest
+from .feedback import FeedbackRequest
+from .markdownify import GetMarkdownifyRequest, MarkdownifyRequest
+from .searchscraper import GetSearchScraperRequest, SearchScraperRequest
+from .smartscraper import GetSmartScraperRequest, SmartScraperRequest
+
+__all__ = [
+    "CrawlRequest",
+    "GetCrawlRequest",
+    "FeedbackRequest",
+    "GetMarkdownifyRequest",
+    "MarkdownifyRequest",
+    "GetSearchScraperRequest",
+    "SearchScraperRequest",
+    "GetSmartScraperRequest",
+    "SmartScraperRequest",
+]
