dca9c4f890e5a8284a6f064709904a5e4d40ad5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 07:08:42 +0000

    Add package tap-canvas/0.0.12/tap_canvas-0.0.12.tar.gz

---
+++ packages/tap-canvas/tap_canvas-0.0.12.tar.gz/tap_canvas-0.0.12/tap_canvas/client.py
@@ -0,0 +1,75 @@
+"""REST client handling, including canvasStream base class."""
+
+import requests
+from urllib import parse
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+
+from memoization import cached
+
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+from singer_sdk.authenticators import BearerTokenAuthenticator
+
+
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+
+
+class CanvasStream(RESTStream):
+    """canvas stream class."""
+
+    @property
+    def url_base(self) -> str:
+        """Return the base URL for the Canvas API."""
+        return self.config["base_url"]
+
+    records_jsonpath = "$[*]"  # Or override `parse_response`.
+    next_page_token_jsonpath = "$.next_page"  # Or override `get_next_page_token`.
+
+    @property
+    def authenticator(self) -> BearerTokenAuthenticator:
+        """Return a new authenticator object."""
+        return BearerTokenAuthenticator.create_for_stream(
+            self,
+            token=self.config.get("api_key")
+        )
+
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed."""
+        headers = {}
+        if "user_agent" in self.config:
+            headers["User-Agent"] = self.config.get("user_agent")
+        return headers
+
+    def get_next_page_token(
+        self, response: requests.Response, previous_token: Optional[Any]
+    ) -> Optional[Any]:
+        """Return a token for identifying next page or None if no more pages."""
+        next_page_dict = response.links.get("next", None)
+        if next_page_dict:
+            next_page = next_page_dict["url"]
+            query = dict(parse.parse_qsl(parse.urlsplit(next_page).query))
+            next_page_token = query["page"]
+        else:
+            next_page_token = None
+
+        return next_page_token
+
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+        params["per_page"] = 100
+        return params
+
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result rows."""
+        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
