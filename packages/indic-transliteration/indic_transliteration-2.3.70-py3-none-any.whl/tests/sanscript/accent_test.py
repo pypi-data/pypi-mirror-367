fb684f4d27ab0e2f60e43722147014744cb6fe59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 03:34:25 +0000

    Add package indic-transliteration/2.3.70/indic_transliteration-2.3.70-py3-none-any.whl

---
+++ packages/indic-transliteration/indic_transliteration-2.3.70-py3-none-any.whl/tests/sanscript/accent_test.py
@@ -0,0 +1,16 @@
+from indic_transliteration.sanscript.schemes.brahmic import accent
+from indic_transliteration import sanscript
+
+
+def test_move_accent_to_previous_syllable():
+  devanagari = sanscript.SCHEMES[sanscript.DEVANAGARI]
+  assert accent.add_accent_to_previous_syllable(scheme=devanagari, text="सैॗषा᳘ निदा᳘नेना यदि᳘डा॥", old_accent="ॗ", new_accent="ॗ") == "ॗसैषा᳘ निदा᳘नेना यदि᳘डा॥"
+  assert accent.add_accent_to_previous_syllable(scheme=devanagari, text="त॑स्माद्वा॑ अप॑ उ॑पस्पृशति॥ सो᳕ऽग्नि॑मेवा᳕भी॑क्षमाणः।", old_accent="᳕", new_accent="ॗ") == "तॖस्माद्वाॖ अप॑ उॖपस्पृशतिॗ॥ सोऽग्निॖमेॗवाभीॖक्षमाणः।".replace("ॖ", "॑")
+
+
+def test_set_diirgha_svaritas():
+  assert accent.set_diirgha_svaritas(scheme=sanscript.SCHEMES[sanscript.DEVANAGARI], text="त॑स्माद्वा॑ अप॑ उ॑पस्पृशति॥ सो᳕ऽग्नि॑मेवा᳕भी॑क्षमाणः।") == "त॑स्माद्वा᳚ अप॑ उ॑पस्पृशति॥ सो᳕ऽग्नि॑मेवा᳕भी᳚क्षमाणः।"
+  
+  
+def test_to_US_accents():
+  assert accent.to_US_accents(scheme=sanscript.SCHEMES[sanscript.DEVANAGARI], text="""ध्रु॒वो॑ऽसि ।  \nध्रु॒वो॒॑ऽहँ स॑जा॒तेषु॑+++(=haya)+++ भूयास॒न्  \nधीर॒श् चेत्ता॑ वसु॒वित्।""") == "ध्रुवो᳕ऽसि ।  \nध्रुवो᳕ऽहँ꣡ सजाते꣡षु+++(=haya)+++ भूयासन्  \nधी꣡रश् चे꣡त्ता वसुवि꣡त्।"
