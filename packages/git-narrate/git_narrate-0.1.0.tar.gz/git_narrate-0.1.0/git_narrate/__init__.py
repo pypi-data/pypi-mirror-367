6d193cd4a3929b41b68527d80fc822959298c369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:06:24 +0000

    Add package git-narrate/0.1.0/git_narrate-0.1.0.tar.gz

---
+++ packages/git-narrate/git_narrate-0.1.0.tar.gz/git_narrate-0.1.0/git_narrate/__init__.py
@@ -0,0 +1,9 @@
+"""
+Git-Narrate: The Repository Storyteller
+
+A tool that analyzes a git repository and generates a human-readable story of its development.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Git-Narrate Team"
+__email__ = "contact@git-narrate.dev"
