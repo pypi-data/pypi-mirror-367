6871b809f24a5a324bf3d2b730df5826aed228ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 05:11:37 +0000

    Add package compassheadinglib/2024.8.1/compassheadinglib-2024.8.1-py2.py3-none-any.whl

---
+++ packages/compassheadinglib/compassheadinglib-2024.8.1-py2.py3-none-any.whl/compassheadinglib/__init__.py
@@ -0,0 +1,13 @@
+
+# geodeltahash/__init__.py
+
+# Package-level variables
+__version__ = "2024.8.5a"
+__author__ = "Peter E Lenz"
+__email__ = "pelenz@pelenz.com"
+
+# Importing submodules to make them accessible at the package level
+
+#english is the default language for this package
+from compassheadinglib.en import *
+
---
+++ packages/compassheadinglib/compassheadinglib-2024.8.1-py2.py3-none-any.whl/compassheadinglib/es.py
@@ -0,0 +1,13 @@
+from .common import _Headings
+
+import importlib.resources
+from json import load
+
+with importlib.resources.open_text('compassheadinglib', 'compass_data.json') as json_file:
+    raw_compass=load(json_file)
+
+_lang='ES'
+
+_compass=[i['Lang'][_lang] | i for i in raw_compass]
+
+Compass = _Headings(_compass)
