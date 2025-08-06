bb957026ae84f5f2243be0219ad7e62ebe65ed9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 15:06:53 +0000

    Add package asynckivy/0.9.0/asynckivy-0.9.0.tar.gz

---
+++ packages/asynckivy/asynckivy-0.9.0.tar.gz/asynckivy-0.9.0/src/asynckivy/_etc.py
@@ -0,0 +1,238 @@
+__all__ = ("transform", "sync_attr", "sync_attrs", )
+import typing as T
+from collections.abc import Iterator
+from contextlib import contextmanager
+from functools import partial
+
+from kivy.event import EventDispatcher
+from kivy.graphics import PushMatrix, PopMatrix, InstructionGroup
+
+
+@contextmanager
+def transform(widget, *, use_outer_canvas=False) -> Iterator[InstructionGroup]:
+    '''
+    Returns a context manager that sandwiches the ``widget``'s existing canvas instructions between
+    a :class:`kivy.graphics.PushMatrix` and a :class:`kivy.graphics.PopMatrix`, and inserts an
+    :class:`kivy.graphics.InstructionGroup` right next to the ``PushMatrix``. Those three instructions will be removed
+    when the context manager exits.
+
+    This may be useful when you want to animate a widget.
+
+    **Usage**
+
+    .. code-block::
+
+        from kivy.graphics import Rotate
+
+        async def rotate_widget(widget, *, angle=360.):
+            with transform(widget) as ig:  # <- InstructionGroup
+                ig.add(rotate := Rotate(origin=widget.center))
+                await anim_attrs(rotate, angle=angle)
+
+    If the position or size of the ``widget`` changes during the animation, you might need :class:`sync_attr`.
+
+    **The use_outer_canvas parameter**
+
+    While the context manager is active, the content of the widget's canvas would be:
+
+    .. code-block:: yaml
+
+        # ... represents existing instructions
+
+        Widget:
+            canvas.before:
+                ...
+            canvas:
+                PushMatrix
+                InstructionGroup
+                ...
+                PopMatrix
+            canvas.after:
+                ...
+
+    but if ``use_outer_canvas`` is True, it would be:
+
+    .. code-block:: yaml
+
+        Widget:
+            canvas.before:
+                PushMatrix
+                InstructionGroup
+                ...
+            canvas:
+                ...
+            canvas.after:
+                ...
+                PopMatrix
+    '''
+
+    c = widget.canvas
+    if use_outer_canvas:
+        before = c.before
+        after = c.after
+        push_mat_idx = 0
+        ig_idx = 1
+    else:
+        if c.has_before:
+            push_mat_idx = 1
+            ig_idx = 2
+        else:
+            push_mat_idx = 0
+            ig_idx = 1
+        before = after = c
+
+    push_mat = PushMatrix()
+    ig = InstructionGroup()
+    pop_mat = PopMatrix()
+
+    before.insert(push_mat_idx, push_mat)
+    before.insert(ig_idx, ig)
+    after.add(pop_mat)
+    try:
+        yield ig
+    finally:
+        after.remove(pop_mat)
+        before.remove(ig)
+        before.remove(push_mat)
+
+
+class sync_attr:
+    '''
+    Creates one-directional binding between attributes.
+
+    .. code-block::
+
+        import types
+
+        widget = Widget(x=100)
+        obj = types.SimpleNamespace()
+
+        sync_attr(from_=(widget, 'x'), to_=(obj, 'xx'))
+        assert obj.xx == 100  # synchronized
+        widget.x = 10
+        assert obj.xx == 10  # synchronized
+        obj.xx = 20
+        assert widget.x == 10  # but not the other way around
+
+    To make its effect temporary, use it with a with-statement:
+
+    .. code-block::
+
+        # The effect lasts only within the with-block.
+        with sync_attr(...):
+            ...
+
+    This can be particularly useful when combined with :func:`transform`.
+
+    .. code-block::
+
+        from kivy.graphics import Rotate
+
+        async def rotate_widget(widget, *, angle=360.):
+            rotate = Rotate()
+            with (
+                transform(widget) as ig,
+                sync_attr(from_=(widget, 'center'), to_=(rotate, 'origin')),
+            ):
+                ig.add(rotate)
+                await anim_attrs(rotate, angle=angle)
+
+    .. versionadded:: 0.6.1
+
+    .. versionchanged:: 0.8.0
+        The context manager now applies its effect upon creation, rather than when its ``__enter__()`` method is
+        called, and ``__enter__()`` no longer performs any action.
+
+        Additionally, the context manager now assigns the ``from_`` value to the ``to_`` upon creation:
+
+        .. code-block::
+
+            with sync_attr((widget, 'x'), (obj, 'xx')):
+                assert widget.x == obj.xx
+    '''
+    __slots__ = ("__exit__", )
+
+    def __init__(self, from_: tuple[EventDispatcher, str], to_: tuple[T.Any, str]):
+        setattr(*to_, getattr(*from_))
+        bind_uid = from_[0].fbind(from_[1], partial(self._sync, setattr, *to_))
+        self.__exit__ = partial(self._unbind, *from_, bind_uid)
+
+    @staticmethod
+    def _sync(setattr, obj, attr_name, event_dispatcher, new_value):
+        setattr(obj, attr_name, new_value)
+
+    @staticmethod
+    def _unbind(event_dispatcher, event_name, bind_uid, *__):
+        event_dispatcher.unbind_uid(event_name, bind_uid)
+
+    def __enter__(self):
+        pass
+
+
+class sync_attrs:
+    '''
+    When multiple :class:`sync_attr` calls take the same ``from_`` argument, they can be merged into a single
+    :class:`sync_attrs` call. For instance, the following code:
+
+    .. code-block::
+
+        with sync_attr((widget, 'x'), (obj1, 'x')), sync_attr((widget, 'x'), (obj2, 'xx')):
+            ...
+
+    can be replaced with the following one:
+
+    .. code-block::
+
+        with sync_attrs((widget, 'x'), (obj1, 'x'), (obj2, 'xx')):
+            ...
+
+    This can be particularly useful when combined with :func:`transform`.
+
+    .. code-block::
+
+        from kivy.graphics import Rotate, Scale
+
+        async def scale_and_rotate_widget(widget, *, scale=2.0, angle=360.):
+            rotate = Rotate()
+            scale = Scale()
+            with (
+                transform(widget) as ig,
+                sync_attrs((widget, 'center'), (rotate, 'origin'), (scale, 'origin')),
+            ):
+                ig.add(rotate)
+                ig.add(scale)
+                await wait_all(
+                    anim_attrs(rotate, angle=angle),
+                    anim_attrs(scale, x=scale, y=scale),
+                )
+
+    .. versionadded:: 0.6.1
+
+    .. versionchanged:: 0.8.0
+        The context manager now applies its effect upon creation, rather than when its ``__enter__()`` method is
+        called, and ``__enter__()`` no longer performs any action.
+
+        Additionally, the context manager now assigns the ``from_`` value to the ``to_`` upon creation:
+
+        .. code-block::
+
+            with sync_attrs((widget, 'x'), (obj, 'xx')):
+                assert widget.x is obj.xx
+    '''
+    __slots__ = ("__exit__", )
+
+    def __init__(self, from_: tuple[EventDispatcher, str], *to_):
+        sync = partial(self._sync, setattr, to_)
+        sync(None, getattr(*from_))
+        bind_uid = from_[0].fbind(from_[1], sync)
+        self.__exit__ = partial(self._unbind, *from_, bind_uid)
+
+    @staticmethod
+    def _sync(setattr, to_, event_dispatcher, new_value):
+        for obj, attr_name in to_:
+            setattr(obj, attr_name, new_value)
+
+    _unbind = staticmethod(sync_attr._unbind)
+
+    def __enter__(self):
+        pass
---
+++ packages/asynckivy/asynckivy-0.9.0.tar.gz/asynckivy-0.9.0/src/asynckivy/_event.py
@@ -0,0 +1,210 @@
+__all__ = ("event", "event_freq", "suppress_event", "rest_of_touch_events", )
+
+from collections.abc import AsyncIterator
+import types
+from functools import partial
+from contextlib import ExitStack
+
+from asyncgui import _current_task, _sleep_forever, move_on_when, ExclusiveEvent, _wait_args
+
+
+@types.coroutine
+def event(event_dispatcher, event_name, *, filter=None, stop_dispatching=False):
+    '''
+    Returns an :class:`~collections.abc.Awaitable` that can be used to wait for:
+
+    * a Kivy event to occur.
+    * a Kivy property's value to change.
+
+    .. code-block::
+
+        # Wait for a button to be pressed.
+        await event(button, 'on_press')
+
+        # Wait for an 'on_touch_down' event to occur.
+        __, touch = await event(widget, 'on_touch_down')
+
+        # Wait for 'widget.x' to change.
+        __, x = await ak.event(widget, 'x')
+
+    The ``filter`` parameter:
+
+    .. code-block::
+
+        # Wait for an 'on_touch_down' event to occur inside a widget.
+        __, touch = await event(widget, 'on_touch_down', filter=lambda w, t: w.collide_point(*t.opos))
+
+        # Wait for 'widget.x' to become greater than 100.
+        if widget.x <= 100:
+            await event(widget, 'x', filter=lambda __, x: x > 100)
+
+    The ``stop_dispatching`` parameter:
+
+      It only works for events not for properties.
+      See :ref:`kivys-event-system` for details.
+    '''
+    task = (yield _current_task)[0][0]
+    bind_id = event_dispatcher.fbind(event_name, partial(_event_callback, filter, task._step, stop_dispatching))
+    assert bind_id  # check if binding succeeded
+    try:
+        return (yield _sleep_forever)[0]
+    finally:
+        event_dispatcher.unbind_uid(event_name, bind_id)
+
+
+def _event_callback(filter, task_step, stop_dispatching, *args, **kwargs):
+    if (filter is None) or filter(*args, **kwargs):
+        task_step(*args)
+        return stop_dispatching
+
+
+class event_freq:
+    '''
+    When handling a frequently occurring event, such as ``on_touch_move``, the following kind of code *might* cause
+    performance issues:
+
+    .. code-block::
+
+        __, touch = await event(widget, 'on_touch_down')
+
+        # This loop registers and unregisters an event handler on every iteration.
+        while True:
+            await event(widget, 'on_touch_move', filter=lambda w, t: t is touch)
+            ...
+
+    If that happens, try the following code instead. It might resolve the issue:
+
+    .. code-block::
+
+        __, touch = await event(widget, 'on_touch_down')
+        async with event_freq(widget, 'on_touch_move', filter=lambda w, t: t is touch) as on_touch_move:
+            while True:
+                await on_touch_move()
+                ...
+
+    .. versionadded:: 0.7.1
+
+    .. versionchanged:: 0.9.0
+        The ``free_to_await`` parameter was added.
+
+    The ``free_to_await`` parameter:
+
+    If set to False (the default), the only permitted async operation within the with-block is ``await xxx()``,
+    where ``xxx`` is the identifier specified in the as-clause. To lift this restriction, set ``free_to_await`` to
+    True — at the cost of slightly reduced performance.
+    '''
+    __slots__ = ('_disp', '_name', '_filter', '_stop', '_bind_id', '_free_to_await')
+
+    def __init__(self, event_dispatcher, event_name, *, filter=None, stop_dispatching=False, free_to_await=False):
+        self._disp = event_dispatcher
+        self._name = event_name
+        self._filter = filter
+        self._stop = stop_dispatching
+        self._free_to_await = free_to_await
+
+    @types.coroutine
+    def __aenter__(self):
+        if self._free_to_await:
+            e = ExclusiveEvent()
+            self._bind_id = self._disp.fbind(self._name, partial(_event_callback, self._filter, e.fire, self._stop))
+            return e.wait_args
+        else:
+            task = (yield _current_task)[0][0]
+            self._bind_id = self._disp.fbind(
+                self._name, partial(_event_callback, self._filter, task._step, self._stop))
+            return _wait_args
+
+    async def __aexit__(self, *args):
+        self._disp.unbind_uid(self._name, self._bind_id)
+
+
+class suppress_event:
+    '''
+    Returns a context manager that prevents the callback functions (including the default handler) bound to an event
+    from being called.
+
+    .. code-block::
+        :emphasize-lines: 4
+
+        from kivy.uix.button import Button
+
+        btn = Button()
+        btn.bind(on_press=lambda __: print("pressed"))
+        with suppress_event(btn, 'on_press'):
+            btn.dispatch('on_press')
+
+    The above code prints nothing because the callback function won't be called.
+
+    Strictly speaking, this context manager doesn't prevent all callback functions from being called.
+    It only prevents the callback functions that were bound to an event before the context manager enters.
+    Thus, the following code prints ``pressed``.
+
+    .. code-block::
+        :emphasize-lines: 5
+
+        from kivy.uix.button import Button
+
+        btn = Button()
+        with suppress_event(btn, 'on_press'):
+            btn.bind(on_press=lambda __: print("pressed"))
+            btn.dispatch('on_press')
+
+    .. warning::
+
+        You need to be careful when you suppress an ``on_touch_xxx`` event.
+        See :ref:`kivys-event-system` for details.
+    '''
+    __slots__ = ('_dispatcher', '_name', '_bind_uid', '_filter', )
+
+    def __init__(self, event_dispatcher, event_name, *, filter=lambda *args, **kwargs: True):
+        self._dispatcher = event_dispatcher
+        self._name = event_name
+        self._filter = filter
+
+    def __enter__(self):
+        self._bind_uid = self._dispatcher.fbind(self._name, self._filter)
+
+    def __exit__(self, *args):
+        self._dispatcher.unbind_uid(self._name, self._bind_uid)
+
+
+async def rest_of_touch_events(widget, touch, *, stop_dispatching=False) -> AsyncIterator[None]:
+    '''
+    Returns an async iterator that yields None on each ``on_touch_move`` event
+    and stops when an ``on_touch_up`` event occurs.
+
+    .. code-block::
+
+        async for __ in rest_of_touch_events(widget, touch):
+            print('on_touch_move')
+        print('on_touch_up')
+
+    :param stop_dispatching: If the ``widget`` is a type that grabs touches on its own, such as
+                             :class:`kivy.uix.button.Button`, you'll likely want to set this to True
+                             in most cases to avoid grab conflicts.
+
+    .. versionchanged:: 0.9.0
+        The ``timeout`` parameter was removed.
+        You are now responsible for handling cases where the ``on_touch_up`` event for the touch does not occur.
+        If you fail to handle this, the iterator will wait indefinitely for an event that never comes.
+    '''
+    touch.grab(widget)
+    try:
+        with ExitStack() as stack:
+            if stop_dispatching:
+                ec = stack.enter_context
+                se = partial(suppress_event, widget, filter=lambda w, t, touch=touch: t is touch)
+                ec(se('on_touch_up'))
+                ec(se('on_touch_move'))
+
+            def filter(w, t, touch=touch):
+                return t is touch and t.grab_current is w
+            async with (
+                move_on_when(event(widget, 'on_touch_up', filter=filter, stop_dispatching=True)),
+                event_freq(widget, 'on_touch_move', filter=filter, stop_dispatching=True) as on_touch_move,
+            ):
+                while True:
+                    await on_touch_move()
+                    yield
+    finally:
+        touch.ungrab(widget)
