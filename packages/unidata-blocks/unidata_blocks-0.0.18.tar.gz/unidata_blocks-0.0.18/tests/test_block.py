c1065bb65ef660dfe955fe5f492b891c19cdb79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 07:09:27 +0000

    Add package unidata-blocks/0.0.18/unidata_blocks-0.0.18.tar.gz

---
+++ packages/unidata-blocks/unidata_blocks-0.0.18.tar.gz/unidata_blocks-0.0.18/tests/test_block.py
@@ -0,0 +1,94 @@
+import unidata_blocks
+
+
+def test_unicode_version():
+    assert unidata_blocks.unicode_version == '16.0.0'
+
+
+def test_no_block():
+    assert unidata_blocks.get_block_by_code_point(-1) is None
+    assert unidata_blocks.get_block_by_code_point(0x999999) is None
+
+
+def test_code_point_query():
+    block = unidata_blocks.get_block_by_code_point(30)
+    assert block.code_start == 0x0000
+    assert block.code_end == 0x007F
+    assert block.name == 'Basic Latin'
+    assert block.capacity == 128
+    assert block.printable_count == 95
+
+    block = unidata_blocks.get_block_by_code_point(130)
+    assert block.code_start == 0x0080
+    assert block.code_end == 0xFF
+    assert block.name == 'Latin-1 Supplement'
+    assert block.capacity == 128
+    assert block.printable_count == 94
+
+
+def test_chr_query():
+    block = unidata_blocks.get_block_by_chr('A')
+    assert block.code_start == 0x0000
+    assert block.code_end == 0x007F
+    assert block.name == 'Basic Latin'
+    assert block == unidata_blocks.get_block_by_chr('B')
+
+    block = unidata_blocks.get_block_by_chr('汉')
+    assert block.code_start == 0x4E00
+    assert block.code_end == 0x9FFF
+    assert block.name == 'CJK Unified Ideographs'
+    assert block == unidata_blocks.get_block_by_chr('字')
+
+
+def test_name_query():
+    block = unidata_blocks.get_block_by_name('CJK Unified Ideographs')
+    assert block.code_start == 0x4E00
+    assert block.code_end == 0x9FFF
+    assert block.name == 'CJK Unified Ideographs'
+    assert block == unidata_blocks.get_block_by_name('CJK-Unified-Ideographs')
+    assert block == unidata_blocks.get_block_by_name('CJK_Unified_Ideographs')
+    assert block == unidata_blocks.get_block_by_name('cjk unified ideographs')
+    assert block == unidata_blocks.get_block_by_name('CJK UNIFIED IDEOGRAPHS')
+
+
+def test_all_query():
+    blocks = unidata_blocks.get_blocks()
+    assert len(blocks) > 0
+    assert blocks[0].name == 'Basic Latin'
+
+
+def test_to_str():
+    block = unidata_blocks.get_block_by_code_point(0x0000)
+    assert str(block) == '0000..007F; Basic Latin'
+
+    block = unidata_blocks.get_block_by_code_point(0x4E00)
+    assert str(block) == '4E00..9FFF; CJK Unified Ideographs'
+
+    block = unidata_blocks.get_block_by_code_point(0xF0000)
+    assert str(block) == 'F0000..FFFFF; Supplementary Private Use Area-A'
+
+    block = unidata_blocks.get_block_by_code_point(0x100000)
+    assert str(block) == '100000..10FFFF; Supplementary Private Use Area-B'
+
+
+def test_contains():
+    block = unidata_blocks.get_block_by_code_point(0x4E00)
+    assert 0x4E00 in block
+    assert 0x9FFF in block
+    assert 0x5000 in block
+    assert 0x1000 not in block
+    assert 0xFFFF not in block
+    assert '0x5000' not in block
+
+
+def test_i18n():
+    block = unidata_blocks.get_block_by_code_point(0x0000)
+    assert block.name_localized('en') == 'Basic Latin'
+    assert block.name_localized('EN') == 'Basic Latin'
+    assert block.name_localized('zh') == '基本拉丁'
+    assert block.name_localized('ZH') == '基本拉丁'
+    assert block.name_localized('zh-hans') == '基本拉丁'
+    assert block.name_localized('zh-chs') == '基本拉丁'
+    assert block.name_localized('zh-cn') == '基本拉丁'
+    assert block.name_localized('no-language') is None
+    assert block.name_localized('no-language', 'abc') == 'abc'
---
+++ packages/unidata-blocks/unidata_blocks-0.0.18.tar.gz/unidata_blocks-0.0.18/tools/update.py
@@ -0,0 +1,47 @@
+import shutil
+
+import httpx
+import langcodes
+
+import unidata_blocks
+from tools import project_root_dir
+
+
+def main():
+    unidata_dir = project_root_dir.joinpath('src', 'unidata_blocks', 'unidata')
+    translations_dir = unidata_dir.joinpath('translations')
+    translations_tmp_dir = project_root_dir.joinpath('build', 'translations')
+
+    response = httpx.get('https://www.unicode.org/Public/UNIDATA/Blocks.txt')
+    assert response.is_success and 'text/plain' in response.headers['Content-Type']
+    unidata_dir.joinpath('Blocks.txt').write_text(response.text, 'utf-8')
+    # noinspection PyProtectedMember
+    unicode_version, blocks = unidata_blocks._parse_blocks(response.text)
+
+    if translations_tmp_dir.exists():
+        shutil.rmtree(translations_tmp_dir)
+    translations_tmp_dir.mkdir(parents=True)
+
+    for file_path in translations_dir.iterdir():
+        if file_path.suffix != '.txt':
+            continue
+        language = langcodes.standardize_tag(file_path.stem)
+        # noinspection PyProtectedMember
+        translation = unidata_blocks._parse_translation(file_path.read_text('utf-8'))
+
+        with translations_tmp_dir.joinpath(f'{language}.txt').open('w', encoding='utf-8', newline='\n') as file:
+            file.write(f'# Unicode: {unicode_version}\n')
+            file.write(f'# {language}\n\n')
+            for block in blocks:
+                localized_name = translation.get(block.name, None)
+                if localized_name is None:
+                    file.write(f'# TODO # {block.name}:\n')
+                else:
+                    file.write(f'{block.name}: {localized_name}\n')
+
+    shutil.rmtree(translations_dir)
+    translations_tmp_dir.rename(translations_dir)
+
+
+if __name__ == '__main__':
+    main()
