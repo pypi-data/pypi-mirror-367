7dd61818c4a9195f5a9faec78b3929c2576b2c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 15:06:12 +0000

    Add package maven-proxy/1.0.92.19/maven_proxy-1.0.92.19.tar.gz

---
+++ packages/maven-proxy/maven_proxy-1.0.92.19.tar.gz/maven_proxy-1.0.92.19/maven_proxy/db.py
@@ -0,0 +1,39 @@
+# 修改 db.py 文件
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+import sqlite3
+import time
+
+class DB:
+    def __init__(self, database_file_path='./database.db'):
+        self.conn = sqlite3.connect(database_file_path, check_same_thread=False)
+        self.init_tables()
+
+    def init_tables(self):
+        # 创建错误记录表
+        self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS fetch_errors (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                remote_url TEXT NOT NULL,
+                error_message TEXT,
+                timestamp INTEGER NOT NULL
+            )
+        ''')
+        self.conn.commit()
+
+    def record_fetch_error(self, remote_url, error_message):
+        self.conn.execute('''
+            INSERT INTO fetch_errors (remote_url, error_message, timestamp)
+            VALUES (?, ?, ?)
+        ''', (remote_url, error_message, int(time.time())))
+        self.conn.commit()
+
+    def has_fetch_failed_before(self, remote_url):
+        """
+        检查指定的URL是否之前抓取失败过
+        """
+        cursor = self.conn.execute('SELECT COUNT(*) FROM fetch_errors WHERE remote_url = ?', (remote_url,))
+        count = cursor.fetchone()[0]
+        return count > 0
+
