ec125d990511e929203e68caa576dadb52f7227d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 15:06:26 +0000

    Add package superb-ai-onprem/0.4.1/superb_ai_onprem-0.4.1-py3-none-any.whl

---
+++ packages/superb-ai-onprem/superb_ai_onprem-0.4.1-py3-none-any.whl/spb_onprem/data/enums/__init__.py
@@ -0,0 +1,12 @@
+from .data_meta_type import DataMetaTypes, DataMetaValue
+from .data_type import DataType
+from .scene_type import SceneType
+from .data_slice_status import DataSliceStatus
+
+__all__ = (
+    "DataType",
+    "SceneType",
+    "DataMetaTypes",
+    "DataMetaValue",
+    "DataSliceStatus",
+)
