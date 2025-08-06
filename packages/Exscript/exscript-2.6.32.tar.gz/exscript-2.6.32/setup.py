11f816f2cf2b1ddf8a68c5dc46e5a6196c5e6a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 05:12:02 +0000

    Add package Exscript/2.6.32/exscript-2.6.32.tar.gz

---
+++ packages/Exscript/exscript-2.6.32.tar.gz/exscript-2.6.32/setup.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from setuptools import setup, find_packages
+sys.path.insert(0, 'Exscript')
+from version import __version__
+
+# Import the project description from the README.
+descr = '''
+Exscript is a Python module and a template processor for automating network
+connections over protocols such as Telnet or SSH. We attempt to create the
+best possible set of tools for working with Telnet and SSH.
+'''.strip()
+
+# Run the setup.
+setup(name             = 'Exscript',
+      version          = __version__,
+      description      = 'Automating Telnet and SSH',
+      long_description = descr,
+      author           = 'Samuel Abels',
+      author_email     = 'knipknap@gmail.com',
+      license          = 'MIT',
+      package_dir      = {'Exscript': 'Exscript'},
+      package_data     = {},
+      packages         = find_packages(),
+      scripts          = ['scripts/exscript', 'scripts/otp'],
+      install_requires = ['future', 'configparser', 'paramiko>=1.17,<5', 'pycryptodomex'],
+      extras_require   = {},
+      keywords         = ' '.join(['exscript',
+                                   'telnet',
+                                   'ssh',
+                                   'network',
+                                   'networking',
+                                   'automate',
+                                   'automation',
+                                   'library']),
+      url              = 'https://github.com/knipknap/exscript/',
+      classifiers      = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ])
