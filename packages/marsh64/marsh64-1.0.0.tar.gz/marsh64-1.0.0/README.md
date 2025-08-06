c6538bef34b08910d8b2576c92e9094ba2923f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 07:08:17 +0000

    Add package marsh64/1.0.0/marsh64-1.0.0.tar.gz

---
+++ packages/marsh64/marsh64-1.0.0.tar.gz/marsh64-1.0.0/README.md
@@ -0,0 +1,8 @@
+# Marsh64
+
+`Marsh64` is a lightweight Python library to encode Python code to a base64 string using marshal serialization, and run it transparently without exposing the internals.
+
+## Installation
+
+```bash
+pip install marsh64
---
+++ packages/marsh64/marsh64-1.0.0.tar.gz/marsh64-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="marsh64",
+    version="1.0.0",
+    description="Lightweight Python code encoder and runner using marshal and base64",
+    author="Ali-jafari",
+    packages=find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
