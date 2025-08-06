66f8e059bcbb5bfdb648c673058cf3c5d06dfffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:20:55 +0000

    Add package fastbuild/0.1.1/fastbuild-0.1.1.tar.gz

---
+++ packages/fastbuild/fastbuild-0.1.1.tar.gz/fastbuild-0.1.1/fastbuild/snippets/init_import_snippet.py
@@ -0,0 +1,4 @@
+# BEGIN AUTO-INJECT:DB_IMPORT
+from src.database import init_db
+init_db()
+# END AUTO-INJECT:DB_IMPORT
---
+++ packages/fastbuild/fastbuild-0.1.1.tar.gz/fastbuild-0.1.1/fastbuild/version.py
@@ -0,0 +1,6 @@
+# fastbuild/fastbuild/version.py
+
+__version__ = "0.1.1"
+
+def get_version():
+    return __version__
