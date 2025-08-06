1a92f80d15016a625c241174a85037069d73c4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 06:05:37 +0000

    Add package k1lib/1.8.1/k1lib-1.8.1-py3-none-any.whl

---
+++ packages/k1lib/k1lib-1.8.1-py3-none-any.whl/k1lib/cli/__init__.py
@@ -0,0 +1,42 @@
+from k1lib import wrapMod as _wrapMod
+
+_scatteredClis = [] # everywhere else in the system can add objects here, and k1lib.imports will make all of them available.
+def scatteredClis(): return [c.__name__ for c in _scatteredClis]
+
+from .init import *
+from .typehint import *
+
+# common utils
+from .filt import *
+from .grep import *
+from .inp import *
+from .modifier import *
+from .output import *
+from .structural import *
+from .conv import *
+from .utils import *
+from .models import *
+from .lsext import *
+from .kjs import *
+
+# bio/chem specific
+from .bio import *
+from .mol import *
+from . import mgi;  mgi  = _wrapMod(mgi)
+from . import cif;  cif  = _wrapMod(cif)
+from . import kapi; kapi  = _wrapMod(kapi)
+
+# file formats
+from . import ktree; ktree = _wrapMod(ktree)
+from . import kxml;  kxml  = _wrapMod(kxml)
+from . import sam;   sam   = _wrapMod(sam)
+from . import gb;    gb    = _wrapMod(gb)
+from . import nb;    nb    = _wrapMod(nb)
+from . import kjs;   kjs   = _wrapMod(kjs)
+from . import kgv;   kgv   = _wrapMod(kgv)
+from . import kcv;   kcv   = _wrapMod(kcv)
+#toJsFunc = kjs.toJsFunc; executeScriptTags = kjs.executeScriptTags
+
+from .optimizations import *
+from .trace import * # has to be last, to wait for others to load up
+
---
+++ packages/k1lib/k1lib-1.8.1-py3-none-any.whl/k1lib/serve/suffix.py
@@ -0,0 +1,27 @@
+import inspect, io, base64, traceback
+from flask import Flask, request
+from flask_cors import CORS
+
+_serve_d = k1lib.serve.analyze(endpoint); _serve_args = _serve_d["args"]; _serve_annos = _serve_d["annos"]; _serve_privates = _serve_d["privates"]; _serve_defaults = _serve_d["defaults"]
+del _serve_d["privates"]; _serve_d | aS(dill.dumps) | file("META_FILE")
+_serve_app = Flask(__name__); CORS(_serve_app)
+
+@_serve_app.route("/healthCheck", methods=["GET", "POST"])
+def _serve_healthCheck(): return "ok"
+
+@_serve_app.route("/", methods=["POST"])
+def _serve_main():
+    try:
+        js = request.json
+        for arg in [arg for arg in _serve_args if _serve_annos[arg] == "apiKey"]: # if has serve.apiKey(), then check if the key matches. If not, then raise exception
+            if js[arg] != _serve_privates[arg]: raise Exception(f"Api key '{arg}' wrong or is not specified") # _serve_defaults[arg][0] part is not airtight, as there're exceptions like dropdown, but good enough for my purpose
+        return {"data": k1lib.serve.pyToWeb(endpoint(*[k1lib.serve.webToPy(js[arg] if arg in js else _serve_defaults[arg][0], _serve_annos[arg]) for arg in _serve_args]), _serve_annos["return"]), "success": True}
+    except Exception as e: return {"data": None, "success": False, "reason": f"Error encountered: {e}\n\n{traceback.format_exc()}"}
+
+try:
+    import waitress
+    waitress.serve(_serve_app, host='0.0.0.0', port=SOCKET_PORT)
+except: _serve_app.run(host='0.0.0.0', port=SOCKET_PORT)
+
+
+
