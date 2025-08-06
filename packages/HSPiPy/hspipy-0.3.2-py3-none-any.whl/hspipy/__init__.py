034f2c28767f5e7e917cdf2733e0012e56948c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:09:17 +0000

    Add package HSPiPy/0.3.2/hspipy-0.3.2-py3-none-any.whl

---
+++ packages/HSPiPy/hspipy-0.3.2-py3-none-any.whl/hspipy/__init__.py
@@ -0,0 +1,13 @@
+"""
+HSPiPy - Hansen Solubility Parameters in Python
+
+A Python package for calculating and visualizing Hansen Solubility Parameters.
+"""
+
+__version__ = "0.3.2"
+__author__ = "Alejandro Gutierrez"
+__email__ = "agutierrez@g-npd.com"
+
+from .hsp import HSP
+
+__all__ = ["HSP"]
