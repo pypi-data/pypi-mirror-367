84cc12e81bd486a967bc35971b4287a7b866b277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:23 +0000

    Add package vulnscan/3.2.0/vulnscan-3.2.0.tar.gz

---
+++ packages/vulnscan/vulnscan-3.2.0.tar.gz/vulnscan-3.2.0/vulnscan/base/__init__.py
@@ -0,0 +1,18 @@
+
+"""
+Base utilities and core functionality for VulnScan
+"""
+
+# Import base utilities
+from .utils import *
+from .sendRequest import *
+from .extractHTMLInformation import *
+from .rangeFinder import *
+from .formEvaluate import *
+from .modifyManipulateAPI import *
+from .crawl import *
+from .prompt import *
+from .formParser import *
+from .entropy import *
+from .colors import *
+from .testing import *
