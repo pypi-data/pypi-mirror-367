47f6d52a28fd27cbf5d1003985d32f3eee287130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 03:33:54 +0000

    Add package cemento/0.9.2/cemento-0.9.2-py3-none-any.whl

---
+++ packages/cemento/cemento-0.9.2-py3-none-any.whl/cemento/cli/constants.py
@@ -0,0 +1,16 @@
+DEFAULT_DOWNLOADS = {
+    # cco as the default download
+    "cco": "https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/refs/heads/develop/src/cco-merged/CommonCoreOntologiesMerged.ttl"
+}
+
+
+header = r"""
+  ____ _____ __  __ _____ _   _ _____ ___  
+ / ___| ____|  \/  | ____| \ | |_   _/ _ \ 
+| |   |  _| | |\/| |  _| |  \| | | || | | |
+| |___| |___| |  | | |___| |\  | | || |_| |
+ \____|_____|_|  |_|_____|_| \_| |_| \___/ 
+
+v0.8.9. Building the road to ontologies for materials data.
+Copyright 2025 CWRU SDLE-MDS3
+"""
---
+++ packages/cemento/cemento-0.9.2-py3-none-any.whl/cemento/rdf/preprocessing.py
@@ -0,0 +1,41 @@
+import re
+
+from rdflib import Literal
+from rdflib.namespace import split_uri
+
+
+def clean_literal_string(literal_term: str) -> str:
+    new_literal_term = literal_term.strip().replace('"', "")
+    new_literal_term = re.sub(r"@\w+", "", new_literal_term)
+    new_literal_term = re.sub(r"\^\^\w+:\w+", "", new_literal_term)
+    return new_literal_term
+
+# TODO: set supppression key as constant
+def remove_suppression_key(term: str) -> str:
+    return term.replace("*", "")
+
+
+def get_term_aliases(term: str) -> list[str]:
+    match = re.search(r"\(([^)]*)\)", term)
+    if match:
+        alt_term_string = match.group(1)
+        alt_term_string = alt_term_string.split(",")
+        return [term.strip() for term in alt_term_string]
+    return []
+
+
+def format_literal(literal: Literal) -> str:
+    literal_str = f'"{literal.value}"'
+    lang_str = (
+        f"@{literal.language}"
+        if hasattr(literal, "language") and literal.language
+        else ""
+    )
+
+    datatype_str = ""
+    if hasattr(literal, "datatype") and literal.datatype:
+        datatype = literal.datatype
+        _, abbrev = split_uri(datatype)
+        datatype_str = f"^^xsd:{abbrev}"
+
+    return f"{literal_str}{lang_str}{datatype_str}"
