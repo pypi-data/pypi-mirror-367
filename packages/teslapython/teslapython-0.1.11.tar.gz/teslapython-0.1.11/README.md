67b3edd530164ba7308789a20db14f2f588b1c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:39 +0000

    Add package teslapython/0.1.11/teslapython-0.1.11.tar.gz

---
+++ packages/teslapython/teslapython-0.1.11.tar.gz/teslapython-0.1.11/README.md
@@ -0,0 +1,19 @@
+Process variable (PV) to 4-20 mA conversion:
+
+    Where,
+    X is Process Variable
+    LRV is Lower Range Value
+    URV is Upper Range Value
+
+4-20 mA to Process variable (PV) conversion:
+
+    Where,
+    Y is 4 -20 mA signal
+    LRV is Lower Range Value
+    URV is Upper Range Value
+
+Difference between PNP and NPN sensors
+
+Safety Integrity Level (SIL) calculations
+
+What is an Active Barrier?
---
+++ packages/teslapython/teslapython-0.1.11.tar.gz/teslapython-0.1.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+
+setup(
+    name='teslapython',
+    version='0.1.11',
+    author='TeslaPython Software Foundation',
+    packages=find_packages(),
+    install_requires=[  
+    
+    ],
+    entry_points={
+        'console_scripts': [
+            'teslapython=teslapython.main:teslapython',
+        ],
+    },
+    long_description=description,
+    long_description_content_type='text/markdown',
+)
