d45d6ebdcf89b4413571ae94e768f52b150d1aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:36 +0000

    Add package zeusdb-vector-database/0.3.0/zeusdb_vector_database-0.3.0-cp313-cp313-win32.whl

---
+++ packages/zeusdb-vector-database/zeusdb_vector_database-0.3.0-cp313-cp313-win32.whl/zeusdb_vector_database/__init__.py
@@ -0,0 +1,8 @@
+"""
+ZeusDB Vector Database Module
+"""
+__version__ = "0.3.0"
+
+from .vector_database import VectorDatabase # imports the VectorDatabase class from the vector_database.py file
+
+__all__ = ["VectorDatabase"]
