528178dfe1daa7b61c7aaffc727861f8246adbe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 06:05:36 +0000

    Add package eyad123/0.1/eyad123-0.1.tar.gz

---
+++ packages/eyad123/eyad123-0.1.tar.gz/eyad123-0.1/hello_world/__init__.py
@@ -0,0 +1,1 @@
+def say_hello(): print("Hello from Hello World!")
---
+++ packages/eyad123/eyad123-0.1.tar.gz/eyad123-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eyad123",
+    version="0.1",
+    packages=find_packages(),
+    description="My hello world package",
+    author="Eyad",
+    author_email="eyadseif17@outlook.com",
+    python_requires=">=3.6",
+    )
