8a650f676c7d8d3b23e6a54fd54059e58f9d43d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 15:06:38 +0000

    Add package elemem-vector-sdk/2.0.0.7/elemem_vector_sdk-2.0.0.7.tar.gz

---
+++ packages/elemem-vector-sdk/elemem_vector_sdk-2.0.0.7.tar.gz/elemem_vector_sdk-2.0.0.7/setup.py
@@ -0,0 +1,51 @@
+import setuptools 
+import subprocess
+
+class BuildProtoCommand(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self): pass
+    def finalize_options(self): pass
+
+    def run(self):
+        subprocess.check_call([
+            'python3', '-m', 'grpc_tools.protoc',
+            '-I../../proto',
+            '--python_out=elemem_sdk/proto',
+            '--grpc_python_out=elemem_sdk/proto',
+            '../../proto/sdk.proto',
+        ])
+
+setuptools.setup(
+    name="elemem_vector_sdk",
+    version="2.0.0.7",
+    author="elemem.tech",
+    author_email="support@elemem.tech",
+    description="A Python SDK for elem",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    py_modules=['hilbert_client', 'hilbert_cli'],
+    entry_points={
+        'console_scripts': [
+            'hilbert-cli = hilbert_cli:main',
+        ],
+    },
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'grpcio==1.73.0',
+        'grpcio-tools==1.73.0',
+        'numpy==1.26.4',
+        'tqdm==4.67.1',
+        'h5py==3.14.0',
+        'protobuf==6.31.1'
+    ],
+    cmdclass={
+        'build_proto': BuildProtoCommand
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
