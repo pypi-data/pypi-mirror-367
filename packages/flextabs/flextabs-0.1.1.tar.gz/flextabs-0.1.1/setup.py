9e659016fbb3f0e5acb65ac3a34ea8241fc07869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 06:05:55 +0000

    Add package flextabs/0.1.1/flextabs-0.1.1.tar.gz

---
+++ packages/flextabs/flextabs-0.1.1.tar.gz/flextabs-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="flextabs",
+    version="0.1.1",
+    author="MS-32154",
+    author_email="msttoffg@gmail.com",
+    description="A flexible and extensible tab manager widget for tkinter applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MS-32154/flextabs",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    keywords="tkinter, ttk, Notebook, tabs, gui, widget, interface",
+    project_urls={
+        "Bug Reports": "https://github.com/MS-32154/flextabs/issues",
+        "Source": "https://github.com/MS-32154/flextabs",
+    },
+)
