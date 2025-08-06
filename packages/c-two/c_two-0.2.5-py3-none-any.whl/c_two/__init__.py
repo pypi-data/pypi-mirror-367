a6a24992d08b45a14d81856f5eedb30a2184f11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:08:43 +0000

    Add package c-two/0.2.5/c_two-0.2.5-py3-none-any.whl

---
+++ packages/c-two/c_two-0.2.5-py3-none-any.whl/c_two/__init__.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from . import rpc
+from . import mcp
+from . import crm
+from . import compo
+from . import error
+from .crm.meta import icrm, iicrm
+from .rpc.transferable import transfer, auto_transfer, Transferable, transferable
+
+__version__ = '0.2.5'
+
+LOGO_ASCII ="""
+                                                  
+                                                  
+                       cccc                       
+                   cccccccccccc                   
+               ccc  ccccccccc    cc               
+          cccc      ccccccccc        ccc          
+      ccccccc       cccc cccc        ccccccc      
+     cccccccc       cccc cccc        ccccccc      
+     cccccccc       cccc cccc        ccccccc      
+     ccccccc        cccc cccc        ccccccc      
+     ccc            cccc cccc        ccc          
+                    cccc cccc                     
+                    cccc cccc                     
+                    cccc cccc                     
+           cc       cccc cccc              c      
+       cccccc       cccc cccc         cccccc      
+     cccccccc       cccc cccc        ccccccc      
+     cccccccc       cccc cccc        ccccccc      
+     cccccccc       cccc cccc        ccccccc      
+         cccc       cccc cccc       cccc          
+              cc    cccc cccc   cccc              
+                  cccccc ccccccc                  
+                      cc cc                       
+                                                  
+                                                  
+"""
