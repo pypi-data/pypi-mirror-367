a20f5e6090e3307bb15fd913aebe5a4eef620869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 06:06:03 +0000

    Add package nci-cidc-api-modules/1.1.37/nci_cidc_api_modules-1.1.37-py3-none-any.whl

---
+++ packages/nci-cidc-api-modules/nci_cidc_api_modules-1.1.37-py3-none-any.whl/cidc_api/config/db.py
@@ -0,0 +1,60 @@
+from os import environ
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, upgrade
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import declarative_base
+
+from .secrets import get_secrets_manager
+
+db = SQLAlchemy()
+BaseModel = declarative_base()
+db.Model = BaseModel
+
+
+def init_db(app: Flask):
+    """Connect `app` to the database and run migrations"""
+    db.init_app(app)
+    db.Model = BaseModel
+    Migrate(app, db, app.config["MIGRATIONS_PATH"])
+    with app.app_context():
+        upgrade(app.config["MIGRATIONS_PATH"])
+
+
+def get_sqlalchemy_database_uri(testing: bool = False) -> str:
+    """Get the PostgreSQL DB URI from environment variables"""
+
+    db_uri = environ.get("POSTGRES_URI")
+    if testing:
+        # Connect to the test database
+        db_uri = environ.get("TEST_POSTGRES_URI", "fake-conn-string")
+    elif not db_uri:
+        secrets = get_secrets_manager(testing)
+
+        # If POSTGRES_URI env variable is not set,
+        # we're connecting to a Cloud SQL instance.
+
+        config: dict = {
+            "drivername": "postgresql",
+            "username": environ.get("CLOUD_SQL_DB_USER"),
+            "password": secrets.get(environ.get("CLOUD_SQL_DB_PASS_ID")),
+            "database": environ.get("CLOUD_SQL_DB_NAME"),
+        }
+
+        if environ.get("CLOUD_SQL_INSTANCE_NAME"):
+            socket_dir = environ.get("CLOUD_SQL_SOCKET_DIR", "/cloudsql/")
+
+            # If CLOUD_SQL_INSTANCE_NAME is defined, we're connecting
+            # via a unix socket from inside App Engine.
+            config["query"] = {"host": f'{socket_dir}{environ.get("CLOUD_SQL_INSTANCE_NAME")}'}
+        else:
+            raise RuntimeError(
+                "Either POSTGRES_URI or CLOUD_SQL_INSTANCE_NAME must be defined to connect " + "to a database."
+            )
+
+        db_uri = str(URL.create(**config).render_as_string(hide_password=False))
+
+    assert db_uri
+
+    return db_uri
