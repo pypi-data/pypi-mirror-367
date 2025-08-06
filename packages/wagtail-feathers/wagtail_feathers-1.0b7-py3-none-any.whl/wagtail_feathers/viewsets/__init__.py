51792f7a8df7bdff45fe3ea28d9277d5529ad2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 07:09:16 +0000

    Add package wagtail-feathers/1.0b7/wagtail_feathers-1.0b7-py3-none-any.whl

---
+++ packages/wagtail-feathers/wagtail_feathers-1.0b7-py3-none-any.whl/wagtail_feathers/viewsets/__init__.py
@@ -0,0 +1,36 @@
+"""Viewsets for wagtail_feathers."""
+from .author import AuthorTypeViewSet
+from .navigation import FlatMenuViewSet, FooterNavigationViewSet, FooterViewSet, MenuViewSet, NestedMenuViewSet
+from .person import PersonGroupViewSet, PersonViewSet
+from .social import SocialMediaSettingsViewSet
+from .taxonomy import CategoryFilterSet, CategoryViewSet, ClassifierGroupViewSet, ClassifierViewSet
+from .viewset_groups import (
+    FeathersAuthorshipViewSetGroup,
+    FeathersNavigationViewSetGroup,
+    FeathersSiteComponentsViewSetGroup,
+    FeathersTaxonomyViewSetGroup,
+    PersonViewSetGroup,
+)
+from .choosers import classifier_chooser_viewset
+
+__all__ = [
+    "AuthorTypeViewSet",
+    "CategoryViewSet",
+    "ClassifierViewSet",
+    "ClassifierGroupViewSet",
+    "CategoryFilterSet",
+    "FooterNavigationViewSet",
+    "FooterViewSet",
+    "MenuViewSet",
+    "NestedMenuViewSet",
+    "FlatMenuViewSet",
+    "PersonViewSet",
+    "PersonGroupViewSet",
+    "FeathersNavigationViewSetGroup",
+    "FeathersTaxonomyViewSetGroup",
+    "FeathersAuthorshipViewSetGroup",
+    "PersonViewSetGroup",
+    "FeathersSiteComponentsViewSetGroup",
+    "SocialMediaSettingsViewSet",
+    "classifier_chooser_viewset"
+]
