5e3b3e0a29f1d881904ef8ef33ac820ebd93cd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:06:51 +0000

    Add package elrahapi/1.2.0.4/elrahapi-1.2.0.4-py3-none-any.whl

---
+++ packages/elrahapi/elrahapi-1.2.0.4-py3-none-any.whl/elrahapi/router/route_additional_config.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import  Type
+from elrahapi.router.router_routes_name import RoutesName
+
+class DefaultRouteConfig:
+    def __init__(self, summary: str, description: str):
+        self.summary = summary
+        self.description = description
+
+
+class ResponseModelConfig:
+
+    def __init__(
+        self,
+        route_name: RoutesName,
+        read_with_relations: bool | None =None,
+        reponse_model: Type[BaseModel] | None = None,
+    ):
+        self.reponse_model = reponse_model
+        self.route_name = route_name
+        self.read_with_relations = read_with_relations
+
+
+class AuthorizationConfig:
+    def __init__(
+        self,
+        route_name: RoutesName,
+        roles: list[str] | None = None,
+        privileges: list[str] | None = None,
+    ):
+        self.route_name = route_name
+        self.roles = roles if roles else []
+        self.privileges = privileges if privileges else []
