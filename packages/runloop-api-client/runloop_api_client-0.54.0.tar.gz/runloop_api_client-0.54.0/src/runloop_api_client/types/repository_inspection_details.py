6d93b6d0ad2d6f6ac54b0885fe31d6e4d3af58fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 07:09:29 +0000

    Add package runloop-api-client/0.54.0/runloop_api_client-0.54.0.tar.gz

---
+++ packages/runloop-api-client/runloop_api_client-0.54.0.tar.gz/runloop_api_client-0.54.0/src/runloop_api_client/types/repository_inspection_details.py
@@ -0,0 +1,50 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from typing_extensions import Literal
+
+from .._models import BaseModel
+from .repository_manifest_view import RepositoryManifestView
+
+__all__ = ["RepositoryInspectionDetails"]
+
+
+class RepositoryInspectionDetails(BaseModel):
+    id: str
+    """The ID of the inspection."""
+
+    commit_sha: str
+    """The sha of the inspected version of the Repository."""
+
+    inspected_at: int
+    """Inspection time of the Repository Version (Unix timestamp milliseconds)."""
+
+    repository_manifest: RepositoryManifestView
+    """Repository manifest containing container config and workspace details."""
+
+    status: Literal[
+        "invalid",
+        "repo_auth_pending",
+        "repo_authentication_failure",
+        "repo_access_failure",
+        "inspection_pending",
+        "inspection_failed",
+        "inspection_success",
+        "inspection_user_manifest_added",
+    ]
+    """The status of the repository inspection."""
+
+    blueprint_id: Optional[str] = None
+    """The blueprint ID associated with this inspection if successful."""
+
+    blueprint_name: Optional[str] = None
+    """The blueprint name associated with this inspection if successful."""
+
+    build_status: Optional[Literal["image_building", "image_build_success", "image_build_failure"]] = None
+    """The status of the linked Blueprint build."""
+
+    user_manifest: Optional[RepositoryManifestView] = None
+    """
+    User uploaded repository manifest containing container config and workspace
+    details.
+    """
