ca0a48c9a2eb31bdd4f5d7b23bc45808adc4a610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:38 +0000

    Add package rq_geo_toolkit/2025.8.1/rq_geo_toolkit-2025.8.1.tar.gz

---
+++ packages/rq_geo_toolkit/rq_geo_toolkit-2025.8.1.tar.gz/rq_geo_toolkit-2025.8.1/rq_geo_toolkit/constants.py
@@ -0,0 +1,9 @@
+"""Constant values used inside the library and for external use."""
+
+GEOMETRY_COLUMN = "geometry"
+
+PARQUET_ROW_GROUP_SIZE = 100_000
+PARQUET_COMPRESSION = "zstd"
+PARQUET_COMPRESSION_LEVEL = 3
+
+MEMORY_1GB = 1024**3
