10b91b1b4417d7b8ddaee9574dc4b72724e81f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:07:05 +0000

    Add package pyftools-stata/0.1.1/pyftools_stata-0.1.1-py3-none-any.whl

---
+++ packages/pyftools-stata/pyftools_stata-0.1.1-py3-none-any.whl/pyftools/__init__.py
@@ -0,0 +1,16 @@
+"""
+PyFtools - Python implementation of ftools
+
+Fast data manipulation tools inspired by Stata's ftools package.
+"""
+
+__version__ = "0.1.1"
+__author__ = "Bryce Wang, Collin Liu"
+__email__ = "brycew6m@stanford.edu, junnebailiu@gmail.com"
+
+from .factor import Factor
+from .fcollapse import fcollapse, fsum, fmean
+from .ftools import fegen, flevelsof, fisid, fsort, join_factors, fcount
+
+__all__ = ["Factor", "fcollapse", "fsum", "fmean", "fcount", 
+          "fegen", "flevelsof", "fisid", "fsort", "join_factors"]
