a8134f5e5be743bbaaebecc18453ff1fa35eb09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 05:12:41 +0000

    Add package privacyschema/0.1.0/privacyschema-0.1.0.tar.gz

---
+++ packages/privacyschema/privacyschema-0.1.0.tar.gz/privacyschema-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='privacyschema',
+    version='0.1.0',
+    description='Pluggable framework for personal data compliance (LGPD/GDPR)',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[],
+)
