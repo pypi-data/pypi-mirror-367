cff16746676ecf193cc0e338b379cb6667da9292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:17 +0000

    Add package komtet-kassa-sdk/7.1.4/komtet_kassa_sdk-7.1.4-py3-none-any.whl

---
+++ packages/komtet-kassa-sdk/komtet_kassa_sdk-7.1.4-py3-none-any.whl/komtet_kassa_sdk/v2/__init__.py
@@ -0,0 +1,34 @@
+from .agent import Agent, AgentType
+from .check import (Check, CorrectionCheck, CorrectionType, Intent, MarkTypes,
+                    MeasureTypes, PaymentMethod, PaymentObject, PaymentType,
+                    Position, TaxSystem, VatRate)
+from .client import Client, EmployeeInfo, OrderInfo, Task, TaskInfo
+from .employee import Employee, EmployeeType
+from .order import Order, OrderItem
+
+
+__all__ = [
+    'Agent',
+    'AgentType',
+    'Check',
+    'Client',
+    'CorrectionCheck',
+    'CorrectionType',
+    'Employee',
+    'EmployeeInfo',
+    'EmployeeType',
+    'Intent',
+    'MarkTypes',
+    'MeasureTypes',
+    'PaymentMethod',
+    'PaymentObject',
+    'PaymentType',
+    'Position',
+    'Order',
+    'OrderInfo',
+    'OrderItem',
+    'Task',
+    'TaskInfo',
+    'TaxSystem',
+    'VatRate'
+]
