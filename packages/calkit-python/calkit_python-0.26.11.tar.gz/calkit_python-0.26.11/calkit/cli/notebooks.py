037a0a9d9705a3478c4c5661be79488c08b92671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 06:05:28 +0000

    Add package calkit-python/0.26.11/calkit_python-0.26.11.tar.gz

---
+++ packages/calkit-python/calkit_python-0.26.11.tar.gz/calkit_python-0.26.11/calkit/cli/notebooks.py
@@ -0,0 +1,130 @@
+"""Notebooks CLI."""
+
+from __future__ import annotations
+
+import os
+import subprocess
+import sys
+from pathlib import PurePosixPath
+
+import typer
+from typing_extensions import Annotated
+
+import calkit.notebooks
+from calkit.cli.core import raise_error
+
+notebooks_app = typer.Typer(no_args_is_help=True)
+
+
+@notebooks_app.command("clean")
+def clean_notebook_outputs(path: str):
+    """Clean notebook and place a copy in the cleaned notebooks directory.
+
+    This can be useful to use as a preprocessing DVC stage to use a clean
+    notebook as a dependency for a stage that caches and executed notebook.
+    """
+    if os.path.isabs(path):
+        raise ValueError("Path must be relative")
+    fpath_out = calkit.notebooks.get_cleaned_notebook_path(path)
+    folder = os.path.dirname(fpath_out)
+    os.makedirs(folder, exist_ok=True)
+    fpath_out = os.path.abspath(fpath_out)
+    subprocess.call(
+        [
+            sys.executable,
+            "-m",
+            "jupyter",
+            "nbconvert",
+            path,
+            "--clear-output",
+            "--to",
+            "notebook",
+            "--output",
+            fpath_out,
+        ]
+    )
+
+
+@notebooks_app.command("execute")
+def execute_notebook(
+    path: str,
+    env_name: Annotated[
+        str,
+        typer.Option(
+            "--environment",
+            "-e",
+            help="Environment name in which to run the notebook.",
+        ),
+    ],
+    to: Annotated[
+        list[str],
+        typer.Option("--to", help="Output format ('html' or 'notebook')."),
+    ] = ["notebook"],
+    no_check: Annotated[
+        bool,
+        typer.Option(
+            "--no-check", help="Do not check environment before executing."
+        ),
+    ] = False,
+    verbose: Annotated[
+        bool, typer.Option("--verbose", "-v", help="Print verbose output.")
+    ] = False,
+):
+    """Execute notebook and place a copy in the relevant directory.
+
+    This can be useful to use as a preprocessing DVC stage to use a clean
+    notebook as a dependency for a stage that caches and executed notebook.
+    """
+    from calkit.cli.main import run_in_env
+
+    if os.path.isabs(path):
+        raise ValueError("Path must be relative")
+    # First, always execute the notebook and save as ipynb
+    fpath_out_exec = calkit.notebooks.get_executed_notebook_path(
+        notebook_path=path, to="notebook", as_posix=True
+    )
+    folder = os.path.dirname(fpath_out_exec)
+    os.makedirs(folder, exist_ok=True)
+    fname = os.path.basename(fpath_out_exec)
+    fpath_out_exec = PurePosixPath(fpath_out_exec).as_posix()
+    cmd = [
+        "jupyter",
+        "nbconvert",
+        path,
+        "--execute",
+        "--to",
+        "notebook",
+        "--output-dir",
+        PurePosixPath(folder).as_posix(),
+        "--output",
+        fname,
+    ]
+    run_in_env(cmd=cmd, env_name=env_name, no_check=no_check, verbose=verbose)
+    for to_fmt in to:
+        if to_fmt != "notebook":
+            try:
+                fpath_out = calkit.notebooks.get_executed_notebook_path(
+                    notebook_path=path,
+                    to=to_fmt,  # type: ignore
+                )
+            except ValueError:
+                raise_error(f"Invalid output format: '{to}'")
+            folder = os.path.dirname(fpath_out)
+            os.makedirs(folder, exist_ok=True)
+            fname_out = os.path.basename(fpath_out)
+            # Now convert without executing or checking the environment
+            cmd = [
+                "jupyter",
+                "nbconvert",
+                fpath_out_exec,
+                "--to",
+                to_fmt,
+                "--output-dir",
+                PurePosixPath(folder).as_posix(),
+                "--output",
+                fname_out,
+            ]
+            typer.echo(f"Exporting {to_fmt}")
+            run_in_env(
+                cmd=cmd, env_name=env_name, no_check=True, verbose=verbose
+            )
