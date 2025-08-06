9a0313bfdc6e7865197f078e1482ee7cf5752705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:08:50 +0000

    Add package cli-ih/0.5.4.1/cli_ih-0.5.4.1.tar.gz

---
+++ packages/cli-ih/cli_ih-0.5.4.1.tar.gz/cli_ih-0.5.4.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="cli_ih",
+    version = "0.5.4.1",
+    packages=find_packages(),
+    install_requires = [
+        'logging>=0.4.9.6'
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
