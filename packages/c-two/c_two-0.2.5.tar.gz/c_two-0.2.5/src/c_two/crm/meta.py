8cb98d39e70bf6cc6e6f7c071d25aa3547cc2d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:08:43 +0000

    Add package c-two/0.2.5/c_two-0.2.5.tar.gz

---
+++ packages/c-two/c_two-0.2.5.tar.gz/c_two-0.2.5/src/c_two/crm/meta.py
@@ -0,0 +1,121 @@
+from inspect import isfunction
+from typing import TypeVar, cast
+from ..rpc.transferable import auto_transfer
+
+T = TypeVar('T')
+
+class ICRMMeta(type):
+    """
+    ICRMMeta
+    --
+    A metaclass for ICRM (Interface of Core Resource Model) classes that automatically sets a 'direction' attribute on new classes.
+    Direction rules:
+    - If a class explicitly defines its own 'direction', that value is preserved
+    - If a class inherits from an ICRM class with direction '->', it gets assigned the direction '<-', meaning it is an implementation of the ICRM  (aka CRM or IICRM)  
+    - Otherwise, it defaults to direction '->' (becoming a base ICRM class)
+    
+    This metaclass helps distinguish between ICRM interfaces ('->' direction) and their implementations (CRM, '<-' direction).
+    """
+    
+    def __new__(mcs, name, bases, attrs, **kwargs):
+        
+        if 'direction' in attrs:
+            return super().__new__(mcs, name, bases, attrs, **kwargs)
+        
+        has_icrm_parent = False
+        for base in bases:
+            if hasattr(base, 'direction') and base.direction == '->':
+                attrs['direction'] = '<-'
+                has_icrm_parent = True
+                break
+        
+        if not has_icrm_parent:
+            attrs['direction'] = '->'
+        
+        cls = super().__new__(mcs, name, bases, attrs, **kwargs)
+        return cls
+    
+def icrm(cls: T) -> T:
+    """
+    Interface of Core Resource Model (ICRM) decorator
+    --
+    Convert a regular class to an ICRM class with direction '->'.
+    
+    This function transforms the given class by applying the ICRMMeta metaclass,
+    making it a proper ICRM that other classes can implement.  
+    Additionally, it decorates all member functions of the class with @auto_transfer,
+    so that they can be automatically transferred between Component and CRM.
+    
+    Returns:
+        A class that has all the attributes of the original class plus a static
+        'connect' method that creates and returns instances connected to a remote service.
+    """
+    
+    decorated_methods = {}
+    for name, value in cls.__dict__.items():
+        if isfunction(value) and name not in ('__dict__', '__weakref__', '__module__', '__qualname__', '__init__'):
+            decorated_methods[name] = auto_transfer(cls, '->', value)
+    
+    # Define a new class with ICRMMeta metaclass that inherits from the original class
+    class_name = cls.__name__
+    bases = (cls,)
+    
+    # Create the new class
+    NewClass = ICRMMeta(class_name, bases, {
+        **{k: v for k, v in cls.__dict__.items() 
+           if k not in decorated_methods and k not in ('__dict__', '__weakref__')},
+        **decorated_methods
+    })
+    
+    # Copy over type hints explicitly to help type checkers
+    try:
+        NewClass.__annotations__ = getattr(cls, '__annotations__', {})
+    except (AttributeError, TypeError):
+        pass
+    
+    # Copy docstring, module name etc.
+    for attr in ['__doc__', '__module__', '__qualname__']:
+        try:
+            setattr(NewClass, attr, getattr(cls, attr))
+        except (AttributeError, TypeError):
+            pass
+
+    return cast(T, NewClass)
+
+def iicrm(cls: T) -> T:
+    """
+    Implementation of ICRM (IICRM) decorator
+    --
+    A decorator for classes that implement an ICRM interface.
+    
+    This decorator ensures that:
+    1. The decorated class properly implements all methods defined in its ICRM parent class
+    2. All implemented methods are automatically decorated with @auto_transfer
+    
+    Note: The name 'iicrm' is used because 'crm' is already used as a sub-module name in c-two.
+    """
+    
+    # Get the base class decorated with @icrm
+    base_class = None
+    for base in cls.__bases__:
+        if isinstance(base, ICRMMeta) and getattr(base, 'direction', None) == '->':
+            base_class = base
+            break
+    
+    if base_class is None:
+        raise TypeError(f'{cls.__name__} must inherit from a class decorated with @icrm.')
+    
+    # Check for unimplemented methods
+    for name, value in base_class.__dict__.items():
+        if isfunction(value) and name not in cls.__dict__:
+            raise NotImplementedError(f"Method '{name}' from base class '{base_class.__name__}' must be implemented in '{cls.__name__}'.")
+    
+    # Decorate implemented methods with @auto_transfer
+    for name, value in cls.__dict__.items():
+        if isfunction(value) and name in base_class.__dict__:
+            # Ensure the module is set as the base class module
+            # So that the auto_transfer decorator can find the correct module
+            value.__module__ = base_class.__module__
+            setattr(cls, name, auto_transfer(cls, '<-', value))
+
+    return cls
---
+++ packages/c-two/c_two-0.2.5.tar.gz/c_two-0.2.5/src/c_two/rpc/server.py
@@ -0,0 +1,206 @@
+import enum
+import logging
+import threading
+
+from .util.wait import wait
+from .util.encoding import parse_message
+from .event import Event, EventTag, EventQueue, CompletionType
+
+from .base import BaseServer
+from .zmq import ZmqServer
+from .http import HttpServer
+from .thread import ThreadServer
+from .memory import MemoryServer
+from .transferable import get_wrapped_function
+
+logger = logging.getLogger(__name__)
+
+# Stage and State structures for Server ###############################
+
+@enum.unique
+class ServerStage(enum.Enum):
+    STOPPED = "stopped"
+    STARTED = "started"
+    GRACE = "grace"
+
+class ServerState(object):
+    crm: object | None
+    stage: ServerStage
+    server: BaseServer
+    lock: threading.RLock
+    event_queue: EventQueue
+    server_deallocated: bool
+    termination_event: threading.Event
+    shutdown_events: list[threading.Event]
+
+    def __init__(self, server: BaseServer, event_queue: EventQueue, crm: object):
+        self.crm = crm
+        self.server = server
+        self.event_queue = event_queue
+
+        self.lock = threading.RLock()
+        self.server_deallocated = False
+        self.stage = ServerStage.STOPPED
+        self.termination_event = threading.Event()
+        self.shutdown_events = [self.termination_event]
+
+# Common Server Operations ############################################
+
+def _stop_serving(state: ServerState) -> bool:
+    # Destroy the server
+    state.server.cancel_all_calls()
+    state.server.destroy()
+    
+    # Set the shutdown events to notify all waiting threads
+    for shutdown_event in state.shutdown_events:
+        shutdown_event.set()
+        
+    state.stage = ServerStage.STOPPED
+    return True
+
+def _process_event_and_continue(state: ServerState, event: Event) -> bool:
+    """
+    Process the received event and determine if server should continue running.
+    
+    Args:
+        state (ServerState): The server state containing all necessary information.
+        event (Event): The received event to process.
+        
+    Returns:
+        bool: True if server should continue running, False if it should stop.
+    """
+    should_continue = True
+    
+    # Process PING event
+    if event.tag is EventTag.PING:
+        state.server.reply(Event(tag=EventTag.PONG, request_id=event.request_id))
+    
+    # Process SHUTDOWN event
+    elif event.tag is EventTag.SHUTDOWN_FROM_CLIENT or event.tag is EventTag.SHUTDOWN_FROM_SERVER:
+        with state.lock:
+            # If the CRM instance has a terminate method, call it
+            if state.crm and hasattr(state.crm, 'terminate'):
+                state.crm.terminate()
+                state.crm = None
+            
+            # Send shutdown acknowledgment if Shutdown event comes from client
+            if event.tag is EventTag.SHUTDOWN_FROM_CLIENT:
+                logger.info('Received shutdown request from client, shutting down server...')
+                state.server.reply(Event(tag=EventTag.SHUTDOWN_ACK, request_id=event.request_id))
+        
+            if _stop_serving(state):
+                should_continue = False
+    
+    # Process CRM_CALL event
+    elif event.tag is EventTag.CRM_CALL:
+        crm = state.crm
+        sub_messages = parse_message(event.data)
+
+        # Get method name
+        method_name = sub_messages[0].tobytes().decode('utf-8')
+        
+        # Get arguments
+        args_bytes = sub_messages[1]
+        
+        # Call method wrapped from CRM method
+        method = get_wrapped_function(crm.__class__, method_name)
+        if method is None:
+            raise ValueError(f'No wrapped function found for method: {crm.__module__}.{crm.__name__}.{method_name}')
+
+        response = method(crm, args_bytes)
+        
+        # Create a serialized response based on the serialized_error and serialized_result
+        state.server.reply(Event(tag=EventTag.CRM_REPLY, data=response, request_id=event.request_id))
+
+    return should_continue
+
+def _serve(state: ServerState):
+    while True:
+        # Get the next event to process
+        event = state.event_queue.poll(timeout=0.1)
+        
+        # Process the event and check if it is able to continue serving
+        if event.completion_type != CompletionType.OP_TIMEOUT:
+            if not _process_event_and_continue(state, event):
+                return
+        
+        # Clear event to free memory
+        event = None
+
+def _begin_shutdown_once(state: ServerState) -> None:
+    with state.lock:
+        if state.stage is ServerStage.STARTED:
+            state.server.shutdown()
+            state.stage = ServerStage.GRACE
+
+def _stop(state: ServerState) -> None:
+    with state.lock:
+        if state.stage is ServerStage.STOPPED:
+            return
+        
+        _begin_shutdown_once(state)
+        shutdown_event = threading.Event()
+        state.shutdown_events.append(shutdown_event)
+
+    shutdown_event.wait()
+    return
+
+def _start(state: ServerState) -> None:
+    with state.lock:
+        if state.stage is not ServerStage.STOPPED:
+            raise RuntimeError('Cannot start already-started server.')
+    
+        state.server.start()
+        state.stage = ServerStage.STARTED
+        
+        # Start serving in a daemon thread
+        thread = threading.Thread(target=_serve, args=(state,))
+        thread.daemon = True
+        thread.start()
+
+# Server Interface ####################################################
+
+class Server:
+    """
+    A generic server interface that can handle different types of servers
+    (ZMQ, HTTP, Memory) based on the provided bind address.
+    """
+    def __init__(self, bind_address: str, crm: object, name: str = ''):
+        self.name = name if name != '' else 'CRM Server'
+        
+        # Check bind_address protocol and create appropriate server
+        if bind_address.startswith(('tcp://', 'ipc://')):
+            self.server = ZmqServer(bind_address)
+        elif bind_address.startswith('http://'):
+            self.server = HttpServer(bind_address)
+        elif bind_address.startswith('memory://'):
+            self.server = MemoryServer(bind_address)
+        elif bind_address.startswith('thread://'):
+            self.server = ThreadServer(bind_address)
+        else:
+            # TODO: Handle other protocols if needed
+            raise ValueError(f'Unsupported protocol in bind_address: {bind_address}')
+        
+        # Create an event queue for the server
+        event_queue = EventQueue()
+        self.server.register_queue(event_queue)
+
+        # Create the server state
+        self._state = ServerState(
+            crm=crm,
+            server=self.server,
+            event_queue=event_queue
+        )
+    
+    def start(self) -> None:
+        _start(self._state)
+    
+    def stop(self) -> None:
+        _stop(self._state)
+    
+    def wait_for_termination(self, timeout: float | None = None) -> bool:
+        return wait(
+            self._state.termination_event.wait,
+            self._state.termination_event.is_set,
+            timeout=timeout
+        )
