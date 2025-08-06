f9665776187590c11e9cbde1092bb4496d84ad28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:07:07 +0000

    Add package agent-zero-lite/1.0.11/agent_zero_lite-1.0.11.tar.gz

---
+++ packages/agent-zero-lite/agent_zero_lite-1.0.11.tar.gz/agent_zero_lite-1.0.11/agent_zero_lite/python/tools/memory_save.py
@@ -0,0 +1,18 @@
+from agent_zero_lite.python.helpers.memory import Memory
+from agent_zero_lite.python.helpers.tool import Tool, Response
+
+
+class MemorySave(Tool):
+
+    async def execute(self, text="", area="", **kwargs):
+
+        if not area:
+            area = Memory.Area.MAIN.value
+
+        metadata = {"area": area, **kwargs}
+
+        db = await Memory.get(self.agent)
+        id = await db.insert_text(text, metadata)
+
+        result = self.agent.read_prompt("fw.memory_saved.md", memory_id=id)
+        return Response(message=result, break_loop=False)
---
+++ packages/agent-zero-lite/agent_zero_lite-1.0.11.tar.gz/agent_zero_lite-1.0.11/agent_zero_lite/webui/js/initFw.js
@@ -0,0 +1,9 @@
+import * as initializer from "./initializer.js"
+import * as _modals from "./modals.js";
+import * as _components from "./components.js";
+
+console.log('initFw.js starting');
+
+// initialize required elements
+await initializer.initialize()
+console.log('Initializer completed');
