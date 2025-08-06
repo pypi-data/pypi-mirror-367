6163db5555880c8fcd4aa77912044459b61f3d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 05:11:54 +0000

    Add package printpop/0.2.1/printpop-0.2.1.tar.gz

---
+++ packages/printpop/printpop-0.2.1.tar.gz/printpop-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="printpop",
+    version="0.2.1",
+    packages=find_packages(),  # Auto-discovers the 'printpop' folder
+    entry_points={
+        "console_scripts": [
+            "printpop = printpop.core:main",
+        ]
+    },
+    include_package_data=True
+)
