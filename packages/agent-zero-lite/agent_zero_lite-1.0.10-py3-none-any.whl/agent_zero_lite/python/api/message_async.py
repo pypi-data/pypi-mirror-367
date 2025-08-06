2d48a398fa807e11f437aa3a30374841222706c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:07:03 +0000

    Add package agent-zero-lite/1.0.10/agent_zero_lite-1.0.10-py3-none-any.whl

---
+++ packages/agent-zero-lite/agent_zero_lite-1.0.10-py3-none-any.whl/agent_zero_lite/python/api/message_async.py
@@ -0,0 +1,11 @@
+from agent_zero_lite.agent import AgentContext
+from agent_zero_lite.python.helpers.defer import DeferredTask
+from agent_zero_lite.python.api.message import Message
+
+
+class MessageAsync(Message):
+    async def respond(self, task: DeferredTask, context: AgentContext):
+        return {
+            "message": "Message received.",
+            "context": context.id,
+        }
