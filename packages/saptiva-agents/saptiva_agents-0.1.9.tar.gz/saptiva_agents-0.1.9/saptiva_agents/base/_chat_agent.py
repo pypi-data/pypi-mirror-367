d7d42d86d4a9310cc3da954c2139077b465c10ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 15:06:31 +0000

    Add package saptiva-agents/0.1.9/saptiva_agents-0.1.9.tar.gz

---
+++ packages/saptiva-agents/saptiva_agents-0.1.9.tar.gz/saptiva_agents-0.1.9/saptiva_agents/base/_chat_agent.py
@@ -0,0 +1,15 @@
+from autogen_agentchat.base import Response, ChatAgent
+
+
+class Response(Response):
+    """A response from calling :meth:`ChatAgent.on_messages`."""
+    pass
+
+
+class ChatAgent(ChatAgent):
+    """Protocol for a chat agent."""
+    pass
+
+
+
+
