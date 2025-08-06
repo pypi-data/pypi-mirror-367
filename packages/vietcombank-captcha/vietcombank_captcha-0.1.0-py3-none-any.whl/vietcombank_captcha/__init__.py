9e72de161efdf88aacbb24dcabdfabdc5bac9679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 15:06:24 +0000

    Add package vietcombank-captcha/0.1.0/vietcombank_captcha-0.1.0-py3-none-any.whl

---
+++ packages/vietcombank-captcha/vietcombank_captcha-0.1.0-py3-none-any.whl/vietcombank_captcha/__init__.py
@@ -0,0 +1,6 @@
+"""Vietcombank CAPTCHA Predictor - Lightweight ONNX-based prediction library."""
+
+from .predictor import VietcombankCaptcha, predict
+
+__version__ = "0.1.0"
+__all__ = ["VietcombankCaptcha", "predict"]
