b8229da7ae08fc1f90418655fda455b25e0c7b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:09:36 +0000

    Add package APICORE-Python/1.0.1/apicore_python-1.0.1-py3-none-any.whl

---
+++ packages/APICORE-Python/apicore_python-1.0.1-py3-none-any.whl/APICORE/__init__.py
@@ -0,0 +1,8 @@
+"""
+APICORE - A Python library for parsing API configuration files following the APICORE specification.
+"""
+
+from .core import APICORE
+
+__all__ = ['APICORE']
+__version__ = '1.0.0'
---
+++ packages/APICORE-Python/apicore_python-1.0.1-py3-none-any.whl/APICORE/parsers/__init__.py
@@ -0,0 +1,1 @@
+"""Parsers for API responses."""
