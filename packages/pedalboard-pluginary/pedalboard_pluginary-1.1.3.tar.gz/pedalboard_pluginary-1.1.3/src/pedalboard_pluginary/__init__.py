03bd0bb850428bc9556d4edc55f4939a703ca9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 08:04:46 +0000

    Add package pedalboard-pluginary/1.1.3/pedalboard_pluginary-1.1.3.tar.gz

---
+++ packages/pedalboard-pluginary/pedalboard_pluginary-1.1.3.tar.gz/pedalboard_pluginary-1.1.3/src/pedalboard_pluginary/__init__.py
@@ -0,0 +1,14 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+from .core import PedalboardPluginary
+from .scanner import PedalboardScanner
+from .scanner_isolated import IsolatedPedalboardScanner
