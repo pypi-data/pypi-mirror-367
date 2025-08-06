26e05a10b46f85f50dc726321884503ce179b332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 03:34:09 +0000

    Add package ango/1.3.42/ango-1.3.42-py3-none-any.whl

---
+++ packages/ango/ango-1.3.42-py3-none-any.whl/imerit_ango/models/utils.py
@@ -0,0 +1,26 @@
+def merge_annotations(available_annotations, new_annotations):
+    external_id = new_annotations["externalId"]
+    classification_list = new_annotations["classifications"]
+    object_list = new_annotations["objects"]
+    relation_list = new_annotations["relations"]
+
+    # Merge Classifications
+    classification_schema_ids = []
+    for classification in classification_list:
+        classification_schema_ids.append(classification["schemaId"])
+    classification_schema_ids = list(set(classification_schema_ids))
+
+    for classification in available_annotations["classifications"]:
+        if classification["schemaId"] not in classification_schema_ids:
+            classification_list.append(classification)
+
+    # Merge Tools
+    for tool in available_annotations["tools"]:
+            object_list.append(tool)
+
+    # Merge Relations
+    for relation in available_annotations["relations"]:
+        relation_list.append(relation)
+
+    annotation = {"externalId": external_id, "objects": object_list, "classifications": classification_list, "relations": relation_list}
+    return annotation
