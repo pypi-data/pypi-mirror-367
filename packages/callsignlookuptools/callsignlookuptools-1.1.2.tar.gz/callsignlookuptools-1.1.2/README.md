2cc6a789fa1eade34231b1a64568e9bfb62e1b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 06:05:47 +0000

    Add package callsignlookuptools/1.1.2/callsignlookuptools-1.1.2.tar.gz

---
+++ packages/callsignlookuptools/callsignlookuptools-1.1.2.tar.gz/callsignlookuptools-1.1.2/README.md
@@ -0,0 +1,46 @@
+# callsignlookuptools
+
+A [QRZ](https://www.qrz.com), [Callook](https://callook.info), [HamQTH](https://www.hamqth.com), and [QRZCQ](https://www.qrzcq.com) API interface in Python with sync and async support.
+
+[![PyPI](https://img.shields.io/pypi/v/callsignlookuptools)](https://pypi.org/project/callsignlookuptools/) ![PyPI - Python Version](https://img.shields.io/pypi/pyversions/callsignlookuptools) ![PyPI - License](https://img.shields.io/pypi/l/callsignlookuptools) [![Documentation Status](https://readthedocs.org/projects/callsignlookuptools/badge/?version=stable)](https://callsignlookuptools.readthedocs.io/en/stable/?badge=stable)
+
+## Installation
+
+`callsignlookuptools` requires Python 3.9 at minimum.
+
+```sh
+# synchronous requests only
+$ pip install callsignlookuptools
+
+# asynchronous aiohttp only
+$ pip install callsignlookuptools[async]
+
+# both sync and async
+$ pip install callsignlookuptools[all]
+
+# enable the CLI
+$ pip install callsignlookuptools[cli]
+```
+
+**Note:** If `requests`, `aiohttp`, or `typer[all]` are installed another way, you will also have access to the sync, async, or command-line interface, respectively.
+
+## Usage and Documentation
+
+Documentation is available on [ReadTheDocs](https://callsignlookuptools.miaow.io/).
+
+## API Support
+
+Some of the supported callsign lookup APIs require accounts and/or paid subscriptions to be used.
+
+| Site                             | Requirements                                                                                                   |
+|----------------------------------|----------------------------------------------------------------------------------------------------------------|
+| [QRZ](https://www.qrz.com)       | QRZ account and [XML Logbook Data or QRZ Premium subscription](https://shop.qrz.com/collections/subscriptions) |
+| [Callook](https://callook.info)  | None                                                                                                           |
+| [HamQTH](https://www.hamqth.com) | HamQTH account                                                                                                 |
+| [QRZCQ](https://www.qrzcq.com)   | QRZCQ account and [QRZCQ Premium subscription](https://www.qrzcq.com/page/premium)                             |
+
+## Copyright
+
+Copyright 2021-2023 classabbyamp, 0x5c  
+Released under the BSD 3-Clause License.  
+See [`LICENSE`](LICENSE) for the full license text.
---
+++ packages/callsignlookuptools/callsignlookuptools-1.1.2.tar.gz/callsignlookuptools-1.1.2/callsignlookuptools/qrzcq/qrzcqasync.py
@@ -0,0 +1,84 @@
+"""
+qrzcqtools: asynchronous editon
+---
+Copyright 2021-2023 classabbyamp, 0x5c
+Released under the terms of the BSD 3-Clause license.
+"""
+
+
+from typing import Optional
+from urllib.parse import urlencode
+
+import aiohttp
+
+from ..common import mixins, dataclasses, exceptions
+from ..common.constants import DEFAULT_USERAGENT
+from ..common.functions import is_callsign
+from .qrzcq import QrzCqClientAbc
+
+
+class QrzCqAsyncClient(mixins.AsyncXmlAuthMixin, mixins.AsyncMixin, QrzCqClientAbc):
+    """Asynchronous QRZCQ API client
+
+    :param username: QRZCQ username
+    :param password: QRZCQ password
+    :param session_key: QRZCQ login session key
+    :param useragent: Useragent for QRZCQ
+    :param session: An aiohttp session to use for requests
+    """
+    def __init__(self, username: str, password: str, session_key: str = "",
+                 useragent: str = DEFAULT_USERAGENT,
+                 session: Optional[aiohttp.ClientSession] = None):
+        self._session = session
+        super().__init__(username, password, session_key=session_key, useragent=useragent)
+
+    @classmethod
+    async def new(cls, username: str, password: str, session_key: str = "",
+                  useragent: str = DEFAULT_USERAGENT,
+                  session: Optional[aiohttp.ClientSession] = None):
+        """Creates a ``QrzCqAsyncClient`` object and automatically starts a session if not provided.
+
+        :param username: QRZCQ username
+        :param password: QRZCQ password
+        :param session_key: QRZCQ login session key
+        :param useragent: Useragent for QRZCQ
+        :param session: An aiohttp session to use for requests
+        """
+        obj = cls(username, password, session_key, useragent, session)
+        if obj.session is None:
+            await obj.start_session()
+        return obj
+
+    async def search(self, callsign: str) -> dataclasses.CallsignData:  # type: ignore[override]
+        if not is_callsign(callsign):
+            raise exceptions.CallsignLookupError("Invalid Callsign")
+        try:
+            await self._check_session(
+                s=self._session_key,
+                agent=self._useragent
+            )
+        except exceptions.CallsignLookupError:
+            await self._login(
+                username=self._username,
+                password=self._password,
+                agent=self._useragent
+            )
+
+        return self._process_search(
+            query=callsign.upper(),
+            resp=await self._do_query(
+                s=self._session_key,
+                callsign=callsign.upper(),
+                agent=self._useragent
+            )
+        )
+
+    async def _do_query(self, **query) -> bytes:  # type: ignore[override]
+        if self._session is not None:
+            async with self._session.get(self._base_url + urlencode(query)) as resp:
+                if resp.status != 200:
+                    raise exceptions.CallsignLookupError(f"Unable to connect to QRZCQ (HTTP Error {resp.status})")
+                return await resp.read()
+        else:
+            raise exceptions.CallsignLookupError(("Session not initialised. "
+                                                  "Hint: Call `.start_session()` once or use the `new()` classmethod."))
