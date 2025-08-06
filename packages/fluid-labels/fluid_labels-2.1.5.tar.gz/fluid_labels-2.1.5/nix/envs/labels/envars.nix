4d84a447a3b3487555acb3e19693a16fd6335586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:20:41 +0000

    Add package fluid-labels/2.1.5/fluid_labels-2.1.5.tar.gz

---
+++ packages/fluid-labels/fluid_labels-2.1.5.tar.gz/fluid_labels-2.1.5/nix/envs/labels/envars.nix
@@ -0,0 +1,8 @@
+{ pkgs, projectPath }:
+pkgs.writeShellApplication {
+  bashOptions = [ ];
+  name = "labels-envars";
+  text = ''
+    export BUGSNAG_API_KEY="82c9f090e2049a63c44a2045b029a7a8"
+  '';
+}
