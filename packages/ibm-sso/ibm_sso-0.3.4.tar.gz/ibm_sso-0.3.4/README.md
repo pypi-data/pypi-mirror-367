5b68d9fed1a4693ac14b70ad1af176e8b01cd010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:07:08 +0000

    Add package ibm-sso/0.3.4/ibm_sso-0.3.4.tar.gz

---
+++ packages/ibm-sso/ibm_sso-0.3.4.tar.gz/ibm_sso-0.3.4/README.md
@@ -0,0 +1,69 @@
+- [ibm-sso](#ibm-sso)
+  - [Getting Started](#getting-started)
+  - [Usage](#usage)
+  - [Examples](#examples)
+  - [Deploy project(memo for developer)](#deploy-projectmemo-for-developer)
+
+# ibm-sso
+
+When using `SSO Self-Service Provisioner` for single sign-on, `ibm-sso` can make your work easier.
+
+> Currently only supports fastapi applications.
+
+## Getting Started
+
+```bash
+pip install ibm-sso
+```
+
+## Usage
+
+1. Add `ibm-sso` to `requirements.txt`
+
+    ```bash
+    ibm-sso==0.3.4
+    ```
+
+    > For versions lower than `0.3.0`, the `token` is returned directly after successful authentication.
+    > 
+    > Starting from version `0.3.0`, you need to create your own `nonce` on the client side and then pass that `nonce` along with the `redirect_uri` to the server side. `code` and `state` are returned after successful authentication. You need to use the `code`, `state`, `nonce` and `redirect_uri` to obtain token information.
+
+2. Set environment variables
+
+    Refer to the [.env.template](./examples/.env.template) in the examples directory.
+
+3. Import `ibm-sso` in startup file
+
+    Refer to the [main.py](./examples/src/main.py) in the examples directory.
+
+4. Protect your API
+
+    If your API requires authentication to access, you can refer to [example.py](./examples/src/api/v1/example.py)
+
+## Examples
+
+There is a full example in the `examples` directory that can be run directly. You can start from the example to learn how to use `ibm-sso`.
+
+## Deploy project(memo for developer)
+
+1. Deploy project
+
+    ```bash
+    # https://test.pypi.org/
+    expect interactive_deploy_test.expect
+
+    # https://pypi.org/
+    # expect interactive_deploy.expect
+    ```
+
+2. Use `test.pypi.org`
+
+    ```bash
+    pip install -i https://test.pypi.org/simple/ ibm-sso
+    ```
+
+3. How to mark a version as yanked
+
+    ```bash
+    twine yank <package_name> --version <version> --reason "Reason this release was yanked: Yanked due to <reason>"
+    ```
---
+++ packages/ibm-sso/ibm_sso-0.3.4.tar.gz/ibm_sso-0.3.4/src/ibm_sso/vo/UserInfoVO.py
@@ -0,0 +1,87 @@
+# https://shiriev.ru/pydantic-two-way-mapping/
+# https://docs.pydantic.dev/latest/migration/#changes-to-dataclasses
+# https://docs.pydantic.dev/latest/concepts/dataclasses/#dataclass-config
+# https://docs.pydantic.dev/latest/migration/#changes-to-config
+# https://www.youtube.com/watch?app=desktop&v=Z0a0Vjd992I
+
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from ibm_sso.util.const import AVATAR_PREFIX
+
+
+class UserInfoVO(BaseModel):
+    display_name: str = Field(alias='displayName')
+    email_address: str = Field(alias='emailAddress')
+    uid: str
+    avatar: Optional[str] = None
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.avatar = AVATAR_PREFIX + self.uid if self.uid else ''
+
+
+class UserInfoMiddleWareVO(BaseModel):
+    display_name: Optional[str] = Field(None, alias="displayName")
+    email_address: Optional[str] = Field(None, alias="emailAddress")
+    uid: Optional[str] = None
+    avatar: Optional[str] = None
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.avatar = AVATAR_PREFIX + self.uid if self.uid else ""
+
+
+class TokenInfoVO(BaseModel):
+    access_token: str
+    refresh_token: str
+    scope: str
+    grant_id: str
+    id_token: str
+    token_type: str
+    expires_in: int
+    expires_at: int
+
+
+class AuthorizeInfoVO(BaseModel):
+    access_token: str
+    refresh_token: str
+    scope: str
+    grant_id: str
+    id_token: str
+    token_type: str
+    expires_in: int
+    expires_at: int
+    user_info: UserInfoVO = Field(alias='userinfo')
+
+
+if __name__ == '__main__':
+    data = {
+        'displayName': 'displayName',
+        'emailAddress': 'emailAddress',
+        'uid': 'uid',
+    }
+    result = UserInfoVO.model_validate(data)
+    print(result)
+
+    print('========================================================================')
+
+    data = {
+        'access_token': 'access_token',
+        'refresh_token': 'refresh_token',
+        'scope': 'scope',
+        'grant_id': 'grant_id',
+        'id_token': 'id_token',
+        'token_type': 'token_type',
+        'expires_in': 3600,
+        'expires_at': 1609459200,
+        'userinfo': {
+            'displayName': 'displayName',
+            'emailAddress': 'emailAddress',
+            'uid': 'uid',
+        }
+
+    }
+    result = AuthorizeInfoVO.model_validate(data)
+    print(result)
