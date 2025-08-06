2f8a634703e0722706da6949600f640c4e8c8ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:08:58 +0000

    Add package adiff/1.0.1/adiff-1.0.1.tar.gz

---
+++ packages/adiff/adiff-1.0.1.tar.gz/adiff-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adiff',  # 👈 this is the pip install name
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'tabulate',
+        'lxml',
+        'openpyxl'
+    ],
+    entry_points={
+        'console_scripts': [
+            'vdiff=vdiff.cli:main',  # 👈 this makes `vdiff` CLI command
+        ],
+    },
+    author='Ashish',
+    author_email='ashishaxm@gmail.com',
+    description='Compare multiple XML files with Excel output and summary statistics',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ashishnxt/vdiff',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Markup :: XML',
+        'Intended Audience :: Developers',
+    ],
+    python_requires='>=3.6',
+)
+
