6fef6e3f35aae6f7d5db91e667906565f863e4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 08:04:47 +0000

    Add package msgflux/0.1.0a5/msgflux-0.1.0a5-py3-none-any.whl

---
+++ packages/msgflux/msgflux-0.1.0a5-py3-none-any.whl/msgflux/models/providers/openrouter.py
@@ -0,0 +1,26 @@
+from os import getenv
+
+from msgflux.models.providers.openai import OpenAIChatCompletion
+
+
+class _BaseOpenRouter:
+    """Configurations to use OpenRouter models."""
+
+    provider: str = "openrouter"
+
+    def _get_base_url(self):
+        base_url = getenv("OPENROUTER_BASE_URL", "https://openrouter.ai/api/v1")
+        if base_url is None:
+            raise ValueError("Please set `OPENROUTER_BASE_URL`")
+        return base_url
+
+    def _get_api_key(self):
+        """Load API keys from environment variable."""
+        keys = getenv("OPENROUTER_API_KEY")
+        self._api_key = [key.strip() for key in keys.split(",")]
+        if not self._api_key:
+            raise ValueError("No valid API keys found")
+
+
+class OpenRouterChatCompletion(_BaseOpenRouter, OpenAIChatCompletion):
+    """OpenRouter Chat Completion."""
