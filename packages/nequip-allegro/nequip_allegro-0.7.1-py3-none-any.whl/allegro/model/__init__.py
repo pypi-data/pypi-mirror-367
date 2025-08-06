475f436941d310321cd207a6a3738db36be2c6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:03 +0000

    Add package nequip-allegro/0.7.1/nequip_allegro-0.7.1-py3-none-any.whl

---
+++ packages/nequip-allegro/nequip_allegro-0.7.1-py3-none-any.whl/allegro/model/__init__.py
@@ -0,0 +1,4 @@
+# This file is a part of the `allegro` package. Please see LICENSE and README at the root for information on using it.
+from .allegro_models import AllegroModel, AllegroEnergyModel
+
+__all__ = ["AllegroModel", "AllegroEnergyModel"]
