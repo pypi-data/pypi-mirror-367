944d31c19edcb02dbceec1ea62d7b7d1b264a4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:07:02 +0000

    Add package pymultirole-plugins/0.5.9/pymultirole_plugins-0.5.9-py3-none-any.whl

---
+++ packages/pymultirole-plugins/pymultirole_plugins-0.5.9-py3-none-any.whl/pymultirole_plugins/schema.py
@@ -0,0 +1,14 @@
+import json
+from pydantic import BaseModel
+
+
+class FormDataModel(BaseModel):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate_to_json
+
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
