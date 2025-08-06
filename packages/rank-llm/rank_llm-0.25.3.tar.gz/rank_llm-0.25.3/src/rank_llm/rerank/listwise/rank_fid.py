57198324636d5c413e6800d772826640e6e4fcb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 07:08:14 +0000

    Add package rank-llm/0.25.3/rank_llm-0.25.3.tar.gz

---
+++ packages/rank-llm/rank_llm-0.25.3.tar.gz/rank_llm-0.25.3/src/rank_llm/rerank/listwise/rank_fid.py
@@ -0,0 +1,479 @@
+from importlib.resources import files
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+import torch
+from tqdm import tqdm
+from transformers import T5Tokenizer
+
+from rank_llm.data import Request, Result
+from rank_llm.rerank.listwise.listwise_rankllm import ListwiseRankLLM
+from rank_llm.rerank.listwise.lit5.model import FiD, FiDCrossAttentionScore
+from rank_llm.rerank.rankllm import PromptMode
+
+TEMPLATES = files("rank_llm.rerank.prompt_templates")
+
+
+class RankFiDDistill(ListwiseRankLLM):
+    def _post_init(self):
+        self._to_precision(self._precision)
+
+    def _tokenize(self, s: str):
+        return self._tokenizer(s)
+
+    def _to_precision(self, precision: str) -> None:
+        """
+        We don't support python12 for now, after python 12, the code should be changed into
+        """
+        if precision == "float32":
+            self._llm = self._llm.float()
+        elif precision == "bfloat16":
+            self._llm = self._llm.bfloat16()
+        elif precision == "float16":
+            self._llm = self._llm.float16()
+
+    def __init__(
+        self,
+        model: str,
+        context_size: int = 150,
+        prompt_mode: Optional[PromptMode] = None,  # Placeholder for actual mode
+        prompt_template_path: str = (TEMPLATES / "rank_fid_template.yaml"),
+        window_size: int = 20,
+        stride: int = 10,
+        precision: str = "bfloat16",
+        device: str = "cuda",
+    ) -> None:
+        """
+        Creates instance of the RankFiDDistill class, a specialized version of RankLLM designed from Lit5-Distill.
+        """
+        super().__init__(
+            model=model,
+            context_size=context_size,
+            prompt_mode=prompt_mode,
+            prompt_template_path=prompt_template_path,
+            window_size=window_size,
+        )
+        self._precision = precision
+        self._tokenizer = T5Tokenizer.from_pretrained(model)
+        self._llm = FiD.from_pretrained(model).to(device).eval()
+
+        self._device = device
+
+        self._window_size = window_size
+        self._stride = stride
+
+        self._answer_maxlength = len(
+            " > ".join(map(lambda x: f"[{x}]", range(1, window_size + 1)))
+        )
+
+        self._output_token_estimate = None
+
+        self._post_init()
+
+    def _run_llm_by_length_unified(
+        self, batch_prompts: List[List[str]]
+    ) -> List[Tuple[str, int]]:
+        if len(batch_prompts) == 0:
+            return []
+
+        self._llm.eval()
+
+        batch_size = len(batch_prompts)
+        n_passages = len(batch_prompts[0])
+
+        # single batch, unsqueeze
+        inputs = {
+            k: v.reshape(batch_size, -1).to(self._device)
+            for k, v in self._tokenizer(
+                [prompt for prompts in batch_prompts for prompt in prompts],
+                return_tensors="pt",
+                padding="max_length",
+                truncation=True,
+                max_length=self.max_tokens(),
+            ).items()
+        }
+
+        with torch.no_grad():
+            self._llm.reset_n_passages(n_passages=n_passages)
+            outputs = self._llm.generate(
+                **inputs,
+                max_length=self._answer_maxlength,
+                do_sample=False,
+            )
+
+        decoded_outputs = [
+            self._tokenizer.decode(outputs[i], skip_special_tokens=True)
+            for i in range(outputs.shape[0])
+        ]
+
+        # all token size should be equal
+        return [
+            (decoded_output, outputs.shape[1]) for decoded_output in decoded_outputs
+        ]
+
+    def rerank_batch(
+        self,
+        requests: List[Request],
+        rank_start: int = 0,
+        rank_end: int = 100,
+        shuffle_candidates: bool = False,
+        logging: bool = False,
+        **kwargs: Any,
+    ) -> List[Result]:
+        top_k_retrieve: int = kwargs.get("top_k_retrieve", rank_end)
+        rank_end = min(top_k_retrieve, rank_end)
+        window_size: int = kwargs.get("window_size", self._window_size)
+        window_size = min(window_size, top_k_retrieve)
+        stride: int = kwargs.get("stride", self._stride)
+        populate_invocations_history: bool = kwargs.get(
+            "populate_invocations_history", False
+        )
+        batch_size = kwargs.get("batch_size", 1)
+
+        if len(set([len(req.candidates) for req in requests])) != 1:
+            raise ValueError("Batched requests must have the same number of candidates")
+
+        result = []
+
+        with tqdm(range(0, len(requests))) as bar:
+            for i in range(0, len(requests), batch_size):
+                batch = requests[i : min(i + batch_size, len(requests))]
+                batch_result = self.sliding_windows_batched(
+                    batch,
+                    rank_start=max(rank_start, 0),
+                    rank_end=min(
+                        rank_end, len(requests[0].candidates)
+                    ),  # TODO: Fails arbitrary hit sizes
+                    window_size=window_size,
+                    stride=stride,
+                    shuffle_candidates=shuffle_candidates,
+                    logging=logging,
+                    populate_invocations_history=populate_invocations_history,
+                )
+                result.extend(batch_result)
+                bar.update(len(batch))
+
+        return result
+
+    def run_llm_batched(
+        self, prompts: List[List[Dict[str, str]]], **kwargs
+    ) -> List[Tuple[str, int]]:
+        if len(prompts) == 0:
+            return []
+
+        # unfortunately, we are not allowed to use VLLM on T5. However, we could unify the prompts by passage size
+        #   (which is commonly the same) then rerank stuff having same passage sizes
+
+        prompt_infos = [list(map(lambda x: x["text"], prompt)) for prompt in prompts]
+
+        return self._run_llm_by_length_unified(prompt_infos)
+
+    def create_prompt_batched(
+        self, results: List[Result], rank_start: int, rank_end: int, batch_size: int
+    ) -> List[Tuple[List[Dict[str, str]], int]]:
+        return [self.create_prompt(result, rank_start, rank_end) for result in results]
+
+    def run_llm(self, prompts: List[Dict[str, str]], **kwargs) -> Tuple[str, int]:
+        """
+        Run the target language model with a passed in prompt.
+        """
+
+        return self._run_llm_by_length_unified(
+            [list(map(lambda x: x["text"], prompts))]
+        )[0]
+
+    def create_prompt(
+        self, result: Result, rank_start: int, rank_end: int
+    ) -> Tuple[List[Dict[str, str]], int]:
+        """
+        Create a prompt based on the result and given ranking range.
+        """
+        prompts = self._inference_handler.generate_prompt(
+            result=result,
+            rank_start=rank_start,
+            rank_end=rank_end,
+            max_tokens=self.max_tokens(),
+        )
+
+        return prompts, sum(self.get_num_tokens(prompt["text"]) for prompt in prompts)
+
+    def get_num_tokens(self, prompt: Union[str, List[Dict[str, str]]]) -> int:
+        """
+        Abstract method to calculate the number of tokens contained in the given prompt.
+        """
+        if isinstance(prompt, str):
+            return len(self._tokenizer.encode(prompt))
+        elif isinstance(prompt, list):
+            return sum(len(self._tokenizer.encode(item["text"])) for item in prompt)
+        else:
+            raise ValueError(
+                "Prompt must be a string or a list of dictionaries with a 'text' key."
+            )
+
+    def cost_per_1k_token(self, input_token: bool) -> float:
+        return 0
+
+    def num_output_tokens(self, current_window_size: Optional[int] = None) -> int:
+        if current_window_size is None:
+            current_window_size = self._window_size
+        if (
+            self._output_token_estimate is not None
+            and self._window_size == current_window_size
+        ):
+            return self._output_token_estimate
+        else:
+            output_token_estimate = (
+                len(
+                    self._tokenizer.encode(
+                        " > ".join([f"[{i + 1}]" for i in range(current_window_size)])
+                    )
+                )
+                - 1
+            )
+            if (
+                self._output_token_estimate is None
+                and self._window_size == current_window_size
+            ):
+                self._output_token_estimate = output_token_estimate
+
+            return output_token_estimate
+
+
+class RankFiDScore(ListwiseRankLLM):
+    def _post_init(self):
+        # set the overwrite forward cross attention
+        self._llm.overwrite_forward_crossattention()
+        self._to_precision(self._precision)
+
+    def _tokenize(self, s: str):
+        return self._tokenizer(s)
+
+    def _to_precision(self, precision: str) -> None:
+        """
+        We don't support python12 for now, after python 12, the code should be changed into
+        """
+        if precision == "float32":
+            self._llm = self._llm.float()
+        elif precision == "bfloat16":
+            self._llm = self._llm.bfloat16()
+        elif precision == "float16":
+            self._llm = self._llm.float16()
+
+    def __init__(
+        self,
+        model: str,
+        context_size: int = 150,
+        prompt_mode: Optional[PromptMode] = None,  # Placeholder for actual mode
+        prompt_template_path: str = (TEMPLATES / "rank_fid_score_template.yaml"),
+        window_size: int = 20,
+        stride: int = 10,
+        precision: str = "bfloat16",
+        device: str = "cuda",
+    ) -> None:
+        super().__init__(
+            model=model,
+            context_size=context_size,
+            prompt_mode=prompt_mode,
+            prompt_template_path=prompt_template_path,
+            window_size=window_size,
+        )
+        self._precision = precision
+        self._tokenizer = T5Tokenizer.from_pretrained(model)
+        self._llm = FiDCrossAttentionScore.from_pretrained(model).to(device).eval()
+
+        self._device = device
+        self._window_size = window_size
+        self._stride = stride
+
+        self._output_token_estimate = None
+
+        self._post_init()
+
+    def _run_llm_by_length_unified(
+        self, batch_prompts: List[List[Tuple[str, str]]]
+    ) -> List[Tuple[str, int]]:
+        if len(batch_prompts) == 0:
+            return []
+
+        # get arbitrary query (they should be the same)
+        queries = [prompts[0][0] for prompts in batch_prompts]
+        batch_size = len(batch_prompts)
+        n_passages = len(batch_prompts[0])
+
+        inputs = {
+            k: v.reshape(batch_size, -1).to(self._device)
+            for k, v in self._tokenizer(
+                [prompt for prompts in batch_prompts for (_, prompt) in prompts],
+                return_tensors="pt",
+                padding="max_length",
+                truncation=True,
+                max_length=self.max_tokens(),
+            ).items()
+        }
+
+        passage_ids = inputs["input_ids"]
+        passage_mask = inputs["attention_mask"]
+
+        with torch.no_grad():
+            self._llm.reset_score_storage()
+
+            outputs = self._llm.generate(
+                **inputs, max_length=20, do_sample=False, n_passages=n_passages
+            )
+
+        output_sequence_lengths = []
+
+        for output in outputs:
+            output_length = 0
+            for j in range(output.shape[0]):
+                if output[j] == FiDCrossAttentionScore.ANSWER_EOS_TOKEN:
+                    output_length = j
+                    break
+            else:
+                output_length = outputs.shape[1]
+            output_sequence_lengths.append(output_length)
+
+        query_mask_reader = self._tokenizer(
+            queries,
+            max_length=self.max_tokens(),
+            padding="longest",
+            truncation=True,
+            return_tensors="pt",
+            add_special_tokens=False,
+        )["attention_mask"].bool()
+
+        with torch.no_grad():
+            crossattention_scores = self._llm.get_crossattention_scores(
+                n_passages,
+                ids=passage_ids.to(self._device),
+                mask=passage_mask.bool().to(self._device),
+                mask_query=query_mask_reader.to(self._device),
+                output_sequence_lengths=output_sequence_lengths,
+            )
+            # only supports normswoquery for now
+            crossattention_score: torch.Tensor = crossattention_scores["normswoquery"]
+            sorted, idxes = torch.sort(crossattention_score, dim=-1, descending=True)
+            idxes = idxes.detach().cpu()
+
+        return [
+            (
+                " > ".join([f"[{x + 1}]" for x in idxes[i].tolist()]),
+                output_sequence_lengths[i] + crossattention_score.shape[1],
+            )
+            for i in range(idxes.shape[0])
+        ]
+
+    def rerank_batch(
+        self,
+        requests: List[Request],
+        rank_start: int = 0,
+        rank_end: int = 100,
+        shuffle_candidates: bool = False,
+        logging: bool = False,
+        **kwargs: Any,
+    ) -> List[Result]:
+        top_k_retrieve: int = kwargs.get("top_k_retrieve", rank_end)
+        rank_end = min(top_k_retrieve, rank_end)
+        window_size: int = kwargs.get("window_size", self._window_size)
+        window_size = min(window_size, top_k_retrieve)
+        stride: int = kwargs.get("stride", self._stride)
+        populate_invocations_history: bool = kwargs.get(
+            "populate_invocations_history", False
+        )
+        batch_size = kwargs.get("batch_size", 1)
+
+        if len(set([len(req.candidates) for req in requests])) != 1:
+            raise ValueError("Batched requests must have the same number of candidates")
+
+        result = []
+
+        with tqdm(range(0, len(requests))) as bar:
+            for i in range(0, len(requests), batch_size):
+                batch = requests[i : min(i + batch_size, len(requests))]
+                batch_result = self.sliding_windows_batched(
+                    batch,
+                    rank_start=max(rank_start, 0),
+                    rank_end=min(
+                        rank_end, len(requests[0].candidates)
+                    ),  # TODO: Fails arbitrary hit sizes
+                    window_size=window_size,
+                    stride=stride,
+                    shuffle_candidates=shuffle_candidates,
+                    logging=logging,
+                    populate_invocations_history=populate_invocations_history,
+                )
+                result.extend(batch_result)
+                bar.update(len(batch))
+
+        return result
+
+    def run_llm_batched(
+        self, prompts: List[List[Dict[str, str]]], **kwargs
+    ) -> List[Tuple[str, int]]:
+        if len(prompts) == 0:
+            return []
+
+        # unfortunately, we are not allowed to use VLLM on T5. However, we could unify the prompts by passage size
+        #   (which is commonly the same) then rerank stuff having same passage sizes
+
+        processed_prompts = [
+            [(x["query"], x["text"]) for x in prmpt] for prmpt in prompts
+        ]
+
+        return self._run_llm_by_length_unified(processed_prompts)
+
+    def create_prompt_batched(
+        self, results: List[Result], rank_start: int, rank_end: int, batch_size: int
+    ) -> List[Tuple[List[Dict[str, str]], int]]:
+        return [self.create_prompt(result, rank_start, rank_end) for result in results]
+
+    def run_llm(self, prompts: List[Dict[str, str]], **kwargs) -> Tuple[str, int]:
+        # get arbitrary query (they should be the same)
+        return self._run_llm_by_length_unified(
+            [[(x["query"], x["text"]) for x in prompts]]
+        )[0]
+
+    def create_prompt(
+        self, result: Result, rank_start: int, rank_end: int
+    ) -> Tuple[List[Dict[str, str]], int]:
+        """
+        Create a prompt based on the result and given ranking range.
+        """
+        prompts = self._inference_handler.generate_prompt(
+            result=result,
+            rank_start=rank_start,
+            rank_end=rank_end,
+            max_tokens=self.max_tokens(),
+        )
+
+        return prompts, sum(self.get_num_tokens(prompt["text"]) for prompt in prompts)
+
+    def get_num_tokens(self, prompt: str) -> int:
+        return len(self._tokenizer.encode(prompt))
+
+    def cost_per_1k_token(self, input_token: bool) -> float:
+        return 0.0
+
+    def num_output_tokens(self, current_window_size: Optional[int] = None) -> int:
+        if current_window_size is None:
+            current_window_size = self._window_size
+        if (
+            self._output_token_estimate is not None
+            and self._window_size == current_window_size
+        ):
+            return self._output_token_estimate
+        else:
+            output_token_estimate = (
+                len(
+                    self._tokenizer.encode(
+                        " > ".join([f"[{i + 1}]" for i in range(current_window_size)])
+                    )
+                )
+                - 1
+            )
+            if (
+                self._output_token_estimate is None
+                and self._window_size == current_window_size
+            ):
+                self._output_token_estimate = output_token_estimate
+
+            return output_token_estimate
---
+++ packages/rank-llm/rank_llm-0.25.3.tar.gz/rank_llm-0.25.3/src/rank_llm/rerank/pointwise/monot5.py
@@ -0,0 +1,137 @@
+import logging
+import math
+from importlib.resources import files
+from typing import List, Optional, Tuple
+
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers.generation import GenerationConfig
+
+from rank_llm.data import Result
+from rank_llm.rerank.pointwise.pointwise_rankllm import PointwiseRankLLM
+from rank_llm.rerank.rankllm import PromptMode
+
+logger = logging.getLogger(__name__)
+
+TEMPLATES = files("rank_llm.rerank.prompt_templates")
+
+
+class MonoT5(PointwiseRankLLM):
+    def __init__(
+        self,
+        model: str,
+        prompt_mode: Optional[PromptMode] = None,
+        prompt_template_path: str = (TEMPLATES / "monot5_template.yaml"),
+        context_size: int = 512,
+        num_few_shot_examples: int = 0,
+        few_shot_file: Optional[str] = None,
+        device: str = "cuda",
+        batch_size: int = 32,
+    ):
+        super().__init__(
+            model=model,
+            context_size=context_size,
+            prompt_mode=prompt_mode,
+            prompt_template_path=prompt_template_path,
+            num_few_shot_examples=num_few_shot_examples,
+            few_shot_file=few_shot_file,
+            device=device,
+            batch_size=batch_size,
+        )
+
+        self._tokenizer = T5Tokenizer.from_pretrained(model)
+        self._llm = T5ForConditionalGeneration.from_pretrained(model).to(self._device)
+        self._context_size = context_size
+
+        self._true_id = self._tokenizer.encode("true", add_special_tokens=False)[0]
+        self._false_id = self._tokenizer.encode("false", add_special_tokens=False)[0]
+
+    def run_llm_batched(
+        self,
+        prompts: List[str],
+    ) -> Tuple[List[str], List[int], List[float]]:
+        gen_cfg = GenerationConfig.from_model_config(self._llm.config)
+        gen_cfg.max_new_tokens = self.num_output_tokens()
+        gen_cfg.min_new_tokens = self.num_output_tokens()
+        gen_cfg.output_scores = True
+        gen_cfg.return_dict_in_generate = True
+        gen_cfg.do_sample = False
+        all_outputs, all_output_token_counts, all_scores = [], [], []
+
+        token_prompts = self._tokenizer(
+            prompts, padding=True, truncation=True, return_tensors="pt"
+        ).to(self._device)
+
+        token_prompts = token_prompts["input_ids"]
+
+        batch_outputs = self._llm.generate(token_prompts, generation_config=gen_cfg)
+
+        batch_output_ids = batch_outputs.sequences
+        batch_logits = batch_outputs.scores
+
+        batch_outputs = [
+            self._tokenizer.decode(
+                single_token_sequence,
+                skip_special_tokens=True,
+                spaces_between_special_tokens=False,
+            )
+            for single_token_sequence in batch_output_ids
+        ]
+
+        for logit_tensor in batch_logits[0]:
+            truth_logit = logit_tensor[self._true_id]
+            false_logit = logit_tensor[self._false_id]
+            score = math.exp(truth_logit) / (
+                math.exp(truth_logit) + math.exp(false_logit)
+            )
+            all_scores.append(score)
+            all_output_token_counts.append(self.num_output_tokens())
+
+        all_outputs.extend(batch_outputs)
+
+        return all_outputs, all_output_token_counts, all_scores
+
+    def run_llm(self, prompt: str) -> Tuple[str, int, float]:
+        ret = self.run_llm_batched([prompt])
+        return ret[0][0], ret[1][0], ret[2][0]
+
+    def create_prompt(self, result: Result, index: int) -> Tuple[str, int]:
+        query = result.query.text
+
+        reserved_for_output = (
+            64  # might need to change depending on what the actual output look like
+        )
+        query_tokens = self.get_num_tokens(f"Query: {query} Document:  Relevant: ")
+
+        few_shot_section = self._inference_handler._generate_fewshot_prompt(
+            num_examples=self._num_few_shot_examples, examples=self._examples
+        )
+        few_shot_tokens = self.get_num_tokens(few_shot_section)
+
+        max_doc_tokens = (
+            self._context_size - few_shot_tokens - query_tokens - reserved_for_output
+        )
+
+        prompt = self._inference_handler.generate_prompt(
+            result=result,
+            index=index,
+            max_doc_tokens=max_doc_tokens,
+            tokenizer=self._tokenizer,
+            num_few_shot_examples=self._num_few_shot_examples,
+            fewshot_exampels=self._examples,
+        )
+
+        final_token_count = self.get_num_tokens(prompt)
+        assert (
+            final_token_count <= self._context_size - reserved_for_output
+        ), f"Prompt overflow: {final_token_count} > {self._context_size - reserved_for_output}"
+
+        return prompt, final_token_count
+
+    def get_num_tokens(self, prompt: str) -> int:
+        return len(self._tokenizer.encode(prompt))
+
+    def num_output_tokens(self) -> int:
+        return 1
+
+    def cost_per_1k_token(self, input_token: bool) -> float:
+        return 0
