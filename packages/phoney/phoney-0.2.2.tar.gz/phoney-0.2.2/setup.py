3106fd6b5aab82e3888fae7b9458adb98665bb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 05:11:41 +0000

    Add package phoney/0.2.2/phoney-0.2.2.tar.gz

---
+++ packages/phoney/phoney-0.2.2.tar.gz/phoney-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="phoney",
+    version="0.2.2",
+    description="Fake Info generator, a library for generating realistic personal data like names, phone numbers, emails, and more.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="rarfile",
+    author_email="d7276250@email.com",
+    url="https://github.com/YTstyo/phoney",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.7",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
