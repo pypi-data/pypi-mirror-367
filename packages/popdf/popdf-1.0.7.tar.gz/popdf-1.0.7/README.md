b70ec56877a89bcbaca113bf1843a394a04b197a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:13 +0000

    Add package popdf/1.0.7/popdf-1.0.7.tar.gz

---
+++ packages/popdf/popdf-1.0.7.tar.gz/popdf-1.0.7/README.md
@@ -0,0 +1,123 @@
+<p align="center">
+	👉 <a target="_blank" href="https://www.python-office.com/">项目官网：https://www.python-office.com/</a> 👈
+</p>
+<p align="center">
+	👉 <a target="_blank" href="http://www.python4office.cn/wechat-group/">本开源项目的交流群</a> 👈
+</p>
+
+
+<p align="center">
+  <a target="_blank" href='https://github.com/CoderWanFeng/popdf'>
+    <img src="https://img.shields.io/github/stars/CoderWanFeng/popdf.svg?style=social" alt="github star"/>
+    </a>
+        <a target="_blank" href='https://gitcode.com/python4office/popdf'>
+		<img src='https://gitcode.com/python4office/popdf/star/badge.svg?theme=dark' alt='gitcode star'/>
+	</a>
+ <a target="_blank" href='https://github.com/CoderWanFeng/popdf'>
+<img src="https://static.pepy.tech/badge/popdf" alt="PyPI Downloads">
+</a>
+</p>
+
+
+
+-------------------------------------------------------------------------------
+
+## 📚简介
+
+popdf 是python自动化办公之**PDF**操作的第三方库。
+
+
+-------------------------------------------------------------------------------
+
+## 📦安装
+
+### 🍊pip 自动下载&更新
+
+#### 源码安装
+
+```
+git clone https://gitcode.com/python4office/popdf.git
+
+cd popdf
+
+pip install -e .
+```
+
+#### 源安装&更新
+
+我使用的是阿里云镜像。如果你的网络环境无法访问阿里云镜像，请自行更换为其他镜像。
+
+```
+pip install -i https://mirrors.aliyun.com/pypi/simple/ popdf -U
+```
+
+-------------------------------------------------------------------------------
+
+## 📝功能
+
+已有功能的说明如下：
+
+| 序号 | 功能         | 视频                                                   | 文档                                                                                             |
+|----|------------|------------------------------------------------------|------------------------------------------------------------------------------------------------|
+| 1  | 下载和安装      | 💻 [播放](https://www.bilibili.com/video/BV1BS9UYGEW7) | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/0-install)             |
+| 2  | PDF 转 Word | 💻 [播放](https://www.bilibili.com/video/BV1pB9UYSEoG) | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/1-pdf2docx/)           |
+| 3  | PDF 转 图片   | 💻[播放](https://www.bilibili.com/video/BV19WRVYKEEY)  | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/2-pdf2imgs/)           |
+| 4  | TXT转PDF    | [播放](https://www.bilibili.com/video/BV1aCQ5YhEBm)    | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/3-txt2pdf/)            |
+| 5  | 按页切割PDF    | 💻[播放](https://www.bilibili.com/video/BV1PYQpY3E8z)  | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/4-split4pdf/)          |
+| 6  | PDF加密      | [播放](https://www.bilibili.com/video/BV1n1QPYJE61)    | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/5-encrypt4pdf/)        |
+| 7  | PDF解密      | [播放](https://www.bilibili.com/video/BV11FQ6YdEU1)    | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/6-decrypt4pdf/)        |
+| 8  | PDF加水印     | [播放](https://www.bilibili.com/video/BV1x7QtYdEJt)    | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/7-add_text_watermark/) |
+| 9  | 合并PDF      | [播放](https://www.bilibili.com/video/BV1NNQhYaEVG)    | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/8-merge2pdf/)          |
+| 10 | 删除PDF      | [播放](https://www.bilibili.com/video/BV1KPQhYAENX)    | [查看](http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/9-del4pdf/)            |
+
+## 🏗️添砖加瓦
+
+本项目欢迎任何人参与开发，如果是小白，可以看一下开发教程👉[如何参与开源项目？0基础入门：怎么打开GitHub？什么是issue？什么是PR？](https://www.bilibili.com/video/BV1EP411d7Np/?spm_id_from=333.999.0.0)
+
+#### 目录结构和pr要求如下：
+
+- docs：存放该项目的文档，包括教程、案例等。
+- examples：存放该项目的案例，**如果你有新增功能或者接口，请在这里新建一个py文件，并在这里写一个使用案例。**
+- popdf：存放该项目的源代码，其中：
+    - api：提供外部调用的接口。
+    - core：存放该项目的核心代码，包括类、函数等。
+    - lib：存放工具类、工具函数等。
+- tests：存放该项目的单元测试的代码，其中：
+    - test_code：存放测试代码，**每新增/修改任一个函数，提交之前必须写单元测试**！
+    - test_files：存放测试文件，包括图片、Excel等。
+- README.md、requirement.txt、setup等文件，一般不修改。
+
+### 🐞提供bug反馈或建议
+
+提交问题反馈时，请务必填写和该项目本身有关的问题，不进行有关python学习，甚至是个人练习的知识答疑和讨论。
+
+- [Github issue](https://github.com/CoderWanFeng/popdf/issues)
+- [gitee issue](https://gitee.com/CoderWanFeng/popdf/issues)
+- [GitCode issue](https://gitcode.com/python4office/popdf/issues)
+
+### 🧬贡献代码的步骤
+
+1. 在Gitee/Github/GitCode上fork项目到自己的repo
+2. 把fork过去的项目也就是你的项目clone到你的本地
+3. 修改代码
+4. commit后push到自己的库
+5. 登录Gitee/Github/GitCode在你首页可以看到一个 pull request 按钮，点击它，填写一些说明信息，然后提交到master/main分支即可。
+6. 等待程序员晚枫合并，＋微信说一声更好👉[python-office](http://python4office.cn/wechat-qrcode/)
+
+-------------------------------------------------------------------------------
+
+## 📕参考文档
+
+- [PyMuPDF](https://pymupdf.readthedocs.io/en/latest/index.html)
+- [click](https://click.palletsprojects.com/en/stable/options/)
+- [python-office](https://www.python-office.com/)
+- [pdf2docx](https://github.com/ArtifexSoftware/pdf2docx)
+
+## 📌联系作者
+
+<p align="center" id='开源交流群-banner'>
+<a target="_blank" href='https://cos.python-office.com/group%2Ffree-group.jpg'>
+<img src="https://cos.python-office.com/group/python-office-qr.jpg" width="100%"/>
+</a> 
+</p>
+
---
+++ packages/popdf/popdf-1.0.7.tar.gz/popdf-1.0.7/tests/test_code/test_pdf.py
@@ -0,0 +1,241 @@
+import os
+import unittest
+
+from popdf.api.pdf import *
+from popdf.api.pdf import split4pdf
+
+
+class TestPDF(unittest.TestCase):
+
+    def test_pdf2docx(self):
+        """
+        version 1.0.1
+        """
+        base_dir = os.path.dirname(__file__)
+        input_file = os.path.join(base_dir, '..', 'test_files', 'pdf2docx', '程序员晚枫.pdf')
+        output_path = os.path.join(base_dir, '..', 'test_files', 'pdf2docx', 'docx', '1.docx')
+        pdf2docx(
+            input_file=input_file,
+            output_path=output_path
+        )
+
+    def test_pdf2docx_single(self):
+        """
+        version 1.0.2
+        """
+        base_dir = os.path.dirname(__file__)
+        input_file = os.path.join(base_dir, '..', 'test_files', 'pdf2docx', '程序员晚枫.pdf')
+        output_file = os.path.join(base_dir, '..', 'test_files', 'pdf2docx', 'docx', '1.docx')
+        pdf2docx(
+            input_file=input_file,
+            output_file=output_file
+        )
+
+    def test_pdf2docx_batch(self):
+        """
+        version 1.0.2
+        """
+        pdf2docx(
+            input_path=r'../test_files/pdf2docx/',
+            output_path=r'../test_files/pdf2docx/docx/'
+        )
+
+    def test_pdf2imgs(self):
+        pdf2imgs(
+            # ~ input_file=r'../test_files/pdf/程序员晚枫.pdf',
+            input_file=r'../test_files/pdf2imgs/程序员晚枫.pdf',
+            output_file='../test_files/pdf2imgs/imgs')
+
+    def test_pdf2imgs_merge(self):
+        pdf2imgs(
+            input_file=r'../test_files/pdf2imgs/程序员晚枫.pdf',
+            output_file='../test_files/pdf2imgs/imgs/1.jpg', merge=True)
+
+    def test_batch_pdf2imgs(self):
+        pdf2imgs(
+            # ~ input_file=r'../test_files/pdf/程序员晚枫.pdf',
+            input_path=r'../test_files/pdf2imgs',
+            output_path='../test_files/pdf2imgs/imgs')
+
+    def test_batch_pdf2imgs_merge(self):
+        pdf2imgs(
+            # ~ input_file=r'../test_files/pdf/程序员晚枫.pdf',
+            input_path=r'../test_files/pdf2imgs',
+            output_path='../test_files/pdf2imgs/imgs', merge=True)
+
+    def test_txt2pdf(self):
+        # 准备测试数据
+        input_file = "../test_files/txt2pdf/程序员晚枫.txt"
+        output_file = "../test_files/txt2pdf/程序员晚枫.pdf"
+        # 调用被测方法
+        txt2pdf(input_file=input_file, output_file=output_file)
+
+    def test_batch_txt2pdf(self):
+        # 准备测试数据
+        input_path = "../test_files/txt2pdf/batch"
+        output_path = "../test_files/txt2pdf/batch_res"
+        # 调用被测方法
+        txt2pdf(input_path=input_path, output_path=output_path)
+
+    def test_split4pdf(self):
+        split4pdf(
+            input_file=r'../test_files/split4pdf/merge2pdf.pdf',
+            from_page=2,
+            to_page=3,
+            output_file=r'../test_files/split4pdf/split4pdf.pdf'
+        )
+
+    def test_batch_split4pdf(self):
+        split4pdf(
+            input_path=r'../test_files/split4pdf',
+            from_page=2,
+            to_page=3,
+            output_path=r'../test_files/split4pdf/out'
+        )
+
+    def test_single_encrypt4pdf(self):
+        encrypt4pdf(
+            input_file=r'../test_files/pdf/程序员晚枫.pdf',
+            password='123456',
+            output_file=r'../test_files/pdf/encrypt4pdf.pdf'
+        )
+
+    def test_batch_encrypt4pdf(self):
+        encrypt4pdf(
+            input_path=r'../test_files/pdf',
+            output_path=r'../test_files/encrypt4pdf/pdf',
+            password='123456'
+        )
+
+    def test_decrypt4pdf(self):
+        decrypt4pdf(
+            input_file=r'./test_files/pdf/encrypt4pdf.pdf',
+            password='123456',
+            output_file=r'./test_files/pdf/decrypt4pdf.pdf'
+        )
+
+    # 兼容1.0.1版本
+    def test_single_decrypt4pdf(self):
+        decrypt4pdf(
+            input_file=r'./test_files/decrypt4pdf/out.pdf',
+            password='123456',
+            output_file=r'./test_files/decrypt4pdf/target/single.pdf'
+        )
+
+    # 批量解密
+    def test_decrypt4pdf2(self):
+        decrypt4pdf(
+            input_path=r'./test_files/decrypt4pdf',
+            password='123456',
+            output_path=r'./test_files/decrypt4pdf/target'
+        )
+
+    # 参数异常
+    def test_decrypt4pdf3(self):
+        decrypt4pdf(
+            input_path=None,
+            password='123456',
+            output_path=r'./test_files/decrypt4pdf/target'
+        )
+
+    # 参数异常
+    def test_decrypt4pdf4(self):
+        decrypt4pdf(
+            input_path=None,
+            password='123456',
+            output_path=None
+        )
+
+    def test_add_text_watermark(self):
+        add_text_watermark(input_file=r'../test_files/pdf/程序员晚枫.pdf', point=(50, 50),
+                           output_file=r'../test_files/pdf/add_text_watermark.pdf')
+
+    def test_merge2pdf(self):
+        merge2pdf(
+            input_file_list=[r'../test_files/merge/程序员晚枫.pdf', r'../test_files/merge/程序员晚枫（作品合集）.pdf'],
+            output_file=r'../test_files/merge/merge2pdf.pdf'
+        )
+
+    def test_del4pdf(self):
+        del_pdf = r'../test_files/del4pdf/程序员晚枫.pdf'
+        del4pdf(
+            input_file=del_pdf,
+            page_nums=[3],
+            output_file=r'../test_files/del4pdf/a//del4pdf.pdf'
+        )
+
+    def test_del4pdf_batch(self):
+        del_pdf = r'../test_files/del4pdf/'
+        del4pdf(
+            input_path=del_pdf,
+            page_nums=[2],
+            output_path=r'../test_files/del4pdf/b'
+        )
+
+    ##############  以下方法未测试  #################
+    def test_add_img_water(self):
+        add_img_water(pdf_file_in='./test_files/pdf/add_img.pdf', pdf_file_mark='../test_files/pdf/程序员晚枫.pdf',
+                      pdf_file_out='add_img_res.pdf')
+
+    def test_add_watermark_by_parameters(self):
+        add_watermark_by_parameters(
+            pdf_file=r'/tests/test_files/pdf/程序员晚枫.pdf',
+            mark_str='python-office',
+            output_path=None,
+            output_file_name=None)
+
+    def test_add_watermark(self):
+        stub_stdin(self, './test_files/pdf/程序员晚枫.pdf\npython-office\n')  # 依次输入
+        add_watermark()
+
+    # def test_del4pdf(self):
+    #     del4pdf(input_file="../test_files/del4pdf/程序员晚枫的粉丝福利.pdf",
+    #             page_nums=[1, 3],
+    #             output_file="tests/test_files/del4pdf/output_text/")
+
+    def test_encrypt4pdf2(self):
+        encrypt4pdf(
+            password='123456',
+            output_file="../test_files/encrypt4pdf/pc/out.pdf",
+            input_path=r'../test_files/encrypt4pdf/pc'
+        )
+
+
+# 当前脚本所在目录
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+class TestOCR(unittest.TestCase):
+    """
+    pdf.py测试用的代码
+    """
+
+    def test_split4pdf(self):
+        input_file = os.path.abspath(os.path.join(base_dir, '..', '..', 'tests', 'test_files', 'pdf', '程序员晚枫.pdf'))
+        output_file = os.path.abspath(os.path.join(base_dir, '..', '..', 'tests', 'test_files', 'pdf', 'split4pdf.pdf'))
+
+        r = split4pdf(
+            input_file=input_file,
+            output_file=output_file,
+            from_page=1,
+            to_page=1,
+        )
+        logger.info(r)
+
+        # 添加断言
+        self.assertTrue(r)
+
+    def test_split4pdfs(self):
+        input_path = os.path.abspath(os.path.join(base_dir, '..', '..', 'tests', 'test_files', 'pdf'))
+        output_path = os.path.abspath(os.path.join(base_dir, '..', '..', 'tests', 'test_files', 'pdf'))
+
+        r = split4pdf(
+            input_path=input_path,
+            output_path=output_path,
+            from_page=1,
+            to_page=1,
+        )
+        logger.info(r)
+
+        # 添加断言
+        self.assertTrue(r)
