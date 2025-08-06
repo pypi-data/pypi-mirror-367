cd0f7ec83762d17727778796e7ece05026f316c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 06:06:22 +0000

    Add package pully/0.5.1/pully-0.5.1.tar.gz

---
+++ packages/pully/pully-0.5.1.tar.gz/pully-0.5.1/pully/main.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2025 UL Research Institutes
+# SPDX-License-Identifier: Apache-2.0
+
+
+from .cli import parser
+
+
+def main():
+    args = parser.parse_args()
+    return args.func(args)
