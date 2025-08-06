f68ba4f9d45e3ddafac445902cc46a20b0335f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 07:08:10 +0000

    Add package entities-python/0.1.0a15/entities_python-0.1.0a15.tar.gz

---
+++ packages/entities-python/entities_python-0.1.0a15.tar.gz/entities_python-0.1.0a15/src/entities_python/types/toolbox/tool.py
@@ -0,0 +1,23 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from ..._models import BaseModel
+
+__all__ = ["Tool"]
+
+
+class Tool(BaseModel):
+    id: str
+
+    description: str
+
+    name: str
+
+    organization: str
+
+    url: str
+
+    adapter: Optional[str] = None
+
+    parameters: Optional[object] = None
