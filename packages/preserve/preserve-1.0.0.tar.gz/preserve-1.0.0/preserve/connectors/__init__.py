4ac845bdb0e8527cd17a75f1b3524ed356ccb0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:08:46 +0000

    Add package preserve/1.0.0/preserve-1.0.0.tar.gz

---
+++ packages/preserve/preserve-1.0.0.tar.gz/preserve-1.0.0/preserve/connectors/__init__.py
@@ -0,0 +1,5 @@
+from preserve.connectors.memory import Memory
+from preserve.connectors.mongo import Mongo  # , MultiMongo
+from preserve.connectors.shelf import Shelf  # , MultiShelf
+
+__all__ = ["Memory", "Mongo", "Shelf"]
