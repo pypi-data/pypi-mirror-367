2d4df470324ae91fbf7472fdac33180bfa0d9318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 08:04:53 +0000

    Add package rcpchgrowth/4.3.8/rcpchgrowth-4.3.8-py3-none-any.whl

---
+++ packages/rcpchgrowth/rcpchgrowth-4.3.8-py3-none-any.whl/rcpchgrowth/__init__.py
@@ -0,0 +1,16 @@
+from .age_advice_strings import comment_prematurity_correction
+from .bmi_functions import bmi_from_height_weight, weight_for_bmi_height
+from .cdc import select_reference_data_for_cdc_chart
+from .centile_bands import centile_band_for_centile
+from .chart_functions import create_chart
+from .constants import *
+from .date_calculations import chronological_decimal_age, corrected_decimal_age, chronological_calendar_age, estimated_date_delivery, corrected_gestational_age
+from .dynamic_growth import create_thrive_line, return_correlation, create_thrive_lines
+from .global_functions import centile, sds_for_measurement, measurement_from_sds, percentage_median_bmi, measurement_for_z, cubic_interpolation, linear_interpolation
+from .fictional_child import generate_fictional_child_data
+from .measurement import Measurement
+from .mid_parental_height import mid_parental_height, mid_parental_height_z, expected_height_z_from_mid_parental_height_z, lower_and_upper_limits_of_expected_height_z
+from .trisomy_21 import select_reference_data_for_trisomy_21
+from .trisomy_21_aap import select_reference_data_for_trisomy_21_aap
+from .turner import select_reference_data_for_turner
+from .uk_who import select_reference_data_for_uk_who_chart
---
+++ packages/rcpchgrowth/rcpchgrowth-4.3.8-py3-none-any.whl/rcpchgrowth/constants/__init__.py
@@ -0,0 +1,5 @@
+from .validation_constants import *
+from .age_constants import *
+from .reference_constants import *
+from .height_predictions_constants import *
+from .bone_age_constants import *
