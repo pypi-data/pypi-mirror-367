97334f03ab83e90537343e1c4f96b1d134244f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:04 +0000

    Add package funpayparsers/0.4.2/funpayparsers-0.4.2-py3-none-any.whl

---
+++ packages/funpayparsers/funpayparsers-0.4.2-py3-none-any.whl/funpayparsers/types/pages/transactions_page.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+
+__all__ = ('TransactionsPage',)
+
+from dataclasses import dataclass
+
+from funpayparsers.types.common import MoneyValue
+from funpayparsers.types.finances import TransactionPreviewsBatch
+from funpayparsers.types.pages.base import FunPayPage
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from funpayparsers.parsers.page_parsers.transactions_page_parser import TransactionsPageParsingOptions
+
+
+@dataclass
+class TransactionsPage(FunPayPage):
+    """Represents the transactions page (https://funpay.com/account/balance)."""
+
+    rub_balance: MoneyValue | None
+    """RUB balance."""
+
+    usd_balance: MoneyValue | None
+    """USD balance."""
+
+    eur_balance: MoneyValue | None
+    """EUR balance."""
+
+    transactions: TransactionPreviewsBatch | None
+    """Transaction previews."""
+
+    @classmethod
+    def from_raw_source(
+            cls,
+            raw_source: str,
+            options: TransactionsPageParsingOptions | None = None
+    ) -> TransactionsPage:
+        from funpayparsers.parsers.page_parsers.transactions_page_parser import TransactionsPageParser, TransactionsPageParsingOptions
+
+        options = options or TransactionsPageParsingOptions()
+        return TransactionsPageParser(raw_source=raw_source, options=options).parse()
