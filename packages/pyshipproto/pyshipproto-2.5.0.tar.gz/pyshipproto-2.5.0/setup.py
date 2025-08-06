d5163478fc95640cd879ab62b5e896a29fff53a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:08:50 +0000

    Add package pyshipproto/2.5.0/pyshipproto-2.5.0.tar.gz

---
+++ packages/pyshipproto/pyshipproto-2.5.0.tar.gz/pyshipproto-2.5.0/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+
+VERSION = '2.5.0' 
+DESCRIPTION = 'PyShipProto Package'
+LONG_DESCRIPTION = 'Pyshipproto package with generated gRPC services.'
+
+# Setting up
+setup(
+    name="pyshipproto",
+    version=VERSION,
+    author="PyShipProto",
+    author_email="<pyshipproto@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/shipthisco/gRPC-protobuf",
+    install_requires=[],
+    keywords=['python', 'gRPC', 'protobuf', 'pyshipproto'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
