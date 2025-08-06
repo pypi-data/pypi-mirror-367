dd5463a533414022aa0c45119df2b1467917b08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 05:12:51 +0000

    Add package manage-django-project/0.9.3/manage_django_project-0.9.3-py3-none-any.whl

---
+++ packages/manage-django-project/manage_django_project-0.9.3-py3-none-any.whl/manage_django_project_example/settings/tests.py
@@ -0,0 +1,16 @@
+# flake8: noqa: E405
+
+"""
+    Django settings for running tests
+"""
+
+from .base import *  # noqa
+
+
+ALLOWED_HOSTS = ['testserver']
+
+
+# _____________________________________________________________________________
+# Manage Django Project
+
+INSTALLED_APPS.append('manage_django_project.apps.ManageDjangoProjectConfig')
