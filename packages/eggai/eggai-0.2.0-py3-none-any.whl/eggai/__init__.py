b085061518ae6dd102a602f539a880e3b79b8c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:21:25 +0000

    Add package eggai/0.2.0/eggai-0.2.0-py3-none-any.whl

---
+++ packages/eggai/eggai-0.2.0-py3-none-any.whl/eggai/__init__.py
@@ -0,0 +1,9 @@
+from .agent import Agent as Agent
+from .channel import Channel as Channel
+from .hooks import (
+    eggai_cleanup as eggai_cleanup,
+    eggai_register_stop as eggai_register_stop,
+    eggai_main as eggai_main,
+    EggaiRunner as EggaiRunner,
+)
+from .adapters.a2a.config import A2AConfig as A2AConfig
