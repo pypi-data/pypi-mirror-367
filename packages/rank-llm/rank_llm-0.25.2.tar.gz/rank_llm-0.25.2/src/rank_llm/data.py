873bae4bc149bbd87aa9f9e95b7ce0f4fdfce9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 07:08:48 +0000

    Add package rank-llm/0.25.2/rank_llm-0.25.2.tar.gz

---
+++ packages/rank-llm/rank_llm-0.25.2.tar.gz/rank_llm-0.25.2/src/rank_llm/data.py
@@ -0,0 +1,122 @@
+import json
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Set, Union
+
+from dacite import from_dict
+
+
+@dataclass
+class Query:
+    text: str
+    qid: Union[str | int]
+
+
+@dataclass
+class Candidate:
+    docid: Union[str | int]
+    score: float
+    doc: Dict[str, Any]
+
+
+@dataclass
+class Request:
+    query: Query
+    candidates: List[Candidate] = field(default_factory=list)
+
+
+@dataclass
+class InferenceInvocation:
+    prompt: Any
+    response: str
+    input_token_count: int
+    output_token_count: int
+    output_validation_regex: Optional[str] = None
+    output_extraction_regex: Optional[str] = None
+
+
+@dataclass
+class Result:
+    query: Query
+    candidates: list[Candidate] = field(default_factory=list)
+    invocations_history: list[InferenceInvocation] = (field(default_factory=list),)
+
+
+@dataclass
+class TemplateSectionConfig:
+    required: bool
+    required_placeholders: Set[str]
+    allowed_placeholders: Set[str]
+
+
+def read_requests_from_file(file_path: str) -> List[Request]:
+    extension = file_path.split(".")[-1]
+    if extension == "jsonl":
+        requests = []
+        with open(file_path, "r") as f:
+            for l in f:
+                if not l.strip():
+                    continue
+                requests.append(from_dict(data_class=Request, data=json.loads(l)))
+        return requests
+    elif extension == "json":
+        with open(file_path, "r") as f:
+            request_dicts = json.load(f)
+        return [
+            from_dict(data_class=Request, data=request_dict)
+            for request_dict in request_dicts
+        ]
+    else:
+        raise ValueError(f"Expected json or jsonl file format, got {extension}")
+
+
+class DataWriter:
+    def __init__(
+        self,
+        data: Union[Request | Result | List[Result] | List[Request]],
+        append: bool = False,
+    ):
+        if isinstance(data, list):
+            self._data = data
+        else:
+            self._data = [data]
+        self._append = append
+
+    def write_inference_invocations_history(self, filename: str):
+        aggregated_history = []
+        for d in self._data:
+            values = []
+            for info in d.invocations_history:
+                values.append(info.__dict__)
+            aggregated_history.append(
+                {"query": d.query.__dict__, "invocations_history": values}
+            )
+        with open(filename, "a" if self._append else "w") as f:
+            output = json.dumps(aggregated_history, indent=2, ensure_ascii=False)
+            f.write(output)
+
+    def write_in_json_format(self, filename: str):
+        results = []
+        for d in self._data:
+            candidates = [candidate.__dict__ for candidate in d.candidates]
+            results.append({"query": d.query.__dict__, "candidates": candidates})
+        with open(filename, "a" if self._append else "w") as f:
+            output = json.dumps(results, indent=2, ensure_ascii=False)
+            f.write(output)
+
+    def write_in_jsonl_format(self, filename: str):
+        with open(filename, "a" if self._append else "w") as f:
+            for d in self._data:
+                candidates = [candidate.__dict__ for candidate in d.candidates]
+                output = json.dumps(
+                    {"query": d.query.__dict__, "candidates": candidates},
+                    ensure_ascii=False,
+                )
+                f.write(output)
+                f.write("\n")
+
+    def write_in_trec_eval_format(self, filename: str):
+        with open(filename, "a" if self._append else "w") as f:
+            for d in self._data:
+                qid = d.query.qid
+                for rank, cand in enumerate(d.candidates, start=1):
+                    f.write(f"{qid} Q0 {cand.docid} {rank} {cand.score} rank_llm\n")
---
+++ packages/rank-llm/rank_llm-0.25.2.tar.gz/rank_llm-0.25.2/src/rank_llm/retrieve_and_rerank.py
@@ -0,0 +1,215 @@
+import copy
+from typing import Any, Dict, List, Optional, Union
+
+from rank_llm.data import Query, Request
+from rank_llm.rerank import IdentityReranker, RankLLM, Reranker
+from rank_llm.rerank.reranker import extract_kwargs
+from rank_llm.retrieve import (
+    TOPICS,
+    RetrievalMethod,
+    RetrievalMode,
+    Retriever,
+    ServiceRetriever,
+)
+
+
+def retrieve_and_rerank(
+    model_path: str,
+    query: str,
+    dataset: Union[str, List[str], List[Dict[str, Any]]],
+    retrieval_mode: RetrievalMode = RetrievalMode.DATASET,
+    retrieval_method: RetrievalMethod = RetrievalMethod.BM25,
+    top_k_retrieve: int = 50,
+    top_k_rerank: int = 10,
+    max_queries: Optional[int] = None,
+    shuffle_candidates: bool = False,
+    print_prompts_responses: bool = False,
+    qid: int = 1,
+    num_passes: int = 1,
+    interactive: bool = False,
+    default_model_coordinator: RankLLM = None,
+    **kwargs: Any,
+):
+    """Retrieve candidates using Anserini API and rerank them
+
+    Returns:
+        - List of top_k_rerank candidates
+    """
+
+    # Get reranking model_coordinator
+    reranker = Reranker(
+        Reranker.create_model_coordinator(
+            model_path,
+            default_model_coordinator,
+            interactive,
+            **kwargs,
+        )
+    )
+
+    # Retrieve initial candidates
+    print(f"Retrieving top {top_k_retrieve} passages...")
+    requests = retrieve(
+        top_k_retrieve,
+        interactive,
+        retrieval_mode,
+        retrieval_method,
+        query,
+        qid,
+        dataset=dataset,
+        **kwargs,
+    )
+
+    if max_queries is not None:
+        requests = requests[: min(len(requests), max_queries)]
+
+    for request in requests:
+        request.candidates = request.candidates[:top_k_retrieve]
+
+    # Reranking stages
+    print(f"Reranking and returning {top_k_rerank} passages with {model_path}...")
+    if reranker.get_model_coordinator() is None:
+        # No reranker. IdentityReranker leaves retrieve candidate results as is or randomizes the order.
+        shuffle_candidates = True if model_path == "rank_random" else False
+        rerank_results = IdentityReranker().rerank_batch(
+            requests,
+            rank_end=top_k_retrieve,
+            shuffle_candidates=shuffle_candidates,
+        )
+    else:
+        # Reranker is of type RankLLM
+        for pass_ct in range(num_passes):
+            print(f"Pass {pass_ct + 1} of {num_passes}:")
+
+            rerank_results = reranker.rerank_batch(
+                requests,
+                rank_end=top_k_retrieve,
+                rank_start=0,
+                shuffle_candidates=shuffle_candidates,
+                logging=print_prompts_responses,
+                top_k_retrieve=top_k_retrieve,
+                **kwargs,
+            )
+
+            if num_passes > 1:
+                requests = [
+                    Request(copy.deepcopy(r.query), copy.deepcopy(r.candidates))
+                    for r in rerank_results
+                ]
+
+    for rr in rerank_results:
+        rr.candidates = rr.candidates[:top_k_rerank]
+
+    # generate trec_eval file & evaluate for named datasets only
+    if isinstance(dataset, str) and reranker.get_model_coordinator() is not None:
+        file_name = reranker.write_rerank_results(
+            retrieval_method.name,
+            rerank_results,
+            shuffle_candidates,
+            top_k_candidates=top_k_retrieve,
+            pass_ct=None if num_passes == 1 else pass_ct,
+            window_size=kwargs.get("window_size", None),
+            dataset_name=dataset,
+            sglang_batched=kwargs.get("sglang_batched", False),
+            tensorrt_batched=kwargs.get("tensorrt_batched", False),
+        )
+        if (
+            dataset in TOPICS
+            and dataset not in ["news"]
+            and TOPICS[dataset] not in ["news"]
+        ):
+            from rank_llm.evaluation.trec_eval import EvalFunction
+
+            print("Evaluating:")
+            EvalFunction.eval(["-c", "-m", "ndcg_cut.1", TOPICS[dataset], file_name])
+            EvalFunction.eval(["-c", "-m", "ndcg_cut.5", TOPICS[dataset], file_name])
+            EvalFunction.eval(["-c", "-m", "ndcg_cut.10", TOPICS[dataset], file_name])
+        else:
+            print(f"Skipping evaluation as {dataset} is not in TOPICS.")
+
+    if interactive:
+        return (rerank_results, reranker.get_model_coordinator())
+    else:
+        return rerank_results
+
+
+def retrieve(
+    top_k_retrieve: int = 50,
+    interactive: bool = False,
+    retrieval_mode: RetrievalMode = RetrievalMode.DATASET,
+    retrieval_method: RetrievalMethod = RetrievalMethod.BM25,
+    query: str = "",
+    qid: int = 1,
+    **kwargs,
+):
+    """Retrieve initial candidates
+
+    Keyword arguments:
+    dataset -- dataset to search if interactive
+    top_k_retrieve -- top k candidates to retrieve
+    retrieval_mode -- Mode of retrieval
+    retrieval_method -- Method of retrieval
+    query -- query to retrieve against
+    qid - qid of query
+
+    Return: requests -- List[Requests]
+    """
+
+    # Retrieve
+    if interactive and retrieval_mode != RetrievalMode.DATASET:
+        raise ValueError(
+            f"Unsupport retrieval mode for interactive retrieval. Currently only DATASET mode is supported."
+        )
+
+    requests: List[Request] = []
+    if retrieval_mode == RetrievalMode.DATASET:
+        dataset: Union[str, List[str], List[Dict[str, Any]]] = kwargs.get(
+            "dataset", None
+        )
+        if dataset == None:
+            raise ValueError("Must provide a dataset")
+
+        if interactive:
+            host: str = kwargs.get("host", "http://localhost:8081")
+            service_retriever = ServiceRetriever(
+                retrieval_method=retrieval_method, retrieval_mode=retrieval_mode
+            )
+            if isinstance(dataset, str):
+                dataset = [dataset]
+            if isinstance(dataset, list):
+                # This check remains the same as the contents check still needs manual iteration
+                if all(isinstance(item, dict) for item in dataset):
+                    raise ValueError(
+                        "List[Dict[str, Any]] dataset input is not supported for interactive retrieval"
+                    )
+
+            requests = []
+            for ds in dataset:
+                # Calls Anserini API
+                requests.append(
+                    service_retriever.retrieve(
+                        dataset=ds,
+                        request=Request(query=Query(text=query, qid=qid)),
+                        k=top_k_retrieve,
+                        host=host,
+                    )
+                )
+        else:
+            requests = Retriever.from_dataset_with_prebuilt_index(
+                dataset_name=dataset,
+                retrieval_method=retrieval_method,
+                k=top_k_retrieve,
+            )
+    elif retrieval_mode == RetrievalMode.CUSTOM:
+        keys_and_defaults = [
+            ("index_path", None),
+            ("topics_path", None),
+            ("index_type", None),
+        ]
+        [index_path, topics_path, index_type] = extract_kwargs(
+            keys_and_defaults, **kwargs
+        )
+        requests = Retriever.from_custom_index(
+            index_path=index_path, topics_path=topics_path, index_type=index_type
+        )
+
+    return requests
