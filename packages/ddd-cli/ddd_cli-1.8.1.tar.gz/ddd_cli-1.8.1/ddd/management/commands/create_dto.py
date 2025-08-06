7b500345ad4ab3620d4c78a8929f55272056fe3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 16:06:52 +0000

    Add package ddd-cli/1.8.1/ddd_cli-1.8.1.tar.gz

---
+++ packages/ddd-cli/ddd_cli-1.8.1.tar.gz/ddd_cli-1.8.1/ddd/management/commands/create_dto.py
@@ -0,0 +1,54 @@
+from .utils import *
+from colorama import Fore, Style
+
+class CreateDTOCommand:
+    def __init__(self, subparsers):
+        parser = subparsers.add_parser('create-dto', help='Create a new DTO')
+        parser.add_argument('app_path', type=str, help='The relative path of the app within the project (for example, "apps/app1")')
+        parser.add_argument('dto_name', type=str, help='The name of the DTO')
+        parser.add_argument(
+            '--split', action='store_true', help='Create a separate file for this DTO'
+        )        
+        parser.set_defaults(func=self.execute) 
+
+    def execute(self, args):
+        self.create_dto(args.app_path, args.dto_name, args.split)
+        
+    def create_dto(self, app_path, dto_name, split=False, **kwargs):
+        """Crea un nuevo DTO"""
+        dtos_dir = app_path if not split else os.path.join(app_path, 'dtos')
+        dtos_path = os.path.join(dtos_dir, 'dtos.py') if not split else os.path.join(dtos_dir, dto_name.lower() + '_dto.py')
+
+        # Crear directorios si no existen
+        try:
+            os.makedirs(dtos_dir, exist_ok=True)
+
+            # Crear archivos __init__.py
+            create__init__files(dtos_dir)
+        except OSError as e:
+            print(Fore.RED + f"Failed to create directory '{dtos_dir}': {e}" + Style.RESET_ALL)
+            return
+        
+        #si split y ya existe el archivo mostrar error
+        if split and os.path.exists(dtos_path):
+            print(Fore.RED + f"File '{dtos_path}' already exists. Cannot create separate file" + Style.RESET_ALL)
+            return
+
+        # Escribir imports en el archivo 
+        # si no existe el archivo template_imports
+        if not os.path.exists(dtos_path):
+            #renderizar imports
+            rendered_content_imports = renderTemplate(templateName = 'dto', fileName='imports.py', render_params={'dto_name':dto_name})
+
+            # Escribir imports en el archivo
+            with open(dtos_path, 'w') as f:
+                f.write(rendered_content_imports + '\n')
+            print(f"Imports of Dto '{dto_name}' created at {dtos_path}")
+
+        #renderizar class
+        rendered_content_class = renderTemplate(templateName = 'dto', fileName='class.py', render_params={'dto_name':dto_name})
+
+        # Escribir class en el archivo
+        with open(dtos_path, 'a') as f:
+            f.write('\n' + rendered_content_class + '\n')
+        print(f"Class of Dto '{dto_name}' created at {dtos_path}")        
---
+++ packages/ddd-cli/ddd_cli-1.8.1.tar.gz/ddd_cli-1.8.1/ddd/management/commands/utils.py
@@ -0,0 +1,113 @@
+import os
+from pathlib import Path
+from jinja2 import Environment, Template, FileSystemLoader
+
+def create__init__file(path):
+    """Crea un archivo __init__.py en el directorio especificado"""
+    with open(os.path.join(path, '__init__.py'), 'w') as f:
+        pass
+
+
+def create__init__files(path):
+    """Crea un archivo __init__.py en el directorio especificado 
+    y en todos los directorios padres hasta el directorio raíz 
+    o hasta encontrar un archivo __init__.py"""
+    while True:
+        if os.path.exists(os.path.join(path, '__init__.py')):
+            break
+        create__init__file(path)
+        path = os.path.dirname(path)
+        if path == '' or path == '/':
+            break
+
+# def renderTemplate(templateName: str, fileName:str, render_params: dict) -> str:
+#     """
+#     Renderiza una plantilla con los parámetros proporcionados.
+#     """
+#     # Construir la ruta absoluta de la plantilla
+#     templates_dir = Path(__file__).resolve().parent.parent / "templates" / templateName
+#     template_path = templates_dir / fileName
+
+#     # Verificar si el archivo existe
+#     if not template_path.exists():
+#         raise FileNotFoundError(f"Template file {template_path} not found")
+
+#     # Cargar la plantilla imports
+#     template_imports = os.path.join(templates_dir, fileName)
+#     with open(template_imports, 'r') as template_file:
+#         template_content = template_file.read()
+
+#     # Renderizar la plantilla imports
+#     return Template(template_content).render(**render_params)
+
+def renderTemplate(templateName: str, fileName: str, render_params: dict) -> str:
+    """
+    Renderiza una plantilla con los delimitadores específicos y los parámetros proporcionados.
+    """
+    # Construir la ruta absoluta de la plantilla
+    templates_dir = Path(__file__).resolve().parent.parent / "templates" / templateName
+    template_path = templates_dir / fileName
+
+    # Verificar si el archivo existe
+    if not template_path.exists():
+        raise FileNotFoundError(f"Template file {template_path} not found")
+
+    # Cargar el contenido de la plantilla
+    with open(template_path, 'r') as template_file:
+        template_content = template_file.read()
+
+    # Configurar un entorno Jinja2 con delimitadores personalizados
+    env = Environment(
+        block_start_string='[%',   # Delimitador de inicio de bloque
+        block_end_string='%]',     # Delimitador de fin de bloque
+        variable_start_string='[[', # Delimitador de inicio de variable
+        variable_end_string=']]'    # Delimitador de fin de variable
+    )
+
+    # Renderizar la plantilla usando el entorno personalizado
+    template = env.from_string(template_content)
+    return template.render(**render_params)
+
+def readWriteTemplate(templateName: str, fileName:str, render_params: dict, repository_path: str, failIfError:bool= False):
+    """
+    Renderiza una plantilla con los parámetros proporcionados y escribe el archivo en repository_path
+    """
+    msgPath = f"{templateName}/{fileName} -> {repository_path}"
+    try:
+        # Renderizar la plantilla
+        rendered_content = renderTemplate(templateName = templateName, fileName=fileName, render_params=render_params)
+
+        # Escribir en el archivo
+        with open(repository_path, 'w') as f:
+            f.write('\n' + rendered_content + '\n') 
+
+        print(f"Written file from: {msgPath}.")
+
+    except Exception as e:
+        if failIfError:
+            print(f"Error writing file from: {msgPath} : ", e)
+            raise Exception(e.args)
+
+def decodeAppPath(app_path: str) -> tuple:
+    """
+    Convierte una ruta de aplicación (ej: apps/manager/app1) a:
+    1. nombre de aplicación (ej: apps.manager.app1)
+    2. ultimo nombre de la aplicación (ej: app1)
+    3. ruta de la aplicación (ej: apps:manager:app1)
+    4. path relativo de la aplicación (ej: manager/app1) quitando el primer prefijo.
+    """
+
+    # Convertir app_path a path relativo
+    relative_app_path = app_path.split('/', 1)[-1] if '/' in app_path else app_path
+
+    # Convertir app_path a app_name
+    app_name = app_path.replace('/', '.').replace('\\', '.').replace('..', '.')
+    relative_app_name = relative_app_path.replace('/', '.').replace('\\', '.').replace('..', '.')
+    
+    # Obtener el último nombre de la aplicación
+    last_app_name = app_name.split('.')[-1]
+
+    # Convertir app_path a ruta de aplicación
+    app_route = relative_app_name.replace('.', ':')
+
+    return app_name, last_app_name, app_route, relative_app_path
