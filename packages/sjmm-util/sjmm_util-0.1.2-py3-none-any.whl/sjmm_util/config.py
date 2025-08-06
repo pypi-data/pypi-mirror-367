8213bc8ec346cb30e187d83583addd824a6c60d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 06:06:15 +0000

    Add package sjmm-util/0.1.2/sjmm_util-0.1.2-py3-none-any.whl

---
+++ packages/sjmm-util/sjmm_util-0.1.2-py3-none-any.whl/sjmm_util/config.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+
+
+def set_plot_defaults():
+	plt.style.use('sjmm_util.sjmdefaults')
+
+	for fontpath in font_manager.findSystemFonts(fontpaths=None, fontext='ttf'):
+	    if 'Times'.lower() in fontpath.lower():
+	        font_manager.fontManager.addfont(fontpath)
