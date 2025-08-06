cd1dd96060d61fbdbd4c7ddeef6315896542d256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 08:04:53 +0000

    Add package kain/0.1.2/kain-0.1.2-py3-none-any.whl

---
+++ packages/kain/kain-0.1.2-py3-none-any.whl/kain/classes.py
@@ -0,0 +1,34 @@
+from kain.internals import Who
+
+__all__ = 'Nothing', 'Singleton'
+
+
+class Singleton(type):
+
+    def __init__(cls, name, parents, attrbutes):
+        super().__init__(name, parents, attrbutes)
+
+    def __call__(cls, *args, **kw):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super().__call__(*args, **kw)
+        return cls.instance
+
+
+class Missing(metaclass=Singleton):
+
+    def __hash__(self):
+        return id(self)
+
+    def __bool__(self):
+        return False
+
+    def __eq__(self, _):
+        return False
+
+    def __repr__(self):
+        return f'<{Who.Name(self, addr=True)}>'
+
+
+# This is a singleton, so it's safe to use it as a sentinel
+
+Nothing = Missing()
