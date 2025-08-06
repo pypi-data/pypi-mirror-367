84de26b6912a4c2c43b5ea3be028456f041c2c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 17:09:01 +0000

    Add package motleycrew/0.3.0/motleycrew-0.3.0-py3-none-any.whl

---
+++ packages/motleycrew/motleycrew-0.3.0-py3-none-any.whl/motleycrew/tools/code/postgresql_linter.py
@@ -0,0 +1,55 @@
+from typing import List, Type
+
+from langchain_core.tools import Tool
+from pydantic import BaseModel, Field
+
+try:
+    from pglast import parse_sql, prettify
+    from pglast.parser import ParseError
+except ImportError:
+    parse_sql = None
+    prettify = None
+    ParseError = None
+
+from motleycrew.common.utils import ensure_module_is_installed
+from motleycrew.tools import MotleyTool
+
+
+class PostgreSQLLinterTool(MotleyTool):
+    """PostgreSQL code verification tool."""
+
+    def __init__(
+        self,
+        return_direct: bool = False,
+        handle_exceptions: bool | List[Type[Exception]] = False,
+    ):
+        ensure_module_is_installed("pglast")
+
+        langchain_tool = create_pgsql_linter_tool()
+        super().__init__(
+            tool=langchain_tool,
+            return_direct=return_direct,
+            handle_exceptions=handle_exceptions,
+        )
+
+
+class PostgreSQLLinterInput(BaseModel):
+    """Input for the PostgreSQLLinterTool."""
+
+    query: str = Field(description="SQL code for validation")
+
+
+def create_pgsql_linter_tool() -> Tool:
+    def parse_func(query: str) -> str:
+        try:
+            parse_sql(query)
+            return prettify(query)
+        except ParseError as e:
+            return str(e)
+
+    return Tool.from_function(
+        func=parse_func,
+        name="postgresql_linter",
+        description="Tool for validating PostgreSQL code",
+        args_schema=PostgreSQLLinterInput,
+    )
