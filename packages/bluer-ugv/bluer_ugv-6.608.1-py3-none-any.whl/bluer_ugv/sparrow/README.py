1d0f3de32c343d4f7ee2ab0b5dce16a6104bcb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-06 04:20:46 +0000

    Add package bluer-ugv/6.608.1/bluer_ugv-6.608.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-6.608.1-py3-none-any.whl/bluer_ugv/sparrow/README.py
@@ -0,0 +1,10 @@
+from bluer_objects.README.items import ImageItems
+
+assets2 = "https://github.com/kamangir/assets2/blob/main/bluer-sparrow"
+
+items = ImageItems(
+    {
+        f"{assets2}/20250722_174115-2.jpg?raw=true": "",
+        f"{assets2}/20250729_234927.jpg?raw=true": "",
+    }
+)
