77484a78a9ebf72388325d9e048eba7e716646f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 16:07:22 +0000

    Add package jax-autovmap/0.3.0/jax_autovmap-0.3.0.tar.gz

---
+++ packages/jax-autovmap/jax_autovmap-0.3.0.tar.gz/jax_autovmap-0.3.0/README.md
@@ -0,0 +1,87 @@
+[![Tests](https://github.com/mathisgerdes/autovmap/actions/workflows/python-pytest.yml/badge.svg)](https://github.com/mathisgerdes/autovmap/actions/workflows/python-package.yml)
+[![PyPI](https://img.shields.io/pypi/v/jax-autovmap)](https://pypi.org/project/jax-autovmap/)
+# Automatically broadcast via JAX vmap
+
+Automatically broadcast a function that takes inputs of specific ranks to accept any batch dimensions using `jax.vmap`.
+See the documentation of [JAX](https://github.com/google/jax) for more information about `vmap`.
+
+This module defines a decorator function which takes as input the base ranks of the arguments of a function.
+The transformed function takes any broadcastable combination of batched inputs and automatically applies `jax.vmap` as required.
+One could equivalently apply `vmap` by hand, however the in-axes have to be chosen differently for different inputs.
+The decorator takes care of this automatically; if the underlying ranks are known, batch dimensions can be inferred.
+
+## Examples
+
+Consider the following function which takes numeric arguments with fixed and known ranks as input:
+```python
+import jax.numpy as jnp
+
+def foo(s, v, m):  # s - scalar, v - vector, m - matrix
+    return v @ m @ v + s * v.size
+```
+If we have inputs of appropriate rank, the function can be applied without problem:
+```python
+s = jnp.array(2.0)    # scalar
+v = jnp.ones(3)       # vector
+m = jnp.ones((3, 3))  # matrix
+foo(s, v, m)   # returns 15.0
+```
+Assume now, however, that we have 5 matrices and 5 vectors for which we want to apply the above function as a batch.
+Many numpy functions can take inputs with leading batch dimensions, but here we have an issue because `v @ m @ v` requires `m` to be a matching matrix.
+```python
+s = jnp.array(2.0)
+v = jnp.ones((5, 3))
+m = jnp.ones((5, 3, 3))
+foo(s, v, m)  # throws TypeError
+```
+There are multiple possible ways we can solve this
+
+- We could try to write the function more carefully, so it can take both single and batch inputs.
+  Or we could always require the first dimension to be a batch index.
+- Given known inputs, we can transform our function: `jax.jit(foo, (None, 0, 0))`.
+  However, the axes change depending on the inputs.
+  If we want to expose functions that accept batched inputs to the user, we need to have some clear naming scheme (to indicate which arguments are batched).
+
+Sometimes, the best solution is one of the above.
+This module provides another more flexible solution.
+If the ranks the function *wants* are known, we can derive which arguments have (leading) batch dimensions.
+Based on that, we can apply `jax.vmap` appropriately.
+That is exactly what the `autovmap` wrapper does.
+Thanks to `jax.jit`, after the transformed function is JIT-compiled, there is no price to pay for this extra flexibility since it only depends on the statically-known input shapes.
+
+We can define the more flexible function as follows:
+```python
+from jax_autovmap import autovmap
+
+@autovmap(s=0, v=1, m=2)
+def foo(s, v, m):
+    return v @ m @ v + s * v.size
+
+foo(s, v, m)  # returns [15. 15. 15. 15. 15.]
+```
+The ranks can be specified by keyword argument as above, or positionally (in this case `@autovmap(0, 1, 2)`).
+This does not have to be applied to all input arguments.
+They can either be omitted or, if ranks are given positionally, specified as `None`.
+
+If the arguments are pytrees (python structures of arrays) and the rank is a single integer, all constituents (leaves) are assumed to have that rank.
+Alternatively, the rank can be a matching pytree, just like the `in_axes` in `jax.vmap`:
+```python
+@autovmap({'s': 0, 'v': 1, 'm': 2})
+def foo(inputs):
+    return inputs['v'] @ inputs['m'] @ inputs['v'] + inputs['s']
+
+foo(dict(s=s, v=v, m=m))
+```
+
+Just like NumPy broadcasting, it is also allowed that one of the "vmap'ed" arguments has length 1:
+```python
+s = jnp.array(2.0)
+v = jnp.ones((7, 1, 3))  # broadcast second axis with vmap over 5 values of `m`
+m = jnp.ones((7, 5, 3, 3))
+
+@autovmap(s=0, v=1, m=2)
+def foo(s, v, m):
+    return v @ m @ v + s
+
+foo(s, v, m)  # shape (7, 5)
+```
---
+++ packages/jax-autovmap/jax_autovmap-0.3.0.tar.gz/jax_autovmap-0.3.0/src/test_autovmap.py
@@ -0,0 +1,127 @@
+from unittest import TestCase
+
+import jax.numpy as jnp
+import numpy as np
+
+from jax_autovmap import autovmap
+
+
+class AutoVmapTests(TestCase):
+    def assertAllEqual(self, x, y):
+        x, y = map(np.asarray, (x, y))
+        self.assertTrue(np.all(x == y))
+
+    def assertAllClose(self, x, y):
+        x, y = map(np.asarray, (x, y))
+        self.assertTrue(np.all(x, y))
+
+    def assertAllTrue(self, t):
+        self.assertTrue(np.all(t))
+
+    def test_basic(self):
+
+        def foo(x, y, val=1):
+            self.assertEqual(jnp.ndim(x), 2)
+            self.assertEqual(jnp.ndim(y), 0)
+            return val
+
+        # args, kwargs
+        possible_args = [
+            ((2, 0), {}),
+            ((2, 0, None), {}),
+            ((), {'x': 2, 'y': 0}),
+            ((), {'x': 2, 'y': 0, 'val': None}),
+        ]
+
+        for args, kwargs in possible_args:
+            vmapped = autovmap(*args, **kwargs)(foo)
+
+            # no vmap
+            self.assertEqual(
+                vmapped(jnp.zeros((3, 3)), 0.0),
+                1,
+            )
+            # vmap second arg
+            self.assertAllEqual(
+                vmapped(jnp.zeros((3, 3)), jnp.array([0.0, 1.0, 2.0])),
+                np.ones([3]),
+            )
+            # vmap first arg
+            self.assertAllEqual(
+                vmapped(jnp.zeros((5, 3, 3)), 0),
+                np.ones([5]),
+            )
+            self.assertAllEqual(
+                vmapped(jnp.zeros((5, 3, 3)), 0, val=0),
+                np.zeros([5]),
+            )
+            self.assertAllEqual(
+                vmapped(jnp.zeros((5, 3, 3)), 0, 0),
+                np.zeros([5]),
+            )
+            # vmap both args
+            self.assertAllEqual(
+                vmapped(jnp.zeros((5, 3, 3)), jnp.zeros(5), 3),
+                np.full(5, 3),
+            )
+            self.assertAllEqual(
+                vmapped(jnp.zeros((2, 5, 3, 3)), jnp.zeros(5), 3),
+                np.full((2, 5), 3),
+            )
+            self.assertAllEqual(
+                vmapped(jnp.zeros((3, 3, 3)), jnp.zeros((4, 3)), 7),
+                np.full((4, 3), 7),
+            )
+
+    def test_varargs(self):
+        @autovmap(x=1, b=0)
+        def foo(x, *args, b=0, c=1):
+            return jnp.sum(x) + sum(args) + b + c
+
+        self.assertAllEqual(
+            foo(jnp.ones((3, 3)), 1, 2, 3, b=jnp.zeros((3,))),
+            np.array([10] * 3),
+        )
+
+    def test_pytree(self):
+        def sum_ranks(objects, offset):
+            total = offset
+            for key in objects:
+                total += objects[key].ndim
+            return total
+
+        names = 'abcde'
+        ranks = [2, 5, 1, 2, 3]
+        obj = {n: np.ones((2,) * r) for r, n in zip(ranks, names)}
+
+        # each is rank 5 so the sum is just the number of arguments
+        total = autovmap(1, 0)(sum_ranks)(obj, 0)
+        self.assertEqual(total.ndim, max(ranks) - 1)
+        self.assertAllEqual(total, len(ranks))
+        total = autovmap(1, 0)(sum_ranks)(obj, 1)
+        self.assertAllEqual(total, len(ranks) + 1)
+
+        # rank is 2 so two times the number of arguments
+        names = 'abcd'
+        ranks = [2, 5, 3, 2]
+        obj = {n: np.ones((2,) * r) for r, n in zip(ranks, names)}
+        total = autovmap(2, 0)(sum_ranks)(obj, 0)
+        self.assertEqual(total.ndim, max(ranks) - 2)
+        self.assertAllEqual(total, 2 * len(ranks))
+
+        # check unmodified code does the right thing
+        names = 'abcd'
+        ranks = [2, 5, 3, 2]
+        obj = {n: np.ones((2,) * r) for r, n in zip(ranks, names)}
+        total = sum_ranks(obj, 0)
+        self.assertEqual(total, sum(ranks))
+
+        # test specification as pytree
+        names = 'abcd'
+        ranks = [2, 5, 3, 2]
+        obj = {n: np.ones((2,) * r) for r, n in zip(ranks, names)}
+        arg_ranks = {'a': 1, 'b': 2, 'c': 1, 'd': 0}
+        total = autovmap(arg_ranks, 0)(sum_ranks)(obj, 0)
+        self.assertAllEqual(total, 1 + 2 + 1 + 0)
+        total_rank = max(r - arg_ranks[n] for r, n in zip(ranks, names))
+        self.assertAllEqual(total.ndim, total_rank)
