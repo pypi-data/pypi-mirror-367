d888e4937c4ffda9f45ac5e4b0702d8a9ba58786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 15:07:41 +0000

    Add package simplecalcZ/0.1.0/simplecalcz-0.1.0.tar.gz

---
+++ packages/simplecalcZ/simplecalcz-0.1.0.tar.gz/simplecalcz-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simplecalcZ',
+    version='0.1.0',
+    description='A simple calculator package with basic math functions.',
+    author='EDharmawan',
+    author_email='egadharmawan53@email.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
