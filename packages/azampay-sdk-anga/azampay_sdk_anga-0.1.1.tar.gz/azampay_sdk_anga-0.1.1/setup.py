0a4a285c3523cf2113726aa2503aee16bd590919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 04:23:03 +0000

    Add package azampay-sdk-anga/0.1.1/azampay_sdk_anga-0.1.1.tar.gz

---
+++ packages/azampay-sdk-anga/azampay_sdk_anga-0.1.1.tar.gz/azampay_sdk_anga-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='azampay-sdk-anga',
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests', 'python-dotenv'
+    ],
+    description='A dynamic Python SDK for AzamPay (AzamPay Python SDK used for native python, Django, Flask, FastApi e.t.c)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Anganile Adam (Anga)',
+    author_email='twaloadam@gmail.com',
+    url='https://github.com/tbwahacker/azampay-sdk-anga',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
