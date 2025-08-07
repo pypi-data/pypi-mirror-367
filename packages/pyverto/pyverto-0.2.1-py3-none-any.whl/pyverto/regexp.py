5f2a781ff92f5de2b458969403cfb10a833fa8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 17:09:20 +0000

    Add package pyverto/0.2.1/pyverto-0.2.1-py3-none-any.whl

---
+++ packages/pyverto/pyverto-0.2.1-py3-none-any.whl/pyverto/regexp.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2025-present phdenzel <phdenzel@gmail.com>
+# SPDX-FileNotice: Part of pyverto
+# SPDX-License-Identifier: MIT
+"""Regular expression for version string parsing."""
+
+VERSION_RE = r'__version__\s*=\s*["\']([^"\']+)["\']'
