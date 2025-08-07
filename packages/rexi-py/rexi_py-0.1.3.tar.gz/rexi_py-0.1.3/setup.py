e43accef4d3a352ec9e3f666c8c7b7f3742c9f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 16:07:44 +0000

    Add package rexi-py/0.1.3/rexi_py-0.1.3.tar.gz

---
+++ packages/rexi-py/rexi_py-0.1.3.tar.gz/rexi_py-0.1.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="rexi-py",
+    version="0.1.3",
+    author="RexiAPI Team",
+    author_email="rexidotsh@outlook.com",
+    description="A Python client library for Rexi API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rexi-sh/rexi-py",
+    project_urls={
+        "Website": "https://rexi.sh",
+        "Documentation": "https://rexi.sh/docs",
+    },
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "aiohttp>=3.8.0",  # For async HTTP requests
+        "websockets>=10.0",  # For WebSocket connections
+    ],
+)
