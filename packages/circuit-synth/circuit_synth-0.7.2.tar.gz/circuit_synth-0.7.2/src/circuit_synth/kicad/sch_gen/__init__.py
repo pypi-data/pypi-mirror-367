28089b4f660a0b99d5fe4ec570a1cb75d1c45187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 04:22:19 +0000

    Add package circuit-synth/0.7.2/circuit_synth-0.7.2.tar.gz

---
+++ packages/circuit-synth/circuit_synth-0.7.2.tar.gz/circuit_synth-0.7.2/src/circuit_synth/kicad/sch_gen/__init__.py
@@ -0,0 +1,7 @@
+from .main_generator import SchematicGenerator
+from .schematic_writer import write_schematic_file
+
+__all__ = [
+    "SchematicGenerator",
+    "write_schematic_file",
+]
---
+++ packages/circuit-synth/circuit_synth-0.7.2.tar.gz/circuit_synth-0.7.2/src/circuit_synth/manufacturing/__init__.py
@@ -0,0 +1,9 @@
+"""
+Manufacturing integrations for Circuit-Synth.
+
+This module provides integrations with various PCB manufacturers and component suppliers:
+- jlcpcb/: JLCPCB manufacturing and assembly services
+- pcbway/: PCBWay manufacturing services (future)
+- oshpark/: OSH Park manufacturing services (future)
+- digikey/: Digi-Key component sourcing (future)
+"""
