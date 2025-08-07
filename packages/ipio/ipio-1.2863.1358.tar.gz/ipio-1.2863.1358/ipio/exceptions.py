068af844d01ca0a7e3de97baea995987e560e2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 03:34:56 +0000

    Add package ipio/1.2863.1358/ipio-1.2863.1358.tar.gz

---
+++ packages/ipio/ipio-1.2863.1358.tar.gz/ipio-1.2863.1358/ipio/exceptions.py
@@ -0,0 +1,34 @@
+class SetDatetimeException(Exception):
+    pass
+
+
+class ConfigNotSetException(Exception):
+    pass
+
+
+class InvalidIPException(Exception):
+    pass
+
+
+class MutedSystemException(Exception):
+    pass
+
+
+class EmptyParamsException(Exception):
+    pass
+
+
+class UnusableSocketException(Exception):
+    pass
+
+
+class MessageSizeException(Exception):
+    pass
+
+
+class AuthenticationException(Exception):
+    pass
+
+
+class AuthorizationException(Exception):
+    pass
