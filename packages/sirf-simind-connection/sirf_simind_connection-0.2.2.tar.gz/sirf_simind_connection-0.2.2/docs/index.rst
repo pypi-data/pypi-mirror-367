2dc08caf9b259f0bd70b103b69ff40e0ac45f946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 03:34:59 +0000

    Add package sirf-simind-connection/0.2.2/sirf_simind_connection-0.2.2.tar.gz

---
+++ packages/sirf-simind-connection/sirf_simind_connection-0.2.2.tar.gz/sirf_simind_connection-0.2.2/docs/index.rst
@@ -0,0 +1,22 @@
+SIRF-SIMIND-Connection Documentation
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   intro
+   installation
+   usage
+   examples
+   api
+   testing
+   changelog
+   contributing
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/sirf-simind-connection/sirf_simind_connection-0.2.2.tar.gz/sirf_simind_connection-0.2.2/tests/test_simind_projector.py
@@ -0,0 +1,13 @@
+import pytest
+
+from sirf_simind_connection import SimindProjector
+
+
+# All tests in this file require SIRF since SimindProjector uses SIRF
+pytestmark = pytest.mark.requires_sirf
+
+
+def test_projector_initialization():
+    """Test initialization of SimindProjector."""
+    projector = SimindProjector()
+    assert projector is not None
