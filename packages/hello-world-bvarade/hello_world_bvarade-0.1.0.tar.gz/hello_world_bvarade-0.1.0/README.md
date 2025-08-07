5c1d20139f53592773d88a196c08add55ce48f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 05:12:26 +0000

    Add package hello-world-bvarade/0.1.0/hello_world_bvarade-0.1.0.tar.gz

---
+++ packages/hello-world-bvarade/hello_world_bvarade-0.1.0.tar.gz/hello_world_bvarade-0.1.0/README.md
@@ -0,0 +1,10 @@
+# hello_world_bvarade
+
+this is the simple hello world library
+
+## Usage
+
+```python
+from hello_world import hello
+hello()
+```
---
+++ packages/hello-world-bvarade/hello_world_bvarade-0.1.0.tar.gz/hello_world_bvarade-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_world_bvarade",
+    version="0.1.0",
+    author="Bhushan Varade",
+    author_email="bvarade02@gmail.com",
+    description="this is the simple hello world library",
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="hello world example",
+)
