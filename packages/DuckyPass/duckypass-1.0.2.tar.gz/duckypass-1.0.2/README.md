3e5886f1912ad1ade7e19cb47dc1a1f8bc560a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 05:12:51 +0000

    Add package DuckyPass/1.0.2/duckypass-1.0.2.tar.gz

---
+++ packages/DuckyPass/duckypass-1.0.2.tar.gz/duckypass-1.0.2/README.md
@@ -0,0 +1,27 @@
+# DuckyPassAPI
+
+A Python module for generating passwords using the DuckyPass API.
+
+## Installation
+
+```bash
+pip install DuckyPass
+```
+
+## Usage
+
+```python
+import DuckyPass
+
+# Generate a single secure password
+password = DuckyPass("secure", 1)
+print(password)
+
+# Generate 5 simple passwords
+passwords = DuckyPass("simple", 5)
+print(passwords)
+```
+
+## License
+
+GPLv3
---
+++ packages/DuckyPass/duckypass-1.0.2.tar.gz/duckypass-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DuckyPassAPI",
+    version="1.0.2",
+    description="A Python module for generating passwords using the DuckyPass API.",
+    author="Rhys Wills",
+    author_email="rhys@rwills.net",
+    license="gpl3",
+    packages=find_packages(),
+    install_requires=["requests"],
+    url="https://github.com/maxrhys/PythonDuckyPassAPI",
+    keywords=["password", "generator", "API", "DuckyPass"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
