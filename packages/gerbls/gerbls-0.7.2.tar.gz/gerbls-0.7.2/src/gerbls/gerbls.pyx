d5bf226bea39cff982f220fe0090b63f994fa2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 06:06:09 +0000

    Add package gerbls/0.7.2/gerbls-0.7.2.tar.gz

---
+++ packages/gerbls/gerbls-0.7.2.tar.gz/gerbls-0.7.2/src/gerbls/gerbls.pyx
@@ -0,0 +1,17 @@
+# cython: language_level = 3str
+# distutils: language = c++
+
+from gerbls.cgerbls cimport *
+from libc.stdlib cimport srand
+from libc.time cimport time as ctime
+from libcpp cimport bool as bool_t
+import numpy as np
+cimport numpy as np
+
+# Initialize random number generator
+srand(ctime(NULL))
+
+# These are just text inclusions
+include "blsmodel.pxi"
+include "struct.pxi"
+include "utils.pxi"
