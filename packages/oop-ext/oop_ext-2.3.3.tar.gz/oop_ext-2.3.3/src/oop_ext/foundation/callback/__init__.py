0fbff5cdb1b401718aba225a03ac34b9b2751f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 04:21:28 +0000

    Add package oop-ext/2.3.3/oop_ext-2.3.3.tar.gz

---
+++ packages/oop-ext/oop_ext-2.3.3.tar.gz/oop_ext-2.3.3/src/oop_ext/foundation/callback/__init__.py
@@ -0,0 +1,43 @@
+from ._callback import Callback
+from ._callbacks import Callbacks
+from ._priority_callback import PriorityCallback
+from ._shortcuts import After
+from ._shortcuts import Before
+from ._shortcuts import Remove
+from ._shortcuts import WrapForCallback
+from ._typed_callback import Callback0
+from ._typed_callback import Callback1
+from ._typed_callback import Callback2
+from ._typed_callback import Callback3
+from ._typed_callback import Callback4
+from ._typed_callback import Callback5
+from ._typed_callback import PriorityCallback0
+from ._typed_callback import PriorityCallback1
+from ._typed_callback import PriorityCallback2
+from ._typed_callback import PriorityCallback3
+from ._typed_callback import PriorityCallback4
+from ._typed_callback import PriorityCallback5
+from ._typed_callback import UnregisterContext
+
+__all__ = [
+    "After",
+    "Before",
+    "Callback",
+    "Callback0",
+    "Callback1",
+    "Callback2",
+    "Callback3",
+    "Callback4",
+    "Callback5",
+    "Callbacks",
+    "PriorityCallback",
+    "PriorityCallback0",
+    "PriorityCallback1",
+    "PriorityCallback2",
+    "PriorityCallback3",
+    "PriorityCallback4",
+    "PriorityCallback5",
+    "UnregisterContext",
+    "Remove",
+    "WrapForCallback",
+]
