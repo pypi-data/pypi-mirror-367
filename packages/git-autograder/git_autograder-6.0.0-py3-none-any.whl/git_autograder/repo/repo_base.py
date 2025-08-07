a7a55fee999ef203788241430f3476568a9bf14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 04:20:59 +0000

    Add package git-autograder/6.0.0/git_autograder-6.0.0-py3-none-any.whl

---
+++ packages/git-autograder/git_autograder-6.0.0-py3-none-any.whl/git_autograder/repo/repo_base.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+from git import Repo
+
+from git_autograder.helpers.branch_helper import BranchHelper
+from git_autograder.helpers.commit_helper import CommitHelper
+from git_autograder.helpers.file_helper import FileHelper
+from git_autograder.helpers.remote_helper import RemoteHelper
+
+
+class GitAutograderRepoBase(ABC):
+    @abstractmethod
+    def repo(self) -> Repo: ...
+
+    @abstractmethod
+    def branches(self) -> BranchHelper: ...
+
+    @abstractmethod
+    def commits(self) -> CommitHelper: ...
+
+    @abstractmethod
+    def remotes(self) -> RemoteHelper: ...
+
+    @abstractmethod
+    def files(self) -> FileHelper: ...
