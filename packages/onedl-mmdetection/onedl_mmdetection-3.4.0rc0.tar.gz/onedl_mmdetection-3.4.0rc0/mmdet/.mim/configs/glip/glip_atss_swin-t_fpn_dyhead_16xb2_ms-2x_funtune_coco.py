a8b5269ab11e1db78b424e76462b301eccf191f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 03:34:32 +0000

    Add package onedl-mmdetection/3.4.0rc0/onedl_mmdetection-3.4.0rc0.tar.gz

---
+++ packages/onedl-mmdetection/onedl_mmdetection-3.4.0rc0.tar.gz/onedl_mmdetection-3.4.0rc0/mmdet/.mim/configs/glip/glip_atss_swin-t_fpn_dyhead_16xb2_ms-2x_funtune_coco.py
@@ -0,0 +1,3 @@
+_base_ = './glip_atss_swin-t_b_fpn_dyhead_16xb2_ms-2x_funtune_coco.py'
+
+load_from = 'https://pub-ed9ed750ddcc469da251e2d1a2cea382.r2.dev/mmdetection/v3.0/glip/glip_tiny_mmdet-c24ce662.pth'  # noqa
