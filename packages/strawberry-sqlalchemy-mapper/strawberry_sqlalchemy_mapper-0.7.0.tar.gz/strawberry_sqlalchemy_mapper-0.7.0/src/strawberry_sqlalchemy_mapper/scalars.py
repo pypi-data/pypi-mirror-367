8859c4840fa2cbbb8a3d86f45efe055c7dccf0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 14:08:12 +0000

    Add package strawberry-sqlalchemy-mapper/0.7.0/strawberry_sqlalchemy_mapper-0.7.0.tar.gz

---
+++ packages/strawberry-sqlalchemy-mapper/strawberry_sqlalchemy_mapper-0.7.0.tar.gz/strawberry_sqlalchemy_mapper-0.7.0/src/strawberry_sqlalchemy_mapper/scalars.py
@@ -0,0 +1,11 @@
+from typing import Union
+
+import strawberry
+
+BigInt = strawberry.scalar(
+    Union[int, str],  # type: ignore
+    serialize=lambda v: int(v),
+    parse_value=lambda v: str(v),
+    description="BigInt field",
+    name="BigInt",
+)
