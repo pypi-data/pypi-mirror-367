d26b1cc7151334b47b2124b2a4990480ec69e1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 06:06:24 +0000

    Add package devDocs/2.0.6/devdocs-2.0.6.tar.gz

---
+++ packages/devDocs/devdocs-2.0.6.tar.gz/devdocs-2.0.6/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup,find_packages as A
+setup(name='devDocs',version='2.0.6',packages=A(),install_requires=['google-generativeai'],entry_points={'console_scripts':['devDocs=devDocs.cli:x']},author='Gantavya Bansal',description="Auto-generate Project's markdown documentation using Gemini AI for Internal teams.",long_description=open('README.md',encoding='utf-8').read(),long_description_content_type='text/markdown',license='MIT',python_requires='>=3.8',classifiers=['Programming Language :: Python :: 3','License :: OSI Approved :: MIT License'])
