e3c35d7967f969eb15adfa70420f763a71572f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 16:07:32 +0000

    Add package agomax/0.1.2/agomax-0.1.2.tar.gz

---
+++ packages/agomax/agomax-0.1.2.tar.gz/agomax-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="agomax",
+    version="0.1.2",
+    description="Drone anomaly detection package",
+    author="shaguntembhurne",
+    author_email="your@email.com",
+    url="https://github.com/shaguntembhurne/agomax",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "scikit-learn"
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'agomax=cli:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        'drone_anomoly': ['data/*.csv', 'data/*.txt'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
