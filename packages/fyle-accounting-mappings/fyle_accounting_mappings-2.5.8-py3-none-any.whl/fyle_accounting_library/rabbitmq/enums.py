ed12fbe447b723c3d769bbcda1b17a819f8c413f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 15:07:59 +0000

    Add package fyle-accounting-mappings/2.5.8/fyle_accounting_mappings-2.5.8-py3-none-any.whl

---
+++ packages/fyle-accounting-mappings/fyle_accounting_mappings-2.5.8-py3-none-any.whl/fyle_accounting_library/rabbitmq/enums.py
@@ -0,0 +1,10 @@
+class RabbitMQExchangeEnum:
+    """
+    Enum for RabbitMQ Exchange
+    """
+    QBO_EXCHANGE = 'qbo_exchange'
+    XERO_EXCHANGE = 'xero_exchange'
+    INTACCT_EXCHANGE = 'intacct_exchange'
+    NETSUITE_EXCHANGE = 'netsuite_exchange'
+    SAGE_DESKTOP_EXCHANGE = 'sage_desktop_exchange'
+    QUICKBOOKS_CONNECTOR_EXCHANGE = 'quickbooks_connector_exchange'
