c82411d613f9a08a0a0011bbe3ee21d484df8a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 07:09:59 +0000

    Add package tronapisync/0.1.1/tronapisync-0.1.1.tar.gz

---
+++ packages/tronapisync/tronapisync-0.1.1.tar.gz/tronapisync-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tronapisync",
+    version="0.1.1",  # Увеличьте версию!
+    packages=find_packages(),
+    package_data={'tronapisync': ['*.py', '*.json']},  # Если есть доп файлы
+)
