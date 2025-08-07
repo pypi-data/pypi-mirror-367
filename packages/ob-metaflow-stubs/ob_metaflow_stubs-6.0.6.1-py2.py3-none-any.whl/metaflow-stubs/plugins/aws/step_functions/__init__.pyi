1388d89f0da55c2401b41004b2fa5c743d193885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 05:12:57 +0000

    Add package ob-metaflow-stubs/6.0.6.1/ob_metaflow_stubs-6.0.6.1-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.6.1-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/step_functions/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.17.0.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-06T15:19:48.482275                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import schedule_decorator as schedule_decorator
+from . import step_functions_deployer as step_functions_deployer
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.6.1-py2.py3-none-any.whl/metaflow-stubs/plugins/exit_hook/__init__.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.17.0.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-06T15:19:48.448766                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import exit_hook_decorator as exit_hook_decorator
+
