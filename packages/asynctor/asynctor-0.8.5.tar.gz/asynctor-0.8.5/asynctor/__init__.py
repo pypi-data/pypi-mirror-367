6ad1ee92eaa96465d202788de89323793409e403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 05:12:50 +0000

    Add package asynctor/0.8.5/asynctor-0.8.5.tar.gz

---
+++ packages/asynctor/asynctor-0.8.5.tar.gz/asynctor-0.8.5/asynctor/__init__.py
@@ -0,0 +1,22 @@
+from .aio import bulk_gather, gather, map_group, run, run_async, start_tasks, wait_for
+from .client import AsyncRedis
+from .timing import timeit
+from .utils import AsyncClientGenerator, AsyncTestClient, AttrDict, cache_attr
+
+__version__ = "0.8.5"
+__all__ = (
+    "__version__",
+    "AsyncClientGenerator",
+    "AsyncRedis",
+    "AsyncTestClient",
+    "AttrDict",
+    "bulk_gather",
+    "cache_attr",
+    "gather",
+    "map_group",
+    "run",
+    "run_async",
+    "start_tasks",
+    "timeit",
+    "wait_for",
+)
---
+++ packages/asynctor/asynctor-0.8.5.tar.gz/asynctor-0.8.5/tests/test_xls.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import anyio
+import pytest
+
+from asynctor.xls import df_to_datas, load_xls, read_excel
+
+
+@pytest.mark.anyio
+async def test_read():
+    demo = Path(__file__).parent / "demo.xlsx"
+    df = await read_excel(demo)
+    df2 = await read_excel(demo.read_bytes())
+    df3 = await read_excel(anyio.Path(demo))
+    assert df2.compare(df).empty and df3.compare(df).empty
+    data = df_to_datas(df)
+    assert data == [
+        {"Column1": "row1-\\t%c", "Column2\nMultiLines": 0, "Column 3": 1, 4: ""},
+        {"Column1": "r2c1\n00", "Column2\nMultiLines": "r2 c2", "Column 3": 2, 4: ""},
+    ]
+    assert data == (await load_xls(demo))
+
+    assert (await load_xls(demo, True)) == [
+        {"Column1": "row1-\\t%c", "Column2\nMultiLines": "0", "Column 3": "1", 4: ""},
+        {"Column1": "r2c1\n00", "Column2\nMultiLines": "r2 c2", "Column 3": "2", 4: ""},
+    ]
