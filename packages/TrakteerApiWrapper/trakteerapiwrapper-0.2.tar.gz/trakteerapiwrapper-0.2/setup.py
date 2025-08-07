6503787af5511e9757e9815931562d0038f6bbf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 17:08:58 +0000

    Add package TrakteerApiWrapper/0.2/trakteerapiwrapper-0.2.tar.gz

---
+++ packages/TrakteerApiWrapper/trakteerapiwrapper-0.2.tar.gz/trakteerapiwrapper-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TrakteerApiWrapper',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Indra Bagus',
+    author_email='indrabusiness00@gmail.com',
+    description='A simple wrapper for the Trakteer API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/IndraBagus0/TrakteerApiWrapper',
+    keywords=['trakteer', 'api', 'wrapper', 'python'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',    
+    ]
+)
