f19016e7023a40a1cb5d0e89b85016bd6557310e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 17:09:02 +0000

    Add package tierkreis-visualization/0.1.2/tierkreis_visualization-0.1.2-py3-none-any.whl

---
+++ packages/tierkreis-visualization/tierkreis_visualization-0.1.2-py3-none-any.whl/tierkreis_visualization/main.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+import uvicorn
+
+from tierkreis_visualization.routers.workflows import router as workflows_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:8000",
+        "http://127.0.0.1:5173",
+        "http://localhost:8000",
+        "http://localhost:5173",
+    ],  # Adjust as necessary
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(workflows_router)
+app.mount(
+    "/static",
+    StaticFiles(directory=(Path(__file__).parent / "static").absolute()),
+    name="static",
+)
+
+
+@app.get("/")
+def read_root(request: Request):
+    return RedirectResponse(url="/static/dist/index.html")
+
+
+def start():
+    uvicorn.run(app)
