154409bc6571fb17c8665187ba8795ba21f60e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 14:07:53 +0000

    Add package easybench/0.1.33/easybench-0.1.33.tar.gz

---
+++ packages/easybench/easybench-0.1.33.tar.gz/easybench-0.1.33/src/easybench/__init__.py
@@ -0,0 +1,22 @@
+"""
+EasyBench: A simple and easy-to-use Python benchmarking library.
+
+This package provides tools for benchmarking Python code with an easy-to-use interface.
+"""
+
+from .core import BenchConfig, BenchParams, EasyBench, customize, fixture, parametrize
+from .decorator import bench
+from .utils import get_bench_env
+
+__version__ = "0.1.33"
+
+__all__ = [
+    "BenchConfig",
+    "BenchParams",
+    "EasyBench",
+    "bench",
+    "customize",
+    "fixture",
+    "get_bench_env",
+    "parametrize",
+]
