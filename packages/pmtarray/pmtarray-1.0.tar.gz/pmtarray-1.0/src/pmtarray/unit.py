867307262bb1d0d4bec3e1d43a0aa6d429b0ef73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 14:08:31 +0000

    Add package pmtarray/1.0/pmtarray-1.0.tar.gz

---
+++ packages/pmtarray/pmtarray-1.0.tar.gz/pmtarray-1.0/src/pmtarray/unit.py
@@ -0,0 +1,446 @@
+from importlib import import_module
+from typing import Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib.patches import Circle, Rectangle
+
+
+class PMTunit():
+    """Class to represent a PMT unit."""
+
+    def __init__(self, model, custom_params={}):
+        if model == 'custom':
+            self.check_custom_params(custom_params)
+            if custom_params['type'] == 'square':
+                self.build_custom_square_model(custom_params)
+            elif custom_params['type'] == 'circular':
+                self.build_custom_circular_model(custom_params)
+        else:
+            self.get_model_file(model)
+            self.get_model_geometry()
+        self.set_dependant_properties()
+
+    def get_model_file(self, model):
+        """Fetch the PMT model from the model library."""
+
+        from pmtarray.models import model_lib
+        if model in model_lib.keys():
+            self.model = model_lib[model]
+        else:
+            raise ValueError('Model not found. Please make a PR to add it.')
+
+    def check_custom_params(self, custom_params):
+        """Check if the custom_params dictionary have all the correct params.
+        """
+        if not isinstance(custom_params, dict):
+            raise ValueError(
+                'The custom_params argument must be a dictionary.')
+        if 'type' not in custom_params.keys():
+            raise ValueError(
+                'The custom_params dictionary must have a type key.')
+
+        if custom_params['type'] == 'square':
+            params_list = ['name',
+                           'type',
+                           'width_package',
+                           'height_package',
+                           'width_tolerance',
+                           'height_tolerance',
+                           'width_active',
+                           'height_active',
+                           'active_area_correction',
+                           'D_corner_x_active',
+                           'D_corner_y_active',
+                           'qe']
+
+        elif custom_params['type'] == 'circular':
+            params_list = ['name',
+                           'type',
+                           'diameter_packaging',
+                           'active_diameter',
+                           'diameter_tolerance',
+                           'active_area_correction',
+                           'qe']
+
+        else:
+            raise ValueError(
+                'The type parameter must be either "square" or "circular".')
+
+        params_missing = [
+            param for param in params_list if param not in custom_params]
+        if len(params_missing) > 0:
+            raise ValueError('The custom_params dictionary must have all the '
+                             'correct parameters.\nMissing parameters: '
+                             f'{params_missing}')
+
+    def build_custom_square_model(self, custom_params):
+        """Build a custom square PMT model."""
+
+        self.name = custom_params['name']
+        self.type = custom_params['type']
+        self.width = self.width_package = custom_params['width_package']
+        self.height = self.height_package = custom_params['height_package']
+        self.width_active = custom_params['width_active']
+        self.height_active = custom_params['height_active']
+        self.width_tolerance = custom_params['width_tolerance']
+        self.height_tolerance = custom_params['height_tolerance']
+        self.D_corner_x_active = custom_params['D_corner_x_active']
+        self.D_corner_y_active = custom_params['D_corner_y_active']
+        self.active_area_correction = custom_params['active_area_correction']
+        self.qe = custom_params['qe']
+
+    def build_custom_circular_model(self, custom_params):
+        """Build a custom circular PMT model."""
+
+        self.name = custom_params['name']
+        self.type = custom_params['type']
+        self.diameter_packaging = custom_params['diameter_packaging']
+        self.active_diameter = custom_params['active_diameter']
+        self.diameter_tolerance = custom_params['diameter_tolerance']
+        self.active_area_correction = custom_params['active_area_correction']
+        self.qe = custom_params['qe']
+
+    def get_model_geometry(self):
+        """Loads model geometric properties from the model file.
+
+        Raises:
+            ModuleNotFoundError: raised if the model file is not found.
+        """
+        try:
+            model_module = import_module(f'pmtarray.models.{self.model}')
+            model_class = getattr(model_module, self.model)
+            _model = model_class()
+
+            self.name = _model.name
+            self.type = _model.type
+
+            if self.type == 'circular':
+                self.diameter_packaging = _model.diameter_packaging
+                self.active_diameter = _model.active_diameter
+                self.diameter_tolerance = _model.diameter_tolerance
+                self.active_area_correction = _model.active_area_correction
+
+            elif self.type == 'square':
+                self.width = self.width_package = _model.width_package
+                self.height = self.height_package = _model.height_package
+                self.width_active = _model.width_active
+                self.height_active = _model.height_active
+                self.width_tolerance = _model.width_tolerance
+                self.height_tolerance = _model.height_tolerance
+                self.D_corner_x_active = _model.D_corner_x_active
+                self.D_corner_y_active = _model.D_corner_y_active
+                self.active_area_correction = _model.active_area_correction
+
+            else:
+                raise ValueError('Model type not recognized.')
+
+            self.qe = _model.qe
+            self.set_dependant_properties()
+
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError(
+                'Model not found. Please make a PR to add it.')
+
+    def set_dependant_properties(self):
+        """Defines dependant properties of the PMT units: total area, active
+        area and active area fraction.
+        """
+        if self.type == 'square':
+            self.width_unit = self.width + 2*self.width_tolerance
+            self.height_unit = self.height + 2*self.height_tolerance
+            self.total_area = self.width_unit*self.height_unit
+            self.active_area = (self.width_active *
+                                self.height_active *
+                                self.active_area_correction)
+
+        elif self.type == 'circular':
+            self.radius = (self.diameter_packaging)/2
+            self.active_radius = self.active_diameter/2
+            self.total_area = np.pi*self.radius**2
+            self.active_area = np.pi*self.active_radius**2 * self.active_area_correction
+        else:
+            raise ValueError('Model type not recognized.')
+        self.active_area_fraction = self.active_area/self.total_area
+
+    def get_unit_centre(self) -> Tuple[float, float]:
+        """Get the centre of the PMT unit
+
+        Returns:
+            tuple: (x,y) of the centre of the PMT unit in PMT unit 
+                coordinates.
+        """
+        if self.type == 'circular':
+            return (0, 0)
+        elif self.type == 'square':
+            return (self.width_unit/2, self.height_unit/2)
+        else:
+            raise ValueError('Model type not recognized.')
+
+    def get_unit_active_centre(self) -> Tuple[float, float]:
+        """Get the centre of the active area of the PMT unit
+
+        Returns:
+            tuple: (x,y) of the centre of the active area of the PMT unit 
+                in PMT unit coordinates.
+        """
+        if self.type == 'circular':
+            return (0, 0)
+        elif self.type == 'square':
+
+            x = self.D_corner_x_active + self.width_active/2
+            y = self.D_corner_y_active + self.height_active/2
+            return (x, y)
+        else:
+            raise ValueError('Model type not recognized.')
+
+    def plot_model(self, xy=(0, 0), figax=None):
+        """Make a plot of the PMT unit model
+
+        Args:
+            xy (tuple, optional): coordinates of the bottom left corner. 
+                Defaults to (0,0).
+            figax (_type_, optional): figure and axis environment. 
+                Defaults to None.
+
+        Returns:
+            _type_: updated figure and axis environment
+        """
+        if self.type == 'circular':
+            return self.plot_round_model(xy, figax)
+        elif self.type == 'square':
+            return self.plot_square_model(xy, figax)
+        else:
+            raise ValueError('Model type not recognized.')
+
+    def plot_square_model(self, xy=(0, 0), figax=None):
+        """Make a plot of the PMT unit model
+
+        Args:
+            xy (tuple, optional): coordinates of the bottom left corner. 
+                Defaults to (0,0).
+            figax (_type_, optional): figure and axis environment. 
+                Defaults to None.
+
+        Returns:
+            _type_: updated figure and axis environment
+        """
+        if self.type != 'square':
+            raise ValueError('This plotting function is for type square PMTs.')
+        if figax == None:
+            fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+        ax.add_patch(Rectangle((xy[0]+self.width_tolerance,
+                                xy[1]+self.height_tolerance),
+                               width=self.width,
+                               height=self.height,
+                               facecolor='gray',
+                               alpha=0.3, edgecolor='k',
+                               label='Packaging area', zorder=1))
+        ax.add_patch(Rectangle((xy[0]+self.D_corner_x_active,
+                                xy[1]+self.D_corner_y_active),
+                               width=self.width_active,
+                               height=self.height_active,
+                               facecolor='k', alpha=0.8, edgecolor='k',
+                               label='Active area', zorder=2))
+
+        geometric_centre = self.get_unit_centre()
+        active_centre = self.get_unit_active_centre()
+
+        ax.plot(geometric_centre[0], geometric_centre[1], 'o',
+                c='g', label='Geometric centre')
+        ax.plot(active_centre[0], active_centre[1], 'x',
+                c='r', label='Active centre')
+
+        ax.set_xlim(xy[0]-0.1*self.width_unit, xy[0]+1.1*self.width_unit)
+        ax.set_ylim(xy[1]-0.1*self.height_unit, xy[1]+1.1*self.height_unit)
+        ax.set_aspect('equal')
+        ax.legend()
+        ax.set_xlabel('x [mm]')
+        ax.set_ylabel('y [mm]')
+        ax.set_aspect('equal')
+        ax.grid(zorder=-10)
+
+        if figax == None:
+            plt.show()
+        else:
+            return fig, ax
+
+    def get_unit_patches(self, xy: np.ndarray) -> list:
+        """Get the patches of the PMT unit for plotting.
+
+        Args:
+            xy (np.ndarray): the coordinates of the bottom left corner of
+                the PMT unit.
+
+        Returns:
+            list: list of patches of the PMT units
+        """
+        if self.type == 'circular':
+            p = [Circle((xy[0], xy[1]),
+                        radius=self.radius,
+                        facecolor='gray',
+                        alpha=0.3, edgecolor='k', zorder=1),
+                 Circle((xy[0], xy[1]),
+                        radius=self.active_radius,
+                        facecolor='k', alpha=0.98, edgecolor='k', zorder=2)
+                 ]
+
+        elif self.type == 'square':
+            p = [Rectangle((xy[0]+self.width_tolerance,
+                            xy[1]+self.height_tolerance),
+                           width=self.width,
+                           height=self.height,
+                           facecolor='gray',
+                           alpha=0.3, edgecolor='k', zorder=3),
+                 Rectangle((xy[0]+self.D_corner_x_active,
+                            xy[1]+self.D_corner_y_active),
+                           width=self.width_active,
+                           height=self.height_active,
+                           facecolor='k', alpha=0.98,
+                           edgecolor='k', zorder=4)
+                 ]
+        return p
+
+    def plot_round_model(self, xy=(0, 0), figax=None):
+        """Make a plot of the PMT unit model
+
+        Args:
+            xy (tuple, optional): coordinates of the bottom left corner. 
+                Defaults to (0,0).
+            figax (_type_, optional): figure and axis environment. 
+                Defaults to None.
+
+        Returns:
+            _type_: updated figure and axis environment
+        """
+
+        if self.type != 'circular':
+            raise ValueError(
+                'This plotting function is for type circular PMTs.')
+
+        if figax == None:
+            fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+
+        ax.add_patch(Circle((xy[0], xy[1]),
+                            radius=self.radius,
+                            facecolor='gray',
+                            alpha=0.3, edgecolor='k',
+                            label='Packaging area', zorder=1))
+        ax.add_patch(Circle((xy[0], xy[1]),
+                            radius=self.active_radius,
+                            facecolor='k', alpha=0.8, edgecolor='k',
+                            label='Active area', zorder=2))
+        geometric_centre = self.get_unit_centre()
+        active_centre = self.get_unit_active_centre()
+
+        ax.plot(geometric_centre[0], geometric_centre[1], 'o',
+                c='g', label='Geometric centre')
+        ax.plot(active_centre[0], active_centre[1], 'x',
+                c='r', label='Active centre')
+
+        ax.set_xlim(xy[0]-1.1*self.radius, xy[0]+1.1*self.radius)
+        ax.set_ylim(xy[1]-1.1*self.radius, xy[1]+1.1*self.radius)
+        ax.set_aspect('equal')
+        fig.legend()
+        ax.set_xlabel('x [mm]')
+        ax.set_ylabel('y [mm]')
+        ax.set_aspect('equal')
+        ax.grid(zorder=-10)
+
+        if figax == None:
+            plt.show()
+        else:
+            return fig, ax
+
+    def get_properties_str(self) -> str:
+        main_string = f'Model: {self.name}\n'
+        main_string += f'--------------------------------------------\n'
+
+        if self.type == 'circular':
+            main_string += f'Diameter: {self.diameter_packaging} mm\n'
+            main_string += f'Active diameter: {self.active_diameter} mm\n'
+            main_string += f'Diameter tolerance: {self.diameter_tolerance} mm\n'
+
+        elif self.type == 'square':
+            main_string += f'Width: {self.width} mm\n'
+            main_string += f'Height: {self.height} mm\n'
+            main_string += f'Active width: {self.width_active} mm\n'
+            main_string += f'Active height: {self.height_active} mm\n'
+            main_string += f'Width tolerance: {self.width_tolerance} mm\n'
+            main_string += f'Height tolerance: {self.height_tolerance} mm\n'
+
+        main_string += '--------------------------------------------\n'
+        main_string += f'Total unit area: {self.total_area:.2f} mm^2\n'
+        main_string += f'Active area geometric correction: {self.active_area_correction:.2f}\n'
+        main_string += f'Active area: {self.active_area:.2f} mm^2\n'
+        main_string += '--------------------------------------------\n'
+        main_string += f'Active area fraction: {self.active_area_fraction*100:.2f} %\n'
+        main_string += f'Quantum efficiency: {self.qe*100:.2f} %'
+
+        return main_string
+
+    def print_properties(self) -> None:
+        """Print the main properties of the PMT model
+        """
+        print(self.get_properties_str())
+
+    def get_properties_df(self) -> pd.DataFrame:
+        """Get the main properties of the PMT model in a pandas DataFrame
+
+        Returns:
+            pd.DataFrame: DataFrame with the main properties of the PMT model
+        """
+        if self.type == 'circular':
+            properties = {'Property': ['Model',
+                                       'Type',
+                                       'Diameter [mm]',
+                                       'Active diameter [mm]',
+                                       'Diameter tolerance [mm]',
+                                       'Total area [mm^2]',
+                                       'Active area geometric correction',
+                                       'Active area [mm^2]',
+                                       'Active area fraction',
+                                       'Quantum efficiency'],
+
+                          'Value': [self.name, self.type,
+                                    self.diameter_packaging,
+                                    self.active_diameter,
+                                    self.diameter_tolerance,
+                                    round(self.total_area, 2),
+                                    round(self.active_area_correction, 2),
+                                    round(self.active_area, 2),
+                                    round(self.active_area_fraction, 2),
+                                    self.qe]}
+
+        elif self.type == 'square':
+            properties = {'Property': ['Model',
+                                       'Type',
+                                       'Width [mm]',
+                                       'Height [mm]',
+                                       'Active width [mm]',
+                                       'Active height [mm]',
+                                       'Width tolerance [mm]',
+                                       'Height tolerance [mm]',
+                                       'Total area [mm^2]',
+                                       'Active area geometric correction',
+                                       'Active area [mm^2]',
+                                       'Active area fraction',
+                                       'Quantum efficiency'],
+
+                          'Value': [self.name,
+                                    self.type,
+                                    self.width,
+                                    self.height,
+                                    self.width_active,
+                                    self.height_active,
+                                    self.width_tolerance,
+                                    self.height_tolerance,
+                                    round(self.total_area, 2),
+                                    round(self.active_area_correction, 2),
+                                    round(self.active_area, 2),
+                                    round(self.active_area_fraction, 2),
+                                    self.qe]}
+        else:
+            raise ValueError('Model type not recognized.')
+        return pd.DataFrame(properties)
