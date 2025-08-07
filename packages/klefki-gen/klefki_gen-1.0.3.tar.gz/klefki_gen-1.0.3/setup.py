ac1b2ffb5b502a9603e83fba3b71bbd6233cf41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 04:21:09 +0000

    Add package klefki-gen/1.0.3/klefki_gen-1.0.3.tar.gz

---
+++ packages/klefki-gen/klefki_gen-1.0.3.tar.gz/klefki_gen-1.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="klefki-gen",  # use sempre letras minúsculas aqui para evitar problemas no PyPI
+    version="1.0.3",
+    description="Secure and customizable password generator.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Matheus Girardi",
+    author_email="girardimatheus27@gmail.com",
+    packages=find_packages(include=["klefki", "klefki.*"]),
+    install_requires=[
+        "pyperclip"
+    ],
+    entry_points={
+        "console_scripts": [
+            "klefki=klefki_gen.__main__:cli"
+        ]
+    },
+    python_requires=">=3.6",
+    include_package_data=True,
+    license="MIT",
+    url="https://github.com/GirardiMatheus/Klefki",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Utilities",
+    ]
+)
