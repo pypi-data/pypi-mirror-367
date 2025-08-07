05e89d0b84a07470616c58cd9b9c27513f02debb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 04:21:11 +0000

    Add package android-mobile-mcp/2.0.0/android_mobile_mcp-2.0.0.tar.gz

---
+++ packages/android-mobile-mcp/android_mobile_mcp-2.0.0.tar.gz/android_mobile_mcp-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='android-mobile-mcp',
+    version='2.0.0',
+    author='erichung0906',
+    author_email='rthung96@gmail.com',
+    description='Android Mobile MCP',
+    url='https://github.com/erichung9060/Android-Mobile-MCP',
+    keywords=['android', "mobile", "mcp", "mobile-mcp", "android-mcp"],
+)
