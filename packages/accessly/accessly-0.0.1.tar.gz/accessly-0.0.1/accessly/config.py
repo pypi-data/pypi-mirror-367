7bd362f6371664df7ecd5169475a1524e39a36db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 05:12:26 +0000

    Add package accessly/0.0.1/accessly-0.0.1.tar.gz

---
+++ packages/accessly/accessly-0.0.1.tar.gz/accessly-0.0.1/accessly/config.py
@@ -0,0 +1,5 @@
+# store global settings and feature registry
+settings = {}  # user-defined settings like {"colorblind": "protanopia"}
+show_hooks = []
+
+registered_features = {}  # maps "feature_name" -> apply_function
