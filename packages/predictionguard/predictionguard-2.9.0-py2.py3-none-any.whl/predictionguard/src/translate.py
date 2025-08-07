257d85d507e0651e352f4ea02e79ec14436aa97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 05:12:16 +0000

    Add package predictionguard/2.9.0/predictionguard-2.9.0-py2.py3-none-any.whl

---
+++ packages/predictionguard/predictionguard-2.9.0-py2.py3-none-any.whl/predictionguard/src/translate.py
@@ -0,0 +1,24 @@
+from typing import Any, Dict, Optional
+
+
+class Translate:
+    """No longer supported."""
+
+    def __init__(self, api_key, url):
+        self.api_key = api_key
+        self.url = url
+
+    def create(
+            self,
+            text: Optional[str],
+            source_lang: Optional[str],
+            target_lang: Optional[str],
+            use_third_party_engine: Optional[bool] = False
+        ) -> Dict[str, Any]:
+        """
+        No longer supported
+        """
+
+        raise ValueError(
+            "The translate functionality is no longer supported."
+        )
