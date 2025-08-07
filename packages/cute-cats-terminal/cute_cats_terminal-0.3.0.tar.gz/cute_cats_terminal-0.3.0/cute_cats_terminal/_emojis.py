805ffae7196da2ca5c757ddaacd8da8435479c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 17:09:16 +0000

    Add package cute-cats-terminal/0.3.0/cute_cats_terminal-0.3.0.tar.gz

---
+++ packages/cute-cats-terminal/cute_cats_terminal-0.3.0.tar.gz/cute_cats_terminal-0.3.0/cute_cats_terminal/_emojis.py
@@ -0,0 +1,27 @@
+emoji1 = """
+≽^•⩊•^≼
+"""
+
+emoji2 = """
+ฅ^•ﻌ•^ฅ
+"""
+
+emoji3 = """
+ᓚ₍ ^. .^₎
+"""
+
+emoji4 = """
+/ᐠ - ˕ -マ
+"""
+
+emoji5 = """
+≽^• ˕ • ྀི≼
+"""
+
+EMOJI_DICT = {
+    "playful cat": emoji1,
+    "waving cat": emoji2,
+    "sleepy cat": emoji3,
+    "shy cat": emoji4,
+    "blushing cat": emoji5,
+}
