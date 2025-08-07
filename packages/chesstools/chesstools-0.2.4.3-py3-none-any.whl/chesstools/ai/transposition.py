c6f2d2ada14ca70e9635b2be3308991460e15714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 15:07:44 +0000

    Add package chesstools/0.2.4.3/chesstools-0.2.4.3-py3-none-any.whl

---
+++ packages/chesstools/chesstools-0.2.4.3-py3-none-any.whl/chesstools/ai/transposition.py
@@ -0,0 +1,68 @@
+from fyg import config as confyg
+from fyg.util import Loggy
+import databae as db
+
+confyg.log.allow.append("db")
+db.config.pool.update("null", False)
+
+class Transposition(db.ModelBase):
+    sig = db.String()
+    score = db.Integer()
+    depth = db.Integer()
+
+class Table(Loggy):
+    def __init__(self):
+        self._all = {}
+        self._deepest = {}
+        self.prepped = 0
+        self.skips = 0
+        self.hits = 0
+
+    def add(self, sig, dtup, withdb=False):
+        if sig not in self._deepest or self._deepest[sig][0] < dtup[0]:
+            self._deepest[sig] = dtup
+        if withdb:
+            if sig not in self._all:
+                self._all[sig] = []
+            self._all[sig].append(dtup)
+
+    def prep(self, allsigs):
+        sigs = list(filter(lambda s : s not in self._deepest, allsigs))
+        transes = Transposition.query(Transposition.sig.in_(sigs)).all()
+        slen = len(sigs)
+        self.prepped += slen
+        self.hits += len(transes)
+        self.skips += len(allsigs) - slen
+        for trans in transes:
+            self.add(trans.sig, (trans.depth, trans.score))
+
+    def get(self, sig, depth, withdb=False):
+        if sig in self._deepest:
+            dtup = self._deepest[sig]
+            if dtup[0] >= depth:
+                return dtup
+        if withdb:
+            trans = Transposition.query(Transposition.sig == sig).order(-Transposition.depth).get()
+            if trans and trans.depth >= depth:
+                return (trans.depth, trans.score)
+
+    def score(self, variation, score, depth, withdb=False):
+        self.add(variation.signature(), (depth, score), withdb)
+
+    def trans(self, sig, tup):
+        trans = Transposition()
+        trans.depth = tup[0]
+        trans.score = tup[1]
+        trans.sig = sig
+        return trans
+
+    def transets(self, sig):
+        return list(map(lambda tup : self.trans(sig, tup), self._all[sig]))
+
+    def flush(self):
+        transes = sum(list(map(self.transets, self._all.keys())), [])
+        db.put_multi(transes)
+        self._all = {}
+        self.log("saved:", len(transes), "cache:", len(self._deepest.keys()),
+            "prepped:", self.prepped, "hits:", self.hits, "skips:", self.skips)
+        self.prepped = self.skips = self.hits = 0
