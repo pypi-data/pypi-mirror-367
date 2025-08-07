2096dd6f33f075a05569281327edc14575cabad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 15:07:01 +0000

    Add package midea-local/6.4.0/midea_local-6.4.0.tar.gz

---
+++ packages/midea-local/midea_local-6.4.0.tar.gz/midea_local-6.4.0/midealocal/devices/ca/__init__.py
@@ -0,0 +1,108 @@
+"""Midea local CA device."""
+
+import logging
+from enum import StrEnum
+from typing import Any
+
+from midealocal.const import DeviceType, ProtocolVersion
+from midealocal.device import MideaDevice
+
+from .message import MessageCAResponse, MessageQuery
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class DeviceAttributes(StrEnum):
+    """Midea CA device attributes."""
+
+    mode = "mode"
+    energy_consumption = "energy_consumption"
+    refrigerator_actual_temp = "refrigerator_actual_temp"
+    freezer_actual_temp = "freezer_actual_temp"
+    flex_zone_actual_temp = "flex_zone_actual_temp"
+    right_flex_zone_actual_temp = "right_flex_zone_actual_temp"
+    refrigerator_setting_temp = "refrigerator_setting_temp"
+    freezer_setting_temp = "freezer_setting_temp"
+    flex_zone_setting_temp = "flex_zone_setting_temp"
+    right_flex_zone_setting_temp = "right_flex_zone_setting_temp"
+    refrigerator_door_overtime = "refrigerator_door_overtime"
+    freezer_door_overtime = "freezer_door_overtime"
+    bar_door_overtime = "bar_door_overtime"
+    flex_zone_door_overtime = "flex_zone_door_overtime"
+    refrigerator_door = "refrigerator_door"
+    freezer_door = "freezer_door"
+    bar_door = "bar_door"
+    flex_zone_door = "flex_zone_door"
+
+
+class MideaCADevice(MideaDevice):
+    """Midea CA device."""
+
+    def __init__(
+        self,
+        name: str,
+        device_id: int,
+        ip_address: str,
+        port: int,
+        token: str,
+        key: str,
+        device_protocol: ProtocolVersion,
+        model: str,
+        subtype: int,
+        customize: str,  # noqa: ARG002
+    ) -> None:
+        """Initialize Midea CA device."""
+        super().__init__(
+            name=name,
+            device_id=device_id,
+            device_type=DeviceType.CA,
+            ip_address=ip_address,
+            port=port,
+            token=token,
+            key=key,
+            device_protocol=device_protocol,
+            model=model,
+            subtype=subtype,
+            attributes={
+                DeviceAttributes.energy_consumption: None,
+                DeviceAttributes.refrigerator_actual_temp: None,
+                DeviceAttributes.freezer_actual_temp: None,
+                DeviceAttributes.flex_zone_actual_temp: None,
+                DeviceAttributes.right_flex_zone_actual_temp: None,
+                DeviceAttributes.refrigerator_setting_temp: None,
+                DeviceAttributes.freezer_setting_temp: None,
+                DeviceAttributes.flex_zone_setting_temp: None,
+                DeviceAttributes.right_flex_zone_setting_temp: None,
+                DeviceAttributes.refrigerator_door_overtime: False,
+                DeviceAttributes.freezer_door_overtime: False,
+                DeviceAttributes.bar_door_overtime: False,
+                DeviceAttributes.flex_zone_door_overtime: False,
+                DeviceAttributes.refrigerator_door: False,
+                DeviceAttributes.freezer_door: False,
+                DeviceAttributes.bar_door: False,
+                DeviceAttributes.flex_zone_door: False,
+            },
+        )
+        self._modes = [""]
+
+    def build_query(self) -> list[MessageQuery]:
+        """Midea CA device build query."""
+        return [MessageQuery(self._message_protocol_version)]
+
+    def process_message(self, msg: bytes) -> dict[str, Any]:
+        """Midea CA device process message."""
+        message = MessageCAResponse(msg)
+        _LOGGER.debug("[%s] Received: %s", self.device_id, message)
+        new_status = {}
+        for status in self._attributes:
+            if hasattr(message, str(status)):
+                self._attributes[status] = getattr(message, str(status))
+                new_status[str(status)] = getattr(message, str(status))
+        return new_status
+
+    def set_attribute(self, attr: str, value: bool | int | str) -> None:
+        """Midea CA device set attribute."""
+
+
+class MideaAppliance(MideaCADevice):
+    """Midea CA appliance."""
---
+++ packages/midea-local/midea_local-6.4.0.tar.gz/midea_local-6.4.0/midealocal/devices/ca/message.py
@@ -0,0 +1,174 @@
+"""Midea local CA message."""
+
+from midealocal.const import DeviceType
+from midealocal.message import (
+    ListTypes,
+    MessageBody,
+    MessageRequest,
+    MessageResponse,
+    MessageType,
+)
+
+MIN_CA_GENERAL_BODY_LENGTH = 20
+TEMP_POS_LOWER_VALUE = 1
+TEMP_POS_UPPER_VALUE = 29
+TEMP_NEG_LOWER_VALUE = 49
+TEMP_NEG_UPPER_VALUE = 54
+
+
+class MessageCABase(MessageRequest):
+    """CA message base."""
+
+    def __init__(
+        self,
+        protocol_version: int,
+        message_type: MessageType,
+        body_type: ListTypes,
+    ) -> None:
+        """Initialize CA message base."""
+        super().__init__(
+            device_type=DeviceType.CA,
+            protocol_version=protocol_version,
+            message_type=message_type,
+            body_type=body_type,
+        )
+
+    @property
+    def _body(self) -> bytearray:
+        raise NotImplementedError
+
+
+class MessageQuery(MessageCABase):
+    """CA message query."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize CA message query."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.query,
+            body_type=ListTypes.X00,
+        )
+
+    @property
+    def _body(self) -> bytearray:
+        return bytearray([])
+
+
+class CAGeneralMessageBody(MessageBody):
+    """CA message general body."""
+
+    def __init__(self, body: bytearray) -> None:
+        """Initialize CA message general body."""
+        super().__init__(body)
+        self.refrigerator_setting_temp = body[2] & 0x0F
+        self.freezer_setting_temp = -12 - ((body[2] & 0xF0) >> 4)
+        flex_zone_setting_temp = body[3]
+        right_flex_zone_setting_temp = body[4]
+
+        if TEMP_POS_LOWER_VALUE <= flex_zone_setting_temp <= TEMP_POS_UPPER_VALUE:
+            self.flex_zone_setting_temp = flex_zone_setting_temp - 19
+        elif TEMP_NEG_LOWER_VALUE <= flex_zone_setting_temp <= TEMP_NEG_UPPER_VALUE:
+            self.flex_zone_setting_temp = 30 - flex_zone_setting_temp
+        else:
+            self.flex_zone_setting_temp = 0
+        if TEMP_POS_LOWER_VALUE <= right_flex_zone_setting_temp <= TEMP_POS_UPPER_VALUE:
+            self.right_flex_zone_setting_temp = right_flex_zone_setting_temp - 19
+        elif (
+            TEMP_NEG_LOWER_VALUE <= right_flex_zone_setting_temp <= TEMP_NEG_UPPER_VALUE
+        ):
+            self.right_flex_zone_setting_temp = 30 - right_flex_zone_setting_temp
+        else:
+            self.right_flex_zone_setting_temp = 0
+
+        self.energy_consumption = (body[13] << 8) + body[12]
+        self.refrigerator_actual_temp = (body[17] - 100) / 2
+        self.freezer_actual_temp = (body[18] - 100) / 2
+        self.flex_zone_actual_temp = (body[19] - 100) / 2
+        self.right_flex_zone_actual_temp = (body[20] - 100) / 2
+
+
+class CAExceptionMessageBody(MessageBody):
+    """CA message exception body."""
+
+    def __init__(self, body: bytearray) -> None:
+        """Initialize CA message exception body."""
+        super().__init__(body)
+        self.refrigerator_door_overtime = (body[1] & 0x01) > 0
+        self.freezer_door_overtime = (body[1] & 0x02) > 0
+        self.bar_door_overtime = (body[1] & 0x04) > 0
+        self.flex_zone_door_overtime = (body[1] & 0x08) > 0
+
+
+class CANotify00MessageBody(MessageBody):
+    """CA message notify00 body."""
+
+    def __init__(self, body: bytearray) -> None:
+        """Initialize CA message notify00 body."""
+        super().__init__(body)
+        self.refrigerator_door = (body[1] & 0x01) > 0
+        self.freezer_door = (body[1] & 0x02) > 0
+        self.bar_door = (body[1] & 0x04) > 0
+        self.flex_zone_door = (body[1] & 0x010) > 0
+
+
+class CANotify01MessageBody(MessageBody):
+    """CA message notify01 body."""
+
+    def __init__(self, body: bytearray) -> None:
+        """Initialize CA message notify01 body."""
+        super().__init__(body)
+        self.refrigerator_setting_temp = body[37]
+        self.freezer_setting_temp = -12 - body[38]
+        flex_zone_setting_temp = body[39]
+        right_flex_zone_setting_temp = body[40]
+
+        if TEMP_POS_LOWER_VALUE <= flex_zone_setting_temp <= TEMP_POS_UPPER_VALUE:
+            self.flex_zone_setting_temp = flex_zone_setting_temp - 19
+        elif TEMP_NEG_LOWER_VALUE <= flex_zone_setting_temp <= TEMP_NEG_UPPER_VALUE:
+            self.flex_zone_setting_temp = 30 - flex_zone_setting_temp
+        else:
+            self.flex_zone_setting_temp = 0
+        if TEMP_POS_LOWER_VALUE <= right_flex_zone_setting_temp <= TEMP_POS_UPPER_VALUE:
+            self.right_flex_zone_setting_temp = right_flex_zone_setting_temp - 19
+        elif (
+            TEMP_NEG_LOWER_VALUE <= right_flex_zone_setting_temp <= TEMP_NEG_UPPER_VALUE
+        ):
+            self.right_flex_zone_setting_temp = 30 - right_flex_zone_setting_temp
+        else:
+            self.right_flex_zone_setting_temp = 0
+
+
+class MessageCAResponse(MessageResponse):
+    """CA message response."""
+
+    def __init__(self, message: bytes) -> None:
+        """Initialize CA message response."""
+        super().__init__(bytearray(message))
+        if (
+            (
+                self.message_type in [MessageType.query, MessageType.set]
+                and self.body_type == ListTypes.X00
+            )
+            or (
+                self.message_type == MessageType.notify1
+                and self.body_type == ListTypes.X02
+            )
+        ) and len(super().body) > MIN_CA_GENERAL_BODY_LENGTH:
+            self.set_body(CAGeneralMessageBody(super().body))
+        elif (
+            self.message_type == MessageType.exception
+            and self.body_type == ListTypes.X01
+        ) or (
+            self.message_type == MessageType.query and self.body_type == ListTypes.X02
+        ):
+            self.set_body(CAExceptionMessageBody(super().body))
+        elif (
+            self.message_type == MessageType.notify1 and self.body_type == ListTypes.X00
+        ):
+            self.set_body(CANotify00MessageBody(super().body))
+        elif (
+            self.message_type in [MessageType.query, MessageType.notify1]
+            and self.body_type == ListTypes.X01
+        ):
+            self.set_body(CANotify01MessageBody(super().body))
+        self.set_attr()
