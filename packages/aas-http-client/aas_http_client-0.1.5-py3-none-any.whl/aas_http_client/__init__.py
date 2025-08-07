fed0f31e84f2d25b2ad24776aa554fb4e7271ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 03:35:17 +0000

    Add package aas-http-client/0.1.5/aas_http_client-0.1.5-py3-none-any.whl

---
+++ packages/aas-http-client/aas_http_client-0.1.5-py3-none-any.whl/aas_http_client/__init__.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import importlib.metadata
+
+__copyright__ = f"Copyright (C) {datetime.now().year} :em engineering methods AG. All rights reserved."
+__author__ = "Daniel Klein"
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0-dev"
+    
+__project__ = "aas-http-client"
+__package__ = "aas-http-client"
+
+from aas_http_client.core.version_check import check_for_update
+from aas_http_client.client import create_client_by_config, create_client_by_url, AasHttpClient
+
+check_for_update()
+
+__all__ = ["create_client_by_config", "create_client_by_url", "AasHttpClient"]
