662927f258ab3abb7fec6e8f5b163b45ced39e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 06:06:06 +0000

    Add package mapillary-tools/0.14.0/mapillary_tools-0.14.0.tar.gz

---
+++ packages/mapillary-tools/mapillary_tools-0.14.0.tar.gz/mapillary_tools-0.14.0/mapillary_tools/api_v4.py
@@ -0,0 +1,416 @@
+from __future__ import annotations
+
+import enum
+import logging
+import os
+import ssl
+import typing as T
+from json import dumps
+
+import requests
+from requests.adapters import HTTPAdapter
+
+LOG = logging.getLogger(__name__)
+MAPILLARY_CLIENT_TOKEN = os.getenv(
+    "MAPILLARY_CLIENT_TOKEN", "MLY|5675152195860640|6b02c72e6e3c801e5603ab0495623282"
+)
+MAPILLARY_GRAPH_API_ENDPOINT = os.getenv(
+    "MAPILLARY_GRAPH_API_ENDPOINT", "https://graph.mapillary.com"
+)
+REQUESTS_TIMEOUT = 60  # 1 minutes
+USE_SYSTEM_CERTS: bool = False
+
+
+class HTTPContentError(Exception):
+    """
+    Raised when the HTTP response is ok (200) but the content is not as expected
+    e.g. not JSON or not a valid response.
+    """
+
+    def __init__(self, message: str, response: requests.Response):
+        self.response = response
+        super().__init__(message)
+
+
+class ClusterFileType(enum.Enum):
+    ZIP = "zip"
+    BLACKVUE = "mly_blackvue_video"
+    CAMM = "mly_camm_video"
+    MLY_BUNDLE_MANIFEST = "mly_bundle_manifest"
+
+
+class HTTPSystemCertsAdapter(HTTPAdapter):
+    """
+    This adapter uses the system's certificate store instead of the certifi module.
+
+    The implementation is based on the project https://pypi.org/project/pip-system-certs/,
+    which has a system-wide effect.
+    """
+
+    def init_poolmanager(self, *args, **kwargs):
+        ssl_context = ssl.create_default_context()
+        ssl_context.load_default_certs()
+        kwargs["ssl_context"] = ssl_context
+
+        super().init_poolmanager(*args, **kwargs)
+
+    def cert_verify(self, *args, **kwargs):
+        super().cert_verify(*args, **kwargs)
+
+        # By default Python requests uses the ca_certs from the certifi module
+        # But we want to use the certificate store instead.
+        # By clearing the ca_certs variable we force it to fall back on that behaviour (handled in urllib3)
+        if "conn" in kwargs:
+            conn = kwargs["conn"]
+        else:
+            conn = args[0]
+
+        conn.ca_certs = None
+
+
+@T.overload
+def _truncate(s: bytes, limit: int = 256) -> bytes | str: ...
+
+
+@T.overload
+def _truncate(s: str, limit: int = 256) -> str: ...
+
+
+def _truncate(s, limit=256):
+    if limit < len(s):
+        if isinstance(s, bytes):
+            try:
+                s = s.decode("utf-8")
+            except UnicodeDecodeError:
+                pass
+        remaining = len(s) - limit
+        if isinstance(s, bytes):
+            return s[:limit] + f"...({remaining} bytes truncated)".encode("utf-8")
+        else:
+            return str(s[:limit]) + f"...({remaining} chars truncated)"
+    else:
+        return s
+
+
+def _sanitize(headers: T.Mapping[T.Any, T.Any]) -> T.Mapping[T.Any, T.Any]:
+    new_headers = {}
+
+    for k, v in headers.items():
+        if k.lower() in [
+            "authorization",
+            "cookie",
+            "x-fb-access-token",
+            "access-token",
+            "access_token",
+            "password",
+            "user_upload_token",
+        ]:
+            new_headers[k] = "[REDACTED]"
+        else:
+            if isinstance(v, (str, bytes)):
+                new_headers[k] = T.cast(T.Any, _truncate(v))
+            else:
+                new_headers[k] = v
+
+    return new_headers
+
+
+def _log_debug_request(
+    method: str,
+    url: str,
+    json: dict | None = None,
+    params: dict | None = None,
+    headers: dict | None = None,
+    timeout: T.Any = None,
+):
+    if logging.getLogger().getEffectiveLevel() <= logging.DEBUG:
+        return
+
+    msg = f"HTTP {method} {url}"
+
+    if USE_SYSTEM_CERTS:
+        msg += " (w/sys_certs)"
+
+    if json:
+        t = _truncate(dumps(_sanitize(json)))
+        msg += f" JSON={t}"
+
+    if params:
+        msg += f" PARAMS={_sanitize(params)}"
+
+    if headers:
+        msg += f" HEADERS={_sanitize(headers)}"
+
+    if timeout is not None:
+        msg += f" TIMEOUT={timeout}"
+
+    msg = msg.replace("\n", "\\n")
+
+    LOG.debug(msg)
+
+
+def _log_debug_response(resp: requests.Response):
+    if logging.getLogger().getEffectiveLevel() <= logging.DEBUG:
+        return
+
+    elapsed = resp.elapsed.total_seconds() * 1000  # Convert to milliseconds
+    msg = f"HTTP {resp.status_code} {resp.reason} ({elapsed:.0f} ms): {str(_truncate_response_content(resp))}"
+
+    LOG.debug(msg)
+
+
+def _truncate_response_content(resp: requests.Response) -> str | bytes:
+    try:
+        json_data = resp.json()
+    except requests.JSONDecodeError:
+        if resp.content is not None:
+            data = _truncate(resp.content)
+        else:
+            data = ""
+    else:
+        if isinstance(json_data, dict):
+            data = _truncate(dumps(_sanitize(json_data)))
+        else:
+            data = _truncate(str(json_data))
+
+    if isinstance(data, bytes):
+        return data.replace(b"\n", b"\\n")
+
+    elif isinstance(data, str):
+        return data.replace("\n", "\\n")
+
+    return data
+
+
+def readable_http_error(ex: requests.HTTPError) -> str:
+    return readable_http_response(ex.response)
+
+
+def readable_http_response(resp: requests.Response) -> str:
+    return f"{resp.request.method} {resp.url} => {resp.status_code} {resp.reason}: {str(_truncate_response_content(resp))}"
+
+
+def request_post(
+    url: str,
+    data: T.Any | None = None,
+    json: dict | None = None,
+    disable_debug=False,
+    **kwargs,
+) -> requests.Response:
+    global USE_SYSTEM_CERTS
+
+    if not disable_debug:
+        _log_debug_request(
+            "POST",
+            url,
+            json=json,
+            params=kwargs.get("params"),
+            headers=kwargs.get("headers"),
+            timeout=kwargs.get("timeout"),
+        )
+
+    if USE_SYSTEM_CERTS:
+        with requests.Session() as session:
+            session.mount("https://", HTTPSystemCertsAdapter())
+            resp = session.post(url, data=data, json=json, **kwargs)
+
+    else:
+        try:
+            resp = requests.post(url, data=data, json=json, **kwargs)
+        except requests.exceptions.SSLError as ex:
+            if "SSLCertVerificationError" not in str(ex):
+                raise ex
+            USE_SYSTEM_CERTS = True
+            # HTTPSConnectionPool(host='graph.mapillary.com', port=443): Max retries exceeded with url: /login (Caused by SSLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1018)')))
+            LOG.warning(
+                "SSL error occurred, falling back to system SSL certificates: %s", ex
+            )
+            return request_post(url, data=data, json=json, **kwargs)
+
+    if not disable_debug:
+        _log_debug_response(resp)
+
+    return resp
+
+
+def request_get(
+    url: str, params: dict | None = None, disable_debug=False, **kwargs
+) -> requests.Response:
+    global USE_SYSTEM_CERTS
+
+    if not disable_debug:
+        _log_debug_request(
+            "GET",
+            url,
+            params=kwargs.get("params"),
+            headers=kwargs.get("headers"),
+            # Do not log timeout here as it's always set to REQUESTS_TIMEOUT
+            timeout=None,
+        )
+
+    if USE_SYSTEM_CERTS:
+        with requests.Session() as session:
+            session.mount("https://", HTTPSystemCertsAdapter())
+            resp = session.get(url, params=params, **kwargs)
+    else:
+        try:
+            resp = requests.get(url, params=params, **kwargs)
+        except requests.exceptions.SSLError as ex:
+            if "SSLCertVerificationError" not in str(ex):
+                raise ex
+            USE_SYSTEM_CERTS = True
+            # HTTPSConnectionPool(host='graph.mapillary.com', port=443): Max retries exceeded with url: /login (Caused by SSLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1018)')))
+            LOG.warning(
+                "SSL error occurred, falling back to system SSL certificates: %s", ex
+            )
+            resp = request_get(url, params=params, **kwargs)
+
+    if not disable_debug:
+        _log_debug_response(resp)
+
+    return resp
+
+
+def is_auth_error(resp: requests.Response) -> bool:
+    if resp.status_code in [401, 403]:
+        return True
+
+    if resp.status_code in [400]:
+        try:
+            error_body = resp.json()
+        except Exception:
+            error_body = {}
+
+        type = error_body.get("debug_info", {}).get("type")
+        if type in ["NotAuthorizedError"]:
+            return True
+
+    return False
+
+
+def extract_auth_error_message(resp: requests.Response) -> str:
+    assert is_auth_error(resp), "has to be an auth error"
+
+    try:
+        error_body = resp.json()
+    except Exception:
+        error_body = {}
+
+    # from Graph APIs
+    message = error_body.get("error", {}).get("message")
+    if message is not None:
+        return str(message)
+
+    # from upload service
+    message = error_body.get("debug_info", {}).get("message")
+    if message is not None:
+        return str(message)
+
+    return resp.text
+
+
+def get_upload_token(email: str, password: str) -> requests.Response:
+    resp = request_post(
+        f"{MAPILLARY_GRAPH_API_ENDPOINT}/login",
+        headers={"Authorization": f"OAuth {MAPILLARY_CLIENT_TOKEN}"},
+        json={"email": email, "password": password, "locale": "en_US"},
+        timeout=REQUESTS_TIMEOUT,
+    )
+    resp.raise_for_status()
+    return resp
+
+
+def fetch_organization(
+    user_access_token: str, organization_id: int | str
+) -> requests.Response:
+    resp = request_get(
+        f"{MAPILLARY_GRAPH_API_ENDPOINT}/{organization_id}",
+        params={
+            "fields": ",".join(["slug", "description", "name"]),
+        },
+        headers={
+            "Authorization": f"OAuth {user_access_token}",
+        },
+        timeout=REQUESTS_TIMEOUT,
+    )
+    resp.raise_for_status()
+    return resp
+
+
+def fetch_user_or_me(
+    user_access_token: str, user_id: int | str | None = None
+) -> requests.Response:
+    if user_id is None:
+        url = f"{MAPILLARY_GRAPH_API_ENDPOINT}/me"
+    else:
+        url = f"{MAPILLARY_GRAPH_API_ENDPOINT}/{user_id}"
+
+    resp = request_get(
+        url,
+        params={
+            "fields": ",".join(["id", "username"]),
+        },
+        headers={
+            "Authorization": f"OAuth {user_access_token}",
+        },
+        timeout=REQUESTS_TIMEOUT,
+    )
+
+    resp.raise_for_status()
+    return resp
+
+
+ActionType = T.Literal[
+    "upload_started_upload", "upload_finished_upload", "upload_failed_upload"
+]
+
+
+def log_event(action_type: ActionType, properties: dict) -> requests.Response:
+    resp = request_post(
+        f"{MAPILLARY_GRAPH_API_ENDPOINT}/logging",
+        json={"action_type": action_type, "properties": properties},
+        headers={
+            "Authorization": f"OAuth {MAPILLARY_CLIENT_TOKEN}",
+        },
+        timeout=REQUESTS_TIMEOUT,
+        disable_debug=True,
+    )
+    resp.raise_for_status()
+    return resp
+
+
+def finish_upload(
+    user_access_token: str,
+    file_handle: str,
+    cluster_filetype: ClusterFileType,
+    organization_id: int | str | None = None,
+) -> requests.Response:
+    data: dict[str, str | int] = {
+        "file_handle": file_handle,
+        "file_type": cluster_filetype.value,
+    }
+    if organization_id is not None:
+        data["organization_id"] = organization_id
+
+    resp = request_post(
+        f"{MAPILLARY_GRAPH_API_ENDPOINT}/finish_upload",
+        headers={
+            "Authorization": f"OAuth {user_access_token}",
+        },
+        json=data,
+        timeout=REQUESTS_TIMEOUT,
+    )
+
+    resp.raise_for_status()
+
+    return resp
+
+
+def jsonify_response(resp: requests.Response) -> T.Any:
+    """
+    Convert the response to JSON, raising HTTPContentError if the response is not JSON.
+    """
+    try:
+        return resp.json()
+    except requests.JSONDecodeError as ex:
+        raise HTTPContentError("Invalid JSON response", resp) from ex
---
+++ packages/mapillary-tools/mapillary_tools-0.14.0.tar.gz/mapillary_tools-0.14.0/mapillary_tools/commands/__main__.py
@@ -0,0 +1,180 @@
+import argparse
+import enum
+import logging
+import sys
+from pathlib import Path
+
+import requests
+
+from .. import api_v4, constants, exceptions, VERSION
+from ..upload import log_exception
+from . import (
+    authenticate,
+    process,
+    process_and_upload,
+    sample_video,
+    upload,
+    video_process,
+    video_process_and_upload,
+    zip,
+)
+
+mapillary_tools_commands = [
+    process,
+    upload,
+    sample_video,
+    video_process,
+    authenticate,
+    process_and_upload,
+    video_process_and_upload,
+    zip,
+]
+
+
+# do not use __name__ here is because if you run tools as a module, __name__ will be "__main__"
+LOG = logging.getLogger("mapillary_tools")
+
+
+# Handle shared arguments/options here
+def add_general_arguments(parser, command):
+    if command in ["sample_video", "video_process", "video_process_and_upload"]:
+        parser.add_argument(
+            "video_import_path",
+            help="Path to a video or directory with one or more video files.",
+            type=Path,
+        )
+        parser.add_argument(
+            "import_path",
+            help=f"Path to where the images from video sampling will be saved. [default: {{VIDEO_IMPORT_PATH}}/{constants.SAMPLED_VIDEO_FRAMES_FILENAME}]",
+            nargs="?",
+            type=Path,
+        )
+        parser.add_argument(
+            "--skip_subfolders",
+            help="Skip all subfolders and import only the images in the given VIDEO_IMPORT_PATH.",
+            action="store_true",
+            default=False,
+            required=False,
+        )
+    elif command in ["upload"]:
+        parser.add_argument(
+            "import_path",
+            help="Paths to your images or videos.",
+            nargs="+",
+            type=Path,
+        )
+    elif command in ["process", "process_and_upload"]:
+        parser.add_argument(
+            "import_path",
+            help="Paths to your images or videos.",
+            nargs="+",
+            type=Path,
+        )
+        parser.add_argument(
+            "--skip_subfolders",
+            help="Skip all subfolders and import only the images in the given IMPORT_PATH.",
+            action="store_true",
+            default=False,
+            required=False,
+        )
+
+
+def configure_logger(logger: logging.Logger, stream=None) -> None:
+    formatter = logging.Formatter("%(asctime)s - %(levelname)-7s - %(message)s")
+    handler = logging.StreamHandler(stream)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+def _log_params(argvars: dict) -> None:
+    MAX_ENTRIES = 5
+
+    def _stringify(x) -> str:
+        if isinstance(x, enum.Enum):
+            return x.value
+        else:
+            return str(x)
+
+    for k, v in argvars.items():
+        if v is None:
+            continue
+        if callable(v):
+            continue
+        if k in ["jwt", "user_password"]:
+            assert isinstance(v, str), type(v)
+            v = "******"
+        if isinstance(v, (list, set, tuple)):
+            entries = [_stringify(x) for x in v]
+            if len(entries) <= MAX_ENTRIES:
+                v = ", ".join(entries)
+            else:
+                v = (
+                    ", ".join(entries[:MAX_ENTRIES])
+                ) + f" and {len(entries) - MAX_ENTRIES} more"
+        else:
+            v = _stringify(v)
+        LOG.debug("CLI param: %s: %s", k, v)
+
+
+def main():
+    version_text = f"mapillary_tools version {VERSION}"
+
+    parser = argparse.ArgumentParser(
+        "mapillary_tool",
+    )
+    parser.add_argument(
+        "--version",
+        help="show the version of mapillary tools and exit",
+        action="version",
+        version=version_text,
+    )
+    parser.add_argument(
+        "--verbose",
+        help="show verbose",
+        action="store_true",
+        default=False,
+        required=False,
+    )
+    parser.set_defaults(func=lambda _: parser.print_help())
+
+    all_commands = [module.Command() for module in mapillary_tools_commands]
+
+    subparsers = parser.add_subparsers(
+        description="please choose one of the available subcommands",
+    )
+    for command in all_commands:
+        cmd_parser = subparsers.add_parser(
+            command.name, help=command.help, conflict_handler="resolve"
+        )
+        add_general_arguments(cmd_parser, command.name)
+        command.add_basic_arguments(cmd_parser)
+        cmd_parser.set_defaults(func=command.run)
+
+    args = parser.parse_args()
+
+    log_level = logging.DEBUG if args.verbose else logging.INFO
+    configure_logger(LOG, sys.stderr)
+    LOG.setLevel(log_level)
+
+    LOG.debug("%s", version_text)
+    argvars = vars(args)
+    _log_params(argvars)
+
+    try:
+        args.func(argvars)
+    except requests.HTTPError as ex:
+        log_exception(ex)
+        # TODO: standardize exit codes as exceptions.MapillaryUserError
+        sys.exit(16)
+
+    except api_v4.HTTPContentError as ex:
+        log_exception(ex)
+        sys.exit(17)
+
+    except exceptions.MapillaryUserError as ex:
+        log_exception(ex)
+        sys.exit(ex.exit_code)
+
+
+if __name__ == "__main__":
+    main()
