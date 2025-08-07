6e45abe24b55e1ce608d60121ac8a6070913ccf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 04:23:03 +0000

    Add package lumify/1.0/lumify-1.0.0.tar.gz

---
+++ packages/lumify/lumify-1.0.0.tar.gz/lumify-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='lumify',
+	version='1.0.0',
+	author='Malix',
+	author_email='malixfloof@gmail.com',
+	description='lumify',
+	packages=['lumify'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.11',
+)
