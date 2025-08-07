78615b582762172636478f63888cdbfd11a38d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 17:08:56 +0000

    Add package pyobs-core/1.26.4/pyobs_core-1.26.4.tar.gz

---
+++ packages/pyobs-core/pyobs_core-1.26.4.tar.gz/pyobs_core-1.26.4/pyobs/images/processors/detection/__init__.py
@@ -0,0 +1,10 @@
+"""
+Source Detection
+----------------
+"""
+
+from .sourcedetection import SourceDetection
+from .pysep import SepSourceDetection
+from .daophot import DaophotSourceDetection
+
+__all__ = ["SourceDetection", "SepSourceDetection", "DaophotSourceDetection"]
