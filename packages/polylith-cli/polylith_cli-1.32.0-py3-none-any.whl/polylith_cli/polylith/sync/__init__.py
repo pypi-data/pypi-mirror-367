3044c4a92d8c484d6174062f28663ed055c4fbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 16:07:47 +0000

    Add package polylith-cli/1.32.0/polylith_cli-1.32.0-py3-none-any.whl

---
+++ packages/polylith-cli/polylith_cli-1.32.0-py3-none-any.whl/polylith_cli/polylith/sync/__init__.py
@@ -0,0 +1,4 @@
+from polylith_cli.polylith.sync import report
+from polylith_cli.polylith.sync.collect import calculate_diff
+from polylith_cli.polylith.sync.update import update_project
+__all__ = ['report', 'calculate_diff', 'update_project']
