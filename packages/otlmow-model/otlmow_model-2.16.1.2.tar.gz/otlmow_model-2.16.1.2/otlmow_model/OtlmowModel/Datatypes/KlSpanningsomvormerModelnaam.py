b5fb7f221e2e27ec4cdb5e26e9adf6d35a9c0509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 15:08:54 +0000

    Add package otlmow-model/2.16.1.2/otlmow_model-2.16.1.2.tar.gz

---
+++ packages/otlmow-model/otlmow_model-2.16.1.2.tar.gz/otlmow_model-2.16.1.2/otlmow_model/OtlmowModel/Datatypes/KlSpanningsomvormerModelnaam.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+from otlmow_model.OtlmowModel.BaseClasses.KeuzelijstField import KeuzelijstField
+from otlmow_model.OtlmowModel.BaseClasses.KeuzelijstWaarde import KeuzelijstWaarde
+
+
+# Generated with OTLEnumerationCreator. To modify: extend, do not edit
+class KlSpanningsomvormerModelnaam(KeuzelijstField):
+    """Lijst van modelnamen van spanningsomvormers volgens de fabrikant."""
+    naam = 'KlSpanningsomvormerModelnaam'
+    label = 'Modelnamen spanningsomvormers'
+    objectUri = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#KlSpanningsomvormerModelnaam'
+    definition = 'Lijst van modelnamen van spanningsomvormers volgens de fabrikant.'
+    status = 'ingebruik'
+    codelist = 'https://wegenenverkeer.data.vlaanderen.be/id/conceptscheme/KlSpanningsomvormerModelnaam'
+    options = {
+        'minipack': KeuzelijstWaarde(invulwaarde='minipack',
+                                     label='Minipack',
+                                     status='ingebruik',
+                                     definitie='Minipack',
+                                     objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlSpanningsomvormerModelnaam/minipack'),
+        'smartpack-s': KeuzelijstWaarde(invulwaarde='smartpack-s',
+                                        label='Smartpack S ',
+                                        status='ingebruik',
+                                        definitie='Smartpack S ',
+                                        objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlSpanningsomvormerModelnaam/smartpack-s'),
+        'trio': KeuzelijstWaarde(invulwaarde='trio',
+                                 label='Trio',
+                                 status='ingebruik',
+                                 definitie='Trio',
+                                 objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlSpanningsomvormerModelnaam/trio')
+    }
+
+    @classmethod
+    def create_dummy_data(cls):
+        return cls.create_dummy_data_keuzelijst(cls.options)
+
