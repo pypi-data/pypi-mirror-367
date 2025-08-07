46d2208a744d23522e0381ed0f97a29b6a223eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 04:21:08 +0000

    Add package smart-fetch/0.4.0/smart_fetch-0.4.0.tar.gz

---
+++ packages/smart-fetch/smart_fetch-0.4.0.tar.gz/smart_fetch-0.4.0/smart_fetch/tasks/inline.py
@@ -0,0 +1,143 @@
+import base64
+import email
+import hashlib
+from functools import partial
+
+import cumulus_fhir_support as cfs
+
+from smart_fetch import hydrate_utils, resources
+
+
+def parse_mimetypes(mimetypes: str | None) -> set[str]:
+    if mimetypes is None:
+        return {"text/plain", "text/html", "application/xhtml+xml"}
+
+    return set(mimetypes.casefold().split(","))
+
+
+def parse_content_type(content_type: str) -> (str, str):
+    """Returns (mimetype, encoding)"""
+    msg = email.message.EmailMessage()
+    msg["content-type"] = content_type
+    return msg.get_content_type(), msg.get_content_charset("utf8")
+
+
+async def _inline_task(
+    *,
+    client: cfs.FhirClient,
+    task_name: str,
+    input_type: str,
+    workdir: str,
+    mimetypes: str | None = None,
+) -> None:
+    mimetypes = parse_mimetypes(mimetypes)
+    stats = await hydrate_utils.process(
+        client=client,
+        task_name=task_name,
+        desc="Inlining",
+        workdir=workdir,
+        input_type=input_type,
+        callback=partial(_inline_resource, mimetypes),
+        append=False,
+    )
+    if stats:
+        stats.print("inlined", f"{input_type}s", "Attachments")
+
+
+async def task_doc_inline(
+    client: cfs.FhirClient,
+    workdir: str,
+    mimetypes: str | None = None,
+    **kwargs,
+):
+    await _inline_task(
+        client=client,
+        task_name="doc-inline",
+        workdir=workdir,
+        input_type=resources.DOCUMENT_REFERENCE,
+        mimetypes=mimetypes,
+    )
+
+
+async def task_dxr_inline(
+    client: cfs.FhirClient,
+    workdir: str,
+    mimetypes: str | None = None,
+    **kwargs,
+):
+    await _inline_task(
+        client=client,
+        task_name="dxr-inline",
+        workdir=workdir,
+        input_type=resources.DIAGNOSTIC_REPORT,
+        mimetypes=mimetypes,
+    )
+
+
+async def _inline_resource(
+    mimetypes: set[str], client, resource: dict, id_pool: set[str]
+) -> hydrate_utils.Result:
+    match resource.get("resourceType"):
+        case "DiagnosticReport":
+            attachments = resource.get("presentedForm", [])
+        case "DocumentReference":
+            attachments = [
+                content["attachment"]
+                for content in resource.get("content", [])
+                if "attachment" in content
+            ]
+        case _:
+            attachments = []  # don't do anything, but we will leave the resource line in place
+
+    if not attachments:
+        return [(resource, hydrate_utils.TaskResultReason.IGNORED)]
+
+    result = [
+        [None, await _inline_attachment(client, attachment, mimetypes=mimetypes)]
+        for attachment in attachments
+    ]
+    result[0][0] = resource  # write back the original resource
+
+    return result
+
+
+async def _inline_attachment(
+    client: cfs.FhirClient, attachment: dict, *, mimetypes: set[str]
+) -> hydrate_utils.TaskResultReason:
+    # First, check if we should even examine this attachment
+    if "contentType" not in attachment:
+        return hydrate_utils.TaskResultReason.IGNORED
+    mimetype, _charset = parse_content_type(attachment["contentType"])
+    if mimetype not in mimetypes:
+        return hydrate_utils.TaskResultReason.IGNORED
+
+    # OK - this is a valid attachment to process
+
+    if "data" in attachment:
+        return hydrate_utils.TaskResultReason.ALREADY_DONE
+
+    if "url" not in attachment:
+        # neither data nor url... nothing to do
+        return hydrate_utils.TaskResultReason.IGNORED
+
+    try:
+        response = await client.request(
+            "GET",
+            attachment["url"],
+            # We need to pass Accept to get the raw data, not a Binary FHIR object.
+            # See https://www.hl7.org/fhir/binary.html
+            headers={"Accept": mimetype},
+        )
+    except cfs.FatalNetworkError:
+        return hydrate_utils.TaskResultReason.FATAL_ERROR
+    except cfs.TemporaryNetworkError:
+        return hydrate_utils.TaskResultReason.RETRY_ERROR
+
+    attachment["data"] = base64.standard_b64encode(response.content).decode("ascii")
+    # Overwrite other associated metadata with latest info (existing metadata might now be stale)
+    attachment["contentType"] = f"{mimetype}; charset={response.encoding}"
+    attachment["size"] = len(response.content)
+    sha1_hash = hashlib.sha1(response.content).digest()  # noqa: S324
+    attachment["hash"] = base64.standard_b64encode(sha1_hash).decode("ascii")
+
+    return hydrate_utils.TaskResultReason.NEWLY_DONE
---
+++ packages/smart-fetch/smart_fetch-0.4.0.tar.gz/smart_fetch-0.4.0/smart_fetch/tasks/obs.py
@@ -0,0 +1,68 @@
+import cumulus_fhir_support as cfs
+
+from smart_fetch import hydrate_utils, resources
+
+
+async def _download_members(client, resource: dict, id_pool: set[str]) -> hydrate_utils.Result:
+    results = [
+        await hydrate_utils.download_reference(
+            client, id_pool, member.get("reference"), resources.OBSERVATION
+        )
+        for member in resource.get("hasMember", [])
+    ]
+    for result in results:
+        if result[0]:
+            results.extend(await _download_members(client, result[0], id_pool))
+    return results
+
+
+async def task_obs_members(
+    client: cfs.FhirClient,
+    workdir: str,
+    source_dir: str | None = None,
+    **kwargs,
+):
+    stats = await hydrate_utils.process(
+        client=client,
+        task_name="obs-members",
+        desc="Downloading member",
+        workdir=workdir,
+        source_dir=source_dir or workdir,
+        input_type=resources.OBSERVATION,
+        callback=_download_members,
+        file_slug="members",
+    )
+    if stats:
+        stats.print("downloaded", f"{resources.OBSERVATION}s", "Members")
+
+
+async def _download_dxr_result(client, resource: dict, id_pool: set[str]) -> hydrate_utils.Result:
+    return [
+        await hydrate_utils.download_reference(
+            client, id_pool, result.get("reference"), resources.OBSERVATION
+        )
+        for result in resource.get("result", [])
+    ]
+
+
+async def task_obs_dxr(
+    client: cfs.FhirClient,
+    workdir: str,
+    source_dir: str | None = None,
+    **kwargs,
+):
+    stats = await hydrate_utils.process(
+        client=client,
+        task_name="dxr-results",
+        desc="Downloading result",
+        workdir=workdir,
+        source_dir=source_dir or workdir,
+        input_type=resources.DIAGNOSTIC_REPORT,
+        output_type=resources.OBSERVATION,
+        callback=_download_dxr_result,
+        file_slug="results",
+    )
+    if stats:
+        stats.print(
+            "downloaded", f"{resources.DIAGNOSTIC_REPORT}s", f"Result {resources.OBSERVATION}s"
+        )
