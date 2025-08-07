a31bc4e4391a2b438414c332a0a09b3ec42abb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 15:08:08 +0000

    Add package greet-aniketpy/0.1.0/greet_aniketpy-0.1.0-py3-none-any.whl

---
+++ packages/greet-aniketpy/greet_aniketpy-0.1.0-py3-none-any.whl/greetpy/__init__.py
@@ -0,0 +1,7 @@
+from .core import greet, greet_formal, greet_casual
+
+__version__ = "0.1.0"
+__author__ = "Aniket Surjuse"
+
+# Optional: Define what's imported with "from greetpy import *"
+__all__ = ["greet", "greet_formal", "greet_casual"]
