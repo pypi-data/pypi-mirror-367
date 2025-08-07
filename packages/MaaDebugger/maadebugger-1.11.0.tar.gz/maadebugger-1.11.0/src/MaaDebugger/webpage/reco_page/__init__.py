aa4ffc7e1bdaf68827a0d2943236c286d594cbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 14:07:55 +0000

    Add package MaaDebugger/1.11.0/maadebugger-1.11.0.tar.gz

---
+++ packages/MaaDebugger/maadebugger-1.11.0.tar.gz/maadebugger-1.11.0/src/MaaDebugger/webpage/reco_page/__init__.py
@@ -0,0 +1,39 @@
+from typing import Dict, Tuple, Optional
+
+from nicegui import ui
+
+from ...utils import cvmat_to_image
+from ...maafw import maafw, RecognitionDetail
+
+class RecoData:
+    data: Dict[int, Tuple[str, bool]] = {}
+
+
+@ui.page("/reco/{reco_id}")
+async def reco_page(reco_id: int):
+    if reco_id == 0 or not reco_id in RecoData.data:
+        ui.markdown("## Not Found")
+        return
+
+    name, hit = RecoData.data[reco_id]
+    status = hit and "✅" or "❌"
+    title = f"{status} {name} ({reco_id})"
+
+    ui.page_title(title)
+    ui.markdown(f"## {title}")
+
+    ui.separator()
+
+    details: Optional[RecognitionDetail] = await maafw.get_reco_detail(reco_id)
+    if not details:
+        ui.markdown("## Not Found")
+        return
+
+    ui.markdown(f"#### {details.algorithm}")
+    # ui.markdown(f"#### Box: {str(details.box)}")
+    ui.markdown(f"#### {details.best_result}")
+
+    for draw in details.draw_images:
+        ui.image(cvmat_to_image(draw)).props("fit=scale-down")
+
+    ui.json_editor({"content": {"json": details.raw_detail}, "readOnly": True})
