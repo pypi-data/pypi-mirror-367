24a3df99366918becf51663008fc2d81088560d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 07:10:21 +0000

    Add package razorpay-py/0.0.2/razorpay_py-0.0.2.tar.gz

---
+++ packages/razorpay-py/razorpay_py-0.0.2.tar.gz/razorpay_py-0.0.2/razorpay/constants/__init__.py
@@ -0,0 +1,5 @@
+# Razorpay SDK local imports
+from .error_code import ERROR_CODE
+from .url import URL
+
+__all__ = ["ERROR_CODE", "URL"]
---
+++ packages/razorpay-py/razorpay_py-0.0.2.tar.gz/razorpay_py-0.0.2/tests/test_multiple_client.py
@@ -0,0 +1,40 @@
+import json
+import unittest
+
+import responses
+
+from .helpers import ClientTestCase, mock_file
+
+
+class TestClientPayment(ClientTestCase):
+
+    def setUp(self):
+        super(TestClientPayment, self).setUp()
+        self.base_url = f'{self.base_url}/payments'
+        self.secondary_base_url = f'{self.secondary_url}/payments'
+
+    @responses.activate
+    def test_payment_primary_url(self):
+        result = mock_file('payment_collection')
+        url = self.base_url
+        responses.add(responses.GET, url, status=200,
+                      body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.client.payment.all(), result)
+
+    @unittest.skip
+    def test_payment_secondary_url(self):
+        result = mock_file('payment_collection')
+        url = self.secondary_base_url
+        responses.add(responses.GET, url, status=200,
+                      body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.secondary_client.payment.all(), result)
+
+    @responses.activate
+    def test_payment_with_headers(self):
+        result = mock_file('payment_collection')
+        url = self.base_url
+        responses.add(responses.GET, url, status=200,
+                      body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.client.payment.all(
+            headers={'Content-type': 'text'}),
+            result)
