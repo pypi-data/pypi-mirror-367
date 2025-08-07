302fe861b44129b67e3476548b7004678d3ed398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 17:08:56 +0000

    Add package pyobs-core/1.26.4/pyobs_core-1.26.4-py3-none-any.whl

---
+++ packages/pyobs-core/pyobs_core-1.26.4-py3-none-any.whl/pyobs/modules/image/__init__.py
@@ -0,0 +1,10 @@
+"""
+Modules for image operations.
+TODO: write doc
+"""
+
+__title__ = "Image operations"
+
+from .imagewatcher import ImageWatcher
+from .imagewriter import ImageWriter
+from .seeing import Seeing
