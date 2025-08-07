2e6052629239ed16eb55db28011ec1507b588270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 06:06:11 +0000

    Add package phylogenie/2.0.14/phylogenie-2.0.14-py3-none-any.whl

---
+++ packages/phylogenie/phylogenie-2.0.14-py3-none-any.whl/phylogenie/generators/__init__.py
@@ -0,0 +1,40 @@
+from typing import Annotated
+
+from pydantic import Field
+
+from phylogenie.generators.alisim import AliSimDatasetGenerator
+from phylogenie.generators.configs import (
+    SkylineMatrixModel,
+    SkylineParameterModel,
+    SkylineVectorModel,
+)
+from phylogenie.generators.dataset import DatasetGenerator
+from phylogenie.generators.trees import (
+    BDEITreeDatasetGenerator,
+    BDSSTreeDatasetGenerator,
+    BDTreeDatasetGenerator,
+    CanonicalTreeDatasetGenerator,
+    EpidemiologicalTreeDatasetGenerator,
+    FBDTreeDatasetGenerator,
+    TreeDatasetGeneratorConfig,
+)
+
+DatasetGeneratorConfig = Annotated[
+    TreeDatasetGeneratorConfig | AliSimDatasetGenerator,
+    Field(discriminator="data_type"),
+]
+
+__all__ = [
+    "DatasetGeneratorConfig",
+    "DatasetGenerator",
+    "AliSimDatasetGenerator",
+    "CanonicalTreeDatasetGenerator",
+    "EpidemiologicalTreeDatasetGenerator",
+    "FBDTreeDatasetGenerator",
+    "BDTreeDatasetGenerator",
+    "BDEITreeDatasetGenerator",
+    "BDSSTreeDatasetGenerator",
+    "SkylineMatrixModel",
+    "SkylineParameterModel",
+    "SkylineVectorModel",
+]
