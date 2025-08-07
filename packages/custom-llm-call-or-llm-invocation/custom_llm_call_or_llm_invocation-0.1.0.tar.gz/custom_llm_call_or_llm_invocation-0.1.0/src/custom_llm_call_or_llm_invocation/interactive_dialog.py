2a2d5572be1598f3e2ec85bc518bb269e5193ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 17:08:56 +0000

    Add package custom-llm-call-or-llm-invocation/0.1.0/custom_llm_call_or_llm_invocation-0.1.0.tar.gz

---
+++ packages/custom-llm-call-or-llm-invocation/custom_llm_call_or_llm_invocation-0.1.0.tar.gz/custom_llm_call_or_llm_invocation-0.1.0/src/custom_llm_call_or_llm_invocation/interactive_dialog.py
@@ -0,0 +1,23 @@
+from custom_llm_call_or_llm_invocation import get_llm
+import os
+from dotenv import load_dotenv
+load_dotenv()
+def start_llm_console():
+    print("💬 Welcome to the Interactive LLM Console!\n")
+    model = input("Enter model name (or press enter if you want to continue with meta-llama/llama-prompt-guard-2-22m): ").strip()
+    print(os.getenv("GROQ_API_KEY"))
+    os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+    if model=="":
+        llm=get_llm("meta-llama/llama-prompt-guard-2-22m")
+    else:
+        llm = get_llm(model)
+    print("\n✅ LLM is ready! Type your queries. Type `exit` to quit.\n")
+    
+    while True:
+        prompt = input("You: ")
+        if prompt.lower() in {"exit", "quit"}:
+            print("👋 Exiting.")
+            break
+        print(prompt)
+        response = llm.invoke(prompt)
+        print("LLM:", response.content)
