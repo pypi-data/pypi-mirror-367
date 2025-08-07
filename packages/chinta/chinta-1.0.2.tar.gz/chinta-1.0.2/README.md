44b4b12a59e147e70fabb454ced4f0ad64b2d81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 07:09:32 +0000

    Add package chinta/1.0.2/chinta-1.0.2.tar.gz

---
+++ packages/chinta/chinta-1.0.2.tar.gz/chinta-1.0.2/README.md
@@ -0,0 +1,19 @@
+# Chinta
+
+"[Chinta](https://en.wikipedia.org/wiki/Chinta_(mentation))" in Hindu philosophy refers to mentation i.e. mental activity, especially thinking.
+
+This tool tracks the word count of a TeX project in a git repository with respect to time
+
+# Instructions
+1. Install `chinta` via pip
+```bash
+python3 -m pip install chinta
+```
+
+1. Run `chinta` and tell it where to find the repo you want to analyze, as well as the name of the default branch.
+The name of the default branch is probably either `"master"` or `"main"`
+```bash
+chinta "path/to/repo" "main"
+```
+
+1. Look at the cool graph!
---
+++ packages/chinta/chinta-1.0.2.tar.gz/chinta-1.0.2/src/chinta/chinta.py
@@ -0,0 +1,57 @@
+from git import Repo
+import argparse
+import matplotlib.pyplot as plt
+import os
+import datetime
+
+def countTexWordsInFile(filename):
+    count = 0
+    with open(filename, "r") as fid:
+        lines = fid.readlines()
+        for line in lines:
+            words = line.split(" ")
+            count += len(words)
+    return(count)
+
+def countTexWordsInDirectory(directory):
+    total_tex_words = 0
+    for entry in os.scandir(directory):
+        if entry.is_file() and (entry.path.endswith(".tex")):
+            file_word_count = countTexWordsInFile(entry.path)
+            total_tex_words += file_word_count
+        elif entry.is_dir():
+            total_tex_words += countTexWordsInDirectory(entry.path)
+    return(total_tex_words)
+
+def trackTexWordsInRepository(repo,name_of_main_branch):
+    counts = []
+    dates = []
+    for commit in repo.iter_commits(name_of_main_branch):
+        repo.git.checkout(commit.hexsha)
+        counts.append(countTexWordsInDirectory(repo.working_tree_dir))
+        dates.append(datetime.datetime.fromtimestamp(commit.committed_date))
+
+    repo.git.checkout(name_of_main_branch)
+    return(counts,dates)
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='chinta',
+        description='Tracks word count in a tex repo over time and plots the result')
+    parser.add_argument('path', type=str, help='path to the repository whose words you wish to count. Example: /Users/you/thesis')
+    parser.add_argument('-b', '--branch', default='main', help='branch whose history you wish to inspect')
+    args = parser.parse_args()
+
+    repo = Repo(args.path)
+    counts,dates = trackTexWordsInRepository(repo,args.branch)
+
+    plt.figure()
+    plt.plot(dates,counts)
+    plt.xlabel("Date")
+    plt.ylabel("Number of Words")
+    plt.title("Word Count")
+    plt.show()
+
+
+if __name__=='__main__':
+    main()
