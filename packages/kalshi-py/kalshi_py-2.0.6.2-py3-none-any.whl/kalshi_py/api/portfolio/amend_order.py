66ae469d6fab9534c34571401b5bbfc8e08ea78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 05:12:30 +0000

    Add package kalshi-py/2.0.6.2/kalshi_py-2.0.6.2-py3-none-any.whl

---
+++ packages/kalshi-py/kalshi_py-2.0.6.2-py3-none-any.whl/kalshi_py/api/portfolio/amend_order.py
@@ -0,0 +1,188 @@
+from http import HTTPStatus
+from typing import Any, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.github_com_kalshi_exchange_infra_svc_api_2_model_amend_order_request import (
+    GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest,
+)
+from ...models.github_com_kalshi_exchange_infra_svc_api_2_model_amend_order_response import (
+    GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse,
+)
+from ...types import Response
+
+
+def _get_kwargs(
+    order_id: str,
+    *,
+    body: GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest,
+) -> dict[str, Any]:
+    headers: dict[str, Any] = {}
+
+    _kwargs: dict[str, Any] = {
+        "method": "post",
+        "url": f"/portfolio/orders/{order_id}/amend",
+    }
+
+    _kwargs["json"] = body.to_dict()
+
+    headers["Content-Type"] = "application/json"
+
+    _kwargs["headers"] = headers
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse]:
+    if response.status_code == 200:
+        response_200 = GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse.from_dict(response.json())
+
+        return response_200
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    order_id: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest,
+) -> Response[GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse]:
+    """Amend Order
+
+      Endpoint for amending the max number of fillable contracts and/or price in an existing order. Max
+    fillable contracts is `remaining_count` + `fill_count`.
+
+    Args:
+        order_id (str): Order ID
+        body (GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse]
+    """
+
+    kwargs = _get_kwargs(
+        order_id=order_id,
+        body=body,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    order_id: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest,
+) -> Optional[GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse]:
+    """Amend Order
+
+      Endpoint for amending the max number of fillable contracts and/or price in an existing order. Max
+    fillable contracts is `remaining_count` + `fill_count`.
+
+    Args:
+        order_id (str): Order ID
+        body (GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse
+    """
+
+    return sync_detailed(
+        order_id=order_id,
+        client=client,
+        body=body,
+    ).parsed
+
+
+async def asyncio_detailed(
+    order_id: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest,
+) -> Response[GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse]:
+    """Amend Order
+
+      Endpoint for amending the max number of fillable contracts and/or price in an existing order. Max
+    fillable contracts is `remaining_count` + `fill_count`.
+
+    Args:
+        order_id (str): Order ID
+        body (GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse]
+    """
+
+    kwargs = _get_kwargs(
+        order_id=order_id,
+        body=body,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    order_id: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest,
+) -> Optional[GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse]:
+    """Amend Order
+
+      Endpoint for amending the max number of fillable contracts and/or price in an existing order. Max
+    fillable contracts is `remaining_count` + `fill_count`.
+
+    Args:
+        order_id (str): Order ID
+        body (GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderRequest):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        GithubComKalshiExchangeInfraSvcApi2ModelAmendOrderResponse
+    """
+
+    return (
+        await asyncio_detailed(
+            order_id=order_id,
+            client=client,
+            body=body,
+        )
+    ).parsed
---
+++ packages/kalshi-py/kalshi_py-2.0.6.2-py3-none-any.whl/kalshi_py/models/github_com_kalshi_exchange_infra_svc_api_2_model_get_market_candlesticks_response.py
@@ -0,0 +1,96 @@
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, Any, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.github_com_kalshi_exchange_infra_svc_api_2_model_get_market_candlesticks_response_candlesticks_item import (
+        GithubComKalshiExchangeInfraSvcApi2ModelGetMarketCandlesticksResponseCandlesticksItem,
+    )
+
+
+T = TypeVar("T", bound="GithubComKalshiExchangeInfraSvcApi2ModelGetMarketCandlesticksResponse")
+
+
+@_attrs_define
+class GithubComKalshiExchangeInfraSvcApi2ModelGetMarketCandlesticksResponse:
+    """
+    Attributes:
+        candlesticks (Union[Unset,
+            list['GithubComKalshiExchangeInfraSvcApi2ModelGetMarketCandlesticksResponseCandlesticksItem']]): Array of
+            candlestick data points for the specified time range.
+        ticker (Union[Unset, str]): Unique identifier for the market.
+    """
+
+    candlesticks: Union[
+        Unset, list["GithubComKalshiExchangeInfraSvcApi2ModelGetMarketCandlesticksResponseCandlesticksItem"]
+    ] = UNSET
+    ticker: Union[Unset, str] = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        candlesticks: Union[Unset, list[dict[str, Any]]] = UNSET
+        if not isinstance(self.candlesticks, Unset):
+            candlesticks = []
+            for candlesticks_item_data in self.candlesticks:
+                candlesticks_item = candlesticks_item_data.to_dict()
+                candlesticks.append(candlesticks_item)
+
+        ticker = self.ticker
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if candlesticks is not UNSET:
+            field_dict["candlesticks"] = candlesticks
+        if ticker is not UNSET:
+            field_dict["ticker"] = ticker
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
+        from ..models.github_com_kalshi_exchange_infra_svc_api_2_model_get_market_candlesticks_response_candlesticks_item import (
+            GithubComKalshiExchangeInfraSvcApi2ModelGetMarketCandlesticksResponseCandlesticksItem,
+        )
+
+        d = dict(src_dict)
+        candlesticks = []
+        _candlesticks = d.pop("candlesticks", UNSET)
+        for candlesticks_item_data in _candlesticks or []:
+            candlesticks_item = (
+                GithubComKalshiExchangeInfraSvcApi2ModelGetMarketCandlesticksResponseCandlesticksItem.from_dict(
+                    candlesticks_item_data
+                )
+            )
+
+            candlesticks.append(candlesticks_item)
+
+        ticker = d.pop("ticker", UNSET)
+
+        github_com_kalshi_exchange_infra_svc_api_2_model_get_market_candlesticks_response = cls(
+            candlesticks=candlesticks,
+            ticker=ticker,
+        )
+
+        github_com_kalshi_exchange_infra_svc_api_2_model_get_market_candlesticks_response.additional_properties = d
+        return github_com_kalshi_exchange_infra_svc_api_2_model_get_market_candlesticks_response
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
