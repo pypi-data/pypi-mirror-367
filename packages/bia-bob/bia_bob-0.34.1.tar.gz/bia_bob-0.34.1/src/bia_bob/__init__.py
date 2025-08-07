e17ee746b4862de4fa3bd0a7ea71437e5d6f0856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 17:09:20 +0000

    Add package bia-bob/0.34.1/bia_bob-0.34.1.tar.gz

---
+++ packages/bia-bob/bia_bob-0.34.1.tar.gz/bia_bob-0.34.1/src/bia_bob/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.34.1"
+
+__all__ = (
+)
+
+from ._machinery import bob, init_assistant, enable_plugins, DEFAULT_SYSTEM_PROMPT
+from ._accelerate import acc
+from ._bug_fixing import fix
+from ._document import doc
+from ._utilities import available_models
+from ._notebook_generation import generate_notebook
+from ._utilities import ask_llm
+
+
+bob.initialize = init_assistant
+bob.__version__ = __version__
+bob.enable_plugins = enable_plugins
+
+
