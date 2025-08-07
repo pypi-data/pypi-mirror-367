3ed003263e4052542587dd3bd004ff279c3db24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 05:12:32 +0000

    Add package schemax/2.1.0/schemax-2.1.0.tar.gz

---
+++ packages/schemax/schemax-2.1.0.tar.gz/schemax-2.1.0/schemax/_data_collector.py
@@ -0,0 +1,273 @@
+import re
+from dataclasses import dataclass
+from typing import Any, Dict, List, Tuple, Union
+
+from d42.declaration.types import GenericSchema
+
+from ._from_json_schema import _from_json_schema
+from ._openapi_normalizer import openapi_normalizer
+
+
+@dataclass
+class SchemaData:
+    """Data collector class.
+
+    Attributes:
+        http_method: HTTP method of the request.
+        path: URL path of the request.
+        converted_path: URL path converted to the camel-case for usage in schemax generation.
+        args: Arguments of the request.
+        queries_schema: Query parameters of the request.
+        queries_schema_d42: Converted to d42 queries_schema.
+        interface_method: Interface name for usage in schemax generation.
+        interface_method_humanized: Interface 'humanized' name for usage in schemax generation.
+        status: Status code for specified schemas.
+        schema_prefix: Schema prefix name for usage in schemax generation.
+        schema_prefix_humanized: Schema prefix 'humanized' name for user in schemax generation.
+        response_schema: Normalized response schema (without $ref).
+        response_schema_d42: Converted to d42 response_schema.
+        request_schema: Normalized request schema (without $ref).
+        request_schema_d42: Converted to d42 request_schema.
+        request_headers: Request headers from OpenAPI schema.
+        request_headers_d42: Converted to d42 request_headers.
+        tags: Tags of the request from OpenAPI schema.
+    """
+    http_method: str
+    path: str
+    converted_path: str
+    args: List[str]
+    queries_schema: Dict[str, Any]
+    queries_schema_d42: GenericSchema
+    interface_method: str
+    interface_method_humanized: str
+    status: Union[str, int]
+    schema_prefix: str
+    schema_prefix_humanized: str
+    response_schema: Dict[str, Any]
+    response_schema_d42: GenericSchema
+    request_schema: Dict[str, Any]
+    request_schema_d42: GenericSchema
+    request_headers: Dict[str, Any]
+    request_headers_d42: GenericSchema
+    tags: List[str]
+
+
+humanizator = {
+    "get": "Get",
+    "post": "Create",
+    "put": "Update",
+    "patch": "Change",
+    "delete": "Delete"
+}
+
+
+def collect_schema_data(value: Dict[str, Any]) -> List[SchemaData]:
+    normalized_schema = openapi_normalizer(value)
+    paths_data = normalized_schema.get("paths", {})
+
+    return [
+        schema_data
+        for path, path_data in paths_data.items()
+        for schema_data in process_paths(path, path_data)
+    ]
+
+
+def process_paths(path: str, path_data: Dict[str, Any]) -> List[SchemaData]:
+    paths = get_enum_paths(path, path_data)
+    if not paths:
+        paths = [path]
+
+    schema_data = []
+    for enum_path in paths:
+        for http_method, method_data in path_data.items():
+            if http_method.lower() in ["get", "post", "put", "patch", "delete"]:
+                for status in method_data.get("responses", {}):
+                    schema_data.append(
+                        process_method_data(enum_path, http_method, method_data, int(status))
+                    )
+
+    return schema_data
+
+
+def get_enum_paths(path: str, path_data: Dict[str, Any]) -> List[str]:
+    paths = []
+    parameters = path_data.get("parameters", [])
+    for parameter in parameters:
+        if parameter.get("in") == "path" and "enum" in parameter["schema"]:
+            for enum_item in parameter["schema"]["enum"]:
+                paths.append(path.replace(f"{{{parameter['name']}}}", enum_item))
+    return paths
+
+
+def process_method_data(
+    path: str, http_method: str, method_data: Dict[str, Any], status: int
+) -> SchemaData:
+    request_schema, response_schema = get_request_response_schemas(method_data, status)
+    queries_schema = get_queries(method_data)
+    headers_schema = get_headers(method_data)
+
+    args = get_path_arguments(path)
+    if request_schema:
+        args.append("body")
+
+    queries_schema_d42 = _from_json_schema(queries_schema)
+    response_schema_d42 = _from_json_schema(response_schema)
+    request_schema_d42 = _from_json_schema(request_schema)
+    headers_schema_d42 = _from_json_schema(headers_schema)
+
+    return SchemaData(
+        http_method=http_method,
+        path=path,
+        converted_path=convert_to_snake_case(path),
+        args=args,
+        queries_schema=queries_schema,
+        queries_schema_d42=queries_schema_d42,
+        interface_method=get_interface_method_name(http_method, path),
+        interface_method_humanized=get_interface_method_name(http_method, path, humanized=True),
+        status=status,
+        schema_prefix=get_schema_prefix(http_method, path),
+        schema_prefix_humanized=get_schema_prefix(http_method, path, humanized=True),
+        response_schema=response_schema,
+        response_schema_d42=response_schema_d42,
+        request_schema=request_schema,
+        request_schema_d42=request_schema_d42,
+        request_headers=headers_schema,
+        request_headers_d42=headers_schema_d42,
+        tags=method_data.get("tags", [])
+    )
+
+
+def get_request_response_schemas(
+    method_data: Dict[str, Any], status: int
+) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    request_schema: Dict[str, Any] = {}
+    response_schema: Dict[str, Any] = {}
+
+    if "requestBody" in method_data:
+        request_schema = get_request_schema(method_data["requestBody"])
+    elif "parameters" in method_data:
+        request_schema = get_request_schema_from_parameters(method_data["parameters"])
+
+    if "responses" in method_data:
+        response_schema = get_response_schema(method_data["responses"], status)
+
+    return request_schema, response_schema
+
+
+def get_request_schema(request_body: Dict[str, Any]) -> Dict[str, Any]:
+    content = request_body.get("content", {})
+    for content_type, content_data in content.items():
+        if "schema" in content_data:
+            return content_data["schema"]  # type: ignore
+    return {}
+
+
+def get_request_schema_from_parameters(parameters: List[Dict[str, Any]]) -> Dict[str, Any]:
+    for param in parameters:
+        if param["in"] == "body":
+            return param["schema"]  # type: ignore
+    return {}
+
+
+def get_response_schema(responses: Dict[str, Any], need_status: int) -> Dict[str, Any]:
+    for status, status_data in responses.items():
+        if int(status) == need_status:
+            content = status_data.get("content", {})
+            if content:
+                content_schema: Dict[str, Any] = (
+                    content.get(next(iter(content)), {}).get("schema", {})
+                )
+                return content_schema
+            schema: Dict[str, Any] = status_data.get("schema", {})
+            if schema:
+                return schema
+    return {}
+
+
+def get_path_arguments(path: str) -> List[str]:
+    return [convert_to_snake_case(arg) for arg in re.findall(r"{([^}]+)}", path)]
+
+
+def get_queries(method_data: Dict[str, Any]) -> Dict[str, Any]:
+    query_schema = {
+        "type": "object",
+        "properties": {},
+        "required": []
+    }
+
+    for parameter in method_data.get("parameters", []):
+        if parameter["in"] == "query":
+            name = parameter["name"]
+            schema = parameter.get("schema", {})
+            query_schema["properties"][name] = schema  # type: ignore[index]
+            if parameter.get("required", False):
+                query_schema["required"].append(name)  # type: ignore[attr-defined]
+
+    if not query_schema["required"]:
+        del query_schema["required"]  # Remove list if it's empty
+
+    return query_schema
+
+
+def get_headers(method_data: Dict[str, Any]) -> Dict[str, Any]:
+    headers_schema = {
+        "type": "object",
+        "properties": {},
+        "required": []
+    }
+
+    for parameter in method_data.get("parameters", []):
+        if parameter["in"] == "header":
+            name = parameter["name"]
+            schema = parameter.get("schema", {})
+            headers_schema["properties"][name] = schema  # type: ignore[index]
+            if parameter.get("required", False):
+                headers_schema["required"].append(name)  # type: ignore[attr-defined]
+
+    if not headers_schema["required"]:
+        del headers_schema["required"]  # Remove list if it's empty
+
+    return headers_schema
+
+
+def get_interface_method_name(http_method: str, path: str, humanized: bool = False) -> str:
+    return (
+        (humanizator[http_method] if humanized else http_method.lower()) +
+        "_".join(
+            convert_to_snake_case(word)
+            .replace("{", "")
+            .replace("}", "")
+            .replace("-", "_")
+            .replace(".", "_")
+            .lower()
+            for word in path.split("/")
+        )
+    )
+
+
+def get_success_status(method_data: Dict[str, Any]) -> Union[str, int]:
+    success_statuses = ["200", 200]
+    for status in success_statuses:
+        if status in method_data.get("responses", {}):
+            return status  # type: ignore
+    return ""
+
+
+def get_schema_prefix(http_method: str, path: str, humanized: bool = False) -> str:
+    return (
+        (humanizator[http_method] if humanized else http_method.capitalize()) +
+        "".join(
+            word
+            .replace("{", "")
+            .replace("}", "")
+            .replace("-", "")
+            .replace("_", "")
+            .replace(".", "")
+            .capitalize()
+            for word in path.split("/")
+        )
+    )
+
+
+def convert_to_snake_case(input_string: str) -> str:
+    return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', input_string).lower()
---
+++ packages/schemax/schemax-2.1.0.tar.gz/schemax-2.1.0/schemax/_generator.py
@@ -0,0 +1,169 @@
+import os
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any, List, Optional
+
+from d42 import schema
+from jinja2 import Environment, FileSystemLoader, Template
+
+from ._data_collector import SchemaData
+
+
+class Generator(ABC):
+    @abstractmethod
+    def _get_template(self, template_name: str) -> Template:
+        pass
+
+    def _create_dir(self, dir_name: str) -> None:
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+
+    def _create_package(self, dir_name: str) -> None:
+        self._create_dir(dir_name=dir_name)
+        init_file_path = f'{dir_name}/__init__.py'
+        if not os.path.exists(init_file_path):
+            Path(init_file_path).touch()
+
+    def _generate_by_template(self, file_path: str, template_name: str, **kwargs: Any) -> None:
+        if not os.path.exists(file_path):
+            template = self._get_template(template_name=template_name)
+            with open(file_path, 'w') as file:
+                file.write(template.render(**kwargs))
+
+    def _append_string(self, lst: List[str], suffix: str) -> List[str]:
+        return [f'{suffix}.{item}' for item in lst]
+
+
+class MainGenerator(Generator):
+    __PATH_TEMPLATES = os.path.dirname(os.path.realpath(__file__)) + '/templates'
+    __TEMPLATE_SCHEMAS = 'schemas.py.j2'
+    __TEMPLATE_SCHEMA_DEFINITION = 'schema_definition.py.j2'
+    __TEMPLATE_INTERFACES = 'interfaces.py.j2'
+    __TEMPLATE_API_ROUTE = 'api_route.py.j2'
+    __TEMPLATE_SCENARIO = 'scenario.py.j2'
+
+    __DIRECTORY_SCHEMAS = 'schemas'
+    __DIRECTORY_INTERFACES = 'interfaces'
+    __DIRECTORY_SCENARIOS = 'scenarios'
+
+    __FILE_API_INTERFACE = 'api.py'
+    __FILE_RESPONSE_SCHEMAS = 'response_schemas.py'
+    __FILE_REQUEST_SCHEMAS = 'request_schemas.py'
+
+    def __init__(
+        self, schema_data: List[SchemaData], base_url: Optional[str] = None, humanize: bool = False
+    ):
+        super().__init__()
+        self.schema_data = schema_data
+        self.__templates = Environment(loader=FileSystemLoader(self.__PATH_TEMPLATES))
+        self.__templates.filters['append_str'] = self._append_string
+        self.base_url = base_url
+        self.humanize = humanize
+
+    def response_schemas(self) -> None:
+        self._create_package(self.__DIRECTORY_SCHEMAS)
+        self._generate_by_template(
+            file_path=f'{self.__DIRECTORY_SCHEMAS}/{self.__FILE_RESPONSE_SCHEMAS}',
+            template_name=self.__TEMPLATE_SCHEMAS)
+
+        with open(f'{self.__DIRECTORY_SCHEMAS}/{self.__FILE_RESPONSE_SCHEMAS}', 'a') as file:
+            for data_item in self.schema_data:
+                template = self._get_template(self.__TEMPLATE_SCHEMA_DEFINITION)
+                schema_name = data_item.schema_prefix_humanized \
+                    if self.humanize else data_item.schema_prefix
+                file.write(
+                    template.render(
+                        schema_name=f'{schema_name}' + f'{data_item.status}' + 'ResponseSchema',
+                        schema_definition=data_item.response_schema_d42
+                    )
+                )
+
+    def request_schemas(self) -> None:
+        self._create_package(self.__DIRECTORY_SCHEMAS)
+        self._generate_by_template(
+            file_path=f'{self.__DIRECTORY_SCHEMAS}/{self.__FILE_REQUEST_SCHEMAS}',
+            template_name=self.__TEMPLATE_SCHEMAS)
+
+        with open(f'{self.__DIRECTORY_SCHEMAS}/{self.__FILE_REQUEST_SCHEMAS}', 'a') as file:
+            for data_item in self.schema_data:
+                if data_item.status == 200:
+                    if data_item.request_schema is not schema.any:
+                        template = self._get_template(self.__TEMPLATE_SCHEMA_DEFINITION)
+                        schema_name = data_item.schema_prefix_humanized \
+                            if self.humanize else data_item.schema_prefix
+                        file.write(
+                            template.render(
+                                schema_name=f'{schema_name}' + 'RequestSchema',
+                                schema_definition=data_item.request_schema_d42
+                            )
+                        )
+                    if data_item.queries_schema is not schema.any:
+                        template = self._get_template(self.__TEMPLATE_SCHEMA_DEFINITION)
+                        schema_name = data_item.schema_prefix_humanized \
+                            if self.humanize else data_item.schema_prefix
+                        file.write(
+                            template.render(
+                                schema_name=f'{schema_name}' + 'QueriesSchema',
+                                schema_definition=data_item.queries_schema_d42
+                            )
+                        )
+
+    def interfaces(self) -> None:
+        self._create_package(self.__DIRECTORY_INTERFACES)
+        self._generate_by_template(
+            file_path=f'{self.__DIRECTORY_INTERFACES}/{self.__FILE_API_INTERFACE}',
+            template_name=self.__TEMPLATE_INTERFACES,
+            base_url=self.base_url
+        )
+
+        with open(f'{self.__DIRECTORY_INTERFACES}/{self.__FILE_API_INTERFACE}', 'a') as file:
+            for data_item in self.schema_data:
+                if data_item.status == 200:
+                    template = self._get_template(self.__TEMPLATE_API_ROUTE)
+                    file.write(
+                        template.render(
+                            interface_method=(
+                                data_item.interface_method_humanized.lower()
+                                if self.humanize
+                                else data_item.interface_method
+                            ),
+                            http_method=data_item.http_method.upper(),
+                            path=data_item.path,
+                            args=data_item.args,
+                            request_schema=(
+                                data_item.request_schema_d42
+                                if data_item.request_schema != schema.any
+                                else None
+                            )
+                        )
+                    )
+
+    def scenarios(self) -> None:
+        self._create_package(self.__DIRECTORY_SCENARIOS)
+        for data_item in self.schema_data:
+            self._generate_by_template(
+                file_path=f'{self.__DIRECTORY_SCENARIOS}/{data_item.interface_method}.py',
+                template_name=self.__TEMPLATE_SCENARIO,
+                subject=data_item.interface_method.split('_'),
+                interface_method=(
+                    data_item.interface_method_humanized.lower()
+                    if self.humanize
+                    else data_item.interface_method
+                ),
+                args=data_item.args,
+                response_schema=data_item.schema_prefix + 'ResponseSchema',
+                request_schema=(
+                    data_item.schema_prefix + 'RequestSchema'
+                    if data_item.request_schema != schema.any
+                    else None
+                )
+            )
+
+    def all(self) -> None:
+        self.request_schemas()
+        self.response_schemas()
+        self.interfaces()
+        self.scenarios()
+
+    def _get_template(self, template_name: str) -> Template:
+        return self.__templates.get_template(name=template_name)
