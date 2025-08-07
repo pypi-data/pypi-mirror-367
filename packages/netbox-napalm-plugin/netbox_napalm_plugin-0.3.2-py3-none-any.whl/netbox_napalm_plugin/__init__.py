4718757be6848f3f776d2675e651e09145ee8d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 07:09:13 +0000

    Add package netbox-napalm-plugin/0.3.2/netbox_napalm_plugin-0.3.2-py3-none-any.whl

---
+++ packages/netbox-napalm-plugin/netbox_napalm_plugin-0.3.2-py3-none-any.whl/netbox_napalm_plugin/__init__.py
@@ -0,0 +1,28 @@
+"""Top-level package for NetBox Napalm Plugin."""
+
+__author__ = """Arthur Hanson"""
+__email__ = "ahanson@netboxlabs.com"
+__version__ = "0.3.1"
+
+
+from netbox.plugins import PluginConfig
+
+
+class NapalmPlatformConfig(PluginConfig):
+    name = "netbox_napalm_plugin"
+    verbose_name = "NetBox Napalm Plugin"
+    description = "NetBox plugin for Napalm."
+    version = __version__
+    author = __author__
+    author_email = __email__
+    base_url = "netbox_napalm_plugin"
+    required_settings = ['NAPALM_USERNAME', 'NAPALM_PASSWORD', ]
+    default_settings = {
+        'NAPALM_TIMEOUT': 30,
+        'NAPALM_ARGS': {},
+    }
+    min_version = '4.0.2'
+    max_version = '4.3.99'
+
+
+config = NapalmPlatformConfig
