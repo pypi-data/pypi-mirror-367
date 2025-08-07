8b371cf613067f612f4a19b03cdddc4c2196bf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 06:06:01 +0000

    Add package qiskit-serverless/0.25.4/qiskit_serverless-0.25.4-py3-none-any.whl

---
+++ packages/qiskit-serverless/qiskit_serverless-0.25.4-py3-none-any.whl/qiskit_serverless/core/enums.py
@@ -0,0 +1,15 @@
+"""This file contains the enums used in the project."""
+
+from enum import Enum
+
+
+class Channel(str, Enum):
+    """
+    This enum identifies the different types of channels with that a user can
+    authenticate against.
+    """
+
+    LOCAL = "local"
+    IBM_QUANTUM = "ibm_quantum"
+    IBM_CLOUD = "ibm_cloud"
+    IBM_QUANTUM_PLATFORM = "ibm_quantum_platform"
