5098800b333f009b91952bede0103bde1ae39b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 14:08:30 +0000

    Add package sipmarray/1.2/sipmarray-1.2-py3-none-any.whl

---
+++ packages/sipmarray/sipmarray-1.2-py3-none-any.whl/sipmarray/__init__.py
@@ -0,0 +1,8 @@
+r'''# sipmarray
+This package provides a quick and easy way to get the goemetry of circular 
+SiPM arrays.
+'''
+
+from .array import SiPMarray
+from .unit import SiPMunit
+from . import models
