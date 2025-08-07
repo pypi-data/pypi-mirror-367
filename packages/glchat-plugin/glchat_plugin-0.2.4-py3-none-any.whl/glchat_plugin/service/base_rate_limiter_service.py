21023de6004ac369f65755cf11566d0fca3a7017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 15:08:04 +0000

    Add package glchat-plugin/0.2.4/glchat_plugin-0.2.4-py3-none-any.whl

---
+++ packages/glchat-plugin/glchat_plugin-0.2.4-py3-none-any.whl/glchat_plugin/service/base_rate_limiter_service.py
@@ -0,0 +1,50 @@
+"""Provides a base class for rate limiter service.
+
+Authors:
+    Kevin Susanto (kevin.susanto@gdplabs.id)
+
+References:
+    NONE
+"""
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class BaseRateLimiterService(ABC):
+    """A base class for tenant service."""
+
+    @abstractmethod
+    async def custom_identifier(self, request: Any, identifier_id: Any, **kwargs: Any) -> str:
+        """Abstract method to get a custom identifier for rate limiting.
+
+        Args:
+            request (Request): The incoming request.
+            identifier_id (str): The identifier ID for rate limiting.
+            **kwargs (Any): Additional keyword arguments.
+
+        Returns:
+            str: A custom identifier for rate limiting.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def setup_rate_limiter(self, **kwargs: Any) -> None:
+        """Abstract method to set up the rate limiter.
+
+        Args:
+            **kwargs (Any): Additional keyword arguments.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_rate_limiter(self, key: str) -> Any:
+        """Abstract method to get a rate limiter by key path.
+
+        Args:
+            key: The keys to navigate to the rate limiter (e.g., "message" or "admin", "chatbot").
+
+        Returns:
+            Any: The requested rate limiter instance.
+        """
+        raise NotImplementedError
