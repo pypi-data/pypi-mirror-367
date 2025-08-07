3fecf25c7d09b998cd4f9a47855333ec1bfa3c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 15:07:23 +0000

    Add package modulke123/0.1.0/modulke123-0.1.0.tar.gz

---
+++ packages/modulke123/modulke123-0.1.0.tar.gz/modulke123-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="modulke123",              # ✅ NEW valid name
+    version="0.1.0",
+    author="Your Name",
+    author_email="your@email.com",
+    description="Your package description",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
