f6330b51d23e74b23e8bc123510c1955cbd5631b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 06:06:25 +0000

    Add package sidetool/0.1.6/sidetool-0.1.6.tar.gz

---
+++ packages/sidetool/sidetool-0.1.6.tar.gz/sidetool-0.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sidetool',
+    version='0.1.6',
+    description='Helper tools for PySide6 and PyInstaller builds',
+    author='Alan Lilly',
+    packages=find_packages(),  # Automatically includes `sidetool` folder
+    include_package_data=True,
+    install_requires=[
+        'PyInstaller>=5.8.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'sidetool.build=sidetool.build:main',
+            'sidetool.clean=sidetool.clean:main',
+            'sidetool.compile=sidetool.compile:main',
+            'sidetool.run=sidetool.run:main',
+        ],
+    },
+)
