25757cef6bcca089485b6521fa3d4a2b409e1c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-07 14:08:31 +0000

    Add package framedisplay/1.1.4/framedisplay-1.1.4-py3-none-any.whl

---
+++ packages/framedisplay/framedisplay-1.1.4-py3-none-any.whl/framedisplay/__init__.py
@@ -0,0 +1,8 @@
+from .__version__ import __version__
+from .display import (
+    dataframe_to_html,
+    frame_display,
+    initialize,
+)
+
+__all__ = ["__version__", "dataframe_to_html", "frame_display", "initialize"]
