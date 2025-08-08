df5965b2e8a82f5b1b7de9ca27292a464270f5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:36:46 +0000

    Add package policyengine-us/1.366.1/policyengine_us-1.366.1.tar.gz

---
+++ packages/policyengine-us/policyengine_us-1.366.1.tar.gz/policyengine_us-1.366.1/policyengine_us/variables/gov/hhs/medicaid/eligibility/is_medicaid_eligible.py
@@ -0,0 +1,39 @@
+from policyengine_us.model_api import *
+
+
+class is_medicaid_eligible(Variable):
+    value_type = bool
+    entity = Person
+    label = "Eligible for Medicaid"
+    definition_period = YEAR
+    reference = (
+        "https://www.law.cornell.edu/uscode/text/42/1396a#a_10"
+        "https://www.kff.org/racial-equity-and-health-policy/fact-sheet/key-facts-on-health-coverage-of-immigrants"
+    )
+
+    def formula(person, period, parameters):
+        category = person("medicaid_category", period)
+        categorically_eligible = category != category.possible_values.NONE
+        istatus = person("immigration_status", period)
+        undocumented = istatus == istatus.possible_values.UNDOCUMENTED
+        state = person.household("state_code_str", period)
+        p = parameters(period).gov.hhs.medicaid.eligibility
+        state_covers_undocumented = p.undocumented_immigrant[state].astype(
+            bool
+        )
+        immigration_status_eligible = (
+            ~undocumented | undocumented & state_covers_undocumented
+        )
+        ca_ffyp_eligible = person("ca_ffyp_eligible", period)
+        if p.work_requirements.applies:
+            work_requirement_eligible = person(
+                "medicaid_work_requirement_eligible", period
+            )
+            return (
+                categorically_eligible
+                & immigration_status_eligible
+                & work_requirement_eligible
+            ) | ca_ffyp_eligible
+        return (
+            categorically_eligible & immigration_status_eligible
+        ) | ca_ffyp_eligible
