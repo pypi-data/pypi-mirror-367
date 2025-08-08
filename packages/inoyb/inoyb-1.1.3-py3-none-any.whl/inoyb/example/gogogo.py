e330adb7c138ffdd34010c79be50de9879a11b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:36:15 +0000

    Add package inoyb/1.1.3/inoyb-1.1.3-py3-none-any.whl

---
+++ packages/inoyb/inoyb-1.1.3-py3-none-any.whl/inoyb/example/gogogo.py
@@ -0,0 +1,28 @@
+"""
+Author: DiChen
+Date: 2025-08-01 02:48:00
+LastEditors: DiChen
+LastEditTime: 2025-08-01 02:59:20
+"""
+
+from inoyb import your_turn
+
+
+@your_turn()
+def model_handler(*inputs):
+    return [
+        "python",
+        "model/inference.py",
+        "--data_files",
+        inputs[0],
+        inputs[1],
+        inputs[2],
+        "--config_path",
+        "model/config.json",
+        "--checkpoint",
+        "model/Prithvi_EO_V1_100M.pt",
+    ]
+
+
+if __name__ == "__main__":
+    model_handler.run()
