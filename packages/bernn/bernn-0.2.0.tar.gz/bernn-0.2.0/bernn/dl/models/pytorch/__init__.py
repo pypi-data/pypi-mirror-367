649810ed852ff52b5105bfe5168679c520f8789f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 06:06:50 +0000

    Add package bernn/0.2.0/bernn-0.2.0.tar.gz

---
+++ packages/bernn/bernn-0.2.0.tar.gz/bernn-0.2.0/bernn/dl/models/pytorch/__init__.py
@@ -0,0 +1,14 @@
+"""PyTorch model definitions for BERNN."""
+
+from .aedann import AutoEncoder2, SHAPAutoEncoder2
+from .aeekandann import KANAutoencoder2, SHAPKANAutoencoder2
+from .aedann import AutoEncoder3, SHAPAutoEncoder3
+
+__all__ = [
+    "AutoEncoder2",
+    "SHAPAutoEncoder2",
+    "KANAutoencoder2",
+    "SHAPKANAutoencoder2",
+    "AutoEncoder3",
+    "SHAPAutoEncoder3",
+]
