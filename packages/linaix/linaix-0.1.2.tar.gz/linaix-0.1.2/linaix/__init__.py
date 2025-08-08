432620b78993d1ed0c91d5229b159a59917568d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 07:09:19 +0000

    Add package linaix/0.1.2/linaix-0.1.2.tar.gz

---
+++ packages/linaix/linaix-0.1.2.tar.gz/linaix-0.1.2/linaix/__init__.py
@@ -0,0 +1,13 @@
+"""
+Linaix - A Linux terminal interface with natural language processing capabilities.
+
+This package provides a command-line interface that allows users to interact
+with their Linux system using natural language commands.
+"""
+
+__version__ = "0.1.2"
+__author__ = "Adir Ali Yerima"
+
+from .linaix import main
+
+__all__ = ["main"] 
