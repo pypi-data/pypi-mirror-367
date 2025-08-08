64a2e8061d24132d6c7854923b18b57a8f1d548d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 05:14:01 +0000

    Add package fastgps/0.0.4.3.1rc0/fastgps-0.0.4.3.1rc0.tar.gz

---
+++ packages/fastgps/fastgps-0.0.4.3.1rc0.tar.gz/fastgps-0.0.4.3.1rc0/fastgps/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.4.3.1c"
+
+from .fast_gp_lattice import FastGPLattice
+from .fast_gp_digital_net_b2 import FastGPDigitalNetB2
+from .standard_gp import StandardGP
+from .plots import plot_fastgps_fit_data
