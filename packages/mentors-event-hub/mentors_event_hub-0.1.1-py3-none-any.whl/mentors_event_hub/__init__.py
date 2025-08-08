f29c2dbf2ad92545847cc33f02fcc454a5b644d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:08:07 +0000

    Add package mentors-event-hub/0.1.1/mentors_event_hub-0.1.1-py3-none-any.whl

---
+++ packages/mentors-event-hub/mentors_event_hub-0.1.1-py3-none-any.whl/mentors_event_hub/__init__.py
@@ -0,0 +1,18 @@
+"""
+Mentors Event Hub - Centralized event logging with Repository Pattern
+"""
+
+__version__ = "0.1.1"
+
+from .event_hub_client import EventHubClient
+from .event_hub import setup_global_hub, send_event, send_error, capture_errors
+from .repository.event_repository import EventRepository
+
+__all__ = [
+    "EventHubClient",
+    "EventRepository",
+    "setup_global_hub", 
+    "send_event",
+    "send_error",
+    "capture_errors"
+]
