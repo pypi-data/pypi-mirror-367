ab08ad4b97100f269c795a47f0c4406906d9b7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 04:22:25 +0000

    Add package cvat-sdk/2.43.0/cvat_sdk-2.43.0.tar.gz

---
+++ packages/cvat-sdk/cvat_sdk-2.43.0.tar.gz/cvat_sdk-2.43.0/cvat_sdk/auto_annotation/__init__.py
@@ -0,0 +1,59 @@
+# Copyright (C) CVAT.ai Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from .driver import annotate_task
+from .exceptions import BadFunctionError
+from .interface import (
+    AutoAnnotationFunction,
+    DetectionFunction,
+    DetectionFunctionContext,
+    DetectionFunctionSpec,
+    TrackableShape,
+    TrackingFunctionContext,
+    TrackingFunctionShapeContext,
+    TrackingFunctionSpec,
+    attribute_spec,
+    checkbox_attribute_spec,
+    keypoint,
+    keypoint_spec,
+    label_spec,
+    mask,
+    number_attribute_spec,
+    polygon,
+    radio_attribute_spec,
+    rectangle,
+    select_attribute_spec,
+    shape,
+    skeleton,
+    skeleton_label_spec,
+    text_attribute_spec,
+)
+
+__all__ = [
+    "AutoAnnotationFunction",
+    "annotate_task",
+    "attribute_spec",
+    "BadFunctionError",
+    "checkbox_attribute_spec",
+    "DetectionFunction",
+    "DetectionFunctionContext",
+    "DetectionFunctionSpec",
+    "TrackableShape",
+    "TrackingFunctionContext",
+    "TrackingFunctionShapeContext",
+    "TrackingFunctionSpec",
+    "keypoint_spec",
+    "keypoint",
+    "label_spec",
+    "mask",
+    "number_attribute_spec",
+    "polygon",
+    "radio_attribute_spec",
+    "rectangle",
+    "select_attribute_spec",
+    "shape",
+    "skeleton_label_spec",
+    "skeleton",
+    "text_attribute_spec",
+]
