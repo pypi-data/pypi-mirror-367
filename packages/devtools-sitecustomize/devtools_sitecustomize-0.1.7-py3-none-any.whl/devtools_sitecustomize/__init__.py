9da3c5f0b15cf38312bc8aed7059b0fc14def089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 05:13:36 +0000

    Add package devtools-sitecustomize/0.1.7/devtools_sitecustomize-0.1.7-py3-none-any.whl

---
+++ packages/devtools-sitecustomize/devtools_sitecustomize-0.1.7-py3-none-any.whl/devtools_sitecustomize/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.1.7'
+
+import builtins
+
+from devtools import debug
+
+
+def add_debug_to_builtins():
+    """
+    This function adds devtools.debug to builtins.
+    """
+    print(
+        '🚀 devtools.debug being added to builtins via devtools-sitecustomize plugin!'
+    )
+    setattr(builtins, 'debug', debug)
