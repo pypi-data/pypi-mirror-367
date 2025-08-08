357be019070b616000c8fd8d02b79bf8c3537eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 04:23:07 +0000

    Add package pytest-plugins/0.3.6/pytest_plugins-0.3.6-py3-none-any.whl

---
+++ packages/pytest-plugins/pytest_plugins-0.3.6-py3-none-any.whl/pytest_plugins/utils/helper.py
@@ -0,0 +1,47 @@
+import json
+import logging
+from dataclasses import is_dataclass
+from enum import Enum, StrEnum
+from pathlib import Path
+
+logger = logging.getLogger('pytest_plugins.utils')
+
+def get_project_root(marker: str = ".git") -> Path | None:
+    path = Path(__file__).resolve()
+    for parent in path.parents:
+        if (parent / marker).exists():
+            return parent
+    return None
+
+
+def serialize_data(obj):
+    if isinstance(obj, (Enum, StrEnum)):
+        return obj.value
+    if is_dataclass(obj):
+        return obj.__dict__
+    if isinstance(obj, type):
+        return obj.__name__
+    logger.error(f'Object is not serializable: {obj}')
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+
+def open_json(path: Path) -> dict:
+    with open(path, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
+
+
+def save_as_json(path: Path, data: dict, default: callable = None) -> None:
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(path, 'w', encoding='utf-8') as json_file:
+        text = json.dumps(data, indent=4, default=default) if default else json.dumps(data, indent=4)
+        json_file.write(text)
+
+
+def save_as_markdown(path: Path, data: str) -> None:
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(path, 'w', encoding='utf-8') as md_file:
+        md_file.write(data)
