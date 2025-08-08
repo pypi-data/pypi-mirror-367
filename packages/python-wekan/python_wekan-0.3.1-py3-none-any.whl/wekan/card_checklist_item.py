3febc8e2b3a6d7f788d15172f4f391f014d6cc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:38:25 +0000

    Add package python-wekan/0.3.1/python_wekan-0.3.1-py3-none-any.whl

---
+++ packages/python-wekan/python_wekan-0.3.1-py3-none-any.whl/wekan/card_checklist_item.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+import typing
+if typing.TYPE_CHECKING:
+	from wekan.card_checklist import CardChecklist
+
+from wekan.base import WekanBase
+
+
+class CardChecklistItem(WekanBase):
+    def __init__(self, parent_checklist: CardChecklist, item_id: str, title: str, is_finished: bool) -> None:
+        """ Reference to a Wekan CardChecklistItem """
+        super().__init__()
+        self.checklist = parent_checklist
+        self.id = item_id
+        self.title = title
+        self.is_finished = is_finished
+
+        uri = f'/api/boards/{self.checklist.card.list.board.id}/cards/{self.checklist.card.id}/' \
+              f'checklists/{self.checklist.id}/items/{self.id}'
+        data = self.checklist.card.list.board.client.fetch_json(uri)
+        self.sort = data['sort']
+
+    def __repr__(self) -> str:
+        return f"<CardChecklistItem (id: {self.id}, title: {self.title}, is_finished: {self.is_finished})>"
+
+    @classmethod
+    def from_dict(cls, parent_checklist: CardChecklist, data: dict) -> CardChecklistItem:
+        """
+        Creates an instance of class CardChecklist by using the API-Response of CardChecklist GET.
+        :param parent_checklist: Instance of Class CardChecklist pointing to the current Checklist of this ChecklistItem
+        :param data: Response of CardChecklist GET.
+        :return: Instance of class CardChecklistItem
+        """
+        return cls(parent_checklist=parent_checklist, item_id=data['_id'],
+                   title=data['title'], is_finished=data['isFinished'])
+
+    @classmethod
+    def from_list(cls, parent_checklist: CardChecklist, data: list) -> list[CardChecklistItem]:
+        """
+        Wrapper around function from_dict to process multiple objects within one function call.
+        :param parent_checklist: Instance of Class CardChecklist pointing to the current Checklist of this ChecklistItem
+        :param data: Response of CardChecklist GET.
+        :return: Instances of class CardChecklistItem
+        """
+        instances = []
+        for item in data:
+            instances.append(cls(parent_checklist=parent_checklist, item_id=item['_id'],
+                                 title=item['title'], is_finished=item['isFinished']))
+        return instances
+
+    def edit(self, is_finished=None, title=None) -> None:
+        """
+        Edit the current instance by sending a PUT Request to the API
+        according to https://wekan.github.io/api/v7.42/#edit_checklist_item
+        :param is_finished: is the item checked?
+        :param title: the new text of the item
+        :return: None
+        """
+        payload = {}
+        if is_finished:
+            payload["isFinished"] = is_finished
+        if title:
+            payload["title"] = title
+
+        uri = f'/api/boards/{self.checklist.card.list.board.id}/cards/{self.checklist.card.id}/' \
+              f'checklists/{self.checklist.id}/items/{self.id}'
+        self.checklist.card.list.board.client.fetch_json(uri, payload=payload, http_method="PUT")
+
+    def mark_as_finished(self) -> None:
+        """
+        Mark this instance as finished.
+        :return: None
+        """
+        self.edit(is_finished=True)
+
+    def change_title(self, new_title: str) -> None:
+        """
+        Set a new title for this instance.
+        :param new_title: The new title.
+        :return: None
+        """
+        self.edit(title=new_title)
+
+    def delete(self) -> None:
+        """
+        Delete the Card Checklist instance according to https://wekan.github.io/api/v7.42/#delete_checklist_item
+        :return: None
+        """
+        uri = f'/api/boards/{self.checklist.card.board.id}/cards/{self.checklist.card.id}/' \
+              f'checklists/{self.checklist.id}/items/{self.id}'
+        self.checklist.card.board.client.fetch_json(uri, http_method="DELETE")
---
+++ packages/python-wekan/python_wekan-0.3.1-py3-none-any.whl/wekan/card_comment.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+import typing
+if typing.TYPE_CHECKING:
+	from wekan.card import WekanCard
+
+from wekan.base import WekanBase
+
+
+class CardComment(WekanBase):
+    def __init__(self, parent_card: WekanCard, comment_id: str) -> None:
+        """ Reference to a Wekan CardComment """
+        super().__init__()
+        self.card = parent_card
+        self.id = comment_id
+
+        uri = f'/api/boards/{self.card.list.board.id}/cards/{self.card.id}/comments/{self.id}'
+        data = self.card.list.board.client.fetch_json(uri)
+        self.text = data['text']
+        self.author_id = data['userId']
+        self.createdAt = self.card.list.board.client.parse_iso_date(data['createdAt'])
+        self.modified_at = self.card.list.board.client.parse_iso_date(data['modifiedAt'])
+
+    def __repr__(self) -> str:
+        return f"<CardComment (id: {self.id}, text: {self.text})>"
+
+    @classmethod
+    def from_dict(cls, parent_card: WekanCard, data: dict) -> CardComment:
+        """
+        Creates an instance of class CardComment by using the API-Response of CardComment GET.
+        :param parent_card: Instance of Class WekanCard pointing to the Card of this Comment
+        :param data: Response of CardComment GET.
+        :return: Instance of class CardComment
+        """
+        return cls(parent_card=parent_card, comment_id=data['_id'])
+
+    @classmethod
+    def from_list(cls, parent_card: WekanCard, data: list) -> list[CardComment]:
+        """
+        Wrapper around function from_dict to process multiple objects within one function call.
+        :param parent_card: Instance of Class WekanCard pointing to the current Card of this Comment
+        :param data: Response of CardComment GET.
+        :return: Instances of class CardComment
+        """
+        instances = []
+        for comment in data:
+            instances.append(cls(parent_card=parent_card, comment_id=comment['_id']))
+        return instances
+
+    def edit(self, data: dict) -> None:
+        """
+        Edit the current instance by sending a PUT Request to the API.
+        Currently, this is not supported by API.
+        See also: https://wekan.github.io/api/v7.42/#wekan-rest-api-cardcomments
+        """
+        raise NotImplementedError
+
+    def delete(self) -> None:
+        """
+        Delete the CardComment instance according to https://wekan.github.io/api/v7.42/#delete_comment
+        :return: None
+        """
+        uri = f'/api/boards/{self.card.list.board.id}/cards/{self.card.id}/comments/{self.id}'
+        self.card.list.board.client.fetch_json(uri, http_method="DELETE")
