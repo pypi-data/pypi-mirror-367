918efe51fb8142e5e69351a9259f60f0cc7ef46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:37:48 +0000

    Add package mengjia/0.1.4/mengjia-0.1.4.tar.gz

---
+++ packages/mengjia/mengjia-0.1.4.tar.gz/mengjia-0.1.4/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup 
+
+setup(
+        name='mengjia',
+        version='0.1.4',
+        description='A apikey generate method, the apikey will expire when current data  over input date',
+        author='zqk',
+        author_email='xx',
+        packages=['mengjia']
+)
