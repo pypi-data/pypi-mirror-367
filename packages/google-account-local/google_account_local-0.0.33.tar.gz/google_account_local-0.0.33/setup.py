51aeaba7fe0c630380b9108cfcc8cd098124e4b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 06:06:21 +0000

    Add package google-account-local/0.0.33/google_account_local-0.0.33.tar.gz

---
+++ packages/google-account-local/google_account_local-0.0.33.tar.gz/google_account_local-0.0.33/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+PACKAGE_NAME = "google-account-local"
+
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version="0.0.33",  # https://pypi.org/project/google-account-local
+    author="Circles",
+    author_email="info@circlez.ai",
+    description=f"PyPI Package for Circles {PACKAGE_NAME} Python",
+    long_description=f"PyPI Package for Circles {PACKAGE_NAME} Python",
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/circles-zone/{PACKAGE_NAME}-python-package",
+    packages=[package_dir],
+    package_dir={package_dir: f"{package_dir}/src"},
+    package_data={package_dir: ["*.py"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "logger-local",
+        "database-mysql-local>=0.1.1",
+        "python-sdk-remote",
+        "google-auth-oauthlib>=1.2.0",
+        "google-auth-httplib2>=0.2.0",
+        "google-api-python-client>=2.128.0",
+        "user-external-local>=0.0.42",
+        "profile-local>=0.0.91",
+    ],
+)
