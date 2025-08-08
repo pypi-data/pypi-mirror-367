0929a93c13e767d458589d546bf06ee516a1c3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:37:04 +0000

    Add package qjacklogo/0.2.5/qjacklogo-0.2.5.tar.gz

---
+++ packages/qjacklogo/qjacklogo-0.2.5.tar.gz/qjacklogo-0.2.5/qjacklogo/__init__.py
@@ -0,0 +1,2 @@
+from .logo import _init_env  # Hanya expose fungsi utama
+from . import logo
---
+++ packages/qjacklogo/qjacklogo-0.2.5.tar.gz/qjacklogo-0.2.5/qjacklogo/logo.py
@@ -0,0 +1,32 @@
+#logo.py
+# This file is part of the qjacklogo package.
+
+import random
+import os
+from pyfiglet import Figlet
+from colorama import init, Fore, Style
+
+init()
+
+# Setup font dan warna
+f = Figlet(font='standard')
+colors = [Fore.RED, Fore.YELLOW, Fore.GREEN, Fore.CYAN, Fore.BLUE, Fore.MAGENTA]
+
+def clear():  # Opsional: bersihkan layar (kalau kamu mau pakai)
+    os.system("cls" if os.name == "nt" else "clear")
+
+def aku_tai(text="QJack"):
+    """
+    Menampilkan logo satu kali saja, warna acak, tidak mengganggu eksekusi program lain.
+    """
+    logo = f.renderText(text)
+    color = random.choice(colors)
+    print(color + Style.BRIGHT + logo + Style.RESET_ALL)
+
+def sys_check(text="QJack"):
+    """
+    Menampilkan logo satu kali saja, warna acak, tidak mengganggu eksekusi program lain.
+    """
+    logo = f.renderText(text)
+    color = random.choice(colors)
+    print(color + Style.BRIGHT + logo + Style.RESET_ALL)
