49f844e140fda38750f45df9ec792ed32462302b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 07:09:44 +0000

    Add package GALINDA/0.1.1/galinda-0.1.1-py3-none-any.whl

---
+++ packages/GALINDA/galinda-0.1.1-py3-none-any.whl/GALINDA/Tests/test_import.py
@@ -0,0 +1,12 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+import glob
+import h5py
+from matplotlib import rc
+rc("animation", html = "html5")
+from GALINDA import GALINDA
+
+# %%
+gal = GALINDA.Bubble("animation_data/", "m11h_star_coordinates")
