b55eb6c5df1fd18aa5a329ee6b6746642e9a46a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 07:09:15 +0000

    Add package agent-zero-lite/1.0.14/agent_zero_lite-1.0.14-py3-none-any.whl

---
+++ packages/agent-zero-lite/agent_zero_lite-1.0.14-py3-none-any.whl/python/tools/call_subordinate.py
@@ -0,0 +1,42 @@
+from agent import Agent, UserMessage
+from python.helpers.tool import Tool, Response
+
+
+class Delegation(Tool):
+
+    async def execute(self, message="", reset="", **kwargs):
+        # create subordinate agent using the data object on this agent and set superior agent to his data object
+        if (
+            self.agent.get_data(Agent.DATA_NAME_SUBORDINATE) is None
+            or str(reset).lower().strip() == "true"
+        ):
+            # crate agent
+            sub = Agent(self.agent.number + 1, self.agent.config, self.agent.context)
+            # register superior/subordinate
+            sub.set_data(Agent.DATA_NAME_SUPERIOR, self.agent)
+            self.agent.set_data(Agent.DATA_NAME_SUBORDINATE, sub)
+            # set default prompt profile to new agents
+            sub.config.profile = ""
+
+        # add user message to subordinate agent
+        subordinate: Agent = self.agent.get_data(Agent.DATA_NAME_SUBORDINATE)
+        subordinate.hist_add_user_message(UserMessage(message=message, attachments=[]))
+
+        # set subordinate prompt profile if provided, if not, keep original
+        agent_profile = kwargs.get("agent_profile")
+        if agent_profile:
+            subordinate.config.profile = agent_profile
+
+        # run subordinate monologue
+        result = await subordinate.monologue()
+
+        # result
+        return Response(message=result, break_loop=False)
+
+    def get_log_object(self):
+        return self.agent.context.log.log(
+            type="tool",
+            heading=f"icon://communication {self.agent.agent_name}: Calling Subordinate Agent",
+            content="",
+            kvps=self.args,
+        )
