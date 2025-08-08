26d4d20218b07993be8783ccac977411baeb2f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 07:09:51 +0000

    Add package fady/0.0.0/fady-0.0.0-py3-none-any.whl

---
+++ packages/fady/fady-0.0.0-py3-none-any.whl/minichain/__init__.py
@@ -0,0 +1,12 @@
+# src/minichain/__init__.py
+"""
+Mini-Chain: A transparent, modular micro-framework for building with LLMs.
+
+This file marks the 'minichain' directory as a Python package.
+To use the components, please import them from their respective sub-modules.
+
+For example:
+    from minichain.chat_models import LocalChatModel
+    from minichain.memory import FAISSVectorStore
+    from minichain.voice import run_stt
+"""
