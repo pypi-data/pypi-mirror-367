bd50929739431d898854855a967819b305f9fcd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:37:18 +0000

    Add package sconekc/0.0.2/sconekc-0.0.2.tar.gz

---
+++ packages/sconekc/sconekc-0.0.2.tar.gz/sconekc-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name='sconekc',
+    version='0.0.2',
+    find_packages=find_packages(),
+    install_requires=[
+        #'base64',
+        'cryptography==3.4.8',
+        #'datetime',
+        #'hashlib',
+        #'json',
+        #'os',
+        #'pprint',
+        'requests==2.25.1'
+        #'socket',
+        #'ssl',
+        #'time',
+        #'zlib'
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
