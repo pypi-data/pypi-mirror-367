8e3b57afa35c9b9c533eee064b9bd1c6d9f277bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:37:21 +0000

    Add package GenericDataSaver/0.2.1/genericdatasaver-0.2.1.tar.gz

---
+++ packages/GenericDataSaver/genericdatasaver-0.2.1.tar.gz/genericdatasaver-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GenericDataSaver',
+    version='0.2.1',
+    author='MondayStuff',
+    author_email='Hoangquocanhmon@icloud.com',
+    description='Saves Data, Don\'t use it in production cuz it\'s not secure and it\'s just my hobby project. \n Works with Windows, MacOS, Linux now instead of just MacOS and Linux, Not tested for Windows yet.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    setup_requires=['setuptools>=61.0'],
+    use_pep517=True
+)
