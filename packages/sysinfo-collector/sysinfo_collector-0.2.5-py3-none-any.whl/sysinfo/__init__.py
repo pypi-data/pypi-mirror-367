a174f1ceeb85e3c8c81f02901871b6718ce2ad7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 05:13:29 +0000

    Add package sysinfo-collector/0.2.5/sysinfo_collector-0.2.5-py3-none-any.whl

---
+++ packages/sysinfo-collector/sysinfo_collector-0.2.5-py3-none-any.whl/sysinfo/__init__.py
@@ -0,0 +1,6 @@
+# 系统信息获取包
+from .core import get_disk_info, get_system_info, get_cpu_info, get_memory_info, get_disk_info
+
+__version__ = "0.2.3"
+__author__ = "yuanni"
+__description__ = "一个获取macOS电脑信息的Python包"
