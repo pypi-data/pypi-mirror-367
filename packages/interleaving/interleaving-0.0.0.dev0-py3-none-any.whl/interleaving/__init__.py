27856ee1ba3da98164e088064e92328c4f13f485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:36:35 +0000

    Add package interleaving/0.0.0.dev0/interleaving-0.0.0.dev0-py3-none-any.whl

---
+++ packages/interleaving/interleaving-0.0.0.dev0-py3-none-any.whl/interleaving/__init__.py
@@ -0,0 +1,5 @@
+from interleaving.core import *
+from interleaving.tests import *
+
+if __name__ == "__main__":
+    main()
---
+++ packages/interleaving/interleaving-0.0.0.dev0-py3-none-any.whl/interleaving/__main__.py
@@ -0,0 +1,4 @@
+from interleaving import main
+
+if __name__ == "__main__":
+    main()
