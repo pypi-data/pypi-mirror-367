4c6cc37975935cf764d61e127bd425fbc773a8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 06:06:23 +0000

    Add package additive-manufacturing/0.0.1/additive_manufacturing-0.0.1.tar.gz

---
+++ packages/additive-manufacturing/additive_manufacturing-0.0.1.tar.gz/additive_manufacturing-0.0.1/README.md
@@ -0,0 +1,21 @@
+# additive-manufacturing
+Additive Manufacturing related software modules
+
+## Getting Started
+### Installation
+```bash
+uv add additive-manufacturing
+```
+
+### CLI
+```bash
+am --help
+```
+
+### Agent
+#### Claude Code
+1. Install mcp server
+```bash
+am mcp install /path/to/your/project --claude-code
+```
+
---
+++ packages/additive-manufacturing/additive_manufacturing-0.0.1.tar.gz/additive_manufacturing-0.0.1/src/am/cli/mcp/install.py
@@ -0,0 +1,51 @@
+import subprocess
+import typer
+
+from typing_extensions import Annotated
+
+from pathlib import Path
+from rich import print as rprint
+
+def register_mcp_install(app: typer.Typer):
+    @app.command(name="install")
+    def mcp_development(
+        claude_code: Annotated[bool, typer.Option("--claude-code")] = False,
+        project_path: Annotated[str | None, typer.Option("--project-path")] = None,
+    ) -> None:
+        import am
+
+        if project_path: 
+            am_path = Path(project_path)
+        else:
+            am_path = Path(am.__file__).parent
+
+        rprint(f"[bold green]Using `additive-manufacturing` packaged under project path:[/bold green] {am_path}")
+
+        if claude_code:
+            try:
+                claude_cmd = [
+                    "claude", "mcp", "add-json", "am",
+                    f'{{"command": "uv", "args": ["--directory", "{am_path}", "run", "-m", "am.mcp"]}}'
+                ]
+            
+                rprint(f"[blue]Running command:[/blue] {' '.join(claude_cmd)}")
+                subprocess.run(claude_cmd, check=True)
+            
+            except subprocess.CalledProcessError as e:
+                rprint(f"[red]Command failed with return code {e.returncode}[/red]")
+                rprint(f"[red]Error output: {e.stderr}[/red]" if e.stderr else "")
+            except Exception as e:
+                rprint(f"[red]Unexpected error running command:[/red] {e}")
+
+        else:
+            rprint(
+                "[yellow]No client provided.[/yellow]\n"
+                "[bold]Please specify where to install with one of the following:[/bold]\n"
+                "  • [green]--claude-code[/green] to install for Claude Code\n"
+                "  • Other options coming soon..."
+            )
+        
+
+    _ = app.command(name="install")(mcp_development)
+    return mcp_development
+
