fa1d8d413fa5592731f606a2d91fb811dd852b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 04:22:16 +0000

    Add package skcv-toolkit/0.1.0/skcv_toolkit-0.1.0-py3-none-any.whl

---
+++ packages/skcv-toolkit/skcv_toolkit-0.1.0-py3-none-any.whl/skcv/__init__.py
@@ -0,0 +1,16 @@
+"""
+SKCV-Toolkit - Santhosh Kumar Computer Vision Toolkit
+
+A comprehensive computer vision toolkit for image processing and analysis.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Santhosh Kumar"
+__email__ = "santhoshkumarsampath.off@gmail.com"
+
+from . import he
+from . import hplp
+from . import neigh
+from . import modules
+
+__all__ = ["he", "hplp", "neigh", "modules"]
