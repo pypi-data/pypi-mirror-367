5bf8a82576a1bec09515d82ca0fb02cf8c5a490a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 06:06:51 +0000

    Add package nuvolos-client-api/1.3.0/nuvolos_client_api-1.3.0-py3-none-any.whl

---
+++ packages/nuvolos-client-api/nuvolos_client_api-1.3.0-py3-none-any.whl/nuvolos_client_api/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from nuvolos_client_api.api.apps_v1_api import AppsV1Api
+from nuvolos_client_api.api.distribution_v1_api import DistributionV1Api
+from nuvolos_client_api.api.files_v1_api import FilesV1Api
+from nuvolos_client_api.api.instances_v1_api import InstancesV1Api
+from nuvolos_client_api.api.organizations_v1_api import OrganizationsV1Api
+from nuvolos_client_api.api.snapshots_v1_api import SnapshotsV1Api
+from nuvolos_client_api.api.spaces_v1_api import SpacesV1Api
+from nuvolos_client_api.api.tables_v1_api import TablesV1Api
+from nuvolos_client_api.api.tasks_v1_api import TasksV1Api
+from nuvolos_client_api.api.workloads_v1_api import WorkloadsV1Api
+
