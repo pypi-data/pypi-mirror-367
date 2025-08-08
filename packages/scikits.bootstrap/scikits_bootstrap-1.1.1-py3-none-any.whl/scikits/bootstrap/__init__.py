339f8f0ee888dce8b4de76c6a4186b04b5c72a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 05:13:08 +0000

    Add package scikits.bootstrap/1.1.1/scikits_bootstrap-1.1.1-py3-none-any.whl

---
+++ packages/scikits.bootstrap/scikits_bootstrap-1.1.1-py3-none-any.whl/scikits/bootstrap/__init__.py
@@ -0,0 +1,12 @@
+from .bootstrap import *
+
+
+__all__ = (
+    "ci",
+    "pval",
+    "bootstrap_indices",
+    "bootstrap_indices_independent",
+    "subsample_indices",
+    "jackknife_indices",
+    "bootstrap_indices_moving_block",
+)
