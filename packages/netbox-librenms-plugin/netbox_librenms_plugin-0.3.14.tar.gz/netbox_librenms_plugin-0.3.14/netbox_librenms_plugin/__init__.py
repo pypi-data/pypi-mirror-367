07a0b95a0cda65d5320796798ff5025c931fdc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 05:13:24 +0000

    Add package netbox-librenms-plugin/0.3.14/netbox_librenms_plugin-0.3.14.tar.gz

---
+++ packages/netbox-librenms-plugin/netbox_librenms_plugin-0.3.14.tar.gz/netbox_librenms_plugin-0.3.14/netbox_librenms_plugin/__init__.py
@@ -0,0 +1,23 @@
+from netbox.plugins import PluginConfig
+
+__author__ = "Andy Norwood"
+__version__ = "0.3.14"
+
+
+class LibreNMSSyncConfig(PluginConfig):
+    name = "netbox_librenms_plugin"
+    verbose_name = "NetBox Librenms Plugin"
+    description = "Netbox plugin to sync data between LibreNMS and Netbox."
+    author = __author__
+    version = __version__
+    base_url = "librenms_plugin"
+    min_version = "4.2.0"
+    required_settings = ["librenms_url", "api_token"]
+    default_settings = {
+        "enable_caching": True,
+        "verify_ssl": True,
+        "interface_name_field": "ifName",
+    }
+
+
+config = LibreNMSSyncConfig
