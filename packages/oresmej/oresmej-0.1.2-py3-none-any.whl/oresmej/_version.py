802c8b06d399aadb262782a802b6080b6ff822fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:37:36 +0000

    Add package oresmej/0.1.2/oresmej-0.1.2-py3-none-any.whl

---
+++ packages/oresmej/oresmej-0.1.2-py3-none-any.whl/oresmej/_version.py
@@ -0,0 +1,9 @@
+# _version.py
+
+__version__ = "0.1.2"
+__license__ = "MIT"
+__description__ = "Oresme numbers refer to the sums related to the harmonic series."
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/WhiteSymmetry/oresmej"
+__docs__ = "https://WhiteSymmetry.github.io/oresmej"  # Opsiyonel: Dokümantasyon linki
+__dependencies__ = ["python>=3.9"]  # Diğer bağımlılıkları da ekleyebilirsiniz
