83562169d795041f3c2101d74a8d4436757d80f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:37:05 +0000

    Add package aek-img-trainer/0.3.0/aek_img_trainer-0.3.0.tar.gz

---
+++ packages/aek-img-trainer/aek_img_trainer-0.3.0.tar.gz/aek_img_trainer-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aek-img-trainer',
+    version='0.3.0',
+    description='Image classification trainer using OpenCV and timm',
+    author='Alp Emre Karaahmet',
+    author_email='alpemre@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'opencv-python',
+        'numpy',
+        'timm',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
+)
