f73d8f2d206c363d1b4946f960476f6bacf867cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 04:21:37 +0000

    Add package devscasino/0.1.0/devscasino-0.1.0.tar.gz

---
+++ packages/devscasino/devscasino-0.1.0.tar.gz/devscasino-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="devscasino",
+    version="0.1.0",
+    author="Your Name",
+    author_email="you@example.com",
+    description="A simple GUI-based casino slot machine library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/devscasino",  # optional
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
