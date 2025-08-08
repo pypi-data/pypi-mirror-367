c95c238049fb64d4c7a1a0f95a6073207edf1241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 06:06:34 +0000

    Add package api-investment-risk/0.3.8.6/api_investment_risk-0.3.8.6.tar.gz

---
+++ packages/api-investment-risk/api_investment_risk-0.3.8.6.tar.gz/api_investment_risk-0.3.8.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+    
+
+setup(
+    name='api_investment_risk',
+    version='0.3.8.6',
+    description='API Riesgo de Inversiones Chile',
+    author='Manuel Progaska',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas>=2.2.3",
+        "requests>=2.32.3",
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+            
+)
