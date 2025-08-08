45806cb52ca551f9b67fc4cb25942511ddc6c301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 06:06:28 +0000

    Add package withpi/1.60.1/withpi-1.60.1-py3-none-any.whl

---
+++ packages/withpi/withpi-1.60.1-py3-none-any.whl/withpi/types/scoring_system/generate_list_response.py
@@ -0,0 +1,29 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from typing_extensions import Literal, TypeAlias
+
+from ..._models import BaseModel
+from ..shared.question import Question
+
+__all__ = ["GenerateListResponse", "GenerateListResponseItem"]
+
+
+class GenerateListResponseItem(BaseModel):
+    detailed_status: List[str]
+    """Detailed status of the job"""
+
+    job_id: str
+    """The job id"""
+
+    state: Literal["QUEUED", "RUNNING", "DONE", "ERROR", "CANCELLED"]
+    """Current state of the job"""
+
+    info: Optional[object] = None
+    """The supplementary information regarding the generation of scoring spec"""
+
+    scoring_spec: Optional[List[Question]] = None
+    """The generated scoring spec"""
+
+
+GenerateListResponse: TypeAlias = List[GenerateListResponseItem]
