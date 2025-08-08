888f52ccdedc3d1a207853da4535da71355fc05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:07:46 +0000

    Add package azure-identity-broker/1.3.0/azure_identity_broker-1.3.0.tar.gz

---
+++ packages/azure-identity-broker/azure_identity_broker-1.3.0.tar.gz/azure_identity_broker-1.3.0/azure/identity/broker/__init__.py
@@ -0,0 +1,11 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+from ._browser import InteractiveBrowserBrokerCredential, PopTokenRequestOptions
+
+
+__all__ = [
+    "InteractiveBrowserBrokerCredential",
+    "PopTokenRequestOptions",
+]
