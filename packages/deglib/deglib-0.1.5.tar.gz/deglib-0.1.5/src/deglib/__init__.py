74ceb176ef1717f4f8f264d56e26f51853bbe9f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 05:12:57 +0000

    Add package deglib/0.1.5/deglib-0.1.5.tar.gz

---
+++ packages/deglib/deglib-0.1.5.tar.gz/deglib-0.1.5/src/deglib/__init__.py
@@ -0,0 +1,19 @@
+from deglib_cpp import avx_usable, avx512_usable, sse_usable
+
+from . import analysis
+from . import benchmark
+from . import builder
+from . import graph
+from . import repository
+from . import search
+from .std import Mt19937
+from .distances import Metric, FloatSpace
+
+
+__version__ = "0.1.5"
+
+
+__all__ = [
+    'avx_usable', 'sse_usable', 'graph', 'benchmark', 'Metric', 'FloatSpace',
+    'builder', 'Mt19937', 'analysis', 'repository', 'search'
+]
