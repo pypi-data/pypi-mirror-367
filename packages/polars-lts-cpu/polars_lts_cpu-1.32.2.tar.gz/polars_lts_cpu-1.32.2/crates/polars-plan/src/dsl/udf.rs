21176331068d269ee90f1313762fae4ae869c53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 03:37:23 +0000

    Add package polars-lts-cpu/1.32.2/polars_lts_cpu-1.32.2.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-1.32.2.tar.gz/polars_lts_cpu-1.32.2/crates/polars-plan/src/dsl/udf.rs
@@ -0,0 +1,50 @@
+use polars_utils::pl_str::PlSmallStr;
+
+use super::{ColumnsUdf, Expr, GetOutput, OpaqueColumnUdf};
+use crate::prelude::{FunctionOptions, new_column_udf};
+
+/// Represents a user-defined function
+#[derive(Clone)]
+pub struct UserDefinedFunction {
+    /// name
+    pub name: PlSmallStr,
+    /// The function output type.
+    pub return_type: GetOutput,
+    /// The function implementation.
+    pub fun: OpaqueColumnUdf,
+    /// Options for the function.
+    pub options: FunctionOptions,
+}
+
+impl std::fmt::Debug for UserDefinedFunction {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("UserDefinedFunction")
+            .field("name", &self.name)
+            .field("fun", &"<FUNC>")
+            .field("options", &self.options)
+            .finish()
+    }
+}
+
+impl UserDefinedFunction {
+    /// Create a new UserDefinedFunction
+    pub fn new(name: PlSmallStr, return_type: GetOutput, fun: impl ColumnsUdf + 'static) -> Self {
+        Self {
+            name,
+            return_type,
+            fun: new_column_udf(fun),
+            options: FunctionOptions::default(),
+        }
+    }
+
+    /// creates a logical expression with a call of the UDF
+    pub fn call(self, args: Vec<Expr>) -> Expr {
+        Expr::AnonymousFunction {
+            input: args,
+            function: self.fun,
+            output_type: self.return_type.clone(),
+            options: self.options,
+            fmt_str: Box::new(PlSmallStr::EMPTY),
+        }
+    }
+}
