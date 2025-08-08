dec28bde4457bf5988cbfeadfcfee4f9bc1a83a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 05:13:00 +0000

    Add package obafgkm/0.1.0/obafgkm-0.1.0.tar.gz

---
+++ packages/obafgkm/obafgkm-0.1.0.tar.gz/obafgkm-0.1.0/obafgkm/__init__.py
@@ -0,0 +1,15 @@
+import os
+import pathlib
+
+__version__ = "0.0.1-alpha"
+
+# set Python env variable to keep track of example data dir
+DATADIR = os.path.dirname(__file__)
+unnormed_data = os.path.join(DATADIR, "unnormalized_spectra/")
+normed_data = os.path.join(DATADIR, "normalized_spectra/")
+STARCSV = os.path.join(DATADIR, "star_types.csv")
+rcparams = os.path.join(DATADIR, "rcparams.txt")
+
+
+print("You have successfully imported obafgkm!")
+print("The spectra in this package were synthesized from MARCS model atmospheres using Korg.\n Due to this are currently only available within [3000, 8000] K. Future releases will aim to expand this range.")
