b6eed756faf7691a3075540ca6c4cf37d969256e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:08:08 +0000

    Add package pyspark-data-sources/0.1.10/pyspark_data_sources-0.1.10.tar.gz

---
+++ packages/pyspark-data-sources/pyspark_data_sources-0.1.10.tar.gz/pyspark_data_sources-0.1.10/pyspark_datasources/github.py
@@ -0,0 +1,72 @@
+import requests
+
+from pyspark.sql import Row
+from pyspark.sql.datasource import DataSource, DataSourceReader
+
+
+class GithubDataSource(DataSource):
+    """
+    A DataSource for reading pull requests data from Github.
+
+    Name: `github`
+
+    Schema: `id int, title string, author string, created_at string, updated_at string`
+
+    Examples
+    --------
+    Register the data source.
+
+    >>> from pyspark_datasources import GithubDataSource
+    >>> spark.dataSource.register(GithubDataSource)
+
+    Load pull requests data from a public Github repository.
+
+    >>> spark.read.format("github").load("apache/spark").show()
+    +---+--------------------+--------+--------------------+--------------------+
+    | id|               title|  author|          created_at|          updated_at|
+    +---+--------------------+--------+--------------------+--------------------+
+    |  1|Initial commit      |  matei |2014-02-03T18:47:...|2014-02-03T18:47:...|
+    |...|                 ...|     ...|                 ...|                 ...|
+    +---+--------------------+--------+--------------------+--------------------+
+
+    Load pull requests data from a private Github repository.
+
+    >>> spark.read.format("github").option("token", "your-token").load("owner/repo").show()
+    """
+
+    @classmethod
+    def name(self):
+        return "github"
+
+    def schema(self):
+        return "id int, title string, author string, created_at string, updated_at string"
+
+    def reader(self, schema):
+        return GithubPullRequestReader(self.options)
+
+
+class GithubPullRequestReader(DataSourceReader):
+    def __init__(self, options):
+        self.token = options.get("token")
+        self.repo = options.get("path")
+        if self.repo is None:
+            raise Exception(f"Must specify a repo in `.load()` method.")
+
+    def read(self, partition):
+        header = {
+            "Accept": "application/vnd.github+json",
+        }
+        if self.token is not None:
+            header["Authorization"] = f"Bearer {self.token}"
+        url = f"https://api.github.com/repos/{self.repo}/pulls"
+        response = requests.get(url)
+        response.raise_for_status()
+        prs = response.json()
+        for pr in prs:
+            yield Row(
+                id=pr.get("number"),
+                title=pr.get("title"),
+                author=pr.get("user", {}).get("login"),
+                created_at=pr.get("created_at"),
+                updated_at=pr.get("updated_at"),
+            )
---
+++ packages/pyspark-data-sources/pyspark_data_sources-0.1.10.tar.gz/pyspark_data_sources-0.1.10/pyspark_datasources/lance.py
@@ -0,0 +1,95 @@
+import lance
+import pyarrow as pa
+
+from dataclasses import dataclass
+from typing import Iterator, List
+from pyspark.sql.datasource import DataSource, DataSourceArrowWriter, WriterCommitMessage
+from pyspark.sql.pandas.types import to_arrow_schema
+
+
+class LanceSink(DataSource):
+    """
+    Write a Spark DataFrame into Lance format: https://lancedb.github.io/lance/index.html
+
+    Note this requires Spark master branch nightly build to support `DataSourceArrowWriter`.
+
+    Examples
+    --------
+    Register the data source:
+
+    >>> from pyspark_datasources import LanceSink
+    >>> spark.dataSource.register(LanceSink)
+
+    Create a Spark dataframe with 2 partitions:
+
+    >>> df = spark.createDataFrame([(1, "a"), (2, "b"), (3, "c")], schema="id int, value string")
+
+    Save the dataframe in lance format:
+
+    >>> df.write.format("lance").mode("append").save("/tmp/test_lance")
+    /tmp/test_lance
+    _transactions _versions     data
+
+    Then you can use lance API to read the dataset:
+
+    >>> import lance
+    >>> ds = lance.LanceDataset("/tmp/test_lance")
+    >>> ds.to_table().to_pandas()
+       id
+    0   0
+    1   1
+    2   2
+
+    Notes
+    -----
+    - Currently this only works with Spark local mode. Cluster mode is not supported.
+    """
+
+    @classmethod
+    def name(cls) -> str:
+        return "lance"
+
+    def writer(self, schema, overwrite: bool):
+        if overwrite:
+            raise Exception("Overwrite mode is not supported")
+        if "path" not in self.options:
+            raise Exception("Dataset URI must be specified when calling save()")
+        return LanceWriter(schema, overwrite, self.options)
+
+
+@dataclass
+class LanceCommitMessage(WriterCommitMessage):
+    fragments: List[lance.FragmentMetadata]
+
+
+class LanceWriter(DataSourceArrowWriter):
+    def __init__(self, schema, overwrite, options):
+        self.options = options
+        self.schema = schema  # Spark Schema (pyspark.sql.types.StructType)
+        self.arrow_schema = to_arrow_schema(schema)  # Arrow schema (pa.StructType)
+        self.uri = options["path"]
+        assert not overwrite
+        self.read_version = self._get_read_version()
+
+    def _get_read_version(self):
+        try:
+            ds = lance.LanceDataset(self.uri)
+            return ds.version
+        except Exception:
+            return None
+
+    def write(self, iterator: Iterator[pa.RecordBatch]):
+        reader = pa.RecordBatchReader.from_batches(self.arrow_schema, iterator)
+        fragments = lance.fragment.write_fragments(reader, self.uri, schema=self.arrow_schema)
+        return LanceCommitMessage(fragments=fragments)
+
+    def commit(self, messages):
+        fragments = [fragment for msg in messages for fragment in msg.fragments]
+        if self.read_version:
+            # This means the dataset already exists.
+            op = lance.LanceOperation.Append(fragments)
+        else:
+            # Create a new dataset.
+            schema = to_arrow_schema(self.schema)
+            op = lance.LanceOperation.Overwrite(schema, fragments)
+        lance.LanceDataset.commit(self.uri, op, read_version=self.read_version)
