e001bc181ffcbbecf8fe777be858bb792f0426c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 06:06:51 +0000

    Add package pappardelle/0.11/pappardelle-0.11-py3-none-any.whl

---
+++ packages/pappardelle/pappardelle-0.11-py3-none-any.whl/pappardelle/string_helpers.py
@@ -0,0 +1,20 @@
+def string_or_default(*args):
+    retVal = None
+    for iter in args:
+        if is_null_or_whitespace(retVal):
+            retVal = iter
+        else:
+            break
+    return retVal
+
+def is_null_or_whitespace(val):
+    return val is None or val.strip() == ''
+
+def is_null_or_empty(val):
+    return val is None or val == ''
+
+def str_ignorecase_equals(str1, str2):
+    return str1.upper() == str2.upper()
+
+def str_ignorecase_index(str1, str2):
+    return str1.upper().index(str2.upper())
